459384c7899a3aaa22a9ff72e93992eab77689db
Dennis Zhuang <killme2008@gmail.com>
2022-06-01 20:56:18 +0800

    temp commit

--- src/common/base/src/bytes.rs
+++ src/common/base/src/bytes.rs
@@ -1,7 +1,8 @@
-/// Bytes buffer.
-#[derive(Debug, Default, Clone, PartialEq)]
-pub struct Bytes(Vec<u8>);
+use serde::Serialize;
+ /// Bytes buffer.
+#[derive(Debug, Default, Clone, PartialEq, Serialize)]
+pub struct Bytes(pub Vec<u8>);
 
 /// String buffer with arbitrary encoding.
-#[derive(Debug, Default, Clone, PartialEq)]
-pub struct StringBytes(Vec<u8>);
+#[derive(Debug, Default, Clone, PartialEq, Serialize)]
+pub struct StringBytes(pub Vec<u8>);
