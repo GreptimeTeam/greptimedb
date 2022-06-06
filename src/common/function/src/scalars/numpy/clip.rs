87f397b7f508251c66729cae36b08f4a57e72f89
Dennis Zhuang <killme2008@gmail.com>
2022-06-06 22:14:25 +0800

    feat: improve clip function returning int64 type when arguments do not have float type

--- src/common/function/src/scalars/numpy/clip.rs
+++ src/common/function/src/scalars/numpy/clip.rs
@@ -4,10 +4,11 @@ use std::sync::Arc;
 use common_query::prelude::{Signature, Volatility};
 use datatypes::data_type::ConcreteDataType;
 use datatypes::data_type::DataType;
+use datatypes::prelude::{Scalar, VectorRef};
 use datatypes::type_id::LogicalTypeId;
-use datatypes::vectors::VectorRef;
 use datatypes::with_match_primitive_type_id;
 use num_traits::AsPrimitive;
+use paste::paste;
 
 use crate::error::Result;
 use crate::scalars::expression::{scalar_binary_op, EvalContext};
@@ -18,13 +19,49 @@ use crate::scalars::numerics;
 #[derive(Clone, Debug, Default)]
 pub struct ClipFuncton;
 
+fn is_float_type(ty: &ConcreteDataType) -> bool {
+    matches!(ty, ConcreteDataType::Float64(_)) || matches!(ty, ConcreteDataType::Float32(_))
+}
+
+macro_rules! define_eval {
+    ($O: ident) => {
+        paste! {
+            fn [<eval_ $O>](columns: &[VectorRef]) -> Result<VectorRef> {
+                with_match_primitive_type_id!(columns[0].data_type().logical_type_id(), |$S| {
+                    with_match_primitive_type_id!(columns[1].data_type().logical_type_id(), |$T| {
+                        with_match_primitive_type_id!(columns[2].data_type().logical_type_id(), |$R| {
+                            // clip(a, min, max) is equals to min(max(a, min), max)
+                            let col: VectorRef = Arc::new(scalar_binary_op::<$S, $T, $O, _>(&columns[0], &columns[1], scalar_max, &mut EvalContext::default())?);
+                            let col = scalar_binary_op::<$O, $R, $O, _>(&col, &columns[2], scalar_min, &mut EvalContext::default())?;
+                            Ok(Arc::new(col))
+                        }, {
+                            unreachable!()
+                        })
+                    }, {
+                        unreachable!()
+                    })
+                }, {
+                    unreachable!()
+                })
+            }
+        }
+    }
+}
+
+define_eval!(i64);
+define_eval!(f64);
+
 impl Function for ClipFuncton {
     fn name(&self) -> &str {
         "clip"
     }
 
-    fn return_type(&self, _input_types: &[ConcreteDataType]) -> Result<ConcreteDataType> {
-        Ok(ConcreteDataType::float64_datatype())
+    fn return_type(&self, input_types: &[ConcreteDataType]) -> Result<ConcreteDataType> {
+        if input_types.iter().any(is_float_type) {
+            Ok(ConcreteDataType::float64_datatype())
+        } else {
+            Ok(ConcreteDataType::int64_datatype())
+        }
     }
 
     fn signature(&self) -> Signature {
@@ -32,46 +69,54 @@ impl Function for ClipFuncton {
     }
 
     fn eval(&self, _func_ctx: FunctionContext, columns: &[VectorRef]) -> Result<VectorRef> {
-        with_match_primitive_type_id!(columns[0].data_type().logical_type_id(), |$S| {
-            with_match_primitive_type_id!(columns[1].data_type().logical_type_id(), |$T| {
-                with_match_primitive_type_id!(columns[2].data_type().logical_type_id(), |$R| {
-                    // clip(a, min, max)
-                    // Equivalent to minimum(max, maximum(a, min))
-                    let col: VectorRef = Arc::new(scalar_binary_op::<$S, $T, f64, _>(&columns[0], &columns[1], scalar_max, &mut EvalContext::default())?);
-                    let col = scalar_binary_op::<f64, $R, f64, _>(&col, &columns[2], scalar_min, &mut EvalContext::default())?;
-                    Ok(Arc::new(col))
-                }, {
-                    unreachable!()
-                })
-            }, {
-                unreachable!()
-            })
-        }, {
-            unreachable!()
-        })
+        if columns.iter().any(|v| is_float_type(&v.data_type())) {
+            eval_f64(columns)
+        } else {
+            eval_i64(columns)
+        }
     }
 }
 
 #[inline]
-fn scalar_min<S, T>(value: Option<S>, base: Option<T>, _ctx: &mut EvalContext) -> Option<f64>
+pub fn min<T: PartialOrd>(input: T, min: T) -> T {
+    if input < min {
+        input
+    } else {
+        min
+    }
+}
+
+#[inline]
+pub fn max<T: PartialOrd>(input: T, max: T) -> T {
+    if input > max {
+        input
+    } else {
+        max
+    }
+}
+
+#[inline]
+fn scalar_min<S, T, O>(left: Option<S>, right: Option<T>, _ctx: &mut EvalContext) -> Option<O>
 where
-    S: AsPrimitive<f64>,
-    T: AsPrimitive<f64>,
+    S: AsPrimitive<O>,
+    T: AsPrimitive<O>,
+    O: Scalar + Copy + PartialOrd,
 {
-    match (value, base) {
-        (Some(value), Some(base)) => Some(f64::min(value.as_(), base.as_())),
+    match (left, right) {
+        (Some(left), Some(right)) => Some(min(left.as_(), right.as_())),
         _ => None,
     }
 }
 
 #[inline]
-fn scalar_max<S, T>(value: Option<S>, base: Option<T>, _ctx: &mut EvalContext) -> Option<f64>
+fn scalar_max<S, T, O>(left: Option<S>, right: Option<T>, _ctx: &mut EvalContext) -> Option<O>
 where
-    S: AsPrimitive<f64>,
-    T: AsPrimitive<f64>,
+    S: AsPrimitive<O>,
+    T: AsPrimitive<O>,
+    O: Scalar + Copy + PartialOrd,
 {
-    match (value, base) {
-        (Some(value), Some(base)) => Some(f64::max(value.as_(), base.as_())),
+    match (left, right) {
+        (Some(left), Some(right)) => Some(max(left.as_(), right.as_())),
         _ => None,
     }
 }
@@ -86,7 +131,7 @@ impl fmt::Display for ClipFuncton {
 mod tests {
     use common_query::prelude::TypeSignature;
     use datatypes::value::Value;
-    use datatypes::vectors::{ConstantVector, Int32Vector};
+    use datatypes::vectors::{ConstantVector, Float32Vector, Int32Vector};
 
     use super::*;
     #[test]
@@ -95,7 +140,7 @@ mod tests {
 
         assert_eq!("clip", clip.name());
         assert_eq!(
-            ConcreteDataType::float64_datatype(),
+            ConcreteDataType::int64_datatype(),
             clip.return_type(&[]).unwrap()
         );
 
@@ -106,6 +151,7 @@ mod tests {
                          } if  valid_types == numerics()
         ));
 
+        // eval with integers
         let args: Vec<VectorRef> = vec![
             Arc::new(Int32Vector::from_values(0..10)),
             Arc::new(ConstantVector::new(
@@ -114,6 +160,33 @@ mod tests {
             )),
             Arc::new(ConstantVector::new(
                 Arc::new(Int32Vector::from_vec(vec![6])),
+                10,
+            )),
+        ];
+
+        let vector = clip.eval(FunctionContext::default(), &args).unwrap();
+        assert_eq!(10, vector.len());
+
+        // clip([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 6) = [3, 3, 3, 3, 4, 5, 6, 6, 6, 6]
+        for i in 0..10 {
+            if i <= 3 {
+                assert!(matches!(vector.get_unchecked(i), Value::Int64(v) if v == 3));
+            } else if i <= 6 {
+                assert!(matches!(vector.get_unchecked(i), Value::Int64(v) if v == (i as i64)));
+            } else {
+                assert!(matches!(vector.get_unchecked(i), Value::Int64(v) if v == 6));
+            }
+        }
+
+        // eval with floats
+        let args: Vec<VectorRef> = vec![
+            Arc::new(Int32Vector::from_values(0..10)),
+            Arc::new(ConstantVector::new(
+                Arc::new(Int32Vector::from_vec(vec![3])),
+                10,
+            )),
+            Arc::new(ConstantVector::new(
+                Arc::new(Float32Vector::from_vec(vec![6f32])),
                 10,
             )),
         ];
