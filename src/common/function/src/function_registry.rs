ca01e04ca4b872022453ec984dd62073fe152cc2
Dennis Zhuang <killme2008@gmail.com>
2022-06-06 14:31:06 +0800

    fix: remove used code

--- src/common/function/src/function_registry.rs
+++
@@ -1,8 +0,0 @@
-//! functions registry
-use std::sync::RwLock;
-use std::collections::HashMap;
-
-#[derive(Debug, Default)]
-pub struct FunctionRegistry {
-    functions: RwLock<HashMap<String, Arc<dyn Function>>>,
-}
