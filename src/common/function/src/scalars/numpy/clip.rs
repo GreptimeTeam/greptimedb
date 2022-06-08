32eb72e49c4b7873ae640eff4476f315f42f1667
Dennis Zhuang <killme2008@gmail.com>
2022-06-08 12:06:45 +0800

    fix: clippy warning

--- src/common/function/src/scalars/numpy/clip.rs
+++ src/common/function/src/scalars/numpy/clip.rs
@@ -170,7 +170,6 @@ mod tests {
         // clip([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 6) = [3, 3, 3, 3, 4, 5, 6, 6, 6, 6]
         for i in 0..10 {
             if i <= 3 {
-                println!("{:?}", vector.get_unchecked(i));
                 assert!(matches!(vector.get_unchecked(i), Value::Int64(v) if v == 3));
             } else if i <= 6 {
                 assert!(matches!(vector.get_unchecked(i), Value::Int64(v) if v == (i as i64)));
@@ -198,7 +197,6 @@ mod tests {
         // clip([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 6) = [3, 3, 3, 3, 4, 5, 6, 6, 6, 6]
         for i in 0..10 {
             if i <= 3 {
-                println!("{:?}", vector.get_unchecked(i));
                 assert!(matches!(vector.get_unchecked(i), Value::UInt64(v) if v == 3));
             } else if i <= 6 {
                 assert!(matches!(vector.get_unchecked(i), Value::UInt64(v) if v == (i as u64)));
