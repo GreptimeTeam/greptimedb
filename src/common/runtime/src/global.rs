bb9b34d6d4bcf07caac9cc1d5aa72153d8f3f190
Dennis Zhuang <killme2008@gmail.com>
2022-06-20 22:50:39 +0800

    feat: tokio Runtime wrapper and global runtime functions

---
+++ src/common/runtime/src/global.rs
@@ -0,0 +1,154 @@
+//! Global runtimes
+use std::future::Future;
+use std::sync::{Arc, Mutex, Once};
+
+use once_cell::sync::Lazy;
+use paste::paste;
+
+use crate::{Builder, JoinHandle, Runtime};
+
+fn create_runtime(thread_name: &str) -> Runtime {
+    Builder::default()
+        .thread_name(thread_name)
+        .build()
+        .expect("Fail to create runtime")
+}
+
+struct GlobalRuntimes {
+    read_runtime: Runtime,
+    write_runtime: Runtime,
+    bg_runtime: Runtime,
+}
+
+macro_rules! define_spawn {
+    ($type: ident) => {
+        paste! {
+
+            pub fn [<spawn_ $type>]<F>(&self, future: F) -> JoinHandle<F::Output>
+            where
+                F: Future + Send + 'static,
+                F::Output: Send + 'static,
+            {
+                self.[<$type _runtime>].spawn(future)
+            }
+
+            pub fn [<spawn_blocking_ $type>]<F, R>(&self, future: F) ->  JoinHandle<R>
+            where
+                F: FnOnce() -> R + Send + 'static,
+                R: Send + 'static,
+            {
+                self.[<$type _runtime>].spawn_blocking(future)
+            }
+        }
+    };
+}
+
+impl GlobalRuntimes {
+    define_spawn!(read);
+    define_spawn!(write);
+    define_spawn!(bg);
+}
+
+impl Default for GlobalRuntimes {
+    fn default() -> Self {
+        let mut c = CONFIG_RUNTIMES.as_ref().lock().unwrap();
+        let read = std::mem::replace(&mut c.0, None);
+        let write = std::mem::replace(&mut c.1, None);
+        let background = std::mem::replace(&mut c.2, None);
+        Self {
+            read_runtime: read.unwrap_or_else(|| create_runtime("read-worker")),
+            write_runtime: write.unwrap_or_else(|| create_runtime("write-worker")),
+            bg_runtime: background.unwrap_or_else(|| create_runtime("bg-worker")),
+        }
+    }
+}
+
+static GLOBAL_RUNTIMES: Lazy<Arc<GlobalRuntimes>> =
+    Lazy::new(|| Arc::new(GlobalRuntimes::default()));
+
+static CONFIG_RUNTIMES: Lazy<Arc<Mutex<(Option<Runtime>, Option<Runtime>, Option<Runtime>)>>> =
+    Lazy::new(|| Arc::new(Mutex::new((None, None, None))));
+
+/// Initialize the global runtimes
+///
+/// You SHOULD call this function before using the global runtimes, otherwise the setting is not effective.
+pub fn init_global_runtimes(
+    read: Option<Runtime>,
+    write: Option<Runtime>,
+    background: Option<Runtime>,
+) {
+    static START: Once = Once::new();
+    START.call_once(move || {
+        let mut c = CONFIG_RUNTIMES.as_ref().lock().unwrap();
+        *c = (read, write, background);
+    });
+}
+
+macro_rules! define_global_runtime_spawn {
+    ($type: ident) => {
+        paste! {
+            #[doc = "Spawn a future and execute it in `" $type "` thread pool."]
+            pub fn [<spawn_ $type>]<F>(future: F) -> JoinHandle<F::Output>
+            where
+                F: Future + Send + 'static,
+                F::Output: Send + 'static,
+            {
+                GLOBAL_RUNTIMES.as_ref().[<spawn_ $type>](future)
+            }
+
+            #[doc = "Run the blocking operation in `" $type "` thread pool."]
+            pub fn [<spawn_blocking_ $type>]<F, R>(future: F) ->  JoinHandle<R>
+            where
+                F: FnOnce() -> R + Send + 'static,
+                R: Send + 'static,
+            {
+                GLOBAL_RUNTIMES.as_ref().[<spawn_blocking_ $type>](future)
+            }
+
+            #[doc = "Returns the global `" $type "` thread pool."]
+            pub fn [<$type _runtime>]() -> Runtime {
+                GLOBAL_RUNTIMES.as_ref().[<$type _runtime>].clone()
+            }
+        }
+    };
+}
+
+define_global_runtime_spawn!(read);
+define_global_runtime_spawn!(write);
+define_global_runtime_spawn!(bg);
+
+#[cfg(test)]
+mod tests {
+    use tokio_test::assert_ok;
+
+    use super::*;
+
+    #[test]
+    fn test_spawn() {
+        let runtime = read_runtime();
+        let handle = spawn_read(async { 1 + 1 });
+        assert_eq!(2, runtime.block_on(handle).unwrap());
+
+        let runtime = write_runtime();
+        let handle = spawn_write(async { 2 + 2 });
+        assert_eq!(4, runtime.block_on(handle).unwrap());
+
+        let runtime = bg_runtime();
+        let handle = spawn_bg(async { 3 + 3 });
+        assert_eq!(6, runtime.block_on(handle).unwrap());
+    }
+
+    #[test]
+    fn spawn_from_blocking() {
+        let runtime = read_runtime();
+        let out = runtime.block_on(async move {
+            let inner = assert_ok!(
+                spawn_blocking_read(move || { spawn_read(async move { "hello" }) }).await
+            );
+
+            assert_ok!(inner.await)
+        });
+
+        assert_eq!(out, "hello")
+    }
+}
--- src/common/runtime/src/lib.rs
+++ src/common/runtime/src/lib.rs
@@ -1,8 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub mod error;
+mod global;
+pub mod metric;
+pub mod runtime;
+
+pub use global::{
+    bg_runtime, init_global_runtimes, read_runtime, spawn_bg, spawn_blocking_bg,
+    spawn_blocking_read, spawn_blocking_write, spawn_read, spawn_write, write_runtime,
+};
+
+pub use crate::runtime::{Builder, JoinHandle, Runtime};
