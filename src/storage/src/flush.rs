f0d9336a91069180c15d6cf82dfd319b117c352f
Dennis Zhuang <killme2008@gmail.com>
2022-07-12 19:11:32 +0800

    feat: impl SizeBasedStrategy for flush

--- src/storage/src/flush.rs
+++ src/storage/src/flush.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use common_telemetry::logging;
 use common_time::RangeMillis;
 use store_api::storage::SequenceNumber;
 
@@ -12,17 +13,73 @@ use crate::region::SharedDataRef;
 use crate::sst::{AccessLayerRef, FileMeta};
 use crate::version::VersionEdit;
 
+/// Default write buffer size (32M).
+const DEFAULT_WRITE_BUFFER_SIZE: usize = 32 * 1024 * 1024;
+
 pub trait FlushStrategy: Send + Sync {
-    fn should_flush(&self, bytes_allocated: usize) -> bool;
+    fn should_flush(
+        &self,
+        shared: &SharedDataRef,
+        bytes_mutable: usize,
+        bytes_total: usize,
+    ) -> bool;
 }
 
 pub type FlushStrategyRef = Arc<dyn FlushStrategy>;
 
-pub struct SizeBasedStrategy;
+#[derive(Debug)]
+pub struct SizeBasedStrategy {
+    /// Write buffer size of memtable.
+    max_write_buffer_size: usize,
+    /// Mutable memtable memory size limitation
+    mutable_limitation: usize,
+}
+
+impl Default for SizeBasedStrategy {
+    fn default() -> Self {
+        let max_write_buffer_size = DEFAULT_WRITE_BUFFER_SIZE;
+        Self {
+            max_write_buffer_size,
+            mutable_limitation: max_write_buffer_size * 7 / 8,
+        }
+    }
+}
 
 impl FlushStrategy for SizeBasedStrategy {
-    fn should_flush(&self, _bytes_allocated: usize) -> bool {
-        unimplemented!()
+    fn should_flush(
+        &self,
+        shared: &SharedDataRef,
+        bytes_mutable: usize,
+        bytes_total: usize,
+    ) -> bool {
+        // Insipired by RocksDB flush strategy
+        // https://github.com/facebook/rocksdb/blob/main/include/rocksdb/write_buffer_manager.h#L94
+
+        if bytes_mutable > self.mutable_limitation {
+            logging::info!("Region should flush, region: {}, bytes_mutable: {}, mutable_limitation: {}, bytes_total: {}, max_write_buffer_size: {} .",
+                            shared.name,
+                            bytes_mutable,
+                            self.mutable_limitation,
+                            bytes_total,
+                            self.max_write_buffer_size);
+
+            return true;
+        }
+
+        let buffer_size = self.max_write_buffer_size;
+
+        let should_flush = bytes_total >= buffer_size && bytes_mutable >= buffer_size / 2;
+
+        if should_flush {
+            logging::info!("Region should flush, region: {}, bytes_mutable: {}, mutable_limitation: {}, bytes_total: {}, max_write_buffer_size: {} .",
+                            shared.name,
+                            bytes_mutable,
+                            self.mutable_limitation,
+                            bytes_total,
+                            buffer_size);
+        }
+
+        should_flush
     }
 }
 
