d8c7a02731f4df44e8a2079420d1d02ddeb5d59c
fariygirl <clickmetoday@163.com>
2022-07-25 15:13:41 +0800

    protobuf

--- src/storage/build.rs
+++ src/storage/build.rs
@@ -2,4 +2,10 @@ fn main() {
     tonic_build::configure()
         .compile(&["proto/wal.proto"], &["."])
         .expect("compile wal proto");
+
+    protobuf_codegen::Codegen::new()
+        .cargo_out_dir("protos")
+        .include(".")
+        .input("proto/write_batch.proto")
+        .run_from_script();
 }
