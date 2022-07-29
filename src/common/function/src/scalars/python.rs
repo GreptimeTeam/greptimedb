2006fcc975131fd5a2caa2ab1d09f25add11dd3d
discord9 <55937128+discord9@users.noreply.github.com>
2022-07-29 17:22:11 +0800

    feat: python coprocessor with type annotation (#96)
    * feat: add coprocessor fn
    
    Signed-off-by: discord9 <zglzy29yzdk@gmail.com>
    
    * feat: cast args into PyVector
    
    * feat: uncomplete coprocessor
    
    * feat: erase decorator in python ast
    
    * feat: strip decorator in ast
    
    * fix: change parse to `Interactive`
    
    * style: format Cargo.toml
    
    * feat: make coprocessor actually work
    
    * feat: move coprocessor fn out of test mod
    
    * feat: add error handling
    
    * style: add some comment
    
    * feat: rm type annotation
    
    * feat: add type annotation support
    
    * style: move compile method to vm closure
    
    * feat: annotation for nullable
    
    * feat: type coercion cast in annotation
    
    * feat: actually cast(NOT TESTED)
    
    * fix: allow single into(type)
    
    * refactor: extract parse_type from parser
    
    * style: cargo fmt
    
    * feat: change to Expr to preserve location info
    
    * feat: add CoprParse to deal parse check error
    
    * style: add type anno doc for coprocessor
    
    * test: add some test
    
    * feat: add underscore as any type in annotation
    
    * test: add parse& runtime testcases
    
    * style: rm dbg! remnant
    
    * style: cargo fmt
    
    * feat: add more error prompt info
    
    * style: cargo fmt
    
    * style: add doc tests' missing `use`
    
    * fix: doc test for coprocessor
    
    * style: cargo fmt
    
    * fix: add missing `use` for `cargo test --doc`
    
    * refactor: according to reviews
    
    * refactor: more tweaks according to reviews
    
    * refactor: merge match arm
    
    * refactor: move into different files(UNCOMPLELTE)
    
    * refactor: split parse_copr into more function
    
    * refactor: split `exec_coprocessor` to more fn
    
    * style: cargo fmt
    
    * feat: print Py Exceptions in String
    
    * feat: error handling conform standards
    
    * test: fix test_coprocessor
    
    * feat: remove `into` in python
    
    * test: remove all `into` in python test
    
    * style: update comment
    
    * refactor: move strip compile fn to impl Copr
    
    * refactor: move `gen_schema` to impl copr
    
    * refactor: move `check_cast_type` to impl copr
    
    * refactor: if let to match
    
    * style: cargo fmt
    
    * refactor: better parse of keyword arg list
    
    * style: cargo fmt
    
    * refactor: some error handling(UNCOMPLETE)
    
    * refactor: error handling to general Error type
    
    * refactor: rm some Vec::new()
    
    * test: modify all tests to ok
    
    * style: reorder item
    
    * refactor: fetch using iter
    
    * style: cargo fmt
    
    * style: fmt macro by hand
    
    * refactor: rename InnerError to Error
    
    * test: use ron to write test
    
    * test: add test for exec_copr
    
    * refactor: add parse_bin_op
    
    * feat: add check_anno
    
    * refactor: add some checker function
    
    * refactor: exec_copr into smaller func
    
    * style: add some comment
    
    * refactor: add check for bin_op
    
    * refactor: rm useless Result
    
    * style: add pretty print for error with location
    
    * feat: more info for pretty print
    
    * refactor: mv pretty print to error.rs
    
    * refactor: rm execute_script
    
    * feat: add pretty print
    
    * feat: add constant column support
    
    * test: add test for constant column
    
    * feat: add pretty print exec fn
    
    * style: cargo fmt
    
    * feat: add macro to chain call `.fail()`
    
    * style: update doc for constant columns
    
    * style: add lint to allow print in test fn
    
    * style: cargo fmt
    
    * docs: update some comment
    
    * fix: ignore doctest for now
    
    * refactor: check_bin_op
    
    * refactor: parse_in_op, check ret anno fn
    
    * refactor: rm check_decorator
    
    * doc: loc add newline explain
    
    * style: cargo fmt
    
    * refactor: use Helper::try_into_vec in try_into_vec
    
    * style: cargo fmt
    
    * test: add ret anno test
    
    * style: cargo fmt
    
    * test: add name for .ron tests for better debug
    
    * test: print emoji in test
    
    * style: rm some comment out line
    
    * style: rename `into` to `try_into` fn
    
    * style: cargo fmt
    
    * refactor: rm unuse serialize derive
    
    * fix: pretty print out of bound fix
    
    * fix: rm some space in pretty print
    
    * style: cargo fmt
    
    * test: not even a python fn def
    
    * style: cargo fmt
    
    * fix: pretty print off by one space
    
    * fix: allow `eprint` in clippy lint

--- src/common/function/src/scalars/python.rs
+++ src/common/function/src/scalars/python.rs
@@ -1,58 +1,11 @@
 //! python udf supports
-use rustpython_vm as vm;
-mod type_;
-use std::sync::Arc;
-
-use common_recordbatch::RecordBatch;
-use datatypes::vectors::*;
-use rustpython_vm::{class::PyClassImpl, AsObject};
-use type_::PyVector;
-
-#[allow(unused)]
-pub fn coprocessor(script: &str, rb: &RecordBatch) -> Result<RecordBatch, String> {
-    // 1. parse the script and check if it's only a function with `@coprocessor` decorator, and get `args` and `returns`,
-    // 2. check for exist of `args` in `rb`, if not found, return error
-    // 3. get args from `rb`, and set_item in a python vm, then call function with given args,
-    // 4. get returns as a PyList, and assign them according to `returns`
-    // 5. return a assembled RecordBatch
-    todo!()
-}
-pub fn execute_script(script: &str) -> vm::PyResult {
-    vm::Interpreter::without_stdlib(Default::default()).enter(|vm| {
-        PyVector::make_class(&vm.ctx);
-        let scope = vm.new_scope_with_builtins();
-        let a: VectorRef = Arc::new(Int32Vector::from_vec(vec![1, 2, 3, 4]));
-        let a = PyVector::from(a);
-        let b: VectorRef = Arc::new(Float32Vector::from_vec(vec![1.2, 2.0, 3.4, 4.5]));
-        let b = PyVector::from(b);
-        scope
-            .locals
-            .as_object()
-            .set_item("a", vm.new_pyobj(a), vm)
-            .expect("failed");
-        scope
-            .locals
-            .as_object()
-            .set_item("b", vm.new_pyobj(b), vm)
-            .expect("failed");
-
-        let code_obj = vm
-            .compile(
-                script,
-                vm::compile::Mode::BlockExpr,
-                "<embedded>".to_owned(),
-            )
-            .map_err(|err| vm.new_syntax_error(&err))?;
-        vm.run_code_obj(code_obj, scope)
-    })
-}
-
+//! use the function [`exec_coprocessor`](exec_coprocessor) to parse and run a python function with arguments from recordBatch, and return a newly assembled RecordBatch
+mod copr_parse;
+mod coprocessor;
+mod error;
 #[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_execute_script() {
-        let result = execute_script("a//b");
-        assert!(result.is_ok());
-    }
-}
+mod test;
+mod type_;
+
+use coprocessor::AnnotationInfo;
+pub use coprocessor::{exec_copr_print, exec_coprocessor};
