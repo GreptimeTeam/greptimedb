866bb5213a56ee73b5ca4b9cba868c8249135867
fys <1113014250@qq.com>
2022-07-29 14:35:54 +0800

    Convert between Vec<u8> and SelectResult

--- src/api/src/convert.rs
+++ src/api/src/convert.rs
@@ -1,7 +1,7 @@
 pub use prost::DecodeError;
 use prost::Message;
 
-use crate::v1::InsertBatch;
+use crate::v1::{InsertBatch, SelectResult};
 
 impl From<InsertBatch> for Vec<u8> {
     fn from(insert: InsertBatch) -> Self {
@@ -14,6 +14,20 @@ impl TryFrom<Vec<u8>> for InsertBatch {
 
     fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
         InsertBatch::decode_length_delimited(value.as_ref())
+    }
+}
+
+impl From<SelectResult> for Vec<u8> {
+    fn from(result: SelectResult) -> Self {
+        result.encode_length_delimited_to_vec()
+    }
+}
+
+impl TryFrom<Vec<u8>> for SelectResult {
+    type Error = DecodeError;
+
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        SelectResult::decode_length_delimited(value.as_ref())
     }
 }
 
@@ -69,6 +83,52 @@ mod tests {
         );
     }
 
+    #[test]
+    fn test_convert_select_result() {
+        let select_result = mock_select_result();
+
+        let bytes: Vec<u8> = select_result.into();
+        let result: SelectResult = bytes.try_into().unwrap();
+
+        assert_eq!(8, result.row_count);
+        assert_eq!(1, result.columns.len());
+
+        let column = &result.columns[0];
+        assert_eq!("foo", column.column_name);
+        assert_eq!(SEMANTIC_TAG, column.semantic_type);
+        assert_eq!(vec![1], column.null_mask);
+        assert_eq!(
+            vec![2, 3, 4, 5, 6, 7, 8],
+            column.values.as_ref().unwrap().i32_values
+        );
+    }
+
+    #[should_panic]
+    #[test]
+    fn test_convert_select_result_wrong() {
+        let select_result = mock_select_result();
+
+        let mut bytes: Vec<u8> = select_result.into();
+
+        // modify some bytes
+        bytes[0] = 0b1;
+        bytes[1] = 0b1;
+
+        let result: SelectResult = bytes.try_into().unwrap();
+
+        assert_eq!(8, result.row_count);
+        assert_eq!(1, result.columns.len());
+
+        let column = &result.columns[0];
+        assert_eq!("foo", column.column_name);
+        assert_eq!(SEMANTIC_TAG, column.semantic_type);
+        assert_eq!(vec![1], column.null_mask);
+        assert_eq!(
+            vec![2, 3, 4, 5, 6, 7, 8],
+            column.values.as_ref().unwrap().i32_values
+        );
+    }
+
     fn mock_insert_batch() -> InsertBatch {
         let values = column::Values {
             i32_values: vec![2, 3, 4, 5, 6, 7, 8],
@@ -82,6 +142,24 @@ mod tests {
             null_mask,
         };
         InsertBatch {
+            columns: vec![column],
+            row_count: 8,
+        }
+    }
+
+    fn mock_select_result() -> SelectResult {
+        let values = column::Values {
+            i32_values: vec![2, 3, 4, 5, 6, 7, 8],
+            ..Default::default()
+        };
+        let null_mask = vec![1];
+        let column = Column {
+            column_name: "foo".to_string(),
+            semantic_type: SEMANTIC_TAG,
+            values: Some(values),
+            null_mask,
+        };
+        SelectResult {
             columns: vec![column],
             row_count: 8,
         }
