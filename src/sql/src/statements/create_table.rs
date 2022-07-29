e5019620914faf28946d286fe6aecb6c5afa3b37
Dennis Zhuang <killme2008@gmail.com>
2022-07-29 17:43:04 +0800

    fix: compile error after rebase develop

--- src/sql/src/statements/create_table.rs
+++ src/sql/src/statements/create_table.rs
@@ -3,7 +3,7 @@ use crate::ast::{ColumnDef, ObjectName, SqlOption, TableConstraint};
 /// Time index name, used in table constraints.
 pub const TIME_INDEX: &str = "__time_index";
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Eq)]
 pub struct CreateTable {
     /// Create if not exists
     pub if_not_exists: bool,
