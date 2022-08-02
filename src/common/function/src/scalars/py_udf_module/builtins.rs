00bf8b9ced0107a1a53a2397f95d43c06bb5ad6c
discord9 <zglzy29yzdk@gmail.com>
2022-08-02 17:15:47 +0800

    feat: add all_to_f64 cast fn

--- src/common/function/src/scalars/py_udf_module/builtins.rs
+++ src/common/function/src/scalars/py_udf_module/builtins.rs
@@ -1,98 +1,140 @@
+use arrow::compute::cast::CastOptions;
+use arrow::datatypes::DataType;
+use datafusion_common::{DataFusionError, ScalarValue};
+use datafusion_expr::ColumnarValue as DFColValue;
+use datafusion_physical_expr::math_expressions;
+use datatypes::vectors::Helper as HelperVec;
 use rustpython_vm::pymodule;
+use rustpython_vm::{
+    builtins::{PyBaseExceptionRef, PyBool, PyFloat, PyInt},
+    AsObject, PyObjectRef, PyPayload, PyRef, PyResult, VirtualMachine,
+};
+
+use crate::scalars::{function::FunctionContext, python::PyVector, Function};
+type PyVectorRef = PyRef<PyVector>;
+
+/// use `rustpython`'s `is_instance` method to check if a PyObject is a instance of class.
+/// if `PyResult` is Err, then this function return `false`
+pub fn is_instance<T: PyPayload>(obj: &PyObjectRef, vm: &VirtualMachine) -> bool {
+    obj.is_instance(T::class(vm).into(), vm).unwrap_or(false)
+}
+
+fn type_cast_error(name: &str, ty: &str, vm: &VirtualMachine) -> PyBaseExceptionRef {
+    vm.new_type_error(format!("Can't cast operand of type `{name}` into `{ty}`."))
+}
+
+/// try to turn a Python Object into a PyVector or a scalar that can be use for calculate
+///
+/// supported scalar are(leftside is python data type, right side is rust type):
+///
+/// | Python | Rust |
+/// | ------ | ---- |
+/// | integer| i64  |
+/// | float  | f64  |
+/// | bool   | bool |
+fn try_into_columnar_value(obj: PyObjectRef, vm: &VirtualMachine) -> PyResult<DFColValue> {
+    if is_instance::<PyVector>(&obj, vm) {
+        let ret = obj
+            .payload::<PyVector>()
+            .ok_or_else(|| type_cast_error(&obj.class().name(), "vector", vm))?;
+        Ok(DFColValue::Array(ret.to_arrow_array()))
+    } else if is_instance::<PyInt>(&obj, vm) {
+        let ret = obj.try_into_value::<i64>(vm)?;
+        Ok(DFColValue::Scalar(ScalarValue::Int64(Some(ret))))
+    } else if is_instance::<PyFloat>(&obj, vm) {
+        let ret = obj.try_into_value::<f64>(vm)?;
+        Ok(DFColValue::Scalar(ScalarValue::Float64(Some(ret))))
+    } else if is_instance::<PyBool>(&obj, vm) {
+        let ret = obj.try_into_value::<bool>(vm)?;
+        Ok(DFColValue::Scalar(ScalarValue::Boolean(Some(ret))))
+    } else {
+        Err(vm.new_type_error(format!(
+            "Can't cast object of type {} into vector or scalar",
+            obj.class().name()
+        )))
+    }
+}
+
+/// cast a columnar value into python object
+///
+/// | Rust   | Python          |
+/// | ------ | --------------- |
+/// | Array  | PyVector        |
+/// | Scalar | int/float/bool  |
+fn try_into_py_obj(col: DFColValue, vm: &VirtualMachine) -> PyResult<PyObjectRef> {
+    match col {
+        DFColValue::Array(arr) => {
+            let ret = PyVector::from(
+                HelperVec::try_into_vector(arr)
+                    .map_err(|err| vm.new_type_error(format!("Unsupported type: {:#?}", err)))?,
+            )
+            .into_pyobject(vm);
+            Ok(ret)
+        }
+        DFColValue::Scalar(val) => {
+            todo!()
+        }
+    }
+}
+
+/// Becuase datafusion's UDF only support f32/64, so cast all to f64 to use datafusion's UDF
+fn all_to_f64(col: DFColValue, vm: &VirtualMachine) -> PyResult<DFColValue> {
+    match col {
+        DFColValue::Array(arr) => {
+            let res = arrow::compute::cast::cast(
+                arr.as_ref(),
+                &DataType::Float64,
+                CastOptions {
+                    wrapped: true,
+                    partial: true,
+                },
+            )
+            .map_err(|err| {
+                vm.new_type_error(format!(
+                    "Arrow Type Cast Fail(from {:#?} to {:#?}): {err:#?}",
+                    arr.data_type(),
+                    DataType::Float64
+                ))
+            })?;
+            Ok(DFColValue::Array(res.into()))
+        }
+        DFColValue::Scalar(val) => todo!(),
+    }
+}
 
 /// GrepTime User Define Function module
 ///  
 /// design to allow Python Coprocessor Function to use already implmented udf functions
 #[pymodule]
 mod udf_builtins {
-    use datafusion_common::{DataFusionError, ScalarValue};
+    use datafusion_common::DataFusionError;
     use datafusion_expr::ColumnarValue as DFColValue;
     use datafusion_physical_expr::math_expressions;
-    use datatypes::vectors::Helper as HelperVec;
     use rustpython_vm::{
-        builtins::{PyBaseExceptionRef, PyBool, PyFloat, PyInt},
-        AsObject, PyObjectRef, PyPayload, PyRef, PyResult, VirtualMachine,
+        builtins::PyBaseExceptionRef, AsObject, PyObjectRef, PyRef, PyResult, VirtualMachine,
     };
 
     use crate::scalars::math::PowFunction;
+    use crate::scalars::py_udf_module::builtins::{
+        try_into_columnar_value, try_into_py_obj, type_cast_error,
+    };
     use crate::scalars::{function::FunctionContext, python::PyVector, Function};
+
+    use super::all_to_f64;
     type PyVectorRef = PyRef<PyVector>;
-    /// use `rustpython`'s `is_instance` method to check if a PyObject is a instance of class.
-    /// if `PyResult` is Err, then this function return `false`
-    pub fn is_instance<T: PyPayload>(obj: &PyObjectRef, vm: &VirtualMachine) -> bool {
-        obj.is_instance(T::class(vm).into(), vm).unwrap_or(false)
-    }
-
-    fn type_cast_error(name: &str, ty: &str, vm: &VirtualMachine) -> PyBaseExceptionRef {
-        vm.new_type_error(format!("Can't cast operand of type `{name}` into `{ty}`."))
-    }
-
-    /// try to turn a Python Object into a PyVector or a scalar that can be use for calculate
-    ///
-    /// supported scalar are(leftside is python data type, right side is rust type):
-    ///
-    /// | Python | Rust |
-    /// | ------ | ---- |
-    /// | integer| i64  |
-    /// | float  | f64  |
-    /// | bool   | bool |
-    fn try_into_columnar_value(obj: PyObjectRef, vm: &VirtualMachine) -> PyResult<DFColValue> {
-        if is_instance::<PyVector>(&obj, vm) {
-            let ret = obj
-                .payload::<PyVector>()
-                .ok_or_else(|| type_cast_error(&obj.class().name(), "vector", vm))?;
-            Ok(DFColValue::Array(ret.to_arrow_array()))
-        } else if is_instance::<PyInt>(&obj, vm) {
-            let ret = obj.try_into_value::<i64>(vm)?;
-            Ok(DFColValue::Scalar(ScalarValue::Int64(Some(ret))))
-        } else if is_instance::<PyFloat>(&obj, vm) {
-            let ret = obj.try_into_value::<f64>(vm)?;
-            Ok(DFColValue::Scalar(ScalarValue::Float64(Some(ret))))
-        } else if is_instance::<PyBool>(&obj, vm) {
-            let ret = obj.try_into_value::<bool>(vm)?;
-            Ok(DFColValue::Scalar(ScalarValue::Boolean(Some(ret))))
-        } else {
-            Err(vm.new_type_error(format!(
-                "Can't cast object of type {} into vector or scalar",
-                obj.class().name()
-            )))
-        }
-    }
-
-    /// cast a columnar value into python object
-    ///
-    /// | Rust   | Python          |
-    /// | ------ | --------------- |
-    /// | Array  | PyVector        |
-    /// | Scalar | int/float/bool  |
-    fn columnar_value_into_py_obj(col: DFColValue, vm: &VirtualMachine) -> PyResult<PyObjectRef> {
-        match col {
-            DFColValue::Array(arr) => {
-                let ret =
-                    PyVector::from(HelperVec::try_into_vector(arr).map_err(|err| {
-                        vm.new_type_error(format!("Unsupported type: {:#?}", err))
-                    })?)
-                    .into_pyobject(vm);
-                Ok(ret)
-            }
-            DFColValue::Scalar(val) => {
-                todo!()
-            }
-        }
-    }
 
     #[inline]
     fn runtime_err(err: DataFusionError, vm: &VirtualMachine) -> PyBaseExceptionRef {
         vm.new_runtime_error(format!("Data Fusion Error: {err:#?}"))
     }
 
-    /// return a general PyObjectRef 
+    /// return a general PyObjectRef
     /// so it can return both PyVector or a scalar PyInt/Float/Bool
     #[pyfunction]
     fn abs(val: PyObjectRef, vm: &VirtualMachine) -> PyResult<PyObjectRef> {
-        let args = &[try_into_columnar_value(val, vm)?];
+        let args = &[all_to_f64(try_into_columnar_value(val, vm)?, vm)?];
         let res = math_expressions::abs(args).map_err(|err| runtime_err(err, vm))?;
-        let ret = columnar_value_into_py_obj(res, vm)?;
+        let ret = try_into_py_obj(res, vm)?;
         Ok(ret)
     }
 
@@ -122,23 +164,26 @@ mod udf_builtins {
             .unwrap();
         Ok(res.into())
     }
-    #[cfg(test)]
-    #[allow(clippy::print_stdout)]
-    mod test {
-        use std::sync::Arc;
+}
 
-        use datatypes::vectors::VectorRef;
-        use rustpython_vm::class::PyClassImpl;
-        use rustpython_vm::{scope::Scope, AsObject, VirtualMachine};
+#[cfg(test)]
+#[allow(clippy::print_stdout)]
+mod test {
+    use std::sync::Arc;
 
-        use super::*;
-        fn set_items_in_scope(
-            scope: &Scope,
-            vm: &VirtualMachine,
-            arg_names: &[&str],
-            args: Vec<PyVector>,
-        ) -> Result<(), String> {
-            let res = arg_names.iter().zip(args).try_for_each(|(name, vector)| {
+    use datatypes::vectors::VectorRef;
+    use rustpython_vm::class::PyClassImpl;
+    use rustpython_vm::{scope::Scope, AsObject, VirtualMachine};
+
+    use super::udf_builtins::*;
+    use super::*;
+    fn set_items_in_scope(
+        scope: &Scope,
+        vm: &VirtualMachine,
+        arg_names: &[&str],
+        args: Vec<PyVector>,
+    ) -> Result<(), String> {
+        let res = arg_names.iter().zip(args).try_for_each(|(name, vector)| {
                 scope
                     .locals
                     .as_object()
@@ -152,57 +197,56 @@ mod udf_builtins {
                         )
                     })
             });
-            res
-        }
+        res
+    }
 
-        #[test]
-        fn test_vm() {
-            rustpython_vm::Interpreter::with_init(Default::default(), |vm| {
-                vm.add_native_module("udf_builtins", Box::new(super::make_module));
-                // this can be in `.enter()` closure, but for clearity, put it in the `with_init()`
-                PyVector::make_class(&vm.ctx);
-            })
-            .enter(|vm| {
-                let values = vec![1.0, 2.0, 3.0];
-                let pows = vec![0i8, -1i8, 3i8];
+    #[test]
+    fn test_vm() {
+        rustpython_vm::Interpreter::with_init(Default::default(), |vm| {
+            vm.add_native_module("udf_builtins", Box::new(udf_builtins::make_module));
+            // this can be in `.enter()` closure, but for clearity, put it in the `with_init()`
+            PyVector::make_class(&vm.ctx);
+        })
+        .enter(|vm| {
+            let values = vec![1.0, 2.0, 3.0];
+            let pows = vec![0i8, -1i8, 3i8];
 
-                let args: Vec<VectorRef> = vec![
-                    Arc::new(datatypes::vectors::Float32Vector::from_vec(values)),
-                    Arc::new(datatypes::vectors::Int8Vector::from_vec(pows)),
-                ];
-                let args: Vec<PyVector> = args.into_iter().map(PyVector::from).collect();
+            let args: Vec<VectorRef> = vec![
+                Arc::new(datatypes::vectors::Float32Vector::from_vec(values)),
+                Arc::new(datatypes::vectors::Int8Vector::from_vec(pows)),
+            ];
+            let args: Vec<PyVector> = args.into_iter().map(PyVector::from).collect();
 
-                let scope = vm.new_scope_with_builtins();
-                set_items_in_scope(&scope, vm, &["values", "pows"], args).unwrap();
-                let code_obj = vm
-                    .compile(
-                        "
-from udf_mod import pow
-pow(values, pows)",
-                        rustpython_vm::compile::Mode::BlockExpr,
-                        "<embedded>".to_owned(),
-                    )
-                    .map_err(|err| vm.new_syntax_error(&err))
-                    .unwrap();
-                let res = vm.run_code_obj(code_obj, scope);
-                println!("{:#?}", res);
-                if let Err(e) = res {
-                    let err_res = to_serde_excep(e, vm).unwrap();
-                    println!("Error:\n{err_res}");
-                }
-            });
-        }
-        fn to_serde_excep(
-            excep: rustpython_vm::builtins::PyBaseExceptionRef,
-            vm: &VirtualMachine,
-        ) -> Result<String, String> {
-            let mut chain = String::new();
-            let r = vm.write_exception(&mut chain, &excep);
-            // FIXME: better error handling, perhaps with chain calls?
-            if let Err(r) = r {
-                return Err(format!("Fail to write to string, error: {:#?}", r));
+            let scope = vm.new_scope_with_builtins();
+            set_items_in_scope(&scope, vm, &["values", "pows"], args).unwrap();
+            let code_obj = vm
+                .compile(
+                    "
+from udf_builtins import *
+abs(pows)",
+                    rustpython_vm::compile::Mode::BlockExpr,
+                    "<embedded>".to_owned(),
+                )
+                .map_err(|err| vm.new_syntax_error(&err))
+                .unwrap();
+            let res = vm.run_code_obj(code_obj, scope);
+            println!("{:#?}", res);
+            if let Err(e) = res {
+                let err_res = to_serde_excep(e, vm).unwrap();
+                println!("Error:\n{err_res}");
             }
-            Ok(chain)
+        });
+    }
+    fn to_serde_excep(
+        excep: rustpython_vm::builtins::PyBaseExceptionRef,
+        vm: &VirtualMachine,
+    ) -> Result<String, String> {
+        let mut chain = String::new();
+        let r = vm.write_exception(&mut chain, &excep);
+        // FIXME: better error handling, perhaps with chain calls?
+        if let Err(r) = r {
+            return Err(format!("Fail to write to string, error: {:#?}", r));
         }
+        Ok(chain)
     }
 }
