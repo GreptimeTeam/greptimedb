05ab10d8e317cc7d31826b88acbace19cf06a1e0
fys <1113014250@qq.com>
2022-08-03 20:10:14 +0800

    add function: aggregate select results to groc select result

--- src/datanode/src/server/grpc/select.rs
+++ src/datanode/src/server/grpc/select.rs
@@ -60,17 +60,80 @@ pub(crate) async fn select_result(select_result: Result<Output>) -> ObjectResult
 }
 
 pub(crate) fn convert_record_batches_to_select_result(
-    _record_batches: Vec<RecordBatch>,
+    record_batches: Vec<RecordBatch>,
 ) -> Result<GrpcSelectResult> {
+    let mut select_results = Vec::with_capacity(record_batches.len());
+    for record_batch in record_batches {
+        select_results.push(convert_record_batch(&record_batch)?);
+    }
+
+    Ok(aggregate_results(select_results))
+}
+
+fn aggregate_results(results: Vec<SelectResult>) -> GrpcSelectResult {
+    if results.is_empty() {
+        return GrpcSelectResult::default();
+    }
+
+    let first = &results[0];
+    let column_count = first.columns.len();
+    let mut columns = Vec::with_capacity(column_count);
+
+    let total_row_count: u32 = results.iter().map(|r| r.row_count).sum();
+
+    for column_name in first.columns.keys() {
+        let null_mask = aggregate_null_mask(column_name, &results, total_row_count);
+
+        // TODO(fys): aggregate values
+        let values = Some(aggregate_values(column_name, &results));
+
+        columns.push(Column {
+            column_name: column_name.to_string(),
+            values,
+            null_mask,
+            ..Default::default()
+        });
+    }
+
+    GrpcSelectResult {
+        columns,
+        row_count: total_row_count,
+    }
+}
+
+fn aggregate_values(_column_name: &str, _results: &[SelectResult]) -> Values {
     todo!()
 }
 
-#[allow(dead_code)]
-fn aggregate_results(_results: Vec<SelectResult>) -> GrpcSelectResult {
-    todo!()
+fn aggregate_null_mask(
+    column_name: &str,
+    results: &[SelectResult],
+    total_row_count: u32,
+) -> Vec<u8> {
+    let mut contain_null = false;
+
+    for result in results {
+        let (_, null_count) = result.columns.get(column_name).unwrap();
+        if *null_count > 0 {
+            contain_null = true;
+        }
+    }
+
+    if !contain_null {
+        Vec::default()
+    } else {
+        let mut null_masks = BitSet::with_size(total_row_count as usize);
+        for result in results {
+            let (column, _) = result.columns.get(column_name).unwrap();
+            null_masks.extend(&BitSet::from_vec(
+                column.null_mask.clone(),
+                result.row_count as usize,
+            ));
+        }
+        null_masks.buffer()
+    }
 }
 
-#[allow(dead_code)]
 fn convert_record_batch(record_batch: &RecordBatch) -> Result<SelectResult> {
     let df_record = &record_batch.df_recordbatch;
     let row_count = df_record.num_rows();
@@ -110,7 +173,7 @@ fn convert_record_batch(record_batch: &RecordBatch) -> Result<SelectResult> {
     Ok(SelectResult {
         columns: column_map,
         row_count: row_count as u32,
-    })   
+    })
 }
 
 pub type ColumnName = String;
@@ -173,16 +236,102 @@ fn convert_arrow_array(array: &Arc<dyn Array>, data_type: &DataType) -> Result<V
 
 #[cfg(test)]
 mod tests {
-    use std::sync::Arc;
+    use std::{collections::HashMap, sync::Arc};
 
-    use arrow::{array::{PrimitiveArray, Array, BooleanArray}, datatypes::{DataType, Field}};
+    use api::v1::Column;
+    use arrow::{
+        array::{Array, BooleanArray, PrimitiveArray},
+        datatypes::{DataType, Field},
+    };
     use common_recordbatch::RecordBatch;
     use datafusion::field_util::SchemaExt;
-    use datatypes::{arrow_array::StringArray, vectors::{VectorRef, UInt32Vector}, schema::Schema};
-
-    use crate::server::grpc::select::{convert_arrow_array, convert_record_batch};
-
     use datatypes::arrow::datatypes::Schema as ArrowSchema;
+    use datatypes::{
+        arrow_array::StringArray,
+        schema::Schema,
+        vectors::{UInt32Vector, VectorRef},
+    };
+
+    use crate::server::grpc::select::{
+        aggregate_null_mask, convert_arrow_array, convert_record_batch, SelectResult,
+    };
+
+    #[test]
+    fn test_aggregate_null_mask() {
+        let select_results = &mock_inner_select_result_1();
+
+        let null_mask1 = aggregate_null_mask("c1", select_results, 7);
+        let null_mask2 = aggregate_null_mask("c2", select_results, 7);
+
+        assert_eq!(vec![0b0100_0100], null_mask1);
+        assert_eq!(vec![0b0100_1100], null_mask2);
+
+        let select_results = &mock_inner_select_result_2();
+
+        let null_mask1 = aggregate_null_mask("c1", select_results, 7);
+        let null_mask2 = aggregate_null_mask("c2", select_results, 7);
+
+        assert!(null_mask1.is_empty());
+        assert!(null_mask2.is_empty());
+    }
+
+    // values contains some "null"
+    fn mock_inner_select_result_1() -> Vec<SelectResult> {
+        let c1 = Column {
+            column_name: "c1".to_string(),
+            null_mask: vec![0b0000_0100],
+            ..Default::default()
+        };
+        let c2 = Column {
+            column_name: "c2".to_string(),
+            null_mask: vec![0b0000_1100],
+            ..Default::default()
+        };
+
+        let mut map = HashMap::new();
+        map.insert("c1".to_string(), (c1, 1));
+        map.insert("c2".to_string(), (c2, 2));
+
+        let s1 = SelectResult {
+            row_count: 4,
+            columns: map.clone(),
+        };
+        let s2 = SelectResult {
+            row_count: 3,
+            columns: map,
+        };
+
+        vec![s1, s2]
+    }
+
+    // values not contains "null"
+    fn mock_inner_select_result_2() -> Vec<SelectResult> {
+        let c1 = Column {
+            column_name: "c1".to_string(),
+            null_mask: vec![0b0000_0000],
+            ..Default::default()
+        };
+        let c2 = Column {
+            column_name: "c2".to_string(),
+            null_mask: vec![0b0000_0000],
+            ..Default::default()
+        };
+
+        let mut map = HashMap::new();
+        map.insert("c1".to_string(), (c1, 0));
+        map.insert("c2".to_string(), (c2, 0));
+
+        let s1 = SelectResult {
+            row_count: 4,
+            columns: map.clone(),
+        };
+        let s2 = SelectResult {
+            row_count: 3,
+            columns: map,
+        };
+
+        vec![s1, s2]
+    }
 
     #[test]
     fn test_convert_batch() {
@@ -254,13 +403,7 @@ mod tests {
     #[test]
     fn test_convert_arrow_array_bool() {
         let data_type = &DataType::Boolean;
-        let array = BooleanArray::from(vec![
-            Some(true),
-            Some(false),
-            None,
-            Some(false),
-            None,
-        ]);
+        let array = BooleanArray::from(vec![Some(true), Some(false), None, Some(false), None]);
         let array: Arc<dyn Array> = Arc::new(array);
 
         let values = convert_arrow_array(&array, data_type).unwrap();
