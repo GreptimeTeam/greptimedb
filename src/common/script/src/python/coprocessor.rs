9130da69b173f4319681d562ef3cac3425ad21c8
discord9 <zglzy29yzdk@gmail.com>
2022-08-11 18:58:18 +0800

    refactor: add `exec_parsed` fn

--- src/common/script/src/python/coprocessor.rs
+++ src/common/script/src/python/coprocessor.rs
@@ -523,6 +523,11 @@ pub fn exec_coprocessor(script: &str, rb: &DfRecordBatch) -> Result<DfRecordBatc
     // 2. also check for exist of `args` in `rb`, if not found, return error
     // TODO: cache the result of parse_copr
     let copr = parse_copr(script)?;
+    exec_parsed(copr, rb)
+}
+
+/// using a parsed `Coprocessor` struct as input to execute python code
+fn exec_parsed(copr: Coprocessor, rb: &DfRecordBatch) -> Result<DfRecordBatch> {
     // 3. get args from `rb`, and cast them into PyVector
     let args: Vec<PyVector> = select_from_rb(rb, &copr.deco_args.arg_names)?;
     check_args_anno_real_type(&args, &copr, rb)?;
