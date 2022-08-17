4b564fb8c5d44f515f398c58a6d33eea79477749
evenyag <realevenyag@gmail.com>
2022-08-17 11:32:03 +0800

    test: Compare std and chrono timestamp
    The original test always success even the current_time_millis returns in
    seconds resolution

--- src/common/time/src/util.rs
+++ src/common/time/src/util.rs
@@ -5,15 +5,27 @@ pub fn current_time_millis() -> i64 {
 
 #[cfg(test)]
 mod tests {
-    use chrono::TimeZone;
+    use std::time::{self, SystemTime};
+
+    use chrono::{Datelike, TimeZone, Timelike};
 
     use super::*;
 
     #[test]
     fn test_current_time_millis() {
         let now = current_time_millis();
-        let datetime = chrono::Utc.timestamp_millis(now);
 
-        assert_eq!(now, datetime.timestamp_millis());
+        let millis_from_std = SystemTime::now()
+            .duration_since(time::UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as i64;
+        let datetime_now = chrono::Utc.timestamp_millis(now);
+        let datetime_std = chrono::Utc.timestamp_millis(millis_from_std);
+
+        assert_eq!(datetime_std.year(), datetime_now.year());
+        assert_eq!(datetime_std.month(), datetime_now.month());
+        assert_eq!(datetime_std.day(), datetime_now.day());
+        assert_eq!(datetime_std.hour(), datetime_now.hour());
+        assert_eq!(datetime_std.minute(), datetime_now.minute());
     }
 }
