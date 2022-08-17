6850a45a6b4da742561a395d9334a9e7874ad159
evenyag <realevenyag@gmail.com>
2022-08-17 11:11:38 +0800

    fix: Rename current_timestamp to current_time_millis, fix resolution
    Fix current_timestamp returns seconds resolution, also add a test for
    this method

--- src/common/time/src/util.rs
+++ src/common/time/src/util.rs
@@ -1,4 +1,19 @@
-/// Calculates the time duration since UNIX_EPOCH in milliseconds.
-pub fn current_timestamp() -> i64 {
-    chrono::Utc::now().timestamp()
+/// Returns the time duration since UNIX_EPOCH in milliseconds.
+pub fn current_time_millis() -> i64 {
+    chrono::Utc::now().timestamp_millis()
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::TimeZone;
+
+    use super::*;
+
+    #[test]
+    fn test_current_time_millis() {
+        let now = current_time_millis();
+        let datetime = chrono::Utc.timestamp_millis(now);
+
+        assert_eq!(now, datetime.timestamp_millis());
+    }
 }
