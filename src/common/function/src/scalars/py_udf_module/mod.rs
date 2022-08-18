438777274a23b5ce1802f0961c51ad1dc3699827
discord9 <zglzy29yzdk@gmail.com>
2022-08-18 11:30:49 +0800

    refsctor: remove unused py files in functions/

--- src/common/function/src/scalars/py_udf_module/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Python udf module, that is udf function that you can use in
-//! python script(in Python Coprocessor more precisely)
-
-mod builtins;
-
-#[cfg(test)]
-#[allow(clippy::print_stdout)]
-mod test;
