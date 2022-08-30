36f5705d6928ef3b1493c5b865600edfab157762
evenyag <realevenyag@gmail.com>
2022-08-30 11:38:28 +0800

    chore: Add doc comments to MutableVector

--- src/datatypes/src/vectors/mutable.rs
+++ src/datatypes/src/vectors/mutable.rs
@@ -2,18 +2,25 @@ use std::any::Any;
 
 use crate::prelude::*;
 
+/// Mutable vector that could be used to build an immutable vector.
 pub trait MutableVector: Send + Sync {
+    /// Returns the data type of the vector.
     fn data_type(&self) -> ConcreteDataType;
 
+    /// Returns the length of the vector.
     fn len(&self) -> usize;
 
+    /// Returns whether the vector is empty.
     fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Convert to Any, to enable dynamic casting.
     fn as_any(&self) -> &dyn Any;
 
+    /// Convert to mutable Any, to enable dynamic casting.
     fn as_mut_any(&mut self) -> &mut dyn Any;
 
+    /// Convert `self` to an (immutable) [VectorRef] and reset `self`.
     fn to_vector(&mut self) -> VectorRef;
 }
