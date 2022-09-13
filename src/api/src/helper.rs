de5ad48b6711a2940f82d99cc33c44278afda1c4
luofucong <luofucong@greptime.com>
2022-09-13 15:08:52 +0800

    add more tests

--- src/api/src/helper.rs
+++ src/api/src/helper.rs
@@ -4,6 +4,7 @@ use snafu::prelude::*;
 use crate::error::{self, Result};
 use crate::v1::ColumnDataType;
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct ColumnDataTypeWrapper(ColumnDataType);
 
 impl ColumnDataTypeWrapper {
@@ -67,5 +68,163 @@ impl TryFrom<ConcreteDataType> for ColumnDataTypeWrapper {
             }
         });
         Ok(datatype)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_concrete_datatype_from_column_datatype() {
+        assert_eq!(
+            ConcreteDataType::boolean_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Boolean).into()
+        );
+        assert_eq!(
+            ConcreteDataType::int8_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Int8).into()
+        );
+        assert_eq!(
+            ConcreteDataType::int16_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Int16).into()
+        );
+        assert_eq!(
+            ConcreteDataType::int32_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Int32).into()
+        );
+        assert_eq!(
+            ConcreteDataType::int64_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Int64).into()
+        );
+        assert_eq!(
+            ConcreteDataType::uint8_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Uint8).into()
+        );
+        assert_eq!(
+            ConcreteDataType::uint16_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Uint16).into()
+        );
+        assert_eq!(
+            ConcreteDataType::uint32_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Uint32).into()
+        );
+        assert_eq!(
+            ConcreteDataType::uint64_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Uint64).into()
+        );
+        assert_eq!(
+            ConcreteDataType::float32_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Float32).into()
+        );
+        assert_eq!(
+            ConcreteDataType::float64_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Float64).into()
+        );
+        assert_eq!(
+            ConcreteDataType::binary_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Binary).into()
+        );
+        assert_eq!(
+            ConcreteDataType::string_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::String).into()
+        );
+        assert_eq!(
+            ConcreteDataType::date_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Date).into()
+        );
+        assert_eq!(
+            ConcreteDataType::datetime_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Datetime).into()
+        );
+        assert_eq!(
+            ConcreteDataType::timestamp_millis_datatype(),
+            ColumnDataTypeWrapper(ColumnDataType::Timestamp).into()
+        );
+    }
+
+    #[test]
+    fn test_column_datatype_from_concrete_datatype() {
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Boolean),
+            ConcreteDataType::boolean_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Int8),
+            ConcreteDataType::int8_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Int16),
+            ConcreteDataType::int16_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Int32),
+            ConcreteDataType::int32_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Int64),
+            ConcreteDataType::int64_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Uint8),
+            ConcreteDataType::uint8_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Uint16),
+            ConcreteDataType::uint16_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Uint32),
+            ConcreteDataType::uint32_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Uint64),
+            ConcreteDataType::uint64_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Float32),
+            ConcreteDataType::float32_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Float64),
+            ConcreteDataType::float64_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Binary),
+            ConcreteDataType::binary_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::String),
+            ConcreteDataType::string_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Date),
+            ConcreteDataType::date_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Datetime),
+            ConcreteDataType::datetime_datatype().try_into().unwrap()
+        );
+        assert_eq!(
+            ColumnDataTypeWrapper(ColumnDataType::Timestamp),
+            ConcreteDataType::timestamp_millis_datatype()
+                .try_into()
+                .unwrap()
+        );
+
+        let result: Result<ColumnDataTypeWrapper> = ConcreteDataType::null_datatype().try_into();
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "Failed to create column datatype from Null(NullType)"
+        );
+
+        let result: Result<ColumnDataTypeWrapper> =
+            ConcreteDataType::list_datatype(ConcreteDataType::boolean_datatype()).try_into();
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "Failed to create column datatype from List(ListType { inner: Boolean(BooleanType) })"
+        );
     }
 }
