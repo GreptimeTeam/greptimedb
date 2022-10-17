697554434a3f05b886312bfecb9cd2c3d3d67938
Lei, HUANG <mrsatangel@gmail.com>
2022-10-17 22:56:11 +0800

    refactor: mock to separate file

--- src/catalog/tests/remote_catalog_tests.rs
+++ src/catalog/tests/remote_catalog_tests.rs
@@ -1,107 +1,36 @@
 #![feature(btree_drain_filter)]
 #![feature(assert_matches)]
 
+mod mock;
+
 #[cfg(test)]
 mod tests {
     use std::assert_matches::assert_matches;
-    use std::collections::{BTreeMap, HashMap, HashSet};
-    use std::fmt::{Display, Formatter};
-    use std::ops::RangeInclusive;
+    use std::collections::HashSet;
+    use std::fmt::Display;
     use std::sync::Arc;
 
-    use async_stream::stream;
     use catalog::error::Error;
     use catalog::remote::helper::{CatalogKey, CatalogValue, SchemaKey, SchemaValue};
     use catalog::remote::{
-        Kv, KvBackend, KvBackendRef, RemoteCatalogManager, RemoteCatalogProvider,
-        RemoteSchemaProvider, ValueIter,
+        KvBackend, KvBackendRef, RemoteCatalogManager, RemoteCatalogProvider, RemoteSchemaProvider,
     };
     use catalog::{
         CatalogManager, CatalogManagerRef, RegisterTableRequest, DEFAULT_CATALOG_NAME,
         DEFAULT_SCHEMA_NAME,
     };
-    use common_recordbatch::RecordBatch;
-    use common_telemetry::info;
-    use datatypes::prelude::ConcreteDataType;
-    use datatypes::schema::{ColumnSchema, Schema};
-    use datatypes::vectors::StringVector;
+    use datatypes::schema::Schema;
     use futures_util::StreamExt;
-    use serde::Serializer;
     use table::engine::{EngineContext, TableEngine, TableEngineRef};
-    use table::requests::{
-        AlterTableRequest, CreateTableRequest, DropTableRequest, OpenTableRequest,
-    };
-    use table::TableRef;
-    use tokio::sync::RwLock;
+    use table::requests::CreateTableRequest;
 
-    struct MockKvBackend {
-        map: RwLock<BTreeMap<Vec<u8>, Vec<u8>>>,
-    }
-
-    impl Display for MockKvBackend {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            futures::executor::block_on(async {
-                let map = self.map.read().await;
-                for (k, v) in map.iter() {
-                    f.serialize_str(&String::from_utf8_lossy(k))?;
-                    f.serialize_str(" -> ")?;
-                    f.serialize_str(&String::from_utf8_lossy(v))?;
-                    f.serialize_str("\n")?;
-                }
-                Ok(())
-            })
-        }
-    }
-
-    #[async_trait::async_trait]
-    impl KvBackend for MockKvBackend {
-        type Error = Error;
-
-        fn range<'a, 'b>(&'a self, key: &[u8]) -> ValueIter<'b, Self::Error>
-        where
-            'a: 'b,
-        {
-            let prefix = key.to_vec();
-            let prefix_string = String::from_utf8_lossy(&prefix).to_string();
-            Box::pin(stream!({
-                let maps = self.map.read().await.clone();
-                for (k, v) in maps.range(prefix.clone()..) {
-                    let key_string = String::from_utf8_lossy(k).to_string();
-                    let matches = key_string.starts_with(&prefix_string);
-                    if matches {
-                        yield Ok(Kv(k.clone(), v.clone()))
-                    } else {
-                        info!("Stream finished");
-                        return;
-                    }
-                }
-            }))
-        }
-
-        async fn set(&self, key: &[u8], val: &[u8]) -> Result<(), Self::Error> {
-            let mut map = self.map.write().await;
-            map.insert(key.to_vec(), val.to_vec());
-            Ok(())
-        }
-
-        async fn delete_range(&self, key: &[u8], end: &[u8]) -> Result<(), Self::Error> {
-            let start = key.to_vec();
-            let end = end.to_vec();
-            let range = RangeInclusive::new(start, end);
-
-            let mut map = self.map.write().await;
-            let _: BTreeMap<_, _> = map.drain_filter(|k, _| range.contains(k)).collect();
-            Ok(())
-        }
-    }
+    use crate::mock::{MockKvBackend, MockTableEngine};
 
     #[tokio::test]
     async fn test_backend() {
         common_telemetry::init_default_ut_logging();
         let node_id = "localhost".to_string();
-        let backend = MockKvBackend {
-            map: Default::default(),
-        };
+        let backend = MockKvBackend::new();
 
         let default_catalog_key = CatalogKey {
             catalog_name: DEFAULT_CATALOG_NAME.to_string(),
@@ -140,82 +69,11 @@ mod tests {
         );
     }
 
-    struct MockTableEngine {
-        tables: RwLock<HashMap<String, TableRef>>,
-    }
-
-    #[async_trait::async_trait]
-    impl TableEngine for MockTableEngine {
-        fn name(&self) -> &str {
-            "MockTableEngine"
-        }
-
-        /// Create a table with only one column
-        async fn create_table(
-            &self,
-            _ctx: &EngineContext,
-            request: CreateTableRequest,
-        ) -> table::Result<TableRef> {
-            let table_name = request.table_name.clone();
-
-            let schema = Arc::new(Schema::new(vec![ColumnSchema::new(
-                "name",
-                ConcreteDataType::string_datatype(),
-                true,
-            )]));
-
-            let data = vec![Arc::new(StringVector::from(vec!["a", "b", "c"])) as _];
-            let record_batch = RecordBatch::new(schema, data).unwrap();
-            let table: TableRef =
-                Arc::new(test_util::MemTable::new(&table_name, record_batch)) as Arc<_>;
-
-            let mut tables = self.tables.write().await;
-            tables.insert(table_name, table.clone() as TableRef);
-            Ok(table)
-        }
-
-        async fn open_table(
-            &self,
-            _ctx: &EngineContext,
-            request: OpenTableRequest,
-        ) -> table::Result<Option<TableRef>> {
-            Ok(self.tables.read().await.get(&request.table_name).cloned())
-        }
-
-        async fn alter_table(
-            &self,
-            _ctx: &EngineContext,
-            _request: AlterTableRequest,
-        ) -> table::Result<TableRef> {
-            unimplemented!()
-        }
-
-        fn get_table(&self, _ctx: &EngineContext, name: &str) -> table::Result<Option<TableRef>> {
-            futures::executor::block_on(async { Ok(self.tables.read().await.get(name).cloned()) })
-        }
-
-        fn table_exists(&self, _ctx: &EngineContext, name: &str) -> bool {
-            futures::executor::block_on(async { self.tables.read().await.contains_key(name) })
-        }
-
-        async fn drop_table(
-            &self,
-            _ctx: &EngineContext,
-            _request: DropTableRequest,
-        ) -> table::Result<()> {
-            unimplemented!()
-        }
-    }
-
     async fn prepare_components(
         node_id: String,
     ) -> (KvBackendRef, TableEngineRef, CatalogManagerRef) {
-        let backend = Arc::new(MockKvBackend {
-            map: Default::default(),
-        }) as KvBackendRef;
-        let table_engine = Arc::new(MockTableEngine {
-            tables: Default::default(),
-        });
+        let backend = Arc::new(MockKvBackend::new()) as KvBackendRef;
+        let table_engine = Arc::new(MockTableEngine::new());
         let catalog_manager =
             RemoteCatalogManager::new(table_engine.clone(), node_id, backend.clone());
         catalog_manager.start().await.unwrap();
