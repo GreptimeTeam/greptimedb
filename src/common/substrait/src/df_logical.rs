020dc94c81fdd221708ff474d202aba8fae99f2f
Ruihang Xia <waynestxia@gmail.com>
2022-10-18 15:46:56 +0800

    feat: impl for both Logical and Execution plan
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ src/common/substrait/src/df_logical.rs
@@ -0,0 +1,252 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use bytes::{Buf, Bytes, BytesMut};
+use catalog::CatalogManagerRef;
+use datafusion::datasource::TableProvider;
+use datafusion::logical_plan::{LogicalPlan, TableScan, ToDFSchema};
+use prost::Message;
+use snafu::ensure;
+use snafu::{OptionExt, ResultExt};
+use substrait::protobuf::plan_rel::RelType as PlanRelType;
+use substrait::protobuf::read_rel::ReadType;
+use substrait::protobuf::rel::RelType;
+use substrait::protobuf::PlanRel;
+use substrait::protobuf::ReadRel;
+use substrait::protobuf::Rel;
+use table::table::adapter::DfTableProviderAdapter;
+
+use crate::error::Error;
+use crate::error::{
+    DFInternalSnafu, DecodeRelSnafu, EmptyPlanSnafu, EncodeRelSnafu, InternalSnafu,
+    InvalidParametersSnafu, MissingFieldSnafu, TableNotFoundSnafu, UnsupportedExprSnafu,
+    UnsupportedPlanSnafu,
+};
+use crate::SubstraitPlan;
+
+pub struct DFLogicalSubstraitConvertor {
+    catalog_manager: CatalogManagerRef,
+}
+
+#[async_trait]
+impl SubstraitPlan for DFLogicalSubstraitConvertor {
+    type Error = Error;
+
+    type Plan = LogicalPlan;
+
+    async fn from_buf<B: Buf + Send>(&self, message: B) -> Result<Self::Plan, Self::Error> {
+        let plan_rel = PlanRel::decode(message).context(DecodeRelSnafu)?;
+        let rel = match plan_rel.rel_type.context(EmptyPlanSnafu)? {
+            PlanRelType::Rel(rel) => rel,
+            PlanRelType::Root(_) => UnsupportedPlanSnafu {
+                name: "Root Relation",
+            }
+            .fail()?,
+        };
+        self.convert_rel(Box::new(rel))
+    }
+
+    fn from_plan(&self, plan: Self::Plan) -> Result<Bytes, Self::Error> {
+        let rel = self.convert_plan(plan)?;
+
+        let mut buf = BytesMut::new();
+        rel.encode(&mut buf).context(EncodeRelSnafu)?;
+
+        Ok(buf.freeze())
+    }
+}
+
+impl DFLogicalSubstraitConvertor {
+    pub fn new(catalog_manager: CatalogManagerRef) -> Self {
+        Self { catalog_manager }
+    }
+}
+
+impl DFLogicalSubstraitConvertor {
+    pub fn convert_rel(&self, rel: Box<Rel>) -> Result<LogicalPlan, Error> {
+        let rel_type = rel.rel_type.context(EmptyPlanSnafu)?;
+        let logical_plan = match rel_type {
+            RelType::Read(read_rel) => self.convert_read_rel(read_rel),
+            RelType::Filter(_filter_rel) => UnsupportedPlanSnafu {
+                name: "Filter Relation",
+            }
+            .fail()?,
+            RelType::Fetch(_fetch_rel) => UnsupportedPlanSnafu {
+                name: "Fetch Relation",
+            }
+            .fail()?,
+            RelType::Aggregate(_aggr_rel) => UnsupportedPlanSnafu {
+                name: "Fetch Relation",
+            }
+            .fail()?,
+            RelType::Sort(_sort_rel) => UnsupportedPlanSnafu {
+                name: "Sort Relation",
+            }
+            .fail()?,
+            RelType::Join(_join_rel) => UnsupportedPlanSnafu {
+                name: "Join Relation",
+            }
+            .fail()?,
+            RelType::Project(_project_rel) => UnsupportedPlanSnafu {
+                name: "Project Relation",
+            }
+            .fail()?,
+            RelType::Set(_set_rel) => UnsupportedPlanSnafu {
+                name: "Set Relation",
+            }
+            .fail()?,
+            RelType::ExtensionSingle(_ext_single_rel) => UnsupportedPlanSnafu {
+                name: "Extension Single Relation",
+            }
+            .fail()?,
+            RelType::ExtensionMulti(_ext_multi_rel) => UnsupportedPlanSnafu {
+                name: "Extension Multi Relation",
+            }
+            .fail()?,
+            RelType::ExtensionLeaf(_ext_leaf_rel) => UnsupportedPlanSnafu {
+                name: "Extension Leaf Relation",
+            }
+            .fail()?,
+            RelType::Cross(_cross_rel) => UnsupportedPlanSnafu {
+                name: "Cross Relation",
+            }
+            .fail()?,
+        }?;
+
+        Ok(logical_plan)
+    }
+
+    fn convert_read_rel(&self, read_rel: Box<ReadRel>) -> Result<LogicalPlan, Error> {
+        // Extract the catalog, schema and table name from NamedTable. Assume the first three are those names.
+
+        let read_type = read_rel.read_type.context(MissingFieldSnafu {
+            field: "read_type",
+            plan: "Read",
+        })?;
+        let (catalog_name, schema_name, table_name) = match read_type {
+            ReadType::NamedTable(mut named_table) => {
+                ensure!(
+                    named_table.names.len() == 3,
+                    InvalidParametersSnafu {
+                        reason:
+                            "NamedTable should contains three names for catalog, schema and table",
+                    }
+                );
+                (
+                    named_table.names.pop().unwrap(),
+                    named_table.names.pop().unwrap(),
+                    named_table.names.pop().unwrap(),
+                )
+            }
+            ReadType::VirtualTable(_) | ReadType::LocalFiles(_) | ReadType::ExtensionTable(_) => {
+                UnsupportedExprSnafu {
+                    name: "Non-NamedTable Read",
+                }
+                .fail()?
+            }
+        };
+
+        // Get table handle from catalog manager
+        let table_ref = self
+            .catalog_manager
+            .table(Some(&catalog_name), Some(&schema_name), &table_name)
+            .map_err(|e| Box::new(e) as _)
+            .context(InternalSnafu)?
+            .context(TableNotFoundSnafu {
+                name: format!("{}-{}-{}", catalog_name, schema_name, table_name),
+            })?;
+        let adapter = Arc::new(DfTableProviderAdapter::new(table_ref));
+        // Get schema direct from the table.
+        // TODO(ruihang): Maybe need to verify the schema with the one in Substrait?
+        let schema = adapter
+            .schema()
+            .to_dfschema_ref()
+            .context(DFInternalSnafu)?;
+
+        // TODO(ruihang): Support projection, filters and limit
+        Ok(LogicalPlan::TableScan(TableScan {
+            table_name,
+            source: adapter,
+            projection: None,
+            projected_schema: schema,
+            filters: vec![],
+            limit: None,
+        }))
+    }
+}
+
+impl DFLogicalSubstraitConvertor {
+    pub fn convert_plan(&self, plan: LogicalPlan) -> Result<Rel, Error> {
+        match plan {
+            LogicalPlan::Projection(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::Filter(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::Window(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::Aggregate(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::Sort(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::Join(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::CrossJoin(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::Repartition(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::Union(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::TableScan(table_scan) => {
+                let read_rel = self.convert_table_scan_plan(table_scan)?;
+                Ok(Rel {
+                    rel_type: Some(RelType::Read(Box::new(read_rel))),
+                })
+            }
+            LogicalPlan::EmptyRelation(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::Limit(_) => UnsupportedPlanSnafu {
+                name: "DataFusion Logical Projection",
+            }
+            .fail()?,
+            LogicalPlan::CreateExternalTable(_)
+            | LogicalPlan::CreateMemoryTable(_)
+            | LogicalPlan::DropTable(_)
+            | LogicalPlan::Values(_)
+            | LogicalPlan::Explain(_)
+            | LogicalPlan::Analyze(_)
+            | LogicalPlan::Extension(_) => InvalidParametersSnafu {
+                reason: format!(
+                    "Trying to convert DDL/DML plan to substrait proto, plan: {:?}",
+                    plan
+                ),
+            }
+            .fail()?,
+        }
+    }
+
+    pub fn convert_table_scan_plan(&self, table_scan: TableScan) -> Result<ReadRel, Error> {
+        let _provider = table_scan.source;
+
+        todo!("get catalog and schema name")
+    }
+}
---
+++ src/common/substrait/src/lib.rs
@@ -0,0 +1,23 @@
+mod df_logical;
+mod df_physical;
+mod error;
+
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use bytes::{Buf, Bytes};
+use datafusion::physical_plan::{ExecutionPlan, PhysicalExpr};
+
+pub type ExecutionPlanRef = Arc<dyn ExecutionPlan>;
+pub type PhysicalExprRef = Arc<dyn PhysicalExpr>;
+
+#[async_trait]
+pub trait SubstraitPlan {
+    type Error: std::error::Error;
+
+    type Plan;
+
+    async fn from_buf<B: Buf + Send>(&self, message: B) -> Result<Self::Plan, Self::Error>;
+
+    fn from_plan(&self, plan: Self::Plan) -> Result<Bytes, Self::Error>;
+}
