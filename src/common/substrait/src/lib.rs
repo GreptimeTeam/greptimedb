1c84f6102bdfce1f223eb65efa4f412af435214d
Ruihang Xia <waynestxia@gmail.com>
2022-10-21 17:49:04 +0800

    drop support of physical plan
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/common/substrait/src/lib.rs
+++ src/common/substrait/src/lib.rs
@@ -1,18 +1,10 @@
 mod df_logical;
-mod df_physical;
 mod error;
-
-use std::sync::Arc;
 
 use async_trait::async_trait;
 use bytes::{Buf, Bytes};
-use datafusion::physical_plan::{ExecutionPlan, PhysicalExpr};
 
 pub use crate::df_logical::DFLogicalSubstraitConvertor;
-pub use crate::df_physical::DFExecutionSubstraitConvertor;
-
-pub type ExecutionPlanRef = Arc<dyn ExecutionPlan>;
-pub type PhysicalExprRef = Arc<dyn PhysicalExpr>;
 
 #[async_trait]
 pub trait SubstraitPlan {
