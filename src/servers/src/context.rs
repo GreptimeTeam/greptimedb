e9d9aeeff7e80e60fe582a8a9eedda599df94486
shuiyisong <xixing.sys@gmail.com>
2022-11-02 15:20:28 +0800

    chore: add context struct

---
+++ src/servers/src/context.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+
+#[derive(Default)]
+pub struct Context {
+    pub exec_info: ExecInfo,
+    pub client_info: ClientInfo,
+    pub user_info: UserInfo,
+}
+
+#[derive(Default)]
+pub struct ExecInfo {
+    pub catalog: Option<String>,
+    pub schema: Option<String>,
+    pub extra_opts: Option<HashMap<String, String>>,
+    pub trace_id: Option<String>,
+}
+
+#[derive(Default)]
+pub struct ClientInfo {
+    pub client_addr: Option<String>,
+    pub client_ip: Option<String>,
+}
+
+#[derive(Default)]
+pub struct UserInfo {
+    pub username: Option<String>,
+    pub from_channel: Option<Channel>,
+    pub auth_method: Option<AuthMethod>,
+}
+
+pub enum Channel {
+    gRPC,
+    HTTP,
+    MySQL,
+}
+
+pub enum AuthMethod {
+    None,
+    Password {
+        hash_method: AuthHashMethod,
+        hashed_value: Vec<u8>,
+    },
+    Token(String),
+}
+
+pub enum AuthHashMethod {
+    DoubleSha1,
+    Sha256,
+}
