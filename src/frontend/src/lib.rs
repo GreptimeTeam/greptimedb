16500b045bf81e55ecb19cff7831b7819704b506
LFC <bayinamine@gmail.com>
2022-11-05 10:41:59 +0800

    feat: distribute table in frontend (#328)
    Co-authored-by: luofucong <luofucong@greptime.com>

--- src/frontend/src/lib.rs
+++ src/frontend/src/lib.rs
@@ -5,6 +5,7 @@ pub mod frontend;
 pub mod grpc;
 pub mod influxdb;
 pub mod instance;
+pub(crate) mod mock;
 pub mod mysql;
 pub mod opentsdb;
 pub mod partitioning;
@@ -12,5 +13,6 @@ pub mod postgres;
 pub mod prometheus;
 mod server;
 pub mod spliter;
+mod table;
 #[cfg(test)]
 mod tests;
