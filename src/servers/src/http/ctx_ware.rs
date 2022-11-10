6f9456f722a7f4d35ad0a5fd79d5f87006d51015
shuiyisong <xixing.sys@gmail.com>
2022-11-10 11:39:18 +0800

    chore: fix test

--- src/servers/src/http/ctx_ware.rs
+++ src/servers/src/http/ctx_ware.rs
@@ -4,32 +4,33 @@ use axum::{
     middleware::Next,
     response::Response,
 };
-use common_telemetry::info;
+use common_telemetry::{error, info};
 
 use crate::context::{AuthMethod, Channel, CtxBuilder};
 
 pub async fn build_ctx<B>(mut req: Request<B>, next: Next<B>) -> Result<Response, StatusCode> {
-    let auth_option = req
-        .headers()
-        .get(http::header::AUTHORIZATION)
-        .map(|header| {
-            if header.as_bytes().starts_with(b"TOKEN ") {
-                let bytes = &header.as_bytes()[b"TOKEN ".len()..];
-                return match String::from_utf8(bytes.to_vec()) {
-                    Ok(token) => AuthMethod::Token(token),
-                    Err(_) => {
-                        info!(
-                            "parse auth token from http header error: {:?}",
-                            header.to_str()
-                        );
-                        AuthMethod::None
-                    }
-                };
-            }
-            AuthMethod::None
-        });
+    let auth_option =
+        req.headers()
+            .get(http::header::AUTHORIZATION)
+            .map_or(AuthMethod::None, |header| {
+                if header.as_bytes().starts_with(b"TOKEN ") {
+                    let bytes = &header.as_bytes()[b"TOKEN ".len()..];
+                    return match String::from_utf8(bytes.to_vec()) {
+                        Ok(token) => AuthMethod::Token(token),
+                        Err(_) => {
+                            info!(
+                                "parse auth token from http header error: {:?}",
+                                header.to_str()
+                            );
+                            AuthMethod::None
+                        }
+                    };
+                }
+                // unrecognized case
+                AuthMethod::None
+            });
 
-    if let Ok(ctx) = CtxBuilder::new()
+    match CtxBuilder::new()
         .client_addr(
             req.headers()
                 .get(http::header::HOST)
@@ -37,12 +38,16 @@ pub async fn build_ctx<B>(mut req: Request<B>, next: Next<B>) -> Result<Response
                 .map(|h| h.to_string()),
         )
         .set_channel(Some(Channel::HTTP))
-        .set_auth_method(auth_option)
+        .set_auth_method(Some(auth_option))
         .build()
     {
-        req.extensions_mut().insert(ctx);
-        Ok(next.run(req).await)
-    } else {
-        Err(StatusCode::INTERNAL_SERVER_ERROR)
+        Ok(ctx) => {
+            req.extensions_mut().insert(ctx);
+            Ok(next.run(req).await)
+        }
+        Err(e) => {
+            error!(e; "fail to create context");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
     }
 }
