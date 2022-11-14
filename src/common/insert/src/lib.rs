09d52f8d6e21ffb9b70ae2b0f269a9a3b4fcd6b4
Lei, HUANG <mrsatangel@gmail.com>
2022-11-15 00:12:15 +0800

    fix: opentsdb/influxdb tags are not put to primary key indices

--- src/common/insert/src/lib.rs
+++ src/common/insert/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod error;
 mod insert;
 pub use insert::{
-    build_alter_table_request, build_create_expr_from_insertion, find_new_columns, insert_batches,
-    insertion_expr_to_request,
+    build_alter_table_request, build_create_expr_from_insertion, column_to_vector,
+    find_new_columns, insert_batches, insertion_expr_to_request,
 };
