7ba512980a89b76f333af34fa55099f2ddab4d13
Ruihang Xia <waynestxia@gmail.com>
2022-11-15 18:05:46 +0800

    chore: add APACHE-2.0 license header (#518)
    * feat: add license checker workflow
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * fix existing header
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * specify license for internal sub-crate
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * fix rustfmt
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/servers/src/mysql/federated.rs
+++ src/servers/src/mysql/federated.rs
@@ -1,3 +1,17 @@
+// Copyright 2022 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! Use regex to filter out some MySQL federated components' emitted statements.
 //! Inspired by Databend's "[mysql_federated.rs](https://github.com/datafuselabs/databend/blob/ac706bf65845e6895141c96c0a10bad6fdc2d367/src/query/service/src/servers/mysql/mysql_federated.rs)".
 
