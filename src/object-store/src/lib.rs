878f9ee87f31ace4a09067b0e816e2fe8629245d
Xuanwo <github@xuanwo.io>
2022-11-26 10:03:12 +0800

    Avoid using raw types when not needed
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/object-store/src/lib.rs
+++ src/object-store/src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub use opendal::raw::{Accessor, ObjectEntry, SeekableReader};
+pub use opendal::raw::SeekableReader;
 pub use opendal::{
     layers, services, Error, ErrorKind, Layer, Object, ObjectLister, ObjectMetadata, ObjectMode,
     Operator as ObjectStore,
