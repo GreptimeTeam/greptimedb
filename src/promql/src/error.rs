6a4e2e597598aa6103647f1798fdea1bbec98b73
dennis zhuang <killme2008@gmail.com>
2022-12-13 17:08:22 +0800

    feat: promql create and skeleton (#720)
    * feat: adds promql crate
    
    * feat: adds promql-parser dependency and rfc doc
    
    * fix: dependencies order in servers crate
    
    * fix: forgot error.rs
    
    * fix: comment
    
    * fix: license header
    
    * fix: remove docs/rfc/20221207_promql.md

---
+++ src/promql/src/error.rs
@@ -0,0 +1,50 @@
+// Copyright 2022 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::any::Any;
+
+use common_error::prelude::*;
+
+common_error::define_opaque_error!(Error);
+
+#[derive(Debug, Snafu)]
+#[snafu(visibility(pub))]
+pub enum InnerError {
+    #[snafu(display("Unsupported expr type: {}", name))]
+    UnsupportedExpr { name: String, backtrace: Backtrace },
+}
+
+impl ErrorExt for InnerError {
+    fn status_code(&self) -> StatusCode {
+        use InnerError::*;
+        match self {
+            UnsupportedExpr { .. } => StatusCode::InvalidArguments,
+        }
+    }
+    fn backtrace_opt(&self) -> Option<&Backtrace> {
+        ErrorCompat::backtrace(self)
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl From<InnerError> for Error {
+    fn from(e: InnerError) -> Error {
+        Error::new(e)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
