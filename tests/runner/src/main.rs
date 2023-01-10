6d7d89a556eafdba85af5778f8d092bd2a12aa8e
Ruihang Xia <waynestxia@gmail.com>
2023-01-10 14:38:59 +0800

    close once_cell feature gate
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests/runner/src/main.rs
+++ tests/runner/src/main.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(once_cell)]
-
 use env::Env;
 use sqlness::{ConfigBuilder, Runner};
 
