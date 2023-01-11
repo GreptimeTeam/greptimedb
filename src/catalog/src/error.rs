e25d12febc3414f9f4542cf83d179481eb5b826e
elijah <f1renze.142857@gmail.com>
2023-01-11 12:55:20 +0800

    chore: improve the code

--- src/catalog/src/error.rs
+++ src/catalog/src/error.rs
@@ -142,15 +142,6 @@ pub enum Error {
         source: table::error::Error,
     },
 
-    #[snafu(display(
-        "Failed to delete table creation record to system catalog, source: {}",
-        source
-    ))]
-    DeleteCatalogRecord {
-        #[snafu(backtrace)]
-        source: table::error::Error,
-    },
-
     #[snafu(display("Illegal catalog manager state: {}", msg))]
     IllegalManagerState { backtrace: Backtrace, msg: String },
 
@@ -246,7 +237,6 @@ impl ErrorExt for Error {
             Error::OpenSystemCatalog { source, .. }
             | Error::CreateSystemCatalog { source, .. }
             | Error::InsertCatalogRecord { source, .. }
-            | Error::DeleteCatalogRecord { source, .. }
             | Error::OpenTable { source, .. }
             | Error::CreateTable { source, .. } => source.status_code(),
             Error::MetaSrv { source, .. } => source.status_code(),
