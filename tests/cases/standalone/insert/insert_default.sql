107d8075d544ebc6cf10a449a09332846bc6d40f
Ruihang Xia <waynestxia@gmail.com>
2023-01-13 15:41:54 +0800

    test: sqlness test for insert default
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ tests/cases/standalone/insert/insert_default.sql
@@ -0,0 +1,18 @@
+CREATE TABLE test1 (i INTEGER, j BIGINT TIME INDEX, k STRING DEFAULT 'blabla');
+
+INSERT INTO test1 VALUES (DEFAULT);
+
+INSERT INTO test1 VALUES (DEFAULT, 1, DEFAULT), (default, 2, default), (DeFaUlT, 3, DeFaUlT), (dEfAuLt, 4, dEfAuLt);
+
+SELECT * FROM test1;
+
+CREATE TABLE test2 (i INTEGER, j BIGINT TIME INDEX DEFAULT CURRENT_TIMESTAMP, k STRING DEFAULT 'blabla');
+
+INSERT INTO test2 VALUES (1,1,'a'), (default, 2, default), (3,3,'b'), (default, 4, default), (5, 5, 'c');
+
+INSERT INTO test2 VALUES (6, 6, default), (7, 7, 'd'), (default, 8, 'e');
+
+SELECT * FROM test2;
+
+DROP TABLE test1;
+DROP TABLE test2;
