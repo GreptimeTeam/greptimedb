b33937f48efa13d5400ba435c0f2d6c51e0fc579
Ruihang Xia <waynestxia@gmail.com>
2023-01-28 11:35:38 +0800

    test: sqlness test for alter table rename (#891)
    * test: sqlness test for alter table rename
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * update test result
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * change show create table to desc table
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    ---------
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ tests/cases/standalone/alter/rename_table.sql
@@ -0,0 +1,27 @@
+CREATE TABLE t(i INTEGER, j BIGINT TIME INDEX);
+
+DESC TABLE t;
+
+INSERT INTO TABLE t VALUES (1, 1), (3, 3), (NULL, 4);
+
+SELECT * from t;
+
+ALTER TABLE t RENAME new_table;
+
+DESC TABLE t;
+
+SELECT * FROM t;
+
+CREATE TABLE t(i INTEGER, j BIGINT TIME INDEX);
+
+DESC TABLE new_table;
+
+SELECT * FROM new_table;
+
+ALTER TABLE new_table RENAME new_table;
+
+ALTER TABLE new_table RENAME t;
+
+DROP TABLE t;
+
+DROP TABLE new_table;
