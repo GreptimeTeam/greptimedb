2e1dceb4197a54980f60fcd1f74c56d785c7da12
xieqijun <qijun@apache.org>
2023-02-09 17:32:12 +0800

    [fix]:modify sql

--- tests/cases/standalone/delete/delete_invalid.sql
+++ tests/cases/standalone/delete/delete_invalid.sql
@@ -2,12 +2,12 @@ CREATE TABLE monitor ( host STRING, ts TIMESTAMP, cpu DOUBLE DEFAULT 0, memory D
 
 insert into monitor(host, cpu, memory, ts) values ('host1', 66.6, 1024, 1655276557000), ('host2', 77.7,  2048, 1655276558000), ('host3', 88.8,  3072, 1655276559000);
 
-delete from test_table where cpu = 66.6 and ts = 1655276557000;
+delete from monitor where cpu = 66.6 and ts = 1655276557000;
 
-delete from test_table where host = 'host1' or ts = 1655276557000;
+delete from monitor where host = 'host1' or ts = 1655276557000;
 
-delete from test_table where host = 'host1' or ts != 1655276557000;
+delete from monitor where host = 'host1' or ts != 1655276557000;
 
-delete from test_table where  ts != 1655276557000;
+delete from monitor where  ts != 1655276557000;
 
 drop table monitor;
