16094bfc0891f100721df2c14396179ca40e986f
xieqijun <1072505283@qq.com>
2023-02-15 11:18:20 +0800

    fix:conservation

--- src/servers/tests/http/http_test.rs
+++ src/servers/tests/http/http_test.rs
@@ -12,60 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::Arc;
-
-use async_trait::async_trait;
 use axum::Router;
 use axum_test_helper::TestClient;
-use common_query::Output;
-use datatypes::schema::Schema;
-use servers::error::{Error, Result};
 use servers::http::{HttpOptions, HttpServer};
-use servers::query_handler::sql::SqlQueryHandler;
-use session::context::QueryContextRef;
+use table::test_util::MemTable;
 
-pub struct DummyInstance {}
-
-#[async_trait]
-impl SqlQueryHandler for DummyInstance {
-    type Error = Error;
-
-    async fn do_query(&self, _: &str, _: QueryContextRef) -> Vec<Result<Output>> {
-        unimplemented!()
-    }
-
-    async fn do_promql_query(
-        &self,
-        _: &str,
-        _: QueryContextRef,
-    ) -> Vec<std::result::Result<Output, Self::Error>> {
-        unimplemented!()
-    }
-
-    async fn do_statement_query(
-        &self,
-        _stmt: sql::statements::statement::Statement,
-        _query_ctx: QueryContextRef,
-    ) -> Result<Output> {
-        unimplemented!()
-    }
-
-    fn do_describe(
-        &self,
-        _stmt: sql::statements::statement::Statement,
-        _query_ctx: QueryContextRef,
-    ) -> Result<Option<Schema>> {
-        unimplemented!()
-    }
-
-    fn is_valid_schema(&self, _catalog: &str, _schema: &str) -> Result<bool> {
-        Ok(true)
-    }
-}
+use crate::create_testing_sql_query_handler;
 
 fn make_test_app() -> Router {
-    let instance = Arc::new(DummyInstance {});
-    let server = HttpServer::new(instance, HttpOptions::default());
+    let server = HttpServer::new(
+        create_testing_sql_query_handler(MemTable::default_numbers_table()),
+        HttpOptions::default(),
+    );
     server.make_app()
 }
 
