c106174f1f3ef5c6115756eed34234e8f28d1401
xieqijun <1072505283@qq.com>
2023-03-02 16:06:40 +0800

    fix: it

--- src/common/procedure/src/local/runner.rs
+++ src/common/procedure/src/local/runner.rs
@@ -189,6 +189,7 @@ impl Runner {
                                 },
                             )))
                         }
+                        return;
                     }
                 }
             }
