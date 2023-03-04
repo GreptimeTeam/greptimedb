f2028d8ca56a966e8b4aef6b1f522a8cdcb5bab1
Discord9 <discord9@163.com>
2023-03-04 22:30:21 +0800

    chore: cargo fmt

--- src/script/src/python/pyo3/builtins.rs
+++ src/script/src/python/pyo3/builtins.rs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 use std::sync::Arc;
+
 use common_function::scalars::{FunctionRef, FUNCTION_REGISTRY};
 use datafusion::arrow::array::{ArrayRef, NullArray};
 use datafusion::physical_plan::expressions;
