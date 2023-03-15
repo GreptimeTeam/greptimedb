b530ac9e60a78f6fdd573a799ba221e988cbf68c
Eugene Tolbakov <ev.tolbakov@gmail.com>
2023-03-15 08:27:09 +0000

    chore(from_unixtime): remove UDF from_unixtime (#1179)
    * chore(from_unixtime): remove UDF from_unixtime
    
    * chore(from_unixtime): restore timestamp.rs for further usage
    
    * chore(from_unixtime): address fmt issue

--- src/common/function/src/scalars/timestamp.rs
+++ src/common/function/src/scalars/timestamp.rs
@@ -12,17 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::Arc;
-mod from_unixtime;
-
-use from_unixtime::FromUnixtimeFunction;
-
 use crate::scalars::function_registry::FunctionRegistry;
 
 pub(crate) struct TimestampFunction;
 
 impl TimestampFunction {
-    pub fn register(registry: &FunctionRegistry) {
-        registry.register(Arc::new(FromUnixtimeFunction::default()));
-    }
+    pub fn register(_registry: &FunctionRegistry) {}
 }
