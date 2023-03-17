9f312a34487a72ff52ce1753976905185fb862ce
luofucong <luofucong@greptime.com>
2023-03-17 15:30:00 +0800

    feat: add GRPC unary call service to our GreptimeDB

--- src/client/src/error.rs
+++ src/client/src/error.rs
@@ -13,9 +13,10 @@
 // limitations under the License.
 
 use std::any::Any;
+use std::str::FromStr;
 
 use common_error::prelude::*;
-use tonic::Code;
+use tonic::{Code, Status};
 
 #[derive(Debug, Snafu)]
 #[snafu(visibility(pub))]
@@ -68,6 +69,13 @@ pub enum Error {
     /// Error deserialized from gRPC metadata
     #[snafu(display("{}", msg))]
     ExternalError { code: StatusCode, msg: String },
+
+    // Server error carried in Tonic Status's metadata.
+    #[snafu(display("{}", msg))]
+    Server { code: StatusCode, msg: String },
+
+    #[snafu(display("Illegal Database response: {err_msg}"))]
+    IllegalDatabaseResponse { err_msg: String },
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -77,7 +85,10 @@ impl ErrorExt for Error {
         match self {
             Error::IllegalFlightMessages { .. }
             | Error::ColumnDataType { .. }
-            | Error::MissingField { .. } => StatusCode::Internal,
+            | Error::MissingField { .. }
+            | Error::IllegalDatabaseResponse { .. } => StatusCode::Internal,
+
+            Error::Server { code, .. } => *code,
             Error::FlightGet { source, .. } => source.status_code(),
             Error::CreateChannel { source, .. } | Error::ConvertFlightData { source } => {
                 source.status_code()
@@ -93,5 +104,23 @@ impl ErrorExt for Error {
 
     fn as_any(&self) -> &dyn Any {
         self
+    }
+}
+
+impl From<Status> for Error {
+    fn from(e: Status) -> Self {
+        fn get_metadata_value(e: &Status, key: &str) -> Option<String> {
+            e.metadata()
+                .get(key)
+                .and_then(|v| String::from_utf8(v.as_bytes().to_vec()).ok())
+        }
+
+        let code = get_metadata_value(&e, INNER_ERROR_CODE)
+            .and_then(|s| StatusCode::from_str(&s).ok())
+            .unwrap_or(StatusCode::Unknown);
+
+        let msg = get_metadata_value(&e, INNER_ERROR_MSG).unwrap_or(e.to_string());
+
+        Self::Server { code, msg }
     }
 }
---
+++ src/servers/src/grpc/database.rs
@@ -0,0 +1,57 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::sync::Arc;
+
+use api::v1::greptime_database_server::GreptimeDatabase;
+use api::v1::{greptime_response, AffectedRows, GreptimeRequest, GreptimeResponse};
+use async_trait::async_trait;
+use common_query::Output;
+use tonic::{Request, Response, Status};
+
+use crate::grpc::handler::GreptimeRequestHandler;
+use crate::grpc::TonicResult;
+
+pub(crate) struct DatabaseService {
+    handler: Arc<GreptimeRequestHandler>,
+}
+
+impl DatabaseService {
+    pub(crate) fn new(handler: Arc<GreptimeRequestHandler>) -> Self {
+        Self { handler }
+    }
+}
+
+#[async_trait]
+impl GreptimeDatabase for DatabaseService {
+    async fn handle(
+        &self,
+        request: Request<GreptimeRequest>,
+    ) -> TonicResult<Response<GreptimeResponse>> {
+        let request = request.into_inner();
+        let output = self.handler.handle_request(request).await?;
+        let response = match output {
+            Output::AffectedRows(rows) => GreptimeResponse {
+                header: None,
+                response: Some(greptime_response::Response::AffectedRows(AffectedRows {
+                    value: rows as _,
+                })),
+            },
+            Output::Stream(_) | Output::RecordBatches(_) => {
+                return Err(Status::unimplemented("GreptimeDatabase::handle for query"));
+            }
+        };
+        Ok(Response::new(response))
+    }
+}
