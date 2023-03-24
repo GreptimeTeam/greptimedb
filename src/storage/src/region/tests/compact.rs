288ce30ef39b4b13cbc06c9ddb8a05befa3ea37a
evenyag <realevenyag@gmail.com>
2023-03-24 19:01:37 +0800

    test: Add s3 object store to test

--- src/storage/src/region/tests/compact.rs
+++ src/storage/src/region/tests/compact.rs
@@ -14,11 +14,15 @@
 
 //! Region compaction tests.
 
+use std::env;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
+use common_telemetry::logging;
 use common_test_util::temp_dir::create_temp_dir;
 use log_store::raft_engine::log_store::RaftEngineLogStore;
+use object_store::services::{Fs, S3};
+use object_store::ObjectStore;
 use store_api::storage::{FlushContext, Region, WriteResponse};
 use tokio::sync::Notify;
 
@@ -34,6 +38,31 @@ use crate::test_util::config_util;
 
 const REGION_NAME: &str = "region-compact-0";
 
+fn new_object_store(store_dir: &str, s3_bucket: Option<String>) -> ObjectStore {
+    if let Some(bucket) = s3_bucket {
+        if !bucket.is_empty() {
+            logging::info!("Use S3 object store");
+
+            let root = uuid::Uuid::new_v4().to_string();
+
+            let mut builder = S3::default();
+            builder
+                .root(&root)
+                .access_key_id(&env::var("GT_S3_ACCESS_KEY_ID").unwrap())
+                .secret_access_key(&env::var("GT_S3_ACCESS_KEY").unwrap())
+                .bucket(&bucket);
+
+            return ObjectStore::new(builder).unwrap().finish();
+        }
+    }
+
+    logging::info!("Use local fs object store");
+
+    let mut builder = Fs::default();
+    builder.root(store_dir);
+    ObjectStore::new(builder).unwrap().finish()
+}
+
 /// Create a new region for compaction test
 async fn create_region_for_compaction<
     H: Handler<Request = FilePurgeRequest> + Send + Sync + 'static,
@@ -42,10 +71,18 @@ async fn create_region_for_compaction<
     enable_version_column: bool,
     engine_config: EngineConfig,
     purge_handler: H,
-) -> RegionImpl<RaftEngineLogStore> {
+    s3_bucket: Option<String>,
+) -> (RegionImpl<RaftEngineLogStore>, ObjectStore) {
     let metadata = tests::new_metadata(REGION_NAME, enable_version_column);
 
-    let mut store_config = config_util::new_store_config(REGION_NAME, store_dir).await;
+    let object_store = new_object_store(store_dir, s3_bucket);
+
+    let mut store_config = config_util::new_store_config_with_object_store(
+        REGION_NAME,
+        store_dir,
+        object_store.clone(),
+    )
+    .await;
     store_config.engine_config = Arc::new(engine_config);
     let picker = SimplePicker::default();
     let handler = CompactionHandler::new(picker);
@@ -59,7 +96,10 @@ async fn create_region_for_compaction<
         purge_handler,
     ));
 
-    RegionImpl::create(metadata, store_config).await.unwrap()
+    (
+        RegionImpl::create(metadata, store_config).await.unwrap(),
+        object_store,
+    )
 }
 
 #[derive(Debug, Default, Clone)]
@@ -92,8 +132,12 @@ impl Handler for MockFilePurgeHandler {
 }
 
 impl MockFilePurgeHandler {
+    fn num_deleted(&self) -> usize {
+        self.num_deleted.load(Ordering::Relaxed)
+    }
+
     async fn wait_until_deleted(&self, num_deleted: usize) {
-        while self.num_deleted.load(Ordering::Relaxed) < num_deleted {
+        while self.num_deleted() < num_deleted {
             self.notifier.notified().await;
         }
     }
@@ -103,28 +147,40 @@ impl MockFilePurgeHandler {
 struct CompactionTester {
     base: Option<FileTesterBase>,
     purge_handler: MockFilePurgeHandler,
+    object_store: ObjectStore,
 }
 
 impl CompactionTester {
-    async fn new(store_dir: &str, engine_config: EngineConfig) -> CompactionTester {
+    async fn new(
+        store_dir: &str,
+        engine_config: EngineConfig,
+        s3_bucket: Option<String>,
+    ) -> CompactionTester {
         let purge_handler = MockFilePurgeHandler::default();
-        let region = create_region_for_compaction(
+        let (region, object_store) = create_region_for_compaction(
             store_dir,
             false,
             engine_config.clone(),
             purge_handler.clone(),
+            s3_bucket,
         )
         .await;
 
         CompactionTester {
             base: Some(FileTesterBase::with_region(region)),
             purge_handler,
+            object_store,
         }
     }
 
     #[inline]
     fn base(&self) -> &FileTesterBase {
         self.base.as_ref().unwrap()
+    }
+
+    #[inline]
+    fn base_mut(&mut self) -> &mut FileTesterBase {
+        self.base.as_mut().unwrap()
     }
 
     async fn put(&self, data: &[(i64, Option<i64>)]) -> WriteResponse {
@@ -148,43 +204,52 @@ impl CompactionTester {
     async fn wait_until_deleted(&self, num_deleted: usize) {
         self.purge_handler.wait_until_deleted(num_deleted).await
     }
+
+    /// Close region and clean up files.
+    async fn clean_up(mut self) {
+        self.base = None;
+
+        self.object_store.remove_all("/").await.unwrap();
+    }
 }
 
-#[tokio::test]
-async fn test_compact_during_read() {
-    common_telemetry::init_default_ut_logging();
-    use common_telemetry::logging;
-
+async fn compact_during_read(s3_bucket: Option<String>) {
     let dir = create_temp_dir("compact_read");
-
     let store_dir = dir.path().to_str().unwrap();
 
     // Use a large max_files_in_l0 to avoid compaction automatically.
-    let tester = CompactionTester::new(
+    let mut tester = CompactionTester::new(
         store_dir,
         EngineConfig {
             max_files_in_l0: 100,
             ..Default::default()
         },
+        s3_bucket,
     )
     .await;
 
+    let expect: Vec<_> = (0..2048).map(|v| (v, Some(v))).collect();
     // Put elements so we have content to flush (In SST1).
-    tester.put(&[(3000, Some(300))]).await;
-    tester.put(&[(2000, Some(200))]).await;
+    tester.put(&expect[0..1024]).await;
 
     // Flush content to SST1.
     tester.flush(None).await;
 
     // Put element (In SST2).
-    tester.put(&[(2000, Some(202))]).await;
-    tester.put(&[(1000, Some(100))]).await;
+    tester.put(&expect[1024..2048]).await;
 
     // Flush content to SST2.
     tester.flush(None).await;
 
+    logging::info!("Create reader");
+
+    tester.base_mut().read_ctx.batch_size = 1;
     // Create a reader.
     let reader = tester.base().full_scan_reader().await;
+
+    assert_eq!(0, tester.purge_handler.num_deleted());
+
+    logging::info!("Compact begin");
 
     // Trigger compaction.
     tester.compact().await;
@@ -198,6 +263,23 @@ async fn test_compact_during_read() {
 
     logging::info!("Collect reader end");
 
-    let expect = vec![(1000, Some(100)), (2000, Some(202)), (3000, Some(300))];
-    assert_eq!(expect, output);
+    assert_eq!(expect.len(), output.len());
+
+    tester.clean_up().await;
+}
+
+#[tokio::test]
+async fn test_compact_during_read_on_fs() {
+    common_telemetry::init_default_ut_logging();
+
+    compact_during_read(None).await;
+}
+
+#[tokio::test]
+async fn test_compact_during_read_on_s3() {
+    if let Ok(bucket) = env::var("GT_S3_BUCKET") {
+        if !bucket.is_empty() {
+            compact_during_read(Some(bucket)).await;
+        }
+    }
 }
