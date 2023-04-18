e8bb00f0beb788d755c6e5a93b7c7ade2a72bc2d
Ruihang Xia <waynestxia@gmail.com>
2023-04-18 23:25:14 +0800

    feat: impl instant query interface (#1410)
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests-integration/tests/http.rs
+++ tests-integration/tests/http.rs
@@ -290,11 +290,16 @@ pub async fn test_prom_http_api(store_type: StorageType) {
     let client = TestClient::new(app);
 
     // instant query
-    let res = client.get("/api/v1/query?query=up").send().await;
+    let res = client.get("/api/v1/query?query=up&time=1").send().await;
     assert_eq!(res.status(), StatusCode::OK);
-    let res = client.post("/api/v1/query?query=up").send().await;
+    let res = client
+        .post("/api/v1/query?query=up&time=1")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .send()
+        .await;
     assert_eq!(res.status(), StatusCode::OK);
 
+    // range query
     let res = client
         .get("/api/v1/query_range?query=up&start=1&end=100&step=5")
         .send()
