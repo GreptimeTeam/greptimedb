d538422bd0fab320309afcd0bae640ebc4e38337
Dennis Zhuang <killme2008@gmail.com>
2023-04-19 22:59:13 +0800

    test: add show create table sqlness test

---
+++ tests/cases/standalone/show/show_create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE system_metrics (
+  id INT UNSIGNED NULL,
+  host STRING NULL,
+  cpu DOUBLE NULL,
+  disk FLOAT NULL,
+  ts TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+  TIME INDEX (ts),
+  PRIMARY KEY (id, host)
+)
+ENGINE=mito
+WITH(
+  regions = 3
+);
+
+SHOW TABLES;
+
+SHOW CREATE TABLE system_metrics;
+
+DROP TABLE system_metrics;
