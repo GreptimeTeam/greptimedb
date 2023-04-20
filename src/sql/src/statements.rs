9c99ebfa505615b25d47a645e6bd3325e7472229
Dennis Zhuang <killme2008@gmail.com>
2023-04-20 17:21:39 +0800

    fix: compile error

--- src/sql/src/statements.rs
+++ src/sql/src/statements.rs
@@ -374,7 +374,9 @@ pub fn concrete_data_type_to_sql_data_type(data_type: &ConcreteDataType) -> Resu
         ConcreteDataType::DateTime(_) => Ok(SqlDataType::Datetime(None)),
         ConcreteDataType::Timestamp(_) => Ok(SqlDataType::Timestamp(None, TimezoneInfo::None)),
         ConcreteDataType::Binary(_) => Ok(SqlDataType::Varbinary(None)),
-        ConcreteDataType::List(_) | ConcreteDataType::Dictionary(_) => unreachable!(),
+        ConcreteDataType::Null(_) | ConcreteDataType::List(_) | ConcreteDataType::Dictionary(_) => {
+            unreachable!()
+        }
     }
 }
 
