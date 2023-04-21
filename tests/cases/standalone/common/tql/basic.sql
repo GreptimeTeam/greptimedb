94cd07724ddf654c4212a99745d0ee64b36838f3
Ruihang Xia <waynestxia@gmail.com>
2023-04-21 19:03:53 +0800

    test: add basic cases for distributed TQL
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ tests/cases/standalone/common/tql/basic.sql
@@ -0,0 +1,10 @@
+CREATE TABLE test(i DOUBLE, j TIMESTAMP TIME INDEX, k STRING PRIMARY KEY);
+
+-- insert two points at 1ms and one point at 2ms
+INSERT INTO test VALUES (1, 1, "a"), (1, 1, "b"), (2, 2, "a");
+
+-- evaluate at 0s, 5s and 10s. No point at 0s.
+TQL EVAL (0, 10, '5s') test;
+
+-- the point at 1ms will be shadowed by the point at 2ms
+TQL EVAL (0, 10, '5s') test{k="a"};
