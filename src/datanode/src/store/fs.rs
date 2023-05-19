112028a84de932fdd6ea224d2a6aa16a88ec2b83
Dennis Zhuang <killme2008@gmail.com>
2023-05-19 13:35:09 +0800

    fix: forgot files

---
+++ src/datanode/src/store/fs.rs
@@ -0,0 +1,43 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::{fs, path};
+
+use common_telemetry::logging::info;
+use object_store::services::Fs as FsBuilder;
+use object_store::{util, ObjectStore};
+use snafu::prelude::*;
+
+use crate::datanode::FileConfig;
+use crate::error::{self, Result};
+use crate::store;
+
+pub(crate) async fn new_fs_object_store(file_config: &FileConfig) -> Result<ObjectStore> {
+    let data_home = util::normalize_dir(&file_config.data_home);
+    fs::create_dir_all(path::Path::new(&data_home))
+        .context(error::CreateDirSnafu { dir: &data_home })?;
+    info!("The file storage home is: {}", &data_home);
+
+    let atomic_write_dir = format!("{data_home}/.tmp/");
+    store::clean_temp_dir(&atomic_write_dir)?;
+
+    let mut builder = FsBuilder::default();
+    builder.root(&data_home).atomic_write_dir(&atomic_write_dir);
+
+    let object_store = ObjectStore::new(builder)
+        .context(error::InitBackendSnafu)?
+        .finish();
+
+    Ok(object_store)
+}
---
+++ src/datanode/src/store/s3.rs
@@ -0,0 +1,49 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use common_telemetry::logging::info;
+use object_store::services::S3 as S3Builder;
+use object_store::{util, ObjectStore};
+use secrecy::ExposeSecret;
+use snafu::prelude::*;
+
+use crate::datanode::S3Config;
+use crate::error::{self, Result};
+
+pub(crate) async fn new_s3_object_store(s3_config: &S3Config) -> Result<ObjectStore> {
+    let root = util::normalize_dir(&s3_config.root);
+
+    info!(
+        "The s3 storage bucket is: {}, root is: {}",
+        s3_config.bucket, &root
+    );
+
+    let mut builder = S3Builder::default();
+    builder
+        .root(&root)
+        .bucket(&s3_config.bucket)
+        .access_key_id(s3_config.access_key_id.expose_secret())
+        .secret_access_key(s3_config.secret_access_key.expose_secret());
+
+    if s3_config.endpoint.is_some() {
+        builder.endpoint(s3_config.endpoint.as_ref().unwrap());
+    }
+    if s3_config.region.is_some() {
+        builder.region(s3_config.region.as_ref().unwrap());
+    }
+
+    Ok(ObjectStore::new(builder)
+        .context(error::InitBackendSnafu)?
+        .finish())
+}
