d8bd402d17b014054ae93ba9b6c61ebdb423d3c6
Ruihang Xia <waynestxia@gmail.com>
2023-05-30 15:11:21 +0800

    move sqlness cases contains plan out of common dir
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ tests/cases/standalone/tql-explain-analyze/analyze.sql
@@ -0,0 +1,13 @@
+CREATE TABLE test(i DOUBLE, j TIMESTAMP TIME INDEX, k STRING PRIMARY KEY);
+
+-- insert two points at 1ms and one point at 2ms
+INSERT INTO test VALUES (1, 1, "a"), (1, 1, "b"), (2, 2, "a");
+
+-- analyze at 0s, 5s and 10s. No point at 0s.
+-- SQLNESS REPLACE (metrics.*) REDACTED
+-- SQLNESS REPLACE (RoundRobinBatch.*) REDACTED
+-- SQLNESS REPLACE (-+) -
+-- SQLNESS REPLACE (\s\s+) _
+TQL ANALYZE (0, 10, '5s') test;
+
+DROP TABLE test;
