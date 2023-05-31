ae8203fafa825944a246a426902f2b793d1b67d9
dennis zhuang <killme2008@gmail.com>
2023-05-31 20:14:58 +0800

    fix: prepare statement doesn't support insert clause (#1680)
    * fix: insert clause doesn't support prepare statement
    
    * fix: manifeste dir
    
    * fix: format
    
    * fix: temp path

--- tests-integration/tests/main.rs
+++ tests-integration/tests/main.rs
@@ -16,10 +16,12 @@
 mod grpc;
 #[macro_use]
 mod http;
-
+#[macro_use]
+mod sql;
 #[macro_use]
 mod region_failover;
 
 grpc_tests!(File, S3, S3WithCache, Oss);
 http_tests!(File, S3, S3WithCache, Oss);
 region_failover_tests!(File, S3, S3WithCache, Oss);
+sql_tests!(File);
