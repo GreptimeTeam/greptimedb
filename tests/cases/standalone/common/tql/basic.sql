25cb6674702893d81813ba2ae609052a8e9dd6bf
Ruihang Xia <waynestxia@gmail.com>
2023-06-30 17:25:24 +0800

    fix: sort unstable sqlness result (#1858)
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests/cases/standalone/common/tql/basic.sql
+++ tests/cases/standalone/common/tql/basic.sql
@@ -3,6 +3,7 @@ CREATE TABLE test(i DOUBLE, j TIMESTAMP TIME INDEX, k STRING PRIMARY KEY);
 -- insert two points at 1ms and one point at 2ms
 INSERT INTO test VALUES (1, 1, "a"), (1, 1, "b"), (2, 2, "a");
 
+-- SQLNESS SORT_RESULT 2 1
 -- evaluate at 0s, 5s and 10s. No point at 0s.
 TQL EVAL (0, 10, '5s') test;
 
