c4a3ad2af7fb4330d89c1b336ad2f0940ad34410
WenyXu <wenymedia@gmail.com>
2023-07-07 02:14:09 +0000

    chore: remove unused attributes

--- src/meta-srv/src/procedure/create_table.rs
+++ src/meta-srv/src/procedure/create_table.rs
@@ -38,15 +38,11 @@ use crate::service::router::create_table_global_value;
 use crate::service::store::txn::{Compare, CompareOp, Txn, TxnOp};
 use crate::table_routes::get_table_global_value;
 
-// TODO(weny): removes in following PRs.
-#[allow(unused)]
 pub struct CreateTableProcedure {
     context: DdlContext,
     creator: TableCreator,
 }
 
-// TODO(weny): removes in following PRs.
-#[allow(dead_code)]
 impl CreateTableProcedure {
     pub(crate) const TYPE_NAME: &'static str = "metasrv-procedure::CreateTable";
 
