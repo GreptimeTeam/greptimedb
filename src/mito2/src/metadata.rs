32ace80409c10e5af617e68536aed67ee301687e
Ruihang Xia <waynestxia@gmail.com>
2023-07-14 11:10:38 +0800

    feat: implement serialize/deserialize for RegionMetadata
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/mito2/src/metadata.rs
+++ src/mito2/src/metadata.rs
@@ -16,12 +16,16 @@
 
 use std::sync::Arc;
 
-use datatypes::schema::{ColumnSchema, SchemaRef};
+use datatypes::schema::{ColumnSchema, Schema, SchemaRef};
+use serde::ser::SerializeStruct;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use store_api::storage::{ColumnId, RegionId};
 
 use crate::region::VersionNumber;
 
 /// Static metadata of a region.
+///
+/// This struct implements [Serialize] and [Deserialize] traits.
 ///
 /// ```mermaid
 /// class RegionMetadata {
@@ -42,8 +46,8 @@ use crate::region::VersionNumber;
 /// RegionMetadata o-- ColumnMetadata
 /// ColumnMetadata o-- SemanticType
 /// ```
-#[derive(Debug)]
-pub(crate) struct RegionMetadata {
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RegionMetadata {
     /// Latest schema of this region
     schema: SchemaRef,
     column_metadatas: Vec<ColumnMetadata>,
@@ -56,11 +60,109 @@ pub(crate) struct RegionMetadata {
     id: RegionId,
 }
 
-pub(crate) type RegionMetadataRef = Arc<RegionMetadata>;
+pub type RegionMetadataRef = Arc<RegionMetadata>;
+
+impl RegionMetadata {
+    // pub fn try_new(id:RegionId, primary_keys:Vec<ColumnId>, version_num)
+}
+
+impl Serialize for RegionMetadata {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut se = serializer.serialize_struct("RegionMetadata", 4)?;
+        se.serialize_field("column_metadatas", &self.column_metadatas)?;
+        se.serialize_field("version", &self.version)?;
+        se.serialize_field("primary_keys", &self.primary_keys)?;
+        se.serialize_field("id", &self.id)?;
+        se.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for RegionMetadata {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        // helper internal struct for deserialization
+        #[derive(Deserialize)]
+        struct RegionMetadataWithoutSchema {
+            column_metadatas: Vec<ColumnMetadata>,
+            version: VersionNumber,
+            primary_keys: Vec<ColumnId>,
+            id: RegionId,
+        }
+
+        let region_metadata_without_schema =
+            RegionMetadataWithoutSchema::deserialize(deserializer)?;
+
+        let column_schemas = region_metadata_without_schema
+            .column_metadatas
+            .iter()
+            .map(|column_metadata| column_metadata.column_schema.clone())
+            .collect();
+        let schema = Arc::new(Schema::new(column_schemas));
+
+        Ok(Self {
+            schema,
+            column_metadatas: region_metadata_without_schema.column_metadatas,
+            version: region_metadata_without_schema.version,
+            primary_keys: region_metadata_without_schema.primary_keys,
+            id: region_metadata_without_schema.id,
+        })
+    }
+}
+
+pub struct RegionMetadataBuilder {
+    schema: SchemaRef,
+    column_metadatas: Vec<ColumnMetadata>,
+    version: VersionNumber,
+    primary_keys: Vec<ColumnId>,
+    id: RegionId,
+}
+
+impl RegionMetadataBuilder {
+    pub fn new(id: RegionId, version: VersionNumber) -> Self {
+        Self {
+            schema: Arc::new(Schema::new(vec![])),
+            column_metadatas: vec![],
+            version,
+            primary_keys: vec![],
+            id,
+        }
+    }
+
+    /// Add a column metadata to this region metadata.
+    /// This method will check the semantic type and add it to primary keys automatically.
+    pub fn add_column_metadata(mut self, column_metadata: ColumnMetadata) -> Self {
+        if column_metadata.semantic_type == SemanticType::Tag {
+            self.primary_keys.push(column_metadata.column_id);
+        }
+        self.column_metadatas.push(column_metadata);
+        self
+    }
+
+    pub fn build(self) -> RegionMetadata {
+        let schema = Arc::new(Schema::new(
+            self.column_metadatas
+                .iter()
+                .map(|column_metadata| column_metadata.column_schema.clone())
+                .collect(),
+        ));
+        RegionMetadata {
+            schema,
+            column_metadatas: self.column_metadatas,
+            version: self.version,
+            primary_keys: self.primary_keys,
+            id: self.id,
+        }
+    }
+}
 
 /// Metadata of a column.
-#[derive(Debug)]
-pub(crate) struct ColumnMetadata {
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct ColumnMetadata {
     /// Schema of this column. Is the same as `column_schema` in [SchemaRef].
     column_schema: ColumnSchema,
     semantic_type: SemanticType,
@@ -68,9 +170,48 @@ pub(crate) struct ColumnMetadata {
 }
 
 /// The semantic type of one column
-#[derive(Debug)]
-pub(crate) enum SemanticType {
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum SemanticType {
     Tag,
     Field,
     Timestamp,
+}
+
+#[cfg(test)]
+mod test {
+    use datatypes::prelude::ConcreteDataType;
+
+    use super::*;
+
+    fn build_test_region_metadata() -> RegionMetadata {
+        let builder = RegionMetadataBuilder::new(RegionId::new(1234, 5678), 9);
+        let builder = builder.add_column_metadata(ColumnMetadata {
+            column_schema: ColumnSchema::new("a", ConcreteDataType::int64_datatype(), false),
+            semantic_type: SemanticType::Tag,
+            column_id: 1,
+        });
+        let builder = builder.add_column_metadata(ColumnMetadata {
+            column_schema: ColumnSchema::new("b", ConcreteDataType::float64_datatype(), false),
+            semantic_type: SemanticType::Field,
+            column_id: 2,
+        });
+        let builder = builder.add_column_metadata(ColumnMetadata {
+            column_schema: ColumnSchema::new(
+                "c",
+                ConcreteDataType::timestamp_millisecond_datatype(),
+                false,
+            ),
+            semantic_type: SemanticType::Timestamp,
+            column_id: 3,
+        });
+        builder.build()
+    }
+
+    #[test]
+    fn test_region_metadata_serde() {
+        let region_metadata = build_test_region_metadata();
+        let serialized = serde_json::to_string(&region_metadata).unwrap();
+        let deserialized: RegionMetadata = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(region_metadata, deserialized);
+    }
 }
