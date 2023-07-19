70ac82e11be3b44c0d5d71e43cbb1c35671aee7b
Lei, HUANG <mrsatangel@gmail.com>
2023-07-19 12:59:55 +0800

    chore: change logstore namespace prefix

--- src/log-store/src/raft_engine/log_store.rs
+++ src/log-store/src/raft_engine/log_store.rs
@@ -33,7 +33,7 @@ use crate::error::{
 };
 use crate::raft_engine::protos::logstore::{EntryImpl, NamespaceImpl as Namespace};
 
-const NAMESPACE_PREFIX: &str = "__sys_namespace_";
+const NAMESPACE_PREFIX: &str = "$sys_namespace$";
 const SYSTEM_NAMESPACE: u64 = 0;
 
 pub struct RaftEngineLogStore {
