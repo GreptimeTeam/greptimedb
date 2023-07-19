3241de0b8558c76c1767e979f31f39845e289d4b
Ben Baoyi <67940783+Gump9@users.noreply.github.com>
2023-07-19 13:22:36 +0800

    refactor: Separating statement parse func (#1975)
    * refactor:Separating statement parse func
    
    * refactor:refactor describe,explain and drop
    
    * Update src/sql/src/parser.rs
    
    ---------
    
    Co-authored-by: LFC <bayinamine@gmail.com>

---
+++ src/sql/src/parsers/describe_parser.rs
@@ -0,0 +1,80 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use snafu::{ensure, ResultExt};
+use sqlparser::keywords::Keyword;
+
+use crate::error::{self, InvalidTableNameSnafu, Result};
+use crate::parser::ParserContext;
+use crate::statements::describe::DescribeTable;
+use crate::statements::statement::Statement;
+
+/// DESCRIBE statement parser implementation
+impl<'a> ParserContext<'a> {
+    pub(crate) fn parse_describe(&mut self) -> Result<Statement> {
+        if self.matches_keyword(Keyword::TABLE) {
+            let _ = self.parser.next_token();
+        }
+        self.parse_describe_table()
+    }
+
+    fn parse_describe_table(&mut self) -> Result<Statement> {
+        let table_idents =
+            self.parser
+                .parse_object_name()
+                .with_context(|_| error::UnexpectedSnafu {
+                    sql: self.sql,
+                    expected: "a table name",
+                    actual: self.peek_token_as_string(),
+                })?;
+        ensure!(
+            !table_idents.0.is_empty(),
+            InvalidTableNameSnafu {
+                name: table_idents.to_string(),
+            }
+        );
+        Ok(Statement::DescribeTable(DescribeTable::new(table_idents)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use sqlparser::ast::Expr;
+
+    use super::*;
+    use crate::dialect::GreptimeDbDialect;
+
+    #[test]
+    fn test_parse_function() {
+        let expr =
+            ParserContext::parse_function("current_timestamp()", &GreptimeDbDialect {}).unwrap();
+        assert!(matches!(expr, Expr::Function(_)));
+    }
+
+    fn assert_describe_table(sql: &str) {
+        let stmt = ParserContext::create_with_dialect(sql, &GreptimeDbDialect {})
+            .unwrap()
+            .pop()
+            .unwrap();
+        assert!(matches!(stmt, Statement::DescribeTable(_)))
+    }
+
+    #[test]
+    fn test_parse_describe_table() {
+        assert_describe_table("desc table t;");
+        assert_describe_table("describe table t;");
+        assert_describe_table("desc t;");
+        assert_describe_table("describe t;");
+    }
+}
---
+++ src/sql/src/parsers/explain_parser.rs
@@ -0,0 +1,100 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use snafu::ResultExt;
+
+use crate::error::{self, Result};
+use crate::parser::ParserContext;
+use crate::statements::explain::Explain;
+use crate::statements::statement::Statement;
+
+/// EXPLAIN statement parser implementation
+impl<'a> ParserContext<'a> {
+    pub(crate) fn parse_explain(&mut self) -> Result<Statement> {
+        let explain_statement =
+            self.parser
+                .parse_explain(false)
+                .with_context(|_| error::UnexpectedSnafu {
+                    sql: self.sql,
+                    expected: "a query statement",
+                    actual: self.peek_token_as_string(),
+                })?;
+
+        Ok(Statement::Explain(Explain::try_from(explain_statement)?))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use sqlparser::ast::{Query as SpQuery, Statement as SpStatement, WildcardAdditionalOptions};
+
+    use super::*;
+    use crate::dialect::GreptimeDbDialect;
+
+    #[test]
+    pub fn test_explain() {
+        let sql = "EXPLAIN select * from foo";
+        let result = ParserContext::create_with_dialect(sql, &GreptimeDbDialect {});
+        let stmts = result.unwrap();
+        assert_eq!(1, stmts.len());
+
+        let select = sqlparser::ast::Select {
+            distinct: None,
+            top: None,
+            projection: vec![sqlparser::ast::SelectItem::Wildcard(
+                WildcardAdditionalOptions::default(),
+            )],
+            into: None,
+            from: vec![sqlparser::ast::TableWithJoins {
+                relation: sqlparser::ast::TableFactor::Table {
+                    name: sqlparser::ast::ObjectName(vec![sqlparser::ast::Ident::new("foo")]),
+                    alias: None,
+                    args: None,
+                    with_hints: vec![],
+                },
+                joins: vec![],
+            }],
+            lateral_views: vec![],
+            selection: None,
+            group_by: vec![],
+            cluster_by: vec![],
+            distribute_by: vec![],
+            sort_by: vec![],
+            having: None,
+            qualify: None,
+            named_window: vec![],
+        };
+
+        let sp_statement = SpStatement::Query(Box::new(SpQuery {
+            with: None,
+            body: Box::new(sqlparser::ast::SetExpr::Select(Box::new(select))),
+            order_by: vec![],
+            limit: None,
+            offset: None,
+            fetch: None,
+            locks: vec![],
+        }));
+
+        let explain = Explain::try_from(SpStatement::Explain {
+            describe_alias: false,
+            analyze: false,
+            verbose: false,
+            statement: Box::new(sp_statement),
+            format: None,
+        })
+        .unwrap();
+
+        assert_eq!(stmts[0], Statement::Explain(explain))
+    }
+}
