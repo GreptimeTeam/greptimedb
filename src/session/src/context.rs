052e2d6f8f8f0accdb2504ddd1c3ed887c8c30a8
shuiyisong <xixing.sys@gmail.com>
2023-07-21 16:55:01 +0800

    chore: generate trace_id while creating query_ctx

--- src/session/src/context.rs
+++ src/session/src/context.rs
@@ -14,8 +14,6 @@
 
 use std::fmt::{Display, Formatter};
 use std::net::SocketAddr;
-use std::sync::atomic::AtomicU64;
-use std::sync::atomic::Ordering::{Relaxed, Release};
 use std::sync::Arc;
 
 use arc_swap::ArcSwap;
@@ -34,7 +32,7 @@ pub struct QueryContext {
     current_schema: ArcSwap<String>,
     time_zone: ArcSwap<Option<TimeZone>>,
     sql_dialect: Box<dyn Dialect + Send + Sync>,
-    trace_id: AtomicU64,
+    trace_id: u64,
 }
 
 impl Default for QueryContext {
@@ -65,7 +63,7 @@ impl QueryContext {
             current_schema: ArcSwap::new(Arc::new(DEFAULT_SCHEMA_NAME.to_string())),
             time_zone: ArcSwap::new(Arc::new(None)),
             sql_dialect: Box::new(GreptimeDbDialect {}),
-            trace_id: AtomicU64::new(0),
+            trace_id: common_telemetry::gen_trace_id(),
         }
     }
 
@@ -83,7 +81,7 @@ impl QueryContext {
             current_schema: ArcSwap::new(Arc::new(schema.to_string())),
             time_zone: ArcSwap::new(Arc::new(None)),
             sql_dialect,
-            trace_id: AtomicU64::new(0),
+            trace_id: common_telemetry::gen_trace_id(),
         }
     }
 
@@ -140,14 +138,7 @@ impl QueryContext {
 
     #[inline]
     pub fn trace_id(&self) -> u64 {
-        self.trace_id.load(Relaxed)
-    }
-
-    #[inline]
-    pub fn set_trace_id(&self, trace_id: u64) {
-        let _ = self
-            .trace_id
-            .compare_exchange(0, trace_id, Release, Relaxed);
+        self.trace_id
     }
 }
 
