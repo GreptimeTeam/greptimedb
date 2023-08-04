9b7928631556aef97da6a729a10fd765698e08b7
jiachun <jiachun_fjc@163.com>
2023-08-04 15:53:10 +0800

    feat: row-inserter by standalone

--- src/datanode/src/error.rs
+++ src/datanode/src/error.rs
@@ -477,6 +477,34 @@ pub enum Error {
         location: Location,
     },
 
+    #[snafu(display("Empty data: {}", msg))]
+    EmptyData { msg: String, location: Location },
+
+    #[snafu(display(
+        "Invalid insert row len, table: {}, expected: {}, actual: {}",
+        table_name,
+        expected,
+        actual
+    ))]
+    InvalidInsertRowLen {
+        table_name: String,
+        expected: usize,
+        actual: usize,
+        location: Location,
+    },
+
+    #[snafu(display("Column datatype error, source: {}", source))]
+    ColumnDataType {
+        location: Location,
+        source: api::error::Error,
+    },
+
+    #[snafu(display("Failed to create vector, source: {}", source))]
+    CreateVector {
+        location: Location,
+        source: datatypes::error::Error,
+    },
+
     #[snafu(display("Unexpected, violated: {}", violated))]
     Unexpected {
         violated: String,
@@ -508,6 +536,7 @@ impl ErrorExt for Error {
             TableEngineNotFound { source, .. } | EngineProcedureNotFound { source, .. } => {
                 source.status_code()
             }
+            CreateVector { source, .. } => source.status_code(),
             TableNotFound { .. } => StatusCode::TableNotFound,
             ColumnNotFound { .. } => StatusCode::TableColumnNotFound,
 
@@ -534,7 +563,10 @@ impl ErrorExt for Error {
             | MissingMetasrvOpts { .. }
             | ColumnNoneDefaultValue { .. }
             | MissingWalDirConfig { .. }
-            | PrepareImmutableTable { .. } => StatusCode::InvalidArguments,
+            | PrepareImmutableTable { .. }
+            | InvalidInsertRowLen { .. }
+            | EmptyData { .. }
+            | ColumnDataType { .. } => StatusCode::InvalidArguments,
 
             EncodeJson { .. } | DecodeJson { .. } | PayloadNotExist { .. } | Unexpected { .. } => {
                 StatusCode::Unexpected
--- src/datanode/src/row_inserter.rs
+++ src/datanode/src/row_inserter.rs
@@ -12,15 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
+
+use api::helper::ColumnDataTypeWrapper;
+use api::v1::value::Value;
 use api::v1::{RowInsertRequest, RowInsertRequests};
 use catalog::CatalogManagerRef;
 use common_query::Output;
+use common_time::time::Time;
+use common_time::Timestamp;
+use datatypes::data_type::{ConcreteDataType, DataType};
+use datatypes::value::Value as InternalValue;
 use futures_util::future;
 use session::context::QueryContextRef;
-use snafu::{OptionExt, ResultExt};
+use snafu::{ensure, OptionExt, ResultExt};
 use table::requests::InsertRequest;
 
-use crate::error::{CatalogSnafu, InsertSnafu, JoinTaskSnafu, Result};
+use crate::error::{
+    CatalogSnafu, ColumnDataTypeSnafu, CreateVectorSnafu, EmptyDataSnafu, InsertSnafu,
+    InvalidInsertRowLenSnafu, JoinTaskSnafu, Result,
+};
 
 pub struct RowInserter {
     catalog_manager: CatalogManagerRef,
@@ -70,10 +81,90 @@ impl RowInserter {
     }
 
     pub fn convert_to_table_insert_request(
-        _catalog_name: &str,
-        _schema_name: &str,
-        _request: RowInsertRequest,
+        catalog_name: &str,
+        schema_name: &str,
+        request: RowInsertRequest,
     ) -> Result<InsertRequest> {
-        todo!("jeremy")
+        let table_name = request.table_name;
+        let region_number = request.region_number;
+        let rows = request.rows.context(EmptyDataSnafu {
+            msg: format!("insert to table: {:?}", table_name),
+        })?;
+        let schema = rows.schema;
+
+        let mut columns_values = HashMap::with_capacity(schema.len());
+        for row in rows.rows {
+            let row_values = row.values;
+
+            ensure!(
+                row_values.len() == schema.len(),
+                InvalidInsertRowLenSnafu {
+                    table_name: format!("{catalog_name}.{schema_name}.{table_name}"),
+                    expected: schema.len(),
+                    actual: row_values.len(),
+                }
+            );
+
+            let row_count = row_values.len();
+            for (i, value) in row_values.into_iter().enumerate() {
+                let column_name = schema[i].column_name.clone();
+                let datatype: ConcreteDataType = ColumnDataTypeWrapper::try_new(schema[i].datatype)
+                    .context(ColumnDataTypeSnafu)?
+                    .into();
+                let column_values_builder = columns_values
+                    .entry(column_name)
+                    .or_insert_with(|| datatype.create_mutable_vector(row_count));
+
+                if let Some(value) = value.value {
+                    let value = Self::convert_value(value);
+                    column_values_builder
+                        .try_push_value_ref(value.as_value_ref())
+                        .context(CreateVectorSnafu)?;
+                } else {
+                    column_values_builder.push_null();
+                }
+            }
+        }
+
+        let columns_values = columns_values
+            .into_iter()
+            .map(|(k, mut v)| (k, v.to_vector()))
+            .collect();
+
+        Ok(InsertRequest {
+            catalog_name: catalog_name.to_string(),
+            schema_name: schema_name.to_string(),
+            table_name,
+            columns_values,
+            region_number,
+        })
+    }
+
+    fn convert_value(value: Value) -> InternalValue {
+        match value {
+            Value::I8Value(v) => (v as i8).into(),
+            Value::I16Value(v) => (v as i16).into(),
+            Value::I32Value(v) => v.into(),
+            Value::I64Value(v) => v.into(),
+            Value::U8Value(v) => (v as u8).into(),
+            Value::U16Value(v) => (v as u16).into(),
+            Value::U32Value(v) => v.into(),
+            Value::U64Value(v) => v.into(),
+            Value::F32Value(v) => v.into(),
+            Value::F64Value(v) => v.into(),
+            Value::BoolValue(v) => v.into(),
+            Value::BinaryValue(v) => v.into(),
+            Value::StringValue(v) => v.into(),
+            Value::DateValue(v) => InternalValue::Date(v.into()),
+            Value::DatetimeValue(v) => InternalValue::DateTime(v.into()),
+            Value::TsSecondValue(v) => InternalValue::Timestamp(Timestamp::new_second(v)),
+            Value::TsMillisecondValue(v) => InternalValue::Timestamp(Timestamp::new_millisecond(v)),
+            Value::TsMicrosecondValue(v) => InternalValue::Timestamp(Timestamp::new_microsecond(v)),
+            Value::TsNanosecondValue(v) => InternalValue::Timestamp(Timestamp::new_nanosecond(v)),
+            Value::TimeSecondValue(v) => InternalValue::Time(Time::new_second(v)),
+            Value::TimeMillisecondValue(v) => InternalValue::Time(Time::new_millisecond(v)),
+            Value::TimeMicrosecondValue(v) => InternalValue::Time(Time::new_microsecond(v)),
+            Value::TimeNanosecondValue(v) => InternalValue::Time(Time::new_nanosecond(v)),
+        }
     }
 }
