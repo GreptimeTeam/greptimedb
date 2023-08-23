89d403c570d4d1b51d1c1b91840a475277887126
Ruihang Xia <waynestxia@gmail.com>
2023-08-23 22:07:03 +0800

    clean up
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/query/src/dist_plan/analyzer.rs
+++ src/query/src/dist_plan/analyzer.rs
@@ -12,15 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::{Arc, Mutex};
-
 use datafusion::datasource::DefaultTableSource;
 use datafusion::error::Result as DfResult;
 use datafusion_common::config::ConfigOptions;
-use datafusion_common::tree_node::{
-    RewriteRecursion, Transformed, TreeNode, TreeNodeRewriter, TreeNodeVisitor, VisitRecursion,
-};
-use datafusion_expr::{Extension, LogicalPlan};
+use datafusion_common::tree_node::{RewriteRecursion, TreeNode, TreeNodeRewriter};
+use datafusion_expr::LogicalPlan;
 use datafusion_optimizer::analyzer::AnalyzerRule;
 use substrait::{DFLogicalSubstraitConvertor, SubstraitPlan};
 use table::metadata::TableType;
@@ -30,7 +26,6 @@ use crate::dist_plan::commutativity::{
     partial_commutative_transformer, Categorizer, Commutativity,
 };
 use crate::dist_plan::merge_scan::MergeScanLogicalPlan;
-use crate::dist_plan::utils;
 
 pub struct DistPlannerAnalyzer;
 
@@ -44,230 +39,8 @@ impl AnalyzerRule for DistPlannerAnalyzer {
         plan: LogicalPlan,
         _config: &ConfigOptions,
     ) -> datafusion_common::Result<LogicalPlan> {
-        // // (1) transform up merge scan
-        // let mut visitor = CommutativeVisitor::new();
-        // let _ = plan.visit(&mut visitor)?;
-        // let state = ExpandState::new();
-        // let plan = plan.transform_down(&|plan| Self::expand(plan, &visitor, &state))?;
-
-        // // (2) remove placeholder merge scan
-        // let plan = plan.transform(&Self::remove_placeholder_merge_scan)?;
-
-        // Ok(plan)
         let mut rewriter = MagicRewriter::default();
         plan.rewrite(&mut rewriter)
-    }
-}
-
-impl DistPlannerAnalyzer {
-    /// Add [MergeScanLogicalPlan] before the table scan
-    #[allow(dead_code)]
-    fn add_merge_scan(plan: LogicalPlan) -> datafusion_common::Result<Transformed<LogicalPlan>> {
-        Ok(match plan {
-            LogicalPlan::TableScan(table_scan) => {
-                let ext_plan = LogicalPlan::Extension(Extension {
-                    node: Arc::new(MergeScanLogicalPlan::new(
-                        LogicalPlan::TableScan(table_scan),
-                        true,
-                    )),
-                });
-                Transformed::Yes(ext_plan)
-            }
-            _ => Transformed::No(plan),
-        })
-    }
-
-    /// Remove placeholder [MergeScanLogicalPlan]
-    fn remove_placeholder_merge_scan(
-        plan: LogicalPlan,
-    ) -> datafusion_common::Result<Transformed<LogicalPlan>> {
-        Ok(match &plan {
-            LogicalPlan::Extension(extension)
-                if extension.node.name() == MergeScanLogicalPlan::name() =>
-            {
-                let merge_scan = extension
-                    .node
-                    .as_any()
-                    .downcast_ref::<MergeScanLogicalPlan>()
-                    .unwrap();
-                if merge_scan.is_placeholder() {
-                    Transformed::Yes(merge_scan.input().clone())
-                } else {
-                    Transformed::No(plan)
-                }
-            }
-            _ => Transformed::No(plan),
-        })
-    }
-
-    /// Expand stages on the stop node
-    fn expand(
-        mut plan: LogicalPlan,
-        visitor: &CommutativeVisitor,
-        state: &ExpandState,
-    ) -> datafusion_common::Result<Transformed<LogicalPlan>> {
-        if state.is_transformed() {
-            // only transform once
-            return Ok(Transformed::No(plan));
-        }
-        if let Some(stop_node) = visitor.stop_node && utils::hash_plan(&plan) != stop_node {
-            // only act with the stop node or the root (the first node seen by this closure) if no stop node
-            return Ok(Transformed::No(plan));
-        }
-
-        if visitor.stop_node.is_some() {
-            // insert merge scan between the stop node and its child
-            let children = plan.inputs();
-            let mut new_children = Vec::with_capacity(children.len());
-            for child in children {
-                let mut new_child =
-                    MergeScanLogicalPlan::new(child.clone(), false).into_logical_plan();
-                // expand stages
-                for new_stage in &visitor.next_stage {
-                    new_child = new_stage.with_new_inputs(&[new_child])?
-                }
-                new_children.push(new_child);
-            }
-            plan = plan.with_new_inputs(&new_children)?;
-        } else {
-            // otherwise add merge scan as the new root
-            plan = MergeScanLogicalPlan::new(plan, false).into_logical_plan();
-            // expand stages
-            for new_stage in &visitor.next_stage {
-                plan = new_stage.with_new_inputs(&[plan])?
-            }
-        }
-
-        state.set_transformed();
-        Ok(Transformed::Yes(plan))
-    }
-}
-
-struct ExpandState {
-    transformed: Mutex<bool>,
-}
-
-impl ExpandState {
-    pub fn new() -> Self {
-        Self {
-            transformed: Mutex::new(false),
-        }
-    }
-
-    pub fn is_transformed(&self) -> bool {
-        *self.transformed.lock().unwrap()
-    }
-
-    /// Set the state to transformed
-    pub fn set_transformed(&self) {
-        *self.transformed.lock().unwrap() = true;
-    }
-}
-
-#[derive(Debug)]
-struct CommutativeVisitor {
-    next_stage: Vec<LogicalPlan>,
-    // hash of the stop node
-    stop_node: Option<u64>,
-    /// Partition columns of current visiting table
-    current_partition_cols: Option<Vec<String>>,
-}
-
-impl TreeNodeVisitor for CommutativeVisitor {
-    type N = LogicalPlan;
-
-    fn pre_visit(&mut self, plan: &LogicalPlan) -> datafusion_common::Result<VisitRecursion> {
-        // find the first merge scan and stop traversing down
-        // todo: check if it works for join
-        Ok(match plan {
-            LogicalPlan::TableScan(table_scan) => {
-                // TODO(ruihang): spawn a sub visitor to retrieve partition columns
-                if let Some(source) = table_scan
-                    .source
-                    .as_any()
-                    .downcast_ref::<DefaultTableSource>()
-                {
-                    if let Some(provider) = source
-                        .table_provider
-                        .as_any()
-                        .downcast_ref::<DfTableProviderAdapter>()
-                    {
-                        if provider.table().table_type() == TableType::Base {
-                            let info = provider.table().table_info();
-                            let partition_key_indices = info.meta.partition_key_indices.clone();
-                            let schema = info.meta.schema.clone();
-                            let partition_cols = partition_key_indices
-                                .into_iter()
-                                .map(|index| schema.column_name_by_index(index).to_string())
-                                .collect::<Vec<String>>();
-                            self.current_partition_cols = Some(partition_cols);
-                        }
-                    }
-                }
-                VisitRecursion::Continue
-            }
-            _ => VisitRecursion::Continue,
-        })
-    }
-
-    fn post_visit(&mut self, plan: &LogicalPlan) -> datafusion_common::Result<VisitRecursion> {
-        if DFLogicalSubstraitConvertor.encode(plan).is_err() {
-            common_telemetry::info!(
-                "substrait error: {:?}",
-                DFLogicalSubstraitConvertor.encode(plan)
-            );
-            self.stop_node = Some(utils::hash_plan(plan));
-            return Ok(VisitRecursion::Stop);
-        }
-
-        match Categorizer::check_plan(plan) {
-            Commutativity::Commutative => {}
-            Commutativity::PartialCommutative => {
-                if let Some(plan) = partial_commutative_transformer(plan) {
-                    self.next_stage.push(plan)
-                }
-            }
-            Commutativity::ConditionalCommutative(transformer) => {
-                if let Some(transformer) = transformer
-                    && let Some(plan) = transformer(plan) {
-                    self.next_stage.push(plan)
-                }
-            },
-            Commutativity::TransformedCommutative(transformer) => {
-                if let Some(transformer) = transformer
-                    && let Some(plan) = transformer(plan) {
-                    self.next_stage.push(plan)
-                }
-            },
-            Commutativity::CheckPartition => {
-                if let Some(partition_cols) = &self.current_partition_cols
-                    && partition_cols.is_empty() {
-                    // no partition columns, and can be encoded skip
-                    return Ok(VisitRecursion::Continue);
-                } else {
-                    self.stop_node = Some(utils::hash_plan(plan));
-                    return Ok(VisitRecursion::Stop);
-                }
-            },
-            Commutativity::NonCommutative
-            | Commutativity::Unimplemented
-            | Commutativity::Unsupported => {
-                self.stop_node = Some(utils::hash_plan(plan));
-                return Ok(VisitRecursion::Stop);
-            }
-        }
-
-        Ok(VisitRecursion::Continue)
-    }
-}
-
-impl CommutativeVisitor {
-    pub fn new() -> Self {
-        Self {
-            next_stage: vec![],
-            stop_node: None,
-            current_partition_cols: None,
-        }
     }
 }
 
@@ -284,6 +57,7 @@ struct MagicRewriter {
     stack: Vec<(LogicalPlan, usize)>,
     stage: Vec<LogicalPlan>,
     status: RewriterStatus,
+    partition_cols: Option<Vec<String>>,
 }
 
 impl MagicRewriter {
@@ -325,22 +99,10 @@ impl MagicRewriter {
                     self.stage.push(plan)
                 }
             },
-            // Commutativity::CheckPartition => {
-            //     if let Some(partition_cols) = &self.current_partition_cols
-            //         && partition_cols.is_empty() {
-            //         // no partition columns, and can be encoded skip
-            //         return Ok(VisitRecursion::Continue);
-            //     } else {
-            //         self.stop_node = Some(utils::hash_plan(plan));
-            //         return Ok(VisitRecursion::Stop);
-            //     }
-            // },
-            Commutativity::CheckPartition|
-            Commutativity::NonCommutative
+            Commutativity::CheckPartition
+            | Commutativity::NonCommutative
             | Commutativity::Unimplemented
             | Commutativity::Unsupported => {
-                // self.stop_node = Some(utils::hash_plan(plan));
-                // return Ok(VisitRecursion::Stop);
                 return true;
             }
         }
@@ -359,6 +121,37 @@ impl MagicRewriter {
     fn set_unexpanded(&mut self) {
         self.status = RewriterStatus::Unexpanded;
     }
+
+    fn maybe_set_partitions(&mut self, plan: &LogicalPlan) {
+        if self.partition_cols.is_some() {
+            return;
+        }
+
+        if let LogicalPlan::TableScan(table_scan) = plan {
+            if let Some(source) = table_scan
+                .source
+                .as_any()
+                .downcast_ref::<DefaultTableSource>()
+            {
+                if let Some(provider) = source
+                    .table_provider
+                    .as_any()
+                    .downcast_ref::<DfTableProviderAdapter>()
+                {
+                    if provider.table().table_type() == TableType::Base {
+                        let info = provider.table().table_info();
+                        let partition_key_indices = info.meta.partition_key_indices.clone();
+                        let schema = info.meta.schema.clone();
+                        let partition_cols = partition_key_indices
+                            .into_iter()
+                            .map(|index| schema.column_name_by_index(index).to_string())
+                            .collect::<Vec<String>>();
+                        self.partition_cols = Some(partition_cols);
+                    }
+                }
+            }
+        }
+    }
 }
 
 impl TreeNodeRewriter for MagicRewriter {
@@ -371,7 +164,7 @@ impl TreeNodeRewriter for MagicRewriter {
         // decendening will clear the stage
         self.stage.clear();
         self.set_unexpanded();
-        common_telemetry::info!("[DEBUG] pre visit: {:?}", node);
+        self.partition_cols = None;
         Ok(RewriteRecursion::Continue)
     }
 
@@ -384,7 +177,7 @@ impl TreeNodeRewriter for MagicRewriter {
             return Ok(node);
         }
 
-        common_telemetry::info!("[DEBUG] mutate: {:?}", node);
+        self.maybe_set_partitions(&node);
 
         let Some(parent) = self.get_parent() else {
             // add merge scan as the new root
@@ -400,11 +193,8 @@ impl TreeNodeRewriter for MagicRewriter {
             return Ok(node);
         };
 
-        common_telemetry::trace!("[DEBUG] parent: {:?}", parent);
-
         // TODO: avoid this clone
         if self.should_expand(&parent.clone()) {
-            common_telemetry::info!("[DEBUG] node before expand: {:?}", node);
             // TODO: does this work for nodes with multiple children?;
             // replace the current node with expanded one
             let mut node = MergeScanLogicalPlan::new(node, false).into_logical_plan();
@@ -412,7 +202,6 @@ impl TreeNodeRewriter for MagicRewriter {
             for new_stage in self.stage.drain(..) {
                 node = new_stage.with_new_inputs(&[node])?
             }
-            common_telemetry::info!("[DEBUG] expanded node: {:?}", node);
             self.set_expanded();
 
             self.level -= 1;
@@ -428,6 +217,8 @@ impl TreeNodeRewriter for MagicRewriter {
 
 #[cfg(test)]
 mod test {
+    use std::sync::Arc;
+
     use datafusion::datasource::DefaultTableSource;
     use datafusion_expr::{avg, col, lit, Expr, LogicalPlanBuilder};
     use table::table::adapter::DfTableProviderAdapter;
