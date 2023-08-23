4dbc32f532ac3f43c44155187869880764f1cbd8
Bamboo1 <zyzhu2001@gmail.com>
2023-08-23 14:28:00 +0800

    refactor: remove associate type in scheduler to simplify it #2153 (#2194)
    * feature: add a simple scheduler using flume
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * fix: only use a sender rather clone many senders
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * fix: use select to avoid loop
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * feat: add parameters in new function to build the flume capacity and number of receivers
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * test: add countdownlatch test concurrency
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * test: add barrier replacing countdownlatch to test concurrency and add wait all tasks finished in stop
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: add some document annotation
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: add license header
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: code format
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: add Cargo.lock
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: Cargo.toml format
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: delete println in test
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: code format
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: code format
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * feat: add error handle
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * fix: fix error handle and add test scheduler stop
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: spelling mistake
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * fix: wait all tasks finished
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: add todo which need wrap Future returned by send_async
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: code format
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * test: remove unnessary sleep in test
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * fix: resolve some conflicts
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * fix: resolve conversation
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: code format
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * chore: code format
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    * feat: modify the function of schedule to synchronize and drop sender after stopping scheduler
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>
    
    ---------
    
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>

--- src/mito2/src/error.rs
+++ src/mito2/src/error.rs
@@ -18,6 +18,7 @@ use std::sync::Arc;
 use common_datasource::compression::CompressionType;
 use common_error::ext::{BoxedError, ErrorExt};
 use common_error::status_code::StatusCode;
+use common_runtime::JoinError;
 use datatypes::arrow::error::ArrowError;
 use datatypes::prelude::ConcreteDataType;
 use prost::{DecodeError, EncodeError};
@@ -376,6 +377,18 @@ pub enum Error {
         source: datatypes::error::Error,
         location: Location,
     },
+
+    #[snafu(display("Invalid flume sender, location: {}", location,))]
+    InvalidFlumeSender { location: Location },
+
+    #[snafu(display("Invalid scheduler state location: {}", location,))]
+    InvalidSchedulerState { location: Location },
+
+    #[snafu(display("Failed to stop scheduler, source: {}", source))]
+    StopScheduler {
+        source: JoinError,
+        location: Location,
+    },
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -435,6 +448,9 @@ impl ErrorExt for Error {
             PrimaryKeyLengthMismatch { .. } => StatusCode::InvalidArguments,
             SortValues { .. } => StatusCode::Unexpected,
             CompactValues { source, .. } => source.status_code(),
+            InvalidFlumeSender { .. } => StatusCode::InvalidArguments,
+            InvalidSchedulerState { .. } => StatusCode::InvalidArguments,
+            StopScheduler { .. } => StatusCode::Internal,
         }
     }
 
