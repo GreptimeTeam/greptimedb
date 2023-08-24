f2e7785a178a18bc91788357e15baf97a49ff4a6
Ruihang Xia <waynestxia@gmail.com>
2023-08-24 22:05:18 +0800

    add sqlness test
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests/cases/standalone/common/tql/binary_operator.sql
+++ tests/cases/standalone/common/tql/binary_operator.sql
@@ -9,3 +9,17 @@ tql eval (0, 30, '10s'), data + (1 < bool 2);
 tql eval (0, 30, '10s'), data + (1 > bool 2);
 
 drop table data;
+
+-- Binary operator on table with multiple field columns
+
+create table data (ts timestamp time index, val1 double, val2 double, val3 double);
+
+insert into data values (0, 1, 100, 10000), (10000, 2, 200, 20000), (20000, 3, 300, 30000);
+
+tql eval (0, 30, '10s'), data / data;
+
+tql eval (0, 30, '10s'), data{__field__="val1"} + data{__field__="val2"};
+
+tql eval (0, 30, '10s'), data{__field__="val1", __field__="val2"} + data{__field__="val2", __field__="val3"};
+
+drop table data;
