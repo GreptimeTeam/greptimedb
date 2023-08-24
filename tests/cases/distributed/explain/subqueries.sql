b633a1666711efdd932b5feac61e820c8bf20bab
Ruihang Xia <waynestxia@gmail.com>
2023-08-24 06:48:04 -0500

    feat: apply rewriter to subquery exprs (#2245)
    * apply rewriter to subquery exprs
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * workaround for datafusion's check
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * clean up
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * add sqlness test
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * fix typo
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * change time index type
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    ---------
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ tests/cases/distributed/explain/subqueries.sql
@@ -0,0 +1,35 @@
+CREATE TABLE integers(i INTEGER, j TIMESTAMP TIME INDEX);
+
+-- SQLNESS REPLACE (-+) -
+-- SQLNESS REPLACE (\s\s+) _
+-- SQLNESS REPLACE (RoundRobinBatch.*) REDACTED
+-- SQLNESS REPLACE (Hash.*) REDACTED
+-- SQLNESS REPLACE (peer-.*) REDACTED
+EXPLAIN SELECT * FROM integers WHERE i IN ((SELECT i FROM integers)) ORDER BY i;
+
+-- SQLNESS REPLACE (-+) -
+-- SQLNESS REPLACE (\s\s+) _
+-- SQLNESS REPLACE (RoundRobinBatch.*) REDACTED
+-- SQLNESS REPLACE (Hash.*) REDACTED
+-- SQLNESS REPLACE (peer-.*) REDACTED
+EXPLAIN SELECT * FROM integers i1 WHERE EXISTS(SELECT i FROM integers WHERE i=i1.i) ORDER BY i1.i;
+
+create table other (i INTEGER, j TIMESTAMP TIME INDEX);
+
+-- SQLNESS REPLACE (-+) -
+-- SQLNESS REPLACE (\s\s+) _
+-- SQLNESS REPLACE (RoundRobinBatch.*) REDACTED
+-- SQLNESS REPLACE (Hash.*) REDACTED
+-- SQLNESS REPLACE (peer-.*) REDACTED
+explain select t.i
+from (
+    select * from integers join other on 1=1
+) t
+where t.i is not null
+order by t.i desc;
+
+EXPLAIN INSERT INTO other SELECT i, 2 FROM integers WHERE i=(SELECT MAX(i) FROM integers);
+
+drop table other;
+
+drop table integers;
