5490819e20f3771c0563f7b65def900a33ddf4f1
Ruihang Xia <waynestxia@gmail.com>
2023-08-24 22:29:56 +0800

    sort result
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests/cases/standalone/common/tql/binary_operator.sql
+++ tests/cases/standalone/common/tql/binary_operator.sql
@@ -16,10 +16,13 @@ create table data (ts timestamp time index, val1 double, val2 double, val3 doubl
 
 insert into data values (0, 1, 100, 10000), (10000, 2, 200, 20000), (20000, 3, 300, 30000);
 
+-- SQLNESS SORT_RESULT 3 1
 tql eval (0, 30, '10s'), data / data;
 
+-- SQLNESS SORT_RESULT 3 1
 tql eval (0, 30, '10s'), data{__field__="val1"} + data{__field__="val2"};
 
+-- SQLNESS SORT_RESULT 3 1
 tql eval (0, 30, '10s'), data{__field__="val1", __field__="val2"} + data{__field__="val2", __field__="val3"};
 
 drop table data;
