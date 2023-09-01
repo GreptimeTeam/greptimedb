4818887e38376f52579204476301893774020286
Weny Xu <wenymedia@gmail.com>
2023-09-01 19:22:56 +0800

    refactor: refactor DistInstance (#2305)

--- src/common/meta/src/ddl_manager.rs
+++ src/common/meta/src/ddl_manager.rs
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,28 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(btree_extract_if)]
+use std::sync::Arc;
 
-pub mod cache_invalidator;
-pub mod datanode_manager;
-pub mod ddl_manager;
-pub mod error;
-pub mod heartbeat;
-// TODO(weny): Removes it
-#[allow(deprecated)]
-pub mod helper;
-pub mod ident;
-pub mod instruction;
-pub mod key;
-pub mod kv_backend;
-pub mod metrics;
-pub mod peer;
-pub mod range_stream;
-pub mod rpc;
-pub mod table_name;
-pub mod util;
+use crate::error::Result;
+use crate::rpc::ddl::{SubmitDdlTaskRequest, SubmitDdlTaskResponse};
 
-pub type ClusterId = u64;
-pub type DatanodeId = u64;
+pub struct Context;
 
-pub use instruction::RegionIdent;
+#[async_trait::async_trait]
+pub trait DdlExecutor: Send + Sync {
+    async fn submit_ddl_task(
+        &self,
+        ctx: &Context,
+        request: SubmitDdlTaskRequest,
+    ) -> Result<SubmitDdlTaskResponse>;
+}
+
+pub type DdlExecutorRef = Arc<dyn DdlExecutor>;
