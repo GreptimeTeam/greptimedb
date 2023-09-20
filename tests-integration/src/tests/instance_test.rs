5a9adcfda89dc9199827faac25c7256f6aec84e8
Ruihang Xia <waynestxia@gmail.com>
2023-09-20 11:34:00 +0800

    replace windows path chars
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests-integration/src/tests/instance_test.rs
+++ tests-integration/src/tests/instance_test.rs
@@ -575,7 +575,12 @@ async fn test_execute_external_create_infer_format(instance: Arc<dyn MockInstanc
     let instance = instance.frontend();
 
     let tmp_dir = temp_dir::create_temp_dir("test_execute_external_create_infer_format");
-    let location = tmp_dir.path().to_str().unwrap();
+    let location = tmp_dir
+        .path()
+        .to_str()
+        .unwrap()
+        .replace(':', "")
+        .replace('\\', "/");
 
     let output = execute_sql(
         &instance,
