1cb63dc02544f54899ef5f760a7119a70a7c2432
Ruihang Xia <waynestxia@gmail.com>
2023-09-26 20:43:24 +0800

    fix: test_child_error
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/common/error/src/ext.rs
+++ src/common/error/src/ext.rs
@@ -35,7 +35,10 @@ pub trait ErrorExt: StackError {
     /// downcast to a specific implementation.
     fn as_any(&self) -> &dyn Any;
 
-    fn output_msg(&self) -> String {
+    fn output_msg(&self) -> String
+    where
+        Self: Sized,
+    {
         let error = self.last();
         if let Some(external_error) = error.source() {
             format!("{error}: {external_error}")
@@ -43,20 +46,25 @@ pub trait ErrorExt: StackError {
             format!("{error}")
         }
     }
-
-    fn last(&self) -> &dyn StackError {
-        let mut result = *self.as_any().downcast_ref::<&dyn StackError>().unwrap();
-        while let Some(err) = self.next() {
-            result = err;
-        }
-        result
-    }
 }
 
 pub trait StackError: std::error::Error {
     fn debug_fmt(&self, layer: usize, buf: &mut Vec<String>);
 
     fn next(&self) -> Option<&dyn StackError>;
+
+    fn last(&self) -> &dyn StackError
+    where
+        Self: Sized,
+    {
+        let Some(mut result) = self.next() else {
+            return self;
+        };
+        while let Some(err) = result.next() {
+            result = err;
+        }
+        result
+    }
 }
 
 impl<T: ?Sized + StackError> StackError for Arc<T> {
