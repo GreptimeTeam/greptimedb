09361690dd2a6560e65fa63027b4fa8f10e3acff
Ruihang Xia <waynestxia@gmail.com>
2023-10-10 12:04:43 +0800

    tweak label name
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/promql/src/metrics.rs
+++ src/promql/src/metrics.rs
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub static PROMQL_SERIES_COUNT: &str = "promql.series.count";
+pub static PROMQL_SERIES_COUNT: &str = "promql.series_count";
