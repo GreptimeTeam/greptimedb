fcacffe0684625872f2813846193bd6b04f66d6f
Lei, HUANG <mrsatangel@gmail.com>
2023-10-24 10:21:14 +0800

    test: add more sqlness cases

--- tests/cases/standalone/common/select/prune.sql
+++ tests/cases/standalone/common/select/prune.sql
@@ -16,5 +16,8 @@ select * from demo where host='test2' and idc='idc2';
 
 select * from demo where host='test3' and idc>'idc1';
 
+select * from demo where idc='idc1';
+
+select * from demo where collector='disk';
 
 drop table demo;
