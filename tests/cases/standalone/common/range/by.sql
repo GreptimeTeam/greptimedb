bbaae9223ac2a4d2821d06fed1dc78e3e0ee1d02
WU Jingdi <taylor-lagrange@qq.com>
2023-11-06 11:45:33 +0800

    feat: enable empty range by (#2697)

--- tests/cases/standalone/common/range/by.sql
+++ tests/cases/standalone/common/range/by.sql
@@ -22,6 +22,15 @@ SELECT ts, length(host), max(val) RANGE '5s' FROM host ALIGN '20s' BY (length(ho
 
 SELECT ts, max(val) RANGE '5s' FROM host ALIGN '20s' BY (2) ORDER BY ts;
 
+-- The user explicitly specifies that the aggregation key is empty. In this case, there is no aggregation key. All data will be aggregated into a group.
+-- Implement by rewrite `BY()` to `BY(1)` automatically through sqlparser. They are semantically equivalent.
+SELECT ts, max(val) RANGE '5s' FROM host ALIGN '20s' BY () ORDER BY ts;
+
 SELECT ts, CAST(length(host) as INT64) + 2, max(val) RANGE '5s' FROM host ALIGN '20s' BY (CAST(length(host) as INT64) + 2) ORDER BY ts;
+
+-- Test error
+
+-- project non-aggregation key
+SELECT ts, host, max(val) RANGE '5s' FROM host ALIGN '20s' BY () ORDER BY ts;
 
 DROP TABLE host;
