a7f48627ed6d27fa5559efab9763c3d42bcb0124
tison <wander4096@gmail.com>
2023-11-09 15:14:09 +0800

    ci: use hawkeye to pin in inceptionYear
    Signed-off-by: tison <wander4096@gmail.com>

--- src/datatypes/src/timestamp.rs
+++ src/datatypes/src/timestamp.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use common_time::timestamp::TimeUnit;
 use common_time::Timestamp;
 use paste::paste;
--- src/sql/src/lib.rs
+++ src/sql/src/lib.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #![feature(box_patterns)]
 #![feature(assert_matches)]
 #![feature(let_chains)]
