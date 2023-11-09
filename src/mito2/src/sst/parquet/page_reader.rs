8a87f627adb55f446f664446d8c9d3718803776e
evenyag <realevenyag@gmail.com>
2023-11-09 12:24:09 +0800

    docs: update comments

--- src/mito2/src/sst/parquet/page_reader.rs
+++ src/mito2/src/sst/parquet/page_reader.rs
@@ -45,10 +45,10 @@ impl PageReader for CachedPageReader {
 
     fn skip_next_page(&mut self) -> Result<()> {
         // When the `SerializedPageReader` is in `SerializedPageReaderState::Pages` state, it never pops
-        // the dictionary page, which is always the first page. See:
+        // the dictionary page. So it always return the dictionary page as the first page. See:
         // https://github.com/apache/arrow-rs/blob/1d6feeacebb8d0d659d493b783ba381940973745/parquet/src/file/serialized_reader.rs#L766-L770
-        // But the `GenericColumnReader` will read the dictionary page before skipping records so it ensures the
-        // dictionary page is read first.
+        // But the `GenericColumnReader` will read the dictionary page before skipping records so it won't skip dictionary page.
+        // So we don't need to handle the dictionary page specifically in this method.
         // https://github.com/apache/arrow-rs/blob/65f7be856099d389b0d0eafa9be47fad25215ee6/parquet/src/column/reader.rs#L322-L331
         self.pages.pop_front();
         Ok(())
