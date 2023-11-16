982b602bc01d45675034d8b4f268b566276a183e
evenyag <realevenyag@gmail.com>
2023-11-16 17:06:12 +0800

    test: fix config api test

--- tests-integration/tests/http.rs
+++ tests-integration/tests/http.rs
@@ -743,7 +743,7 @@ global_write_buffer_size = "1GiB"
 global_write_buffer_reject_size = "2GiB"
 sst_meta_cache_size = "128MiB"
 vector_cache_size = "512MiB"
-page_cache_size = "2GiB"
+page_cache_size = "512MiB"
 sst_write_buffer_size = "8MiB"
 
 [[datanode.region_engine]]
