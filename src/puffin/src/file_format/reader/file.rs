f8c84f0ef2355a00eba8a4f30937a3422bbfdfd6
Zhenchi <zhongzc_arch@outlook.com>
2023-11-17 16:15:57 +0800

    Update src/puffin/src/file_format/reader/file.rs
    Co-authored-by: Yingwen <realevenyag@gmail.com>

--- src/puffin/src/file_format/reader/file.rs
+++ src/puffin/src/file_format/reader/file.rs
@@ -28,8 +28,10 @@ use crate::partial_reader::PartialReader;
 
 /// Puffin file reader, implemented [`PuffinSyncReader`] and [`PuffinAsyncReader`]
 ///
+/// ```text
 /// File layout: Magic Blob₁ Blob₂ ... Blobₙ Footer
 ///              [4]   [?]   [?]       [?]   [?]
+/// ```
 pub struct PuffinFileReader<R> {
     /// The source of the puffin file
     source: R,
