b38a7cad93282c07bd350393db428477726d6be7
Dennis Zhuang <killme2008@gmail.com>
2023-12-06 17:38:15 +0800

    fix: capacity

--- src/common/function/src/helper.rs
+++ src/common/function/src/helper.rs
@@ -17,7 +17,7 @@ use datatypes::prelude::ConcreteDataType;
 
 /// Create a function signature with oneof signatures of interleaving two arguments.
 pub fn one_of_sigs2(args1: Vec<ConcreteDataType>, args2: Vec<ConcreteDataType>) -> Signature {
-    let mut sigs = Vec::with_capacity(args1.len() + args2.len());
+    let mut sigs = Vec::with_capacity(args1.len() * args2.len());
 
     for arg1 in &args1 {
         for arg2 in &args2 {
