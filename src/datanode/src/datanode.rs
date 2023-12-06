c835ec47a90bdab070993a245a6432997629dbd2
NiwakaDev <te1823ya@gmail.com>
2023-12-07 00:48:46 +0900

    chore: revert

--- src/datanode/src/datanode.rs
+++ src/datanode/src/datanode.rs
@@ -64,7 +64,7 @@ use crate::event_listener::{
 };
 use crate::greptimedb_telemetry::get_greptimedb_telemetry_task;
 use crate::heartbeat::HeartbeatTask;
-use crate::region_server::RegionServer;
+use crate::region_server::{DummyTableProviderFactory, RegionServer};
 use crate::store;
 
 const OPEN_REGION_PARALLELISM: usize = 16;
@@ -415,8 +415,14 @@ impl DatanodeBuilder {
                 .context(RuntimeResourceSnafu)?,
         );
 
-        let mut region_server =
-            RegionServer::new(query_engine.clone(), runtime.clone(), event_listener);
+        let table_provider_factory = Arc::new(DummyTableProviderFactory);
+        let mut region_server = RegionServer::with_table_provider(
+            query_engine,
+            runtime,
+            event_listener,
+            table_provider_factory,
+        );
+
         let object_store_manager = Self::build_object_store_manager(opts).await?;
         let engines = Self::build_store_engines(opts, log_store, object_store_manager).await?;
         for engine in engines {
