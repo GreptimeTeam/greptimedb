83de399bef99361a68ab315f714ac16334c3d8e0
Zhenchi <zhongzc_arch@outlook.com>
2023-12-19 16:14:37 +0800

    feat(inverted_index.create): add external sorter (#2950)
    * feat(inverted_index.create): add read/write for external intermediate files
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * chore: MAGIC_CODEC_V1 -> CODEC_V1_MAGIC
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * chore: polish comments
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * chore: fix typos intermedia -> intermediate
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * fix: typos
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * feat(inverted_index.create): add external sorter
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * chore: fix typos intermedia -> intermediate
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * chore: polish comments
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * chore: polish comments
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * refactor: drop the stream as early as possible to avoid recursive calls to poll
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * refactor: project merge sorted stream
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * feat: add total_row_count to SortOutput
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * feat: remove change of format
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * refactor: rename segment null bitmap
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * refactor: test type alias
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * feat: allow `memory_usage_threshold` to be None to turn off dumping
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * feat: change segment_row_count type to NonZeroUsize
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * refactor: accept BytesRef instead
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * feat: add `push_n` to adapt mito2
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * chore: add k-way merge TODO
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * refactor: more sorter cases
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * refactor: make the merge tree balance
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * Update src/index/src/inverted_index/create/sort/external_sort.rs
    
    Co-authored-by: Yingwen <realevenyag@gmail.com>
    
    * chore: address comments
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * chore: stable feature
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    ---------
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    Co-authored-by: Yingwen <realevenyag@gmail.com>

--- src/index/src/inverted_index/create/sort.rs
+++ src/index/src/inverted_index/create/sort.rs
@@ -12,13 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod external_provider;
+mod external_sort;
+mod intermediate_rw;
+mod merge_stream;
+
+use async_trait::async_trait;
 use common_base::BitVec;
 use futures::Stream;
 
 use crate::inverted_index::error::Result;
-use crate::inverted_index::Bytes;
-
-mod intermediate_rw;
+use crate::inverted_index::{Bytes, BytesRef};
 
 /// A stream of sorted values along with their associated bitmap
 pub type SortedStream = Box<dyn Stream<Item = Result<(Bytes, BitVec)>> + Send + Unpin>;
+
+/// Output of a sorting operation, encapsulating a bitmap for null values and a stream of sorted items
+pub struct SortOutput {
+    /// Bitmap indicating which segments have null values
+    pub segment_null_bitmap: BitVec,
+
+    /// Stream of sorted items
+    pub sorted_stream: SortedStream,
+
+    /// Total number of rows in the sorted data
+    pub total_row_count: usize,
+}
+
+/// Handles data sorting, supporting incremental input and retrieval of sorted output
+#[async_trait]
+pub trait Sorter: Send {
+    /// Inputs a non-null or null value into the sorter.
+    /// Should be equivalent to calling `push_n` with n = 1
+    async fn push(&mut self, value: Option<BytesRef<'_>>) -> Result<()> {
+        self.push_n(value, 1).await
+    }
+
+    /// Pushing n identical non-null or null values into the sorter.
+    /// Should be equivalent to calling `push` n times
+    async fn push_n(&mut self, value: Option<BytesRef<'_>>, n: usize) -> Result<()>;
+
+    /// Completes the sorting process and returns the sorted data
+    async fn output(&mut self) -> Result<SortOutput>;
+}
