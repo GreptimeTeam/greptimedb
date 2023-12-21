594db453b8df992d8ae4f6d27cf3934b0977b162
niebayes <niebayes@gmail.com>
2023-12-21 11:23:45 +0800

    feat: introduce client manager

--- src/log-store/src/kafka.rs
+++ src/log-store/src/kafka.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod client_manager;
 pub mod log_store;
 
 use common_meta::wal::KafkaWalTopic as Topic;
