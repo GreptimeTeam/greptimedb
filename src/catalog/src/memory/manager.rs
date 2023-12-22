d7b2e791b9fb54d96cdc72c8077cc38c3b57cdca
dennis zhuang <killme2008@gmail.com>
2023-12-22 22:02:11 +0800

    fix: duplicate information_schema (#2979)
    * fix: duplicate information_schema
    
    * chore: style
    
    * fix: comment in sqlness

--- src/catalog/src/memory/manager.rs
+++ src/catalog/src/memory/manager.rs
@@ -143,6 +143,12 @@ impl MemoryCatalogManager {
                 schema: DEFAULT_PRIVATE_SCHEMA_NAME.to_string(),
             })
             .unwrap();
+        manager
+            .register_schema_sync(RegisterSchemaRequest {
+                catalog: DEFAULT_CATALOG_NAME.to_string(),
+                schema: INFORMATION_SCHEMA_NAME.to_string(),
+            })
+            .unwrap();
 
         manager
     }
@@ -256,6 +262,7 @@ impl MemoryCatalogManager {
             Arc::downgrade(self) as Weak<dyn CatalogManager>,
         );
         let information_schema = information_schema_provider.tables().clone();
+
         let mut catalog = HashMap::new();
         catalog.insert(INFORMATION_SCHEMA_NAME.to_string(), information_schema);
         catalog
