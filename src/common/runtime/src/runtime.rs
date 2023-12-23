de1f340e3b082d0bbc7a70f045a699260488c605
SSebo <ssebo.zzz@gmail.com>
2023-12-23 18:22:39 +0800

    feat: export runtime metric to promethues

--- src/common/runtime/src/runtime.rs
+++ src/common/runtime/src/runtime.rs
@@ -95,7 +95,7 @@ pub struct Builder {
 impl Default for Builder {
     fn default() -> Self {
         Self {
-            runtime_name: format!("runtime-{}", RUNTIME_ID.fetch_add(1, Ordering::Relaxed)),
+            runtime_name: format!("runtime_{}", RUNTIME_ID.fetch_add(1, Ordering::Relaxed)),
             thread_name: "default-worker".to_string(),
             builder: RuntimeBuilder::new_multi_thread(),
         }
@@ -152,6 +152,7 @@ impl Builder {
             .build()
             .context(BuildRuntimeSnafu)?;
 
+        let name = self.runtime_name.clone();
         let handle = runtime.handle().clone();
         let (send_stop, recv_stop) = oneshot::channel();
         // Block the runtime to shutdown.
@@ -159,14 +160,22 @@ impl Builder {
             .name(format!("{}-blocker", self.thread_name))
             .spawn(move || runtime.block_on(recv_stop));
 
+        register_collector(name.clone(), &handle);
+
         Ok(Runtime {
-            name: self.runtime_name.clone(),
+            name,
             handle,
             _dropper: Arc::new(Dropper {
                 close: Some(send_stop),
             }),
         })
     }
+}
+
+pub fn register_collector(name: String, handle: &Handle) {
+    let monitor = tokio_metrics::RuntimeMonitor::new(handle);
+    let collector = tokio_metrics_collector::RuntimeCollector::new(monitor, name.clone());
+    let _ = prometheus::register(Box::new(collector));
 }
 
 fn on_thread_start(thread_name: String) -> impl Fn() + 'static {
@@ -241,6 +250,9 @@ mod tests {
 
         assert!(metric_text.contains("runtime_threads_idle{thread_name=\"test_runtime_metric\"}"));
         assert!(metric_text.contains("runtime_threads_alive{thread_name=\"test_runtime_metric\"}"));
+        assert!(metric_text.contains("runtime_0_tokio_budget_forced_yield_count"));
+        assert!(metric_text.contains("runtime_0_tokio_injection_queue_depth"));
+        assert!(metric_text.contains("runtime_0_tokio_workers_count"));
     }
 
     #[test]
