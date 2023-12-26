329df4c05354625117bfaba7d9ec94ac9299523d
luofucong <luofc@foxmail.com>
2023-12-26 16:41:28 +0800

    test: data files compatibility test

--- src/common/test-util/src/ports.rs
+++ src/common/test-util/src/ports.rs
@@ -13,9 +13,14 @@
 // limitations under the License.
 
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
 
 use once_cell::sync::OnceCell;
 use rand::Rng;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpSocket;
+
+const PORT_CHECK_INTERVAL: Duration = Duration::from_millis(100);
 
 static PORTS: OnceCell<AtomicUsize> = OnceCell::new();
 
@@ -24,4 +29,24 @@ pub fn get_port() -> usize {
     PORTS
         .get_or_init(|| AtomicUsize::new(rand::thread_rng().gen_range(3000..3800)))
         .fetch_add(1, Ordering::Relaxed)
+}
+
+/// Spin awaits a socket address until it is connectable, or timeout.
+pub async fn check_connectable(ip_addr: &str, timeout: Duration) -> bool {
+    let ip_addr = ip_addr.parse().unwrap();
+
+    let check_task = async {
+        loop {
+            let socket = TcpSocket::new_v4().unwrap();
+            match socket.connect(ip_addr).await {
+                Ok(mut stream) => {
+                    let _ = stream.shutdown().await;
+                    break;
+                }
+                Err(_) => tokio::time::sleep(PORT_CHECK_INTERVAL).await,
+            }
+        }
+    };
+
+    tokio::time::timeout(timeout, check_task).await.is_ok()
 }
