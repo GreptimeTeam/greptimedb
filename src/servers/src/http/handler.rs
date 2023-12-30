f039c18cb70893b8b6a672a2ec01d5c473fa7c3d
tison <wander4096@gmail.com>
2023-12-30 23:48:36 +0800

    chore: fix typo
    Signed-off-by: tison <wander4096@gmail.com>

--- src/servers/src/http/handler.rs
+++ src/servers/src/http/handler.rs
@@ -35,7 +35,7 @@ use crate::query_handler::sql::ServerSqlQueryHandlerRef;
 pub struct SqlQuery {
     pub db: Option<String>,
     pub sql: Option<String>,
-    // (Optional) result format: [`gerptimedb_v1`, `influxdb_v1`],
+    // (Optional) result format: [`greptimedb_v1`, `influxdb_v1`],
     // the default value is `greptimedb_v1`
     pub format: Option<String>,
     // Returns epoch timestamps with the specified precision.
