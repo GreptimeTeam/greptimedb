be2a7b323b20d8c07e75e66a9991a3129502ec7a
WenyXu <wenymedia@gmail.com>
2023-12-31 07:27:03 +0000

    fix: fix test_object_store_cache_policy

--- src/object-store/tests/object_store_test.rs
+++ src/object-store/tests/object_store_test.rs
@@ -258,7 +258,11 @@ async fn assert_lru_cache<C: Accessor + Clone>(
     file_names: &[&str],
 ) {
     for file_name in file_names {
-        assert!(cache_layer.contains_file(file_name).await);
+        assert!(
+            cache_layer.contains_file(file_name).await,
+            "file: {} is not found",
+            file_name
+        );
     }
 }
 
@@ -303,10 +307,12 @@ async fn test_object_store_cache_policy() -> Result<()> {
 
     // create file cache layer
     let cache_dir = create_temp_dir("test_object_store_cache_policy_cache");
+    let tmp_dir = create_temp_dir("test_object_store_cache_policy_cache_tmp");
+
     let mut builder = Fs::default();
     let _ = builder
         .root(&cache_dir.path().to_string_lossy())
-        .atomic_write_dir(&cache_dir.path().to_string_lossy());
+        .atomic_write_dir(&tmp_dir.path().to_string_lossy());
     let file_cache = Arc::new(builder.build().unwrap());
     let cache_store = OperatorBuilder::new(file_cache.clone()).finish();
 
@@ -334,9 +340,9 @@ async fn test_object_store_cache_policy() -> Result<()> {
     assert_cache_files(
         &cache_store,
         &[
-            "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-",
-            "ecfe0dce85de452eb0a325158e7bfb75.cache-bytes=7-",
-            "ecfe0dce85de452eb0a325158e7bfb75.cache-bytes=0-",
+            "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-14",
+            "ecfe0dce85de452eb0a325158e7bfb75.cache-bytes=7-14",
+            "ecfe0dce85de452eb0a325158e7bfb75.cache-bytes=0-14",
         ],
         &["Hello, object1!", "object2!", "Hello, object2!"],
     )
@@ -344,9 +350,9 @@ async fn test_object_store_cache_policy() -> Result<()> {
     assert_lru_cache(
         &cache_layer,
         &[
-            "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-",
-            "ecfe0dce85de452eb0a325158e7bfb75.cache-bytes=7-",
-            "ecfe0dce85de452eb0a325158e7bfb75.cache-bytes=0-",
+            "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-14",
+            "ecfe0dce85de452eb0a325158e7bfb75.cache-bytes=7-14",
+            "ecfe0dce85de452eb0a325158e7bfb75.cache-bytes=0-14",
         ],
     )
     .await;
@@ -357,13 +363,13 @@ async fn test_object_store_cache_policy() -> Result<()> {
     assert_eq!(cache_layer.read_cache_stat().await, (1, 15));
     assert_cache_files(
         &cache_store,
-        &["6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-"],
+        &["6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-14"],
         &["Hello, object1!"],
     )
     .await?;
     assert_lru_cache(
         &cache_layer,
-        &["6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-"],
+        &["6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-14"],
     )
     .await;
 
@@ -376,13 +382,21 @@ async fn test_object_store_cache_policy() -> Result<()> {
     let _ = store.read(p3).await.unwrap();
     let _ = store.read_with(p3).range(0..5).await.unwrap();
 
-    // The entry count is 4, because we have the p2 `NotFound` cache.
-    assert_eq!(cache_layer.read_cache_stat().await, (4, 35));
+    // Read the deleted file without a deterministic range size requires an extra `stat.`
+    // Therefore, it won't go into the cache.
+    assert_eq!(cache_layer.read_cache_stat().await, (3, 35));
+
+    // However, The real open file happens after the reader is created.
+    // The reader will throw an error during the reading
+    // instead of returning `NotFound` during the reader creation.
+    assert!(store.read_with(p2).range(0..4).await.is_err());
+    assert_eq!(cache_layer.read_cache_stat().await, (3, 35));
+
     assert_cache_files(
         &cache_store,
         &[
-            "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-",
-            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-",
+            "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-14",
+            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-14",
             "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-4",
         ],
         &["Hello, object1!", "Hello, object3!", "Hello"],
@@ -391,8 +405,8 @@ async fn test_object_store_cache_policy() -> Result<()> {
     assert_lru_cache(
         &cache_layer,
         &[
-            "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-",
-            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-",
+            "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=0-14",
+            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-14",
             "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-4",
         ],
     )
@@ -404,12 +418,13 @@ async fn test_object_store_cache_policy() -> Result<()> {
     assert!(store.read(p2).await.is_err());
     // Read p1 with range `1..` , the existing p1 with range `0..` must be evicted.
     let _ = store.read_with(p1).range(1..15).await.unwrap();
-    assert_eq!(cache_layer.read_cache_stat().await, (4, 34));
+    // Again, The entry count should be 3.
+    assert_eq!(cache_layer.read_cache_stat().await, (3, 34));
     assert_cache_files(
         &cache_store,
         &[
             "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=1-14",
-            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-",
+            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-14",
             "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-4",
         ],
         &["ello, object1!", "Hello, object3!", "Hello"],
@@ -419,7 +434,7 @@ async fn test_object_store_cache_policy() -> Result<()> {
         &cache_layer,
         &[
             "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=1-14",
-            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-",
+            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-14",
             "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-4",
         ],
     )
@@ -440,7 +455,7 @@ async fn test_object_store_cache_policy() -> Result<()> {
         &cache_layer,
         &[
             "6d29752bdc6e4d5ba5483b96615d6c48.cache-bytes=1-14",
-            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-",
+            "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-14",
             "a8b1dc21e24bb55974e3e68acc77ed52.cache-bytes=0-4",
         ],
     )
