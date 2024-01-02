c4d7b0d91deeb06098ed54937dc3d39cba8f3769
dimbtp <mohell@qq.com>
2024-01-02 12:10:59 +0800

    feat: add some tables for `information_schema` (#3060)
    * feat: add information_schema.optimizer_trace
    
    * feat: add information_schema.parameters
    
    * feat: add information_schema.profiling
    
    * feat: add information_schema.referential_constraints
    
    * feat: add information_schema.routines
    
    * feat: add information_schema.schema_privileges
    
    * feat: add information_schema.table_privileges
    
    * feat: add information_schema.triggers
    
    * fix: update sql test result
    
    * feat: add information_schema.global_status
    
    * feat: add information_schema.session_status
    
    * fix: update sql test result
    
    * fix: add TODO for some tables
    
    * Update src/catalog/src/information_schema/memory_table/tables.rs
    
    Co-authored-by: Yingwen <realevenyag@gmail.com>
    
    ---------
    
    Co-authored-by: dennis zhuang <killme2008@gmail.com>
    Co-authored-by: Yingwen <realevenyag@gmail.com>

--- src/catalog/src/information_schema.rs
+++ src/catalog/src/information_schema.rs
@@ -61,6 +61,16 @@ lazy_static! {
         CHECK_CONSTRAINTS,
         EVENTS,
         FILES,
+        OPTIMIZER_TRACE,
+        PARAMETERS,
+        PROFILING,
+        REFERENTIAL_CONSTRAINTS,
+        ROUTINES,
+        SCHEMA_PRIVILEGES,
+        TABLE_PRIVILEGES,
+        TRIGGERS,
+        GLOBAL_STATUS,
+        SESSION_STATUS,
     ];
 }
 
@@ -179,6 +189,16 @@ impl InformationSchemaProvider {
             CHECK_CONSTRAINTS => setup_memory_table!(CHECK_CONSTRAINTS),
             EVENTS => setup_memory_table!(EVENTS),
             FILES => setup_memory_table!(FILES),
+            OPTIMIZER_TRACE => setup_memory_table!(OPTIMIZER_TRACE),
+            PARAMETERS => setup_memory_table!(PARAMETERS),
+            PROFILING => setup_memory_table!(PROFILING),
+            REFERENTIAL_CONSTRAINTS => setup_memory_table!(REFERENTIAL_CONSTRAINTS),
+            ROUTINES => setup_memory_table!(ROUTINES),
+            SCHEMA_PRIVILEGES => setup_memory_table!(SCHEMA_PRIVILEGES),
+            TABLE_PRIVILEGES => setup_memory_table!(TABLE_PRIVILEGES),
+            TRIGGERS => setup_memory_table!(TRIGGERS),
+            GLOBAL_STATUS => setup_memory_table!(GLOBAL_STATUS),
+            SESSION_STATUS => setup_memory_table!(SESSION_STATUS),
             KEY_COLUMN_USAGE => Some(Arc::new(InformationSchemaKeyColumnUsage::new(
                 self.catalog_name.clone(),
                 self.catalog_manager.clone(),
