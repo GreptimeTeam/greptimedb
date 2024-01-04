96b6235f259c638f27e97a36d009666d5f8e3f5c
Yingwen <realevenyag@gmail.com>
2024-01-04 18:53:43 +0800

    feat(mito): Add WriteCache struct and write SSTs to write cache (#2999)
    * docs: remove todo
    
    * feat: add upload cache
    
    * feat: add cache to sst write path
    
    * feat: add storage to part
    
    * feat: add dir to part
    
    * feat: revert storage name
    
    * feat: flush use upload part writer
    
    * feat: use upload part writer in compaction task
    
    * refactor: upload part writer builds parquet writer
    
    * chore: suppress warnings
    
    * refactor: rename UploadCache to WriteCache
    
    * refactor: move source to write_all()
    
    * chore: typos
    
    * chore: remove output mod
    
    * feat: changes upload to async method
    
    * docs: update cache
    
    * chore: fix compiler errors
    
    * docs: remove comment
    
    * chore: simplify upload part
    
    * refactor: remove option from cache manager param to access layer
    
    * feat: remove cache home from file cache
    
    * feat: write cache holds file cache
    
    * feat: add recover and pub some methods
    
    * feat: remove usages of UploadPartWriter
    
    * refactor: move sst_file_path to sst mod
    
    * refactor: use write cache in access layer
    
    * refactor: remove upload
    
    * style: fix clippy
    
    * refactor: pub write cache method/structs

--- src/mito2/src/sst/parquet/writer.rs
+++ src/mito2/src/sst/parquet/writer.rs
@@ -36,8 +36,6 @@ use crate::sst::parquet::{SstInfo, WriteOptions, PARQUET_METADATA_KEY};
 pub struct ParquetWriter {
     /// SST output file path.
     file_path: String,
-    /// Input data source.
-    source: Source,
     /// Region metadata of the source and the target SST.
     metadata: RegionMetadataRef,
     object_store: ObjectStore,
@@ -48,12 +46,10 @@ impl ParquetWriter {
     pub fn new(
         file_path: String,
         metadata: RegionMetadataRef,
-        source: Source,
         object_store: ObjectStore,
     ) -> ParquetWriter {
         ParquetWriter {
             file_path,
-            source,
             metadata,
             object_store,
         }
@@ -62,7 +58,11 @@ impl ParquetWriter {
     /// Iterates source and writes all rows to Parquet file.
     ///
     /// Returns the [SstInfo] if the SST is written.
-    pub async fn write_all(&mut self, opts: &WriteOptions) -> Result<Option<SstInfo>> {
+    pub async fn write_all(
+        &mut self,
+        mut source: Source,
+        opts: &WriteOptions,
+    ) -> Result<Option<SstInfo>> {
         let json = self.metadata.to_json().context(InvalidMetadataSnafu)?;
         let key_value_meta = KeyValue::new(PARQUET_METADATA_KEY.to_string(), json);
 
@@ -88,7 +88,7 @@ impl ParquetWriter {
         .context(WriteBufferSnafu)?;
 
         let mut stats = SourceStats::default();
-        while let Some(batch) = self.source.next_batch().await? {
+        while let Some(batch) = source.next_batch().await? {
             stats.update(&batch);
             let arrow_batch = write_format.convert_batch(&batch)?;
 
