fcb3c4c8602524a6386101bd828816ac419aa9a5
evenyag <realevenyag@gmail.com>
2024-01-05 14:46:11 +0800

    test: fix config test

--- tests-integration/tests/http.rs
+++ tests-integration/tests/http.rs
@@ -804,8 +804,8 @@ global_write_buffer_reject_size = "2GiB"
 sst_meta_cache_size = "128MiB"
 vector_cache_size = "512MiB"
 page_cache_size = "512MiB"
-write_cache_path = ""
-write_cache_size = "512MiB"
+experimental_write_cache_path = ""
+experimental_write_cache_size = "512MiB"
 sst_write_buffer_size = "8MiB"
 parallel_scan_channel_size = 32
 
