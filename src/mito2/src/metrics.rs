1679f0fcbdd3f4d70b15e0d2296a173f78e239c4
zyy17 <zyylsxm@gmail.com>
2024-01-05 15:48:35 +0800

    chore: add some prefix for new metrics

--- src/mito2/src/metrics.rs
+++ src/mito2/src/metrics.rs
@@ -156,13 +156,13 @@ lazy_static! {
     .unwrap();
     /// Gauge of index apply memory usage.
     pub static ref INDEX_APPLY_MEMORY_USAGE: IntGauge = register_int_gauge!(
-        "index_apply_memory_usage",
+        "greptime_index_apply_memory_usage",
         "index apply memory usage",
     )
     .unwrap();
     /// Counter of r/w bytes on index related IO operations.
     pub static ref INDEX_IO_BYTES_TOTAL: IntCounterVec = register_int_counter_vec!(
-        "index_io_bytes_total",
+        "greptime_index_io_bytes_total",
         "index io bytes total",
         &[TYPE_LABEL, FILE_TYPE_LABEL]
     )
@@ -173,7 +173,7 @@ lazy_static! {
 
     /// Counter of r/w operations on index related IO operations, e.g. read, write, seek and flush.
     pub static ref INDEX_IO_OP_TOTAL: IntCounterVec = register_int_counter_vec!(
-        "index_io_op_total",
+        "greptime_index_io_op_total",
         "index io op total",
         &[TYPE_LABEL, FILE_TYPE_LABEL]
     )
