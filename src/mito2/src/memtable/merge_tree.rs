ed2b9d3d58edac8ea0ebd160c43d076f40b7477d
evenyag <realevenyag@gmail.com>
2024-01-15 20:25:59 +0800

    chore: define merge tree memtable

---
+++ src/mito2/src/memtable/merge_tree.rs
@@ -0,0 +1,117 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Memtable implementation based on a merge tree:
+//! - Flushes mutable parts to immutable parts
+//! - Merges small immutable parts into a big immutable part
+
+use std::fmt;
+use std::sync::atomic::{AtomicI64, Ordering};
+
+use store_api::metadata::RegionMetadataRef;
+use store_api::storage::ColumnId;
+use table::predicate::Predicate;
+
+use crate::error::Result;
+use crate::flush::WriteBufferManagerRef;
+use crate::memtable::{
+    AllocTracker, BoxedBatchIterator, KeyValues, Memtable, MemtableId, MemtableStats,
+};
+
+/// Memtable based on a merge tree.
+pub struct MergeTreeMemtable {
+    id: MemtableId,
+    region_metadata: RegionMetadataRef,
+    alloc_tracker: AllocTracker,
+    max_timestamp: AtomicI64,
+    min_timestamp: AtomicI64,
+}
+
+impl MergeTreeMemtable {
+    /// Returns a new memtable.
+    pub fn new(
+        id: MemtableId,
+        region_metadata: RegionMetadataRef,
+        write_buffer_manager: Option<WriteBufferManagerRef>,
+    ) -> Self {
+        Self {
+            id,
+            region_metadata,
+            alloc_tracker: AllocTracker::new(write_buffer_manager),
+            max_timestamp: AtomicI64::new(i64::MIN),
+            min_timestamp: AtomicI64::new(i64::MAX),
+        }
+    }
+}
+
+impl fmt::Debug for MergeTreeMemtable {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("MergeTreeMemtable")
+            .field("id", &self.id)
+            .finish()
+    }
+}
+
+impl Memtable for MergeTreeMemtable {
+    fn id(&self) -> MemtableId {
+        todo!()
+    }
+
+    fn write(&self, _kvs: &KeyValues) -> Result<()> {
+        todo!()
+    }
+
+    fn iter(
+        &self,
+        _projection: Option<&[ColumnId]>,
+        _predicate: Option<Predicate>,
+    ) -> BoxedBatchIterator {
+        todo!()
+    }
+
+    fn is_empty(&self) -> bool {
+        todo!()
+    }
+
+    fn mark_immutable(&self) {
+        todo!()
+    }
+
+    fn stats(&self) -> MemtableStats {
+        let estimated_bytes = self.alloc_tracker.bytes_allocated();
+
+        if estimated_bytes == 0 {
+            // no rows ever written
+            return MemtableStats {
+                estimated_bytes,
+                time_range: None,
+            };
+        }
+
+        let ts_type = self
+            .region_metadata
+            .time_index_column()
+            .column_schema
+            .data_type
+            .clone()
+            .as_timestamp()
+            .expect("Timestamp column must have timestamp type");
+        let max_timestamp = ts_type.create_timestamp(self.max_timestamp.load(Ordering::Relaxed));
+        let min_timestamp = ts_type.create_timestamp(self.min_timestamp.load(Ordering::Relaxed));
+        MemtableStats {
+            estimated_bytes,
+            time_range: Some((min_timestamp, max_timestamp)),
+        }
+    }
+}
