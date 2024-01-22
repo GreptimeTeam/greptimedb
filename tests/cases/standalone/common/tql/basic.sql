7dfb5a63ac3d0a4229d8fa3675e82b5399f5b8c7
Ruihang Xia <waynestxia@gmail.com>
2024-01-22 16:16:50 +0800

    fix: remove __name__ matcher from processed matcher list
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests/cases/standalone/common/tql/basic.sql
+++ tests/cases/standalone/common/tql/basic.sql
@@ -7,6 +7,15 @@ INSERT INTO test VALUES (1, 1, "a"), (1, 1, "b"), (2, 2, "a");
 -- evaluate at 0s, 5s and 10s. No point at 0s.
 TQL EVAL (0, 10, '5s') test;
 
+-- SQLNESS SORT_RESULT 2 1
+TQL EVAL (0, 10, '5s') {__name__="test"};
+
+-- SQLNESS SORT_RESULT 2 1
+TQL EVAL (0, 10, '5s') {__name__="test", __field__="i"};
+
+-- NOT SUPPORTED: `__name__` matcher without equal condition
+TQL EVAL (0, 10, '5s') {__name__!="test"};
+
 -- the point at 1ms will be shadowed by the point at 2ms
 TQL EVAL (0, 10, '5s') test{k="a"};
 
