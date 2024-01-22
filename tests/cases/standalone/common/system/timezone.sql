0290c96de5534b6cdc47c79ade5afd2f51eecbbc
Dennis Zhuang <killme2008@gmail.com>
2024-01-22 15:13:48 +0800

    test: adds string with timezone

--- tests/cases/standalone/common/system/timezone.sql
+++ tests/cases/standalone/common/system/timezone.sql
@@ -9,7 +9,8 @@ INSERT INTO test values
        (1, '2024-01-01 00:00:00'),
        (2, '2024-01-02 08:00:00'),
        (3, '2024-01-03 16:00:00'),
-       (4, '2024-01-04 00:00:00');
+       (4, '2024-01-04 00:00:00'),
+       (5, '2024-01-05 00:00:00+08:00');
 
 SELECT * from test;
 
@@ -20,6 +21,8 @@ SELECT * from test where ts <= '2024-01-03 16:00:00';
 select date_format(ts, '%Y-%m-%d %H:%M:%S:%3f') from test;
 
 select to_unixtime('2024-01-02 00:00:00');
+
+select to_unixtime('2024-01-02T00:00:00+08:00');
 
 --- UTC+8 ---
 SET TIME_ZONE = '+8:00';
@@ -38,6 +41,8 @@ select date_format(ts, '%Y-%m-%d %H:%M:%S:%3f') from test;
 
 select to_unixtime('2024-01-02 00:00:00');
 
+select to_unixtime('2024-01-02 00:00:00+08:00');
+
 --- UTC-8 ---
 SET TIME_ZONE = '-8:00';
 
@@ -54,6 +59,8 @@ SELECT * from test where ts <= '2024-01-03 16:00:00';
 select date_format(ts, '%Y-%m-%d %H:%M:%S:%3f') from test;
 
 select to_unixtime('2024-01-02 00:00:00');
+
+select to_unixtime('2024-01-02 00:00:00+08:00');
 
 drop table test;
 
