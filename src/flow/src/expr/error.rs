0a9361a63c319394342a30ab4e7ae9bd5a51c098
discord9 <55937128+discord9@users.noreply.github.com>
2024-01-30 15:48:22 +0800

    feat: basic types for Dataflow Framework (#3186)
    * feat: basic types for Dataflow Framework
    
    * docs: license header
    
    * chore: add name in todo, remove deprecated code
    
    * chore: typo
    
    * test: linear&repr unit test
    
    * refactor: avoid mod.rs
    
    * feat: Relation Type
    
    * feat: unmat funcs
    
    * feat: simple temporal filter(sliding window)
    
    * refactor: impl Snafu for EvalError
    
    * feat: cast as type
    
    * feat: temporal filter
    
    * feat: time index in RelationType
    
    * refactor: move EvalError to expr
    
    * refactor: error handling for func
    
    * chore: fmt&comment
    
    * make EvalError pub again
    
    * refactor: move ScalarExpr to scalar.rs
    
    * refactor: remove mod.rs for relation
    
    * chore: slim down PR size
    
    * chore: license header
    
    * chore: per review
    
    * chore: more fix per review
    
    * chore: even more fix per review
    
    * chore: fmt
    
    * chore: fmt
    
    * feat: impl From/Into ProtoRow instead
    
    * chore: use cast not cast_with_opt&`Key` struct
    
    * chore: new_unchecked
    
    * feat: `Key::subset_of` method
    
    * chore: toml in order

---
+++ src/flow/src/expr/error.rs
@@ -0,0 +1,61 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::any::Any;
+
+use common_macro::stack_trace_debug;
+use common_telemetry::common_error::ext::ErrorExt;
+use common_telemetry::common_error::status_code::StatusCode;
+use datatypes::data_type::ConcreteDataType;
+use serde::{Deserialize, Serialize};
+use snafu::{Location, Snafu};
+
+/// EvalError is about errors happen on columnar evaluation
+///
+/// TODO(discord9): add detailed location of column/operator(instead of code) to errors tp help identify related column
+#[derive(Snafu)]
+#[snafu(visibility(pub))]
+#[stack_trace_debug]
+pub enum EvalError {
+    #[snafu(display("Division by zero"))]
+    DivisionByZero,
+
+    #[snafu(display("Type mismatch: expected {expected}, actual {actual}"))]
+    TypeMismatch {
+        expected: ConcreteDataType,
+        actual: ConcreteDataType,
+        location: Location,
+    },
+
+    /// can't nest datatypes error because EvalError need to be store in map and serialization
+    #[snafu(display("Fail to unpack from value to given type: {msg}"))]
+    TryFromValue { msg: String, location: Location },
+
+    #[snafu(display("Fail to cast value of type {from} to given type {to}"))]
+    CastValue {
+        from: ConcreteDataType,
+        to: ConcreteDataType,
+        source: datatypes::Error,
+        location: Location,
+    },
+
+    #[snafu(display("Invalid argument: {reason}"))]
+    InvalidArgument { reason: String, location: Location },
+
+    #[snafu(display("Internal error: {reason}"))]
+    Internal { reason: String, location: Location },
+
+    #[snafu(display("Optimize error: {reason}"))]
+    Optimize { reason: String, location: Location },
+}
