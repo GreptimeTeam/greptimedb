dd431c5ef72eaaccb3bc0595b5fb2a96897205f5
Dennis Zhuang <killme2008@gmail.com>
2024-02-02 17:48:07 +0800

    fix: license header

--- src/common/function/src/state.rs
+++ src/common/function/src/state.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use crate::handlers::{MetaServiceHandlerRef, TableMutationHandlerRef};
 
 /// Shared state for SQL functions.
