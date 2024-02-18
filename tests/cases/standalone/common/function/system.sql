dda0d76a32458b066a5a2b66746b30a8730555b0
tison <wander4096@gmail.com>
2024-02-18 19:24:24 +0800

    fixup! fix: commit_short test case
    Signed-off-by: tison <wander4096@gmail.com>

--- tests/cases/standalone/common/function/system.sql
+++ tests/cases/standalone/common/function/system.sql
@@ -1,6 +1,6 @@
 -- SQLNESS REPLACE branch:\s+.+ branch: BRANCH
 -- SQLNESS REPLACE commit:\s+.+ commit: COMMIT
--- SQLNESS REPLACE commit+\s+short:\s+.+ commit short: COMMIT SHORT
+-- SQLNESS REPLACE commit_short:\s+.+ commit_short: COMMIT_SHORT
 -- SQLNESS REPLACE dirty:\s+.+ dirty: DIRTY
 -- SQLNESS REPLACE version:\s+.+ version: VERSION
 -- SQLNESS REPLACE [\s\-]+
