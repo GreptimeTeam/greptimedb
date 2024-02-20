a875903f5149172971a2bcddd6937391e488ed3a
tison <wander4096@gmail.com>
2024-02-20 19:56:49 +0800

    refactor: put together HTTP headers
    Signed-off-by: tison <wander4096@gmail.com>

--- src/common/error/src/lib.rs
+++ src/common/error/src/lib.rs
@@ -18,8 +18,4 @@ pub mod ext;
 pub mod format;
 pub mod mock;
 pub mod status_code;
-
-pub const GREPTIME_DB_HEADER_ERROR_CODE: &str = "x-greptime-err-code";
-pub const GREPTIME_DB_HEADER_ERROR_MSG: &str = "x-greptime-err-msg";
-
 pub use snafu;
