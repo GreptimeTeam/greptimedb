9e5a10547b72a5d9f4719d0c1af5f09dd6a406aa
luofucong <luofc@foxmail.com>
2024-02-20 10:56:58 +0800

    Stop printing ANSI colors in file logging layer.

--- src/common/telemetry/src/logging.rs
+++ src/common/telemetry/src/logging.rs
@@ -144,7 +144,7 @@ pub fn init_global_logging(
     // JSON log layer.
     let rolling_appender = RollingFileAppender::new(Rotation::HOURLY, dir, app_name);
     let (rolling_writer, rolling_writer_guard) = tracing_appender::non_blocking(rolling_appender);
-    let file_logging_layer = Layer::new().with_writer(rolling_writer);
+    let file_logging_layer = Layer::new().with_writer(rolling_writer).with_ansi(false);
     guards.push(rolling_writer_guard);
 
     // error JSON log layer.
