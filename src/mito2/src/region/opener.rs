fc630aaa4f47aab43a0b3cce31decc8c80ac0929
niebayes <niebayes@gmail.com>
2024-03-01 12:22:46 +0800

    fix: fix memory spike during startup

--- src/mito2/src/region/opener.rs
+++ src/mito2/src/region/opener.rs
@@ -427,11 +427,11 @@ pub(crate) async fn replay_memtable<S: LogStore>(
                 .unwrap_or(0);
             region_write_ctx.push_mutation(mutation.op_type, mutation.rows, OptionOutputTx::none());
         }
-    }
 
-    // set next_entry_id and write to memtable.
-    region_write_ctx.set_next_entry_id(last_entry_id + 1);
-    region_write_ctx.write_memtable();
+        // set next_entry_id and write to memtable.
+        region_write_ctx.set_next_entry_id(last_entry_id + 1);
+        region_write_ctx.write_memtable();
+    }
 
     if allow_stale_entries && stale_entry_found {
         wal.obsolete(region_id, flushed_entry_id, wal_options)
