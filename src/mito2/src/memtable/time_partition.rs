ce61ef47e9e84dad517ebaac2c69239034062961
evenyag <realevenyag@gmail.com>
2024-03-12 22:03:52 +0800

    feat: implement non write methods

--- src/mito2/src/memtable/time_partition.rs
+++ src/mito2/src/memtable/time_partition.rs
@@ -18,7 +18,7 @@ use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 use common_time::Timestamp;
-use smallvec::SmallVec;
+use smallvec::{smallvec, SmallVec};
 use store_api::metadata::RegionMetadataRef;
 
 use crate::error::Result;
@@ -78,47 +78,89 @@ impl TimePartitions {
 
     /// Append memtables in partitions to `memtables`.
     pub fn list_memtables(&self, memtables: &mut Vec<MemtableRef>) {
-        unimplemented!()
+        let inner = self.inner.lock().unwrap();
+        memtables.extend(inner.parts.iter().map(|part| part.memtable.clone()));
     }
 
     /// Returns the number of partitions.
     pub fn num_partitions(&self) -> usize {
-        unimplemented!()
+        let inner = self.inner.lock().unwrap();
+        inner.parts.len()
     }
 
     /// Returns true if all memtables are empty.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        let inner = self.inner.lock().unwrap();
+        inner.parts.is_empty()
     }
 
     /// Freezes all memtables.
     pub fn freeze(&self) -> Result<()> {
-        unimplemented!()
+        let inner = self.inner.lock().unwrap();
+        for part in &*inner.parts {
+            part.memtable.freeze()?;
+        }
+        Ok(())
     }
 
     /// Forks latest partition.
     pub fn fork(&self, metadata: &RegionMetadataRef) -> Self {
-        unimplemented!()
+        let mut inner = self.inner.lock().unwrap();
+        let latest_part = inner
+            .parts
+            .iter()
+            .max_by_key(|part| part.time_range.map(|range| range.min_timestamp))
+            .cloned();
+
+        let Some(old_part) = latest_part else {
+            return Self::new(
+                metadata.clone(),
+                self.builder.clone(),
+                inner.next_memtable_id,
+                self.part_duration,
+            );
+        };
+        let memtable = old_part.memtable.fork(inner.alloc_memtable_id(), metadata);
+        let new_part = TimePartition {
+            memtable,
+            time_range: old_part.time_range,
+        };
+        Self {
+            inner: Mutex::new(PartitionsInner::with_partition(
+                new_part,
+                inner.next_memtable_id,
+            )),
+            part_duration: self.part_duration,
+            metadata: metadata.clone(),
+            builder: self.builder.clone(),
+        }
     }
 
     /// Returns partition duration.
     pub(crate) fn part_duration(&self) -> Option<Duration> {
-        unimplemented!()
+        self.part_duration
     }
 
     /// Returns memory usage.
     pub(crate) fn memory_usage(&self) -> usize {
-        unimplemented!()
+        let inner = self.inner.lock().unwrap();
+        inner
+            .parts
+            .iter()
+            .map(|part| part.memtable.stats().estimated_bytes)
+            .sum()
     }
 
     /// Append memtables in partitions to small vec.
     pub(crate) fn list_memtables_to_small_vec(&self, memtables: &mut SmallMemtableVec) {
-        unimplemented!()
+        let inner = self.inner.lock().unwrap();
+        memtables.extend(inner.parts.iter().map(|part| part.memtable.clone()));
     }
 
     /// Returns the next memtable id.
     pub(crate) fn next_memtable_id(&self) -> MemtableId {
-        unimplemented!()
+        let inner = self.inner.lock().unwrap();
+        inner.next_memtable_id
     }
 }
 
@@ -132,12 +174,28 @@ struct PartitionsInner {
 
 impl PartitionsInner {
     fn new(next_memtable_id: MemtableId) -> Self {
-        unimplemented!()
+        Self {
+            parts: Default::default(),
+            next_memtable_id,
+        }
+    }
+
+    fn with_partition(part: TimePartition, next_memtable_id: MemtableId) -> Self {
+        Self {
+            parts: smallvec![part],
+            next_memtable_id,
+        }
+    }
+
+    fn alloc_memtable_id(&mut self) -> MemtableId {
+        let id = self.next_memtable_id;
+        self.next_memtable_id += 1;
+        id
     }
 }
 
 /// Time range of a partition.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 struct PartTimeRange {
     /// Inclusive min timestamp of rows in the partition.
     min_timestamp: Timestamp,
