601ed9086dd00abf2773c8b47a65a666e22bd657
Discord9 <discord9@163.com>
2024-03-18 11:53:38 +0800

    feat: new type

--- src/flow/src/compute/types.rs
+++ src/flow/src/compute/types.rs
@@ -1,1 +1,6 @@
+use hydroflow::scheduled::handoff::TeeingHandoff;
+use hydroflow::scheduled::port::RecvPort;
+
+/// A collection, represent a collections of data that is received from a handoff.
+pub type Collection<T> = RecvPort<TeeingHandoff<T>>;
 
