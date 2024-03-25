467706bef1078da1eb345b9ef8e05dfac71de687
Ruihang Xia <waynestxia@gmail.com>
2024-03-25 19:58:27 +0800

    add sqlness case
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests/cases/standalone/common/alter/alter_table.sql
+++ tests/cases/standalone/common/alter/alter_table.sql
@@ -2,9 +2,17 @@ CREATE TABLE test_alt_table(i INTEGER, j TIMESTAMP TIME INDEX);
 
 DESC TABLE test_alt_table;
 
+INSERT INTO test_alt_table VALUES (1, 0), (2, 1);
+
 ALTER TABLE test_alt_table ADD COLUMN k INTEGER;
 
 DESC TABLE test_alt_table;
+
+SELECT * FROM test_alt_table;
+
+SELECT * FROM test_alt_table WHERE k IS NULL;
+
+SELECT * FROM test_alt_table WHERE i = 1;
 
 -- SQLNESS ARG restart=true
 ALTER TABLE test_alt_table ADD COLUMN m INTEGER;
