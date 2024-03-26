d1dbeb174d895d6dfe73d50af609d38c65134df9
Lei, HUANG <mrsatangel@gmail.com>
2024-03-26 09:59:34 +0000

    fix: sqlness

--- tests/cases/standalone/common/copy/copy_to_fs.sql
+++ tests/cases/standalone/common/copy/copy_to_fs.sql
@@ -1,14 +1,11 @@
---- Set time zone ---
-set time_zone='Asia/Shanghai';
-
 CREATE TABLE demo(host string, cpu DOUBLE, memory DOUBLE, ts TIMESTAMP TIME INDEX);
 
 insert into demo(host, cpu, memory, ts) values ('host1', 66.6, 1024, 1655276557000), ('host2', 88.8,  333.3, 1655276558000);
 
-COPY demo TO '/tmp/export/demo.parquet' WITH (start_time='2022-06-15 15:02:37', end_time='2022-06-15 15:02:38');
+COPY demo TO '/tmp/export/demo.parquet' WITH (start_time='2022-06-15 07:02:37', end_time='2022-06-15 07:02:38');
 
-COPY demo TO '/tmp/export/demo.csv' WITH (format='csv', start_time='2022-06-15 15:02:37', end_time='2022-06-15 15:02:38');
+COPY demo TO '/tmp/export/demo.csv' WITH (format='csv', start_time='2022-06-15 07:02:37', end_time='2022-06-15 07:02:38');
 
-COPY demo TO '/tmp/export/demo.json' WITH (format='json', start_time='2022-06-15 15:02:37', end_time='2022-06-15 15:02:38');
+COPY demo TO '/tmp/export/demo.json' WITH (format='json', start_time='2022-06-15 07:02:37', end_time='2022-06-15 07:02:38');
 
 drop table demo;
