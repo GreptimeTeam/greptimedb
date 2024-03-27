a35b8f5cbc7d5b32eb7ed34a732e2117097bfb5b
jeremyhi <jiachun_fjc@163.com>
2024-03-27 19:21:36 +0800

    chore: do not reply for broadcast msg

--- src/frontend/src/heartbeat/handler/tests.rs
+++ src/frontend/src/heartbeat/handler/tests.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::assert_matches::assert_matches;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
@@ -22,7 +21,7 @@ use common_meta::heartbeat::handler::{
     HandlerGroupExecutor, HeartbeatResponseHandlerContext, HeartbeatResponseHandlerExecutor,
 };
 use common_meta::heartbeat::mailbox::{HeartbeatMailbox, MessageMeta};
-use common_meta::instruction::{CacheIdent, Instruction, InstructionReply, SimpleReply};
+use common_meta::instruction::{CacheIdent, Instruction};
 use common_meta::key::table_info::TableInfoKey;
 use common_meta::key::TableMetaKey;
 use partition::manager::TableRouteCacheInvalidator;
@@ -67,7 +66,7 @@ async fn test_invalidate_table_cache_handler() {
         InvalidateTableCacheHandler::new(backend.clone()),
     )]));
 
-    let (tx, mut rx) = mpsc::channel(8);
+    let (tx, _) = mpsc::channel(8);
     let mailbox = Arc::new(HeartbeatMailbox::new(tx));
 
     // removes a valid key
@@ -78,11 +77,6 @@ async fn test_invalidate_table_cache_handler() {
     )
     .await;
 
-    let (_, reply) = rx.recv().await.unwrap();
-    assert_matches!(
-        reply,
-        InstructionReply::InvalidateTableCache(SimpleReply { result: true, .. })
-    );
     assert!(!backend
         .inner
         .lock()
@@ -96,12 +90,6 @@ async fn test_invalidate_table_cache_handler() {
         Instruction::InvalidateCaches(vec![CacheIdent::TableId(0)]),
     )
     .await;
-
-    let (_, reply) = rx.recv().await.unwrap();
-    assert_matches!(
-        reply,
-        InstructionReply::InvalidateTableCache(SimpleReply { result: true, .. })
-    );
 }
 
 pub fn test_message_meta(id: u64, subject: &str, to: &str, from: &str) -> MessageMeta {
