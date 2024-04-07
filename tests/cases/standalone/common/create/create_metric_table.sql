bb601fa3bf91ac8076c4b6ed60d47d538208a633
Ruihang Xia <waynestxia@gmail.com>
2024-04-07 17:11:52 +0900

    fix: construct correct pk list with pre-existing pk (#3614)
    * fix: construct correct pk list with pre-existing pk
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * update UT
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    ---------
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests/cases/standalone/common/create/create_metric_table.sql
+++ tests/cases/standalone/common/create/create_metric_table.sql
@@ -23,3 +23,31 @@ DROP TABLE t1;
 DROP TABLE t2;
 
 DROP TABLE phy;
+
+-- create one with other primary keys
+CREATE TABLE phy2 (ts timestamp time index, val double, abc string, def string, primary key (abc, def)) engine=metric with ("physical_metric_table" = "");
+
+DESC TABLE phy2;
+
+DROP TABLE phy2;
+
+-- fuzz test case https://github.com/GreptimeTeam/greptimedb/issues/3612
+CREATE TABLE `auT`(
+  incidunt TIMESTAMP(3) TIME INDEX,
+  `QuaErAT` BOOLEAN,
+  `REPREHenDERIt` BOOLEAN DEFAULT true,
+  `Et` INT NULL,
+  `AutEM` INT,
+  esse DOUBLE,
+  `Tempore` BOOLEAN,
+  `reruM` BOOLEAN,
+  `eRrOR` BOOLEAN NULL,
+  `cOMmodi` BOOLEAN,
+  `PERfERENdIS` DOUBLE,
+  `eSt` FLOAT DEFAULT 0.70978713,
+  PRIMARY KEY(`cOMmodi`, `PERfERENdIS`, esse)
+) ENGINE = metric with ("physical_metric_table" = "");
+
+DESC TABLE `auT`;
+
+DROP TABLE `auT`;
