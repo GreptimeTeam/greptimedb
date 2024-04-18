41265160ea664c6377308994a57e4124382f1497
discord9 <discord9@163.com>
2024-04-18 19:09:44 +0800

    feat: use strong type for metadata

--- src/flow/src/adapter.rs
+++ src/flow/src/adapter.rs
@@ -15,22 +15,22 @@
 //! for getting data from source and sending results to sink
 //! and communicating with other parts of the database
 
+// allow unused code for now since we are just defining the structure
+#![allow(unused)]
+
 pub(crate) mod error;
 use common_meta::error::Result as MetaResult;
 use common_meta::kv_backend::KvBackendRef;
 use serde::{Deserialize, Serialize};
+use table::metadata::TableId;
 
-pub struct FlowTaskMetadataManager {
-    /// table_name: Vec<String> to a list of task ids(u64)
-    source_table_to_tasks: KvBackendRef,
-    /// task_id: u64 -> flow node id: u64
-    task_to_node: KvBackendRef,
+pub struct FlowTaskManager {
+    /// table_id: TableId to a list of task ids(u64)
+    mirror_write_manager: TaskRouteManager,
+    /// task_id: u64 -> TaskMetadata
+    task_info: TaskMetadata,
 }
-
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct TaskId(pub usize);
-
-impl FlowTaskMetadataManager {
+impl FlowTaskManager {
     pub async fn create_flow_task(&self, sql: &str) -> MetaResult<TaskId> {
         // TODO: send sql to flow node, which will try to parse it into a flow task
         // Then it will return task metadata including source table used
@@ -40,3 +40,56 @@ impl FlowTaskMetadataManager {
         Ok(())
     }
 }
+
+/// Manage the routing of write request from a table to a list of tasks
+pub struct TaskRouteManager {
+    /// table_id: TableId to a list of task ids(u64)
+    source_table_to_tasks: KvBackendRef,
+}
+
+impl TaskRouteManager {
+    /// Get all task ids that are reading from this table
+    pub async fn get(&self, table_id: TableId) -> MetaResult<Vec<TaskId>> {
+        // get all task ids that are writing to this table
+        todo!()
+    }
+
+    /// Set a task to read from this table
+    pub async fn set(&self, table_id: TableId, task_id: TaskId) -> MetaResult<()> {
+        // set a task id to write to this table
+        todo!()
+    }
+}
+
+pub struct TaskMetadataManager {
+    task_id_to_metadata: KvBackendRef,
+}
+
+impl TaskMetadataManager {
+    /// Get task metadata by task id
+    pub async fn get(&self, task_id: TaskId) -> MetaResult<TaskMetadata> {
+        // get task metadata by task id
+        todo!()
+    }
+
+    /// Set task metadata by task id
+    pub async fn set(&self, task_id: TaskId, metadata: TaskMetadata) -> MetaResult<TaskMetadata> {
+        // set task metadata by task id
+        todo!()
+    }
+}
+
+pub struct TaskMetadata {
+    /// The source tables used by this task
+    pub source_tables: Vec<TableId>,
+    /// The sink tables used by this task
+    pub sink_tables: Vec<TableId>,
+    /// Which flow node this task is running on
+    pub flow_node: FlowNodeId,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct TaskId(pub usize);
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct FlowNodeId(pub usize);
