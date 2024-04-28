a6302fc26a58a807fb5cc6cf9c5f95b9bcb41d03
evenyag <realevenyag@gmail.com>
2024-04-28 15:21:09 +0800

    refactor: move precise_filter to PartitionContext

--- src/mito2/src/sst/parquet/partition.rs
+++ src/mito2/src/sst/parquet/partition.rs
@@ -15,12 +15,19 @@
 //! Structs and functions for reading partitions from a parquet file. A partition
 //! is usually a row group in a parquet file.
 
+use std::ops::BitAnd;
 use std::sync::Arc;
 
+use api::v1::SemanticType;
 use common_recordbatch::filter::SimpleFilterEvaluator;
+use datatypes::arrow::array::BooleanArray;
+use datatypes::arrow::buffer::BooleanBuffer;
 use parquet::arrow::arrow_reader::{ParquetRecordBatchReader, RowSelection};
+use snafu::ResultExt;
 
-use crate::error::Result;
+use crate::error::{FieldTypeMismatchSnafu, FilterRecordBatchSnafu, Result};
+use crate::read::Batch;
+use crate::row_converter::{McmpRowCodec, RowCodec};
 use crate::sst::parquet::format::ReadFormat;
 use crate::sst::parquet::reader::RowGroupReaderBuilder;
 
@@ -64,7 +71,9 @@ pub(crate) struct PartitionContext {
     /// Filters pushed down.
     filters: Vec<SimpleFilterEvaluator>,
     /// Helper to read the SST.
-    format: ReadFormat,
+    read_format: ReadFormat,
+    /// Decoder for primary keys
+    codec: McmpRowCodec,
 }
 
 pub(crate) type PartitionContextRef = Arc<PartitionContext>;
@@ -74,12 +83,101 @@ impl PartitionContext {
     pub(crate) fn new(
         reader_builder: RowGroupReaderBuilder,
         filters: Vec<SimpleFilterEvaluator>,
-        format: ReadFormat,
+        read_format: ReadFormat,
+        codec: McmpRowCodec,
     ) -> Self {
         Self {
             reader_builder,
             filters,
-            format,
+            read_format,
+            codec,
         }
+    }
+
+    /// Returns the path of the file to read.
+    pub(crate) fn file_path(&self) -> &str {
+        self.reader_builder.file_path()
+    }
+
+    /// Returns filters pushed down.
+    pub(crate) fn filters(&self) -> &[SimpleFilterEvaluator] {
+        &self.filters
+    }
+
+    /// Returns the format helper.
+    pub(crate) fn read_format(&self) -> &ReadFormat {
+        &self.read_format
+    }
+
+    /// TRY THE BEST to perform pushed down predicate precisely on the input batch.
+    /// Return the filtered batch. If the entire batch is filtered out, return None.
+    ///
+    /// Supported filter expr type is defined in [SimpleFilterEvaluator].
+    ///
+    /// When a filter is referencing primary key column, this method will decode
+    /// the primary key and put it into the batch.
+    pub(crate) fn precise_filter(&self, mut input: Batch) -> Result<Option<Batch>> {
+        let mut mask = BooleanBuffer::new_set(input.num_rows());
+
+        // Run filter one by one and combine them result
+        // TODO(ruihang): run primary key filter first. It may short circuit other filters
+        for filter in &self.filters {
+            let column_name = filter.column_name();
+            let Some(column_metadata) = self.read_format.metadata().column_by_name(column_name)
+            else {
+                // column not found, skip
+                // in situation like an column is added later
+                continue;
+            };
+            let result = match column_metadata.semantic_type {
+                SemanticType::Tag => {
+                    let pk_values = if let Some(pk_values) = input.pk_values() {
+                        pk_values
+                    } else {
+                        input.set_pk_values(self.codec.decode(input.primary_key())?);
+                        input.pk_values().unwrap()
+                    };
+                    // Safety: this is a primary key
+                    let pk_index = self
+                        .read_format
+                        .metadata()
+                        .primary_key_index(column_metadata.column_id)
+                        .unwrap();
+                    let pk_value = pk_values[pk_index]
+                        .try_to_scalar_value(&column_metadata.column_schema.data_type)
+                        .context(FieldTypeMismatchSnafu)?;
+                    if filter
+                        .evaluate_scalar(&pk_value)
+                        .context(FilterRecordBatchSnafu)?
+                    {
+                        continue;
+                    } else {
+                        // PK not match means the entire batch is filtered out.
+                        return Ok(None);
+                    }
+                }
+                SemanticType::Field => {
+                    let Some(field_index) = self
+                        .read_format
+                        .field_index_by_id(column_metadata.column_id)
+                    else {
+                        continue;
+                    };
+                    let field_col = &input.fields()[field_index].data;
+                    filter
+                        .evaluate_vector(field_col)
+                        .context(FilterRecordBatchSnafu)?
+                }
+                SemanticType::Timestamp => filter
+                    .evaluate_vector(input.timestamps())
+                    .context(FilterRecordBatchSnafu)?,
+            };
+
+            mask = mask.bitand(&result);
+        }
+
+        input.filter(&BooleanArray::from(mask).into())?;
+
+        Ok(Some(input))
     }
 }
