e7b56090acc4b2b7f9431b60d8e085f70400e952
Dennis Zhuang <killme2008@gmail.com>
2024-04-29 21:52:32 +0800

    chore: simplify sqlness commands

--- tests/cases/distributed/information_schema/cluster_info.sql
+++ tests/cases/distributed/information_schema/cluster_info.sql
@@ -6,9 +6,8 @@ DESC TABLE CLUSTER_INFO;
 -- SQLNESS REPLACE unknown UNKNOWN
 -- SQLNESS REPLACE (\s\d\.\d\.\d\s) Version
 -- SQLNESS REPLACE (\s[a-z0-9]{7}\s) Hash
--- SQLNESS REPLACE (\s[\-0-9T:\.]{23}\s) Start_time
--- SQLNESS REPLACE (\s+uptime\s+) uptime
--- SQLNESS REPLACE (\s+(\d+(s|ms|m)\s\s*)+) Uptime
+-- SQLNESS REPLACE (\s[\-0-9T:\.]{23}) Start_time
+-- SQLNESS REPLACE ((\d+(s|ms|m)\s)+) Uptime
 -- SQLNESS REPLACE [\s\-]+
 SELECT * FROM CLUSTER_INFO ORDER BY peer_type;
 
@@ -16,9 +15,8 @@ SELECT * FROM CLUSTER_INFO ORDER BY peer_type;
 -- SQLNESS REPLACE unknown UNKNOWN
 -- SQLNESS REPLACE (\s\d\.\d\.\d\s) Version
 -- SQLNESS REPLACE (\s[a-z0-9]{7}\s) Hash
--- SQLNESS REPLACE (\s[\-0-9T:\.]{23}\s) Start_time
--- SQLNESS REPLACE (\s+uptime\s+) uptime
--- SQLNESS REPLACE (\s+(\d+(s|ms|m)\s)+\s*) Uptime
+-- SQLNESS REPLACE (\s[\-0-9T:\.]{23}) Start_time
+-- SQLNESS REPLACE ((\d+(s|ms|m)\s)+) Uptime
 -- SQLNESS REPLACE [\s\-]+
 SELECT * FROM CLUSTER_INFO WHERE PEER_TYPE = 'METASRV' ORDER BY peer_type;
 
@@ -26,9 +24,8 @@ SELECT * FROM CLUSTER_INFO WHERE PEER_TYPE = 'METASRV' ORDER BY peer_type;
 -- SQLNESS REPLACE unknown UNKNOWN
 -- SQLNESS REPLACE (\s\d\.\d\.\d\s) Version
 -- SQLNESS REPLACE (\s[a-z0-9]{7}\s) Hash
--- SQLNESS REPLACE (\s[\-0-9T:\.]{23}\s) Start_time
--- SQLNESS REPLACE (\s+uptime\s+) uptime
--- SQLNESS REPLACE (\s+(\d+(s|ms|m)\s)+\s*) Uptime
+-- SQLNESS REPLACE (\s[\-0-9T:\.]{23}) Start_time
+-- SQLNESS REPLACE ((\d+(s|ms|m)\s)+) Uptime
 -- SQLNESS REPLACE [\s\-]+
 SELECT * FROM CLUSTER_INFO WHERE PEER_TYPE = 'FRONTEND' ORDER BY peer_type;
 
@@ -36,9 +33,8 @@ SELECT * FROM CLUSTER_INFO WHERE PEER_TYPE = 'FRONTEND' ORDER BY peer_type;
 -- SQLNESS REPLACE unknown UNKNOWN
 -- SQLNESS REPLACE (\s\d\.\d\.\d\s) Version
 -- SQLNESS REPLACE (\s[a-z0-9]{7}\s) Hash
--- SQLNESS REPLACE (\s[\-0-9T:\.]{23}\s) Start_time
--- SQLNESS REPLACE (\s+uptime\s+) uptime
--- SQLNESS REPLACE (\s+(\d+(s|ms|m)\s)+\s*) Uptime
+-- SQLNESS REPLACE (\s[\-0-9T:\.]{23}) Start_time
+-- SQLNESS REPLACE ((\d+(s|ms|m)\s)+) Uptime
 -- SQLNESS REPLACE [\s\-]+
 SELECT * FROM CLUSTER_INFO WHERE PEER_TYPE != 'FRONTEND' ORDER BY peer_type;
 
@@ -46,9 +42,8 @@ SELECT * FROM CLUSTER_INFO WHERE PEER_TYPE != 'FRONTEND' ORDER BY peer_type;
 -- SQLNESS REPLACE unknown UNKNOWN
 -- SQLNESS REPLACE (\s\d\.\d\.\d\s) Version
 -- SQLNESS REPLACE (\s[a-z0-9]{7}\s) Hash
--- SQLNESS REPLACE (\s[\-0-9T:\.]{23}\s) Start_time
--- SQLNESS REPLACE (\s+uptime\s+) uptime
--- SQLNESS REPLACE (\s+(\d+(s|ms|m)\s)+\s*) Uptime
+-- SQLNESS REPLACE (\s[\-0-9T:\.]{23}) Start_time
+-- SQLNESS REPLACE ((\d+(s|ms|m)\s)+) Uptime
 -- SQLNESS REPLACE [\s\-]+
 SELECT * FROM CLUSTER_INFO WHERE PEER_ID > 1 ORDER BY peer_type;
 
