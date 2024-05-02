8ee5fa817eeccaca7cb5f9c3b1ae576b008889a7
irenjj <renj.jiang@gmail.com>
2024-05-02 11:59:05 +0800

    fix test

--- tests/cases/standalone/common/copy/copy_from_fs_parquet.sql
+++ tests/cases/standalone/common/copy/copy_from_fs_parquet.sql
@@ -4,6 +4,12 @@ insert into demo(host, cpu, memory, ts) values ('host1', 66.6, 1024, 16552765570
 
 Copy demo TO '/tmp/demo/export/parquet/demo.parquet';
 
+CREATE TABLE demo2(host string, cpu double, memory double, ts TIMESTAMP time index);
+
+insert into demo2(host, cpu, memory, ts) values ('host3', 77.7, 1111, 1655276555000), ('host4', 99.9,  444.4, 1655276556000);
+
+Copy demo2 TO '/tmp/demo/export/parquet/demo2.parquet';
+
 CREATE TABLE with_filename(host string, cpu double, memory double, ts timestamp time index);
 
 Copy with_filename FROM '/tmp/demo/export/parquet/demo.parquet';
@@ -22,11 +28,17 @@ Copy with_pattern FROM '/tmp/demo/export/parquet/' WITH (PATTERN = 'demo.*');
 
 select * from with_pattern order by ts;
 
+CREATE TABLE without_limit_rows(host string, cpu double, memory double, ts timestamp time index);
+
+Copy without_limit_rows FROM '/tmp/demo/export/parquet/';
+
+select * from without_limit_rows;
+
 CREATE TABLE with_limit_rows(host string, cpu double, memory double, ts timestamp time index);
 
-Copy with_limit_rows FROM '/tmp/demo/export/parquet/' WITH (MAX_INSERT_ROWS = 10);
+Copy with_limit_rows FROM '/tmp/demo/export/parquet/' WITH (MAX_INSERT_ROWS = 2);
 
-select * from with_limit_rows order by ts;
+select * from with_limit_rows;
 
 drop table demo;
 
