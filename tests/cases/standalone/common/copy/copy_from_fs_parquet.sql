b39b481487ee168edc4e0ce7e44766a85cf41a23
irenjj <renj.jiang@gmail.com>
2024-05-02 15:46:32 +0800

    fix test

--- tests/cases/standalone/common/copy/copy_from_fs_parquet.sql
+++ tests/cases/standalone/common/copy/copy_from_fs_parquet.sql
@@ -2,47 +2,47 @@ CREATE TABLE demo(host string, cpu double, memory double, ts TIMESTAMP time inde
 
 insert into demo(host, cpu, memory, ts) values ('host1', 66.6, 1024, 1655276557000), ('host2', 88.8,  333.3, 1655276558000);
 
-Copy demo TO '/tmp/demo/export/parquet/demo.parquet';
+Copy demo TO '/tmp/demo/export/parquet_files/demo.parquet';
 
-CREATE TABLE demo2(host string, cpu double, memory double, ts TIMESTAMP time index);
+CREATE TABLE demo_2(host string, cpu double, memory double, ts TIMESTAMP time index);
 
-insert into demo2(host, cpu, memory, ts) values ('host3', 77.7, 1111, 1655276555000), ('host4', 99.9,  444.4, 1655276556000);
+insert into demo_2(host, cpu, memory, ts) values ('host3', 77.7, 1111, 1655276555000), ('host4', 99.9,  444.4, 1655276556000);
 
-Copy demo2 TO '/tmp/demo/export/parquet/demo2.parquet';
+Copy demo_2 TO '/tmp/demo/export/parquet_files/demo_2.parquet';
 
 CREATE TABLE with_filename(host string, cpu double, memory double, ts timestamp time index);
 
-Copy with_filename FROM '/tmp/demo/export/parquet/demo.parquet';
+Copy with_filename FROM '/tmp/demo/export/parquet_files/demo.parquet';
 
 select * from with_filename order by ts;
 
 CREATE TABLE with_path(host string, cpu double, memory double, ts timestamp time index);
 
-Copy with_path FROM '/tmp/demo/export/parquet/';
+Copy with_path FROM '/tmp/demo/export/parquet_files/';
 
 select * from with_path order by ts;
 
 CREATE TABLE with_pattern(host string, cpu double, memory double, ts timestamp time index);
 
-Copy with_pattern FROM '/tmp/demo/export/parquet/' WITH (PATTERN = 'demo.*');
+Copy with_pattern FROM '/tmp/demo/export/parquet_files/' WITH (PATTERN = 'demo.*');
 
 select * from with_pattern order by ts;
 
 CREATE TABLE without_limit_rows(host string, cpu double, memory double, ts timestamp time index);
 
-Copy without_limit_rows FROM '/tmp/demo/export/parquet/';
+Copy without_limit_rows FROM '/tmp/demo/export/parquet_files/';
 
 select * from without_limit_rows;
 
 CREATE TABLE with_limit_rows(host string, cpu double, memory double, ts timestamp time index);
 
-Copy with_limit_rows FROM '/tmp/demo/export/parquet/' WITH (MAX_INSERT_ROWS = 2);
+Copy with_limit_rows FROM '/tmp/demo/export/parquet_files/' WITH (MAX_INSERT_ROWS = 2);
 
 select * from with_limit_rows;
 
 drop table demo;
 
-drop table demo2;
+drop table demo_2;
 
 drop table with_filename;
 
