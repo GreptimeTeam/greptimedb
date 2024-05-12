62d2c55581bd60913c20d032712922fa8dea284d
Dennis Zhuang <killme2008@gmail.com>
2024-05-12 22:54:11 +0800

    test: add create view test

---
+++ tests/cases/standalone/common/view/create.sql
@@ -0,0 +1,32 @@
+--- test CREATE VIEW ---
+
+CREATE DATABASE for_test_view;
+
+USE for_test_view;
+
+CREATE VIEW test_view;
+
+CREATE VIEW test_view as DELETE FROM public.numbers;
+
+CREATE VIEW test_view as SELECT * FROM public.numbers;
+
+SHOW TABLES;
+
+SHOW FULL TABLES;
+
+SELECT * FROM INFORMATION_SCHEMA.TABLES ORDER BY TABLE_NAME, TABLE_TYPE;
+
+SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'VIEW';
+
+SHOW COLUMNS FROM test_view;
+
+SHOW FULL COLUMNS FROM test_view;
+
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'test_view';
+
+--- FIXED in the following PR ---
+SELECT * FROM test_view;
+
+USE public;
+
+DROP DATABASE for_test_view;
