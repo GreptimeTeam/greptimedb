52aafde976fb7408deac3b9ade64d8b169c68734
Dennis Zhuang <killme2008@gmail.com>
2024-05-13 18:29:29 +0800

    test: adds more sqlness test for creating views

--- tests/cases/standalone/common/view/create.sql
+++ tests/cases/standalone/common/view/create.sql
@@ -4,18 +4,38 @@ CREATE DATABASE for_test_view;
 
 USE for_test_view;
 
+CREATE TABLE test_table(a STRING, ts TIMESTAMP TIME INDEX);
+
 CREATE VIEW test_view;
 
 CREATE VIEW test_view as DELETE FROM public.numbers;
 
+--- Table already exists ---
+CREATE VIEW test_table as SELECT * FROM public.numbers;
+
+--- Table already exists even when create_if_not_exists ---
+CREATE VIEW IF NOT EXISTS test_table as SELECT * FROM public.numbers;
+
+--- Table already exists even when or_replace ---
+CREATE OR REPLACE VIEW test_table as SELECT * FROM public.numbers;
+
 CREATE VIEW test_view as SELECT * FROM public.numbers;
+
+--- View already exists ----
+CREATE VIEW test_view as SELECT * FROM public.numbers;
+
+CREATE VIEW IF NOT EXISTS test_view as SELECT * FROM public.numbers;
+
+CREATE OR REPLACE VIEW test_view as SELECT * FROM public.numbers;
 
 SHOW TABLES;
 
 SHOW FULL TABLES;
 
+-- SQLNESS REPLACE (\s\d+\s) ID
 SELECT * FROM INFORMATION_SCHEMA.TABLES ORDER BY TABLE_NAME, TABLE_TYPE;
 
+-- SQLNESS REPLACE (\s\d+\s) ID
 SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'VIEW';
 
 SHOW COLUMNS FROM test_view;
