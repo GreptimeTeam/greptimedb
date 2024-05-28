9dd6e033a72b42d9f2da7bb7dbd479aec755c432
tison <wander4096@gmail.com>
2024-05-28 11:21:43 +0800

    refactor: move Database to client crate behind testing feature (#4059)
    * refactor: move Database to client crate behind testing feature
    
    Signed-off-by: tison <wander4096@gmail.com>
    
    * partial move
    
    Signed-off-by: tison <wander4096@gmail.com>
    
    * catch up more
    
    Signed-off-by: tison <wander4096@gmail.com>
    
    * fix imports
    
    Signed-off-by: tison <wander4096@gmail.com>
    
    * finish
    
    Signed-off-by: tison <wander4096@gmail.com>
    
    * tidy
    
    Signed-off-by: tison <wander4096@gmail.com>
    
    ---------
    
    Signed-off-by: tison <wander4096@gmail.com>

--- src/cmd/src/cli.rs
+++ src/cmd/src/cli.rs
@@ -22,8 +22,8 @@ mod helper;
 
 // Wait for https://github.com/GreptimeTeam/greptimedb/issues/2373
 #[allow(unused)]
-// mod repl;
-// TODO(weny): Removes it
+mod repl;
+// TODO(tisonkun): migrate deprecated methods
 #[allow(deprecated)]
 mod upgrade;
 
@@ -31,8 +31,8 @@ use async_trait::async_trait;
 use bench::BenchTableMetadataCommand;
 use clap::Parser;
 use common_telemetry::logging::{LoggingOptions, TracingOptions};
+pub use repl::Repl;
 use tracing_appender::non_blocking::WorkerGuard;
-// pub use repl::Repl;
 use upgrade::UpgradeCommand;
 
 use self::export::ExportCommand;
