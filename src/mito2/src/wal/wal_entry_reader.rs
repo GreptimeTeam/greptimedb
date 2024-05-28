0e1fd6147a74850b34a95af189ef8407c5c0a92a
WenyXu <wenymedia@gmail.com>
2024-05-28 15:32:33 +0000

    test: add tests for handling corrupted raw entry stream

--- src/mito2/src/wal/wal_entry_reader.rs
+++ src/mito2/src/wal/wal_entry_reader.rs
@@ -24,16 +24,10 @@ use crate::error::{CorruptedEntrySnafu, DecodeWalSnafu, Result};
 use crate::wal::raw_entry_reader::RawEntryReader;
 use crate::wal::{EntryId, WalEntryStream};
 
-pub(crate) fn decode_raw_entry(
-    raw_entry: Entry,
-    ignore_incomplete_entry: bool,
-) -> Result<(EntryId, WalEntry)> {
+pub(crate) fn decode_raw_entry(raw_entry: Entry) -> Result<(EntryId, WalEntry)> {
     let entry_id = raw_entry.entry_id();
     let region_id = raw_entry.region_id();
-    ensure!(
-        raw_entry.is_complete() || ignore_incomplete_entry,
-        CorruptedEntrySnafu { region_id }
-    );
+    ensure!(raw_entry.is_complete(), CorruptedEntrySnafu { region_id });
     // TODO(weny): implement the [Buf] for return value, avoid extra memory allocation.
     let bytes = raw_entry.into_bytes();
     let wal_entry = WalEntry::decode(bytes.as_slice()).context(DecodeWalSnafu { region_id })?;
@@ -65,14 +59,126 @@ impl<R: RawEntryReader> WalEntryReader for LogStoreEntryReader<R> {
         let stream = async_stream::stream! {
             if let Some(entry) = stream.next().await {
                 let entry = entry?;
-                yield decode_raw_entry(entry, true)
+                if entry.is_complete() {
+                    yield decode_raw_entry(entry)
+                }
+                // Ignores tail corrupted data.
             }
             while let Some(entry) = stream.next().await {
                 let entry = entry?;
-                yield decode_raw_entry(entry, false)
+                yield decode_raw_entry(entry)
             }
         };
 
         Ok(Box::pin(stream))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::assert_matches::assert_matches;
+
+    use api::v1::{Mutation, OpType, WalEntry};
+    use futures::{stream, TryStreamExt};
+    use prost::Message;
+    use store_api::logstore::entry::{Entry, MultiplePartEntry, MultiplePartHeader};
+    use store_api::logstore::provider::Provider;
+    use store_api::storage::RegionId;
+
+    use crate::error::{self, Result};
+    use crate::wal::raw_entry_reader::{EntryStream, RawEntryReader};
+    use crate::wal::wal_entry_reader::{LogStoreEntryReader, WalEntryReader};
+    use crate::wal::EntryId;
+
+    struct MockRawEntryStream {
+        entries: Vec<Entry>,
+    }
+
+    impl RawEntryReader for MockRawEntryStream {
+        fn read(&self, ns: &Provider, start_id: EntryId) -> Result<EntryStream<'static>> {
+            let entries = self.entries.clone().into_iter().map(Ok);
+
+            Ok(Box::pin(stream::iter(entries)))
+        }
+    }
+
+    #[tokio::test]
+    async fn test_tail_corrupted_stream() {
+        let provider = Provider::kafka_provider("my_topic".to_string());
+        let wal_entry = WalEntry {
+            mutations: vec![Mutation {
+                op_type: OpType::Put as i32,
+                sequence: 1u64,
+                rows: None,
+            }],
+        };
+        let encoded_entry = wal_entry.encode_to_vec();
+        let parts = encoded_entry
+            .chunks(encoded_entry.len() / 2)
+            .map(Into::into)
+            .collect::<Vec<_>>();
+        let raw_entry_stream = MockRawEntryStream {
+            entries: vec![
+                Entry::MultiplePart(MultiplePartEntry {
+                    provider: provider.clone(),
+                    region_id: RegionId::new(1, 1),
+                    entry_id: 1,
+                    headers: vec![MultiplePartHeader::Last],
+                    parts: vec![vec![1; 100]],
+                }),
+                Entry::MultiplePart(MultiplePartEntry {
+                    provider: provider.clone(),
+                    region_id: RegionId::new(1, 1),
+                    entry_id: 2,
+                    headers: vec![MultiplePartHeader::First, MultiplePartHeader::Last],
+                    parts,
+                }),
+            ],
+        };
+
+        let reader = LogStoreEntryReader::new(raw_entry_stream);
+        let entries = reader
+            .read(&provider, 0)
+            .unwrap()
+            .try_collect::<Vec<_>>()
+            .await
+            .unwrap()
+            .into_iter()
+            .map(|(_, entry)| entry)
+            .collect::<Vec<_>>();
+
+        assert_eq!(entries, vec![wal_entry]);
+    }
+
+    #[tokio::test]
+    async fn test_corrupted_stream() {
+        let provider = Provider::kafka_provider("my_topic".to_string());
+        let raw_entry_stream = MockRawEntryStream {
+            entries: vec![
+                Entry::MultiplePart(MultiplePartEntry {
+                    provider: provider.clone(),
+                    region_id: RegionId::new(1, 1),
+                    entry_id: 1,
+                    headers: vec![MultiplePartHeader::Last],
+                    parts: vec![vec![1; 100]],
+                }),
+                Entry::MultiplePart(MultiplePartEntry {
+                    provider: provider.clone(),
+                    region_id: RegionId::new(1, 1),
+                    entry_id: 2,
+                    headers: vec![MultiplePartHeader::First],
+                    parts: vec![vec![1; 100]],
+                }),
+            ],
+        };
+
+        let reader = LogStoreEntryReader::new(raw_entry_stream);
+        let err = reader
+            .read(&provider, 0)
+            .unwrap()
+            .try_collect::<Vec<_>>()
+            .await
+            .unwrap_err();
+        assert_matches!(err, error::Error::CorruptedEntry { .. });
     }
 }
