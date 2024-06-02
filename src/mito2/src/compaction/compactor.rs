37b0464db229b779d2d69208072cf60744c050e5
zyy17 <zyylsxm@gmail.com>
2024-06-02 23:58:52 +0800

    refactor: add CompactionRequest and open_compaction_region

--- src/mito2/src/compaction/compactor.rs
+++ src/mito2/src/compaction/compactor.rs
@@ -12,30 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
 use std::sync::Arc;
 use std::time::Duration;
 
+use api::v1::region::compact_request;
 use common_telemetry::info;
+use object_store::manager::ObjectStoreManager;
+use object_store::util::join_dir;
 use smallvec::SmallVec;
-use snafu::ResultExt;
+use snafu::{OptionExt, ResultExt};
 use store_api::metadata::RegionMetadataRef;
+use store_api::path_utils::region_dir;
 use store_api::storage::RegionId;
 
-use crate::access_layer::{AccessLayerRef, SstWriteRequest};
+use crate::access_layer::{AccessLayer, AccessLayerRef, SstWriteRequest};
 use crate::cache::CacheManagerRef;
 use crate::compaction::build_sst_reader;
 use crate::compaction::picker::{Picker, PickerOutput};
 use crate::config::MitoConfig;
-use crate::error;
-use crate::error::Result;
+use crate::error::{JoinSnafu, ObjectStoreNotFoundSnafu, Result};
+use crate::flush::WriteBufferManagerImpl;
 use crate::manifest::action::{RegionEdit, RegionMetaAction, RegionMetaActionList};
+use crate::manifest::manager::{RegionManifestManager, RegionManifestOptions};
+use crate::manifest::storage::manifest_compress_type;
+use crate::memtable::time_partition::TimePartitions;
+use crate::memtable::MemtableBuilderProvider;
 use crate::read::Source;
 use crate::region::options::RegionOptions;
-use crate::region::version::VersionControlRef;
+use crate::region::version::{VersionBuilder, VersionControl, VersionControlRef};
 use crate::region::ManifestContext;
 use crate::region::RegionState::Writable;
+use crate::schedule::scheduler::LocalScheduler;
 use crate::sst::file::{FileMeta, IndexType};
-use crate::sst::file_purger::FilePurgerRef;
+use crate::sst::file_purger::{FilePurgerRef, LocalFilePurger};
+use crate::sst::index::intermediate::IntermediateManager;
 use crate::sst::parquet::WriteOptions;
 
 #[derive(Clone)]
@@ -50,6 +61,125 @@ pub struct CompactionRegion {
     pub access_layer: AccessLayerRef,
     pub version_control: VersionControlRef,
     pub file_purger: FilePurgerRef,
+}
+
+pub struct CompactionRequest {
+    pub catalog: String,
+    pub schema: String,
+    pub region_id: RegionId,
+    pub options: HashMap<String, String>,
+    pub compaction_options: compact_request::Options,
+}
+
+pub async fn open_compaction_region(
+    req: CompactionRequest,
+    mito_config: Arc<MitoConfig>,
+    object_store_manager: ObjectStoreManager,
+    cache_manager: CacheManagerRef,
+) -> Result<CompactionRegion> {
+    let region_options = RegionOptions::try_from(&req.options)?;
+    let region_dir = region_dir(
+        format!("{}/{}", &req.catalog, &req.schema).as_str(),
+        req.region_id,
+    );
+
+    let object_store = {
+        let name = &region_options.storage;
+        if let Some(name) = name {
+            object_store_manager
+                .find(name)
+                .context(ObjectStoreNotFoundSnafu {
+                    object_store: name.to_string(),
+                })?
+        } else {
+            object_store_manager.default_object_store()
+        }
+    };
+
+    let access_layer = {
+        // TODO(zyy17): Should we really need to create intermediate manager here?
+        let intermediate_manager =
+            IntermediateManager::init_fs(mito_config.inverted_index.intermediate_path.clone())
+                .await?;
+
+        Arc::new(AccessLayer::new(
+            region_dir.to_string(),
+            object_store.clone(),
+            intermediate_manager,
+        ))
+    };
+
+    let manifest_manager = {
+        let region_manifest_options = RegionManifestOptions {
+            // TODO(zyy17): It needs to add a new function instead of using join_dir.
+            manifest_dir: join_dir(&region_dir, "manifest"),
+            object_store: object_store.clone(),
+            compress_type: manifest_compress_type(mito_config.compress_manifest),
+            checkpoint_distance: mito_config.manifest_checkpoint_distance,
+        };
+
+        // TODO(zyy17): handle error.
+        RegionManifestManager::open(region_manifest_options, Default::default())
+            .await?
+            .unwrap()
+    };
+
+    let manifest = manifest_manager.manifest();
+    let region_metadata = manifest.metadata.clone();
+    let manifest_ctx = Arc::new(ManifestContext::new(manifest_manager, Writable));
+
+    let file_purger = {
+        let purge_scheduler = Arc::new(LocalScheduler::new(mito_config.max_background_jobs));
+        Arc::new(LocalFilePurger::new(
+            purge_scheduler.clone(),
+            access_layer.clone(),
+            None,
+        ))
+    };
+
+    let version_control = {
+        let write_buffer_manager = Arc::new(WriteBufferManagerImpl::new(
+            mito_config.global_write_buffer_size.as_bytes() as usize,
+        ));
+        let memtable_builder_provider =
+            MemtableBuilderProvider::new(Some(write_buffer_manager.clone()), mito_config.clone());
+
+        let memtable_builder = memtable_builder_provider.builder_for_options(
+            region_options.memtable.as_ref(),
+            !region_options.append_mode,
+        );
+
+        // Initial memtable id is 0.
+        let part_duration = region_options.compaction.time_window();
+        let mutable = Arc::new(TimePartitions::new(
+            region_metadata.clone(),
+            memtable_builder.clone(),
+            0,
+            part_duration,
+        ));
+
+        let version = VersionBuilder::new(region_metadata.clone(), mutable)
+            .add_files(file_purger.clone(), manifest.files.values().cloned())
+            .flushed_entry_id(manifest.flushed_entry_id)
+            .flushed_sequence(manifest.flushed_sequence)
+            .truncated_entry_id(manifest.truncated_entry_id)
+            .compaction_time_window(manifest.compaction_time_window)
+            .options(region_options.clone())
+            .build();
+        Arc::new(VersionControl::new(version))
+    };
+
+    Ok(CompactionRegion {
+        region_options: region_options.clone(),
+        manifest_ctx,
+        access_layer,
+        version_control,
+        region_id: req.region_id,
+        cache_manager: cache_manager.clone(),
+        engine_config: mito_config.clone(),
+        region_metadata: region_metadata.clone(),
+        file_purger: file_purger.clone(),
+    })
 }
 
 /// MergeOutput represents the output of merging SST files.
@@ -256,7 +386,7 @@ async fn do_merge_ssts(
         }
         let metas = futures::future::try_join_all(task_chunk)
             .await
-            .context(error::JoinSnafu)?
+            .context(JoinSnafu)?
             .into_iter()
             .collect::<Result<Vec<_>>>()?;
         output_files.extend(metas.into_iter().flatten());
