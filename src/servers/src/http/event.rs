77ef015c022b3676a8b243add61a6154f54220d6
paomian <xpaomian@gmail.com>
2024-06-06 14:17:14 +0800

    chore: update upload pipeline api

--- src/servers/src/http/event.rs
+++ src/servers/src/http/event.rs
@@ -13,11 +13,16 @@
 // limitations under the License.
 
 use std::collections::HashMap;
+use std::result::Result as StdResult;
 
 use api::v1::{RowInsertRequest, RowInsertRequests, Rows};
-use axum::extract::{Json, Query, State};
+use axum::body::HttpBody;
+use axum::extract::{FromRequest, Multipart, Path, Query, State};
 use axum::headers::ContentType;
-use axum::{Extension, TypedHeader};
+use axum::http::header::CONTENT_TYPE;
+use axum::http::{Request, StatusCode};
+use axum::response::{IntoResponse, Response};
+use axum::{async_trait, BoxError, Extension, TypedHeader};
 use common_telemetry::{error, warn};
 use mime_guess::mime;
 use pipeline::error::{CastTypeSnafu, ExecPipelineSnafu};
@@ -43,24 +48,75 @@ pub struct LogIngesterQueryParams {
     pub ignore_errors: Option<bool>,
 }
 
+pub struct PipelineContent(String);
+
+#[async_trait]
+impl<S, B> FromRequest<S, B> for PipelineContent
+where
+    B: HttpBody + Send + 'static,
+    B::Data: Send,
+    bytes::Bytes: std::convert::From<<B as HttpBody>::Data>,
+    B::Error: Into<BoxError>,
+    S: Send + Sync,
+{
+    type Rejection = Response;
+
+    async fn from_request(req: Request<B>, state: &S) -> StdResult<Self, Self::Rejection> {
+        let content_type_header = req.headers().get(CONTENT_TYPE);
+        let content_type = content_type_header.and_then(|value| value.to_str().ok());
+        if let Some(content_type) = content_type {
+            if content_type.ends_with("yaml") {
+                let payload = String::from_request(req, state)
+                    .await
+                    .map_err(IntoResponse::into_response)?;
+                return Ok(Self(payload));
+            }
+
+            if content_type.starts_with("multipart/form-data") {
+                let mut payload: Multipart = Multipart::from_request(req, state)
+                    .await
+                    .map_err(IntoResponse::into_response)?;
+                let file = payload
+                    .next_field()
+                    .await
+                    .map_err(IntoResponse::into_response)?;
+                let payload = file
+                    .ok_or(StatusCode::UNSUPPORTED_MEDIA_TYPE.into_response())?
+                    .text()
+                    .await
+                    .map_err(IntoResponse::into_response)?;
+                return Ok(Self(payload));
+            }
+        }
+
+        Err(StatusCode::UNSUPPORTED_MEDIA_TYPE.into_response())
+    }
+}
+
 #[axum_macros::debug_handler]
 pub async fn add_pipeline(
     State(handler): State<LogHandlerRef>,
+    Path(pipeline_name): Path<String>,
     Extension(query_ctx): Extension<QueryContextRef>,
-    Json(payload): Json<Value>,
+    PipelineContent(payload): PipelineContent,
 ) -> Result<String> {
-    let name = payload["name"].as_str().context(InvalidParameterSnafu {
-        reason: "name is required in payload",
-    })?;
-    let pipeline = payload["pipeline"]
-        .as_str()
-        .context(InvalidParameterSnafu {
-            reason: "pipeline is required in payload",
-        })?;
+    if pipeline_name.is_empty() {
+        return Err(InvalidParameterSnafu {
+            reason: "pipeline_name is required in path",
+        }
+        .build());
+    }
+
+    if payload.is_empty() {
+        return Err(InvalidParameterSnafu {
+            reason: "pipeline is required in body",
+        }
+        .build());
+    }
 
     let content_type = "yaml";
     let result = handler
-        .insert_pipeline(name, content_type, pipeline, query_ctx)
+        .insert_pipeline(&pipeline_name, content_type, &payload, query_ctx)
         .await;
 
     result.map(|_| "ok".to_string()).map_err(|e| {
