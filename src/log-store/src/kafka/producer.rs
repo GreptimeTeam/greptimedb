ac898ed58531b4fd517cb6d71a42d9488af7e575
WenyXu <wenymedia@gmail.com>
2024-06-13 09:06:37 +0000

    test: add tests

--- src/log-store/src/kafka/producer.rs
+++ src/log-store/src/kafka/producer.rs
@@ -41,7 +41,7 @@ impl ProduceResultReceiver {
         self.receivers.push(receiver)
     }
 
-    pub(crate) async fn wait(self) -> Result<Vec<i64>> {
+    async fn wait(self) -> Result<Vec<i64>> {
         Ok(try_join_all(self.receivers)
             .await
             .into_iter()
@@ -249,5 +249,219 @@ impl OrderedBatchProducer {
         };
 
         Ok(ProduceResultHandle { receiver })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
+    use std::time::Duration;
+
+    use chrono::{TimeZone, Utc};
+    use common_base::readable_size::ReadableSize;
+    use common_telemetry::debug;
+    use futures::future::BoxFuture;
+    use futures::stream::FuturesUnordered;
+    use futures::{FutureExt, StreamExt};
+    use rskafka::client::error::{Error as ClientError, RequestContext};
+    use rskafka::client::partition::Compression;
+    use rskafka::client::producer::ProducerClient;
+    use rskafka::protocol::error::Error as ProtocolError;
+    use rskafka::record::Record;
+
+    use crate::kafka::producer::OrderedBatchProducer;
+
+    #[derive(Debug)]
+    struct MockClient {
+        error: Option<ProtocolError>,
+        panic: Option<String>,
+        delay: Duration,
+        batch_sizes: Mutex<Vec<usize>>,
+    }
+
+    impl ProducerClient for MockClient {
+        fn produce(
+            &self,
+            records: Vec<Record>,
+            _compression: Compression,
+        ) -> BoxFuture<'_, Result<Vec<i64>, ClientError>> {
+            Box::pin(async move {
+                tokio::time::sleep(self.delay).await;
+
+                if let Some(e) = self.error {
+                    return Err(ClientError::ServerError {
+                        protocol_error: e,
+                        error_message: None,
+                        request: RequestContext::Partition("foo".into(), 1),
+                        response: None,
+                        is_virtual: false,
+                    });
+                }
+
+                if let Some(p) = self.panic.as_ref() {
+                    panic!("{}", p);
+                }
+
+                let mut batch_sizes = self.batch_sizes.lock().unwrap();
+                let offset_base = batch_sizes.iter().sum::<usize>();
+                let offsets = (0..records.len())
+                    .map(|x| (x + offset_base) as i64)
+                    .collect();
+                batch_sizes.push(records.len());
+                debug!("Return offsets: {offsets:?}");
+                Ok(offsets)
+            })
+        }
+    }
+
+    fn record() -> Record {
+        Record {
+            key: Some(vec![0; 4]),
+            value: Some(vec![0; 6]),
+            headers: Default::default(),
+            timestamp: Utc.timestamp_millis_opt(320).unwrap(),
+        }
+    }
+
+    #[tokio::test]
+    async fn test_producer() {
+        common_telemetry::init_default_ut_logging();
+        let record = record();
+        let delay = Duration::from_secs(0);
+        let client = Arc::new(MockClient {
+            error: None,
+            panic: None,
+            delay,
+            batch_sizes: Default::default(),
+        });
+
+        let producer = OrderedBatchProducer::new(
+            client.clone(),
+            Compression::NoCompression,
+            128,
+            64,
+            ReadableSize((record.approximate_size() * 2) as u64).as_bytes() as usize,
+        );
+
+        // Produces 3 records
+        let handle = producer
+            .produce(vec![record.clone(), record.clone(), record.clone()])
+            .await
+            .unwrap();
+        assert_eq!(handle.wait().await.unwrap(), vec![0, 1, 2]);
+        assert_eq!(client.batch_sizes.lock().unwrap().as_slice(), &[2, 1]);
+
+        // Produces 2 records
+        let handle = producer
+            .produce(vec![record.clone(), record.clone()])
+            .await
+            .unwrap();
+        assert_eq!(handle.wait().await.unwrap(), vec![3, 4]);
+        assert_eq!(client.batch_sizes.lock().unwrap().as_slice(), &[2, 1, 2]);
+
+        // Produces 1 records
+        let handle = producer.produce(vec![record.clone()]).await.unwrap();
+        assert_eq!(handle.wait().await.unwrap(), vec![5]);
+        assert_eq!(client.batch_sizes.lock().unwrap().as_slice(), &[2, 1, 2, 1]);
+    }
+
+    #[tokio::test]
+    async fn test_producer_client_error() {
+        let record = record();
+        let client = Arc::new(MockClient {
+            error: Some(ProtocolError::NetworkException),
+            panic: None,
+            delay: Duration::from_millis(1),
+            batch_sizes: Default::default(),
+        });
+
+        let producer = OrderedBatchProducer::new(
+            client.clone(),
+            Compression::NoCompression,
+            128,
+            64,
+            ReadableSize((record.approximate_size() * 2) as u64).as_bytes() as usize,
+        );
+
+        let mut futures = FuturesUnordered::new();
+        futures.push(
+            producer
+                .produce(vec![record.clone(), record.clone(), record.clone()])
+                .await
+                .unwrap()
+                .wait(),
+        );
+        futures.push(
+            producer
+                .produce(vec![record.clone(), record.clone()])
+                .await
+                .unwrap()
+                .wait(),
+        );
+        futures.push(producer.produce(vec![record.clone()]).await.unwrap().wait());
+
+        futures.next().await.unwrap().unwrap_err();
+        futures.next().await.unwrap().unwrap_err();
+        futures.next().await.unwrap().unwrap_err();
+    }
+
+    #[tokio::test]
+    async fn test_producer_cancel() {
+        let record = record();
+        let client = Arc::new(MockClient {
+            error: None,
+            panic: None,
+            delay: Duration::from_millis(1),
+            batch_sizes: Default::default(),
+        });
+
+        let producer = OrderedBatchProducer::new(
+            client.clone(),
+            Compression::NoCompression,
+            128,
+            64,
+            ReadableSize((record.approximate_size() * 2) as u64).as_bytes() as usize,
+        );
+
+        let a = producer
+            .produce(vec![record.clone(), record.clone(), record.clone()])
+            .await
+            .unwrap()
+            .wait()
+            .fuse();
+
+        let b = producer.produce(vec![record]).await.unwrap().wait().fuse();
+
+        let mut b = Box::pin(b);
+
+        {
+            // Cancel a when it exits this block
+            let mut a = Box::pin(a);
+
+            // Select biased to encourage `a` to be the one with the linger that
+            // expires first and performs the produce operation
+            futures::select_biased! {
+                _ = &mut a => panic!("a should not have flushed"),
+                _ = &mut b => panic!("b should not have flushed"),
+                _ = tokio::time::sleep(Duration::from_millis(1)).fuse() => {},
+            }
+        }
+
+        // But `b` should still complete successfully
+        tokio::time::timeout(Duration::from_secs(1), b)
+            .await
+            .unwrap()
+            .unwrap();
+
+        assert_eq!(
+            client
+                .batch_sizes
+                .lock()
+                .unwrap()
+                .as_slice()
+                .iter()
+                .sum::<usize>(),
+            4
+        );
     }
 }
