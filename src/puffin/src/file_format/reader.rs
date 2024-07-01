17717dd1e2d949f4ecd785765fe012bfe0855ff7
Zhenchi <zhongzc_arch@outlook.com>
2024-07-01 14:23:01 +0000

    fix: fmt
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- src/puffin/src/file_format/reader.rs
+++ src/puffin/src/file_format/reader.rs
@@ -30,7 +30,7 @@ pub trait PuffinSyncReader<'a> {
     fn metadata(&'a mut self) -> Result<FileMetadata>;
 
     /// Reads particular blob data based on given metadata.
-    /// 
+    ///
     /// Data read from the reader is compressed leaving the caller to decompress the data.
     fn blob_reader(&'a mut self, blob_metadata: &BlobMetadata) -> Result<Self::Reader>;
 }
@@ -44,7 +44,7 @@ pub trait PuffinAsyncReader<'a> {
     async fn metadata(&'a mut self) -> Result<FileMetadata>;
 
     /// Reads particular blob data based on given metadata.
-    /// 
+    ///
     /// Data read from the reader is compressed leaving the caller to decompress the data.
     fn blob_reader(&'a mut self, blob_metadata: &BlobMetadata) -> Result<Self::Reader>;
 }
