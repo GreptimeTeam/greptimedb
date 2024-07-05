a9b2d91b858ec846c64a8bf3ec250f03773019f0
Eugene Tolbakov <ev.tolbakov@gmail.com>
2024-07-05 08:36:07 +0100

    fix(sql): address CR feedback

--- src/sql/src/statements/transform/expand_interval.rs
+++ src/sql/src/statements/transform/expand_interval.rs
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use core::time::Duration as StdDuration;
 use std::collections::HashMap;
 use std::ops::ControlFlow;
+use std::time::Duration as StdDuration;
 
 use itertools::Itertools;
 use lazy_static::lazy_static;
