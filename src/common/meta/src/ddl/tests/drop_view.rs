1b3b5d51ed1f1ace775a245e9b2601a5445cdab7
Dennis Zhuang <killme2008@gmail.com>
2024-07-09 13:06:35 -0700

    test: drop view meets a table

--- src/common/meta/src/ddl/tests/drop_view.rs
+++ src/common/meta/src/ddl/tests/drop_view.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use common_error::ext::ErrorExt;
@@ -20,7 +21,9 @@ use common_procedure_test::execute_procedure_until_done;
 use store_api::storage::TableId;
 
 use crate::ddl::drop_view::{DropViewProcedure, DropViewState};
+use crate::ddl::test_util::create_table::test_create_table_task;
 use crate::ddl::tests::create_view::{test_create_view_task, test_table_names};
+use crate::key::table_route::TableRouteValue;
 use crate::rpc::ddl::DropViewTask;
 use crate::test_util::{new_ddl_context, MockDatanodeManager};
 
@@ -60,6 +63,32 @@ async fn test_on_prepare_view_not_exists_err() {
     let mut procedure = DropViewProcedure::new(cluster_id, task, ddl_context);
     let err = procedure.on_prepare().await.unwrap_err();
     assert_eq!(err.status_code(), StatusCode::TableNotFound);
+}
+
+#[tokio::test]
+async fn test_on_prepare_not_view_err() {
+    let node_manager = Arc::new(MockDatanodeManager::new(()));
+    let ddl_context = new_ddl_context(node_manager);
+    let cluster_id = 1;
+    let view_id = 1024;
+    let view_name = "foo";
+    let task = test_create_table_task(view_name, view_id);
+    // Create a table, not a view.
+    ddl_context
+        .table_metadata_manager
+        .create_table_metadata(
+            task.table_info.clone(),
+            TableRouteValue::physical(vec![]),
+            HashMap::new(),
+        )
+        .await
+        .unwrap();
+
+    let task = new_drop_view_task(view_name, view_id, false);
+    let mut procedure = DropViewProcedure::new(cluster_id, task, ddl_context);
+    // It's not a view, expect error
+    let err = procedure.on_prepare().await.unwrap_err();
+    assert_eq!(err.status_code(), StatusCode::InvalidArguments);
 }
 
 #[tokio::test]
