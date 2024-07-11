9f6280509f97688281b276b1afcda70c67c9b58c
tison <wander4096@gmail.com>
2024-07-10 18:16:56 -0700

    fix more
    Signed-off-by: tison <wander4096@gmail.com>

--- tests/cases/standalone/information_schema/cluster_info.sql
+++ tests/cases/standalone/information_schema/cluster_info.sql
@@ -4,7 +4,7 @@ DESC TABLE CLUSTER_INFO;
 
 -- SQLNESS REPLACE version node_version
 -- SQLNESS REPLACE (\d\.\d\.\d) Version
--- SQLNESS REPLACE (\s[a-z0-9]{7}\s) Hash
+-- SQLNESS REPLACE (\s[a-z0-9]{7,8}\s) Hash
 -- SQLNESS REPLACE (\s[\-0-9T:\.]{15,}) Start_time
 -- SQLNESS REPLACE ((\d+(s|ms|m)\s)+) Duration
 -- SQLNESS REPLACE [\s\-]+
@@ -12,7 +12,7 @@ SELECT * FROM CLUSTER_INFO;
 
 -- SQLNESS REPLACE version node_version
 -- SQLNESS REPLACE (\d\.\d\.\d) Version
--- SQLNESS REPLACE (\s[a-z0-9]{7}\s) Hash
+-- SQLNESS REPLACE (\s[a-z0-9]{7,8}\s) Hash
 -- SQLNESS REPLACE (\s[\-0-9T:\.]{15,}) Start_time
 -- SQLNESS REPLACE ((\d+(s|ms|m)\s)+) Duration
 -- SQLNESS REPLACE [\s\-]+
@@ -22,7 +22,7 @@ SELECT * FROM CLUSTER_INFO WHERE PEER_TYPE != 'STANDALONE';
 
 -- SQLNESS REPLACE version node_version
 -- SQLNESS REPLACE (\d\.\d\.\d) Version
--- SQLNESS REPLACE (\s[a-z0-9]{7}\s) Hash
+-- SQLNESS REPLACE (\s[a-z0-9]{7,8}\s) Hash
 -- SQLNESS REPLACE (\s[\-0-9T:\.]{15,}) Start_time
 -- SQLNESS REPLACE ((\d+(s|ms|m)\s)+) Duration
 -- SQLNESS REPLACE [\s\-]+
