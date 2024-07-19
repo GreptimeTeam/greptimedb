ce900e850a3aaa527564156b3c69f34563e87b84
zyy17 <zyylsxm@gmail.com>
2024-07-19 16:41:29 +0800

    fix: user provider can't be configured by config file or environment variables (#4398)

--- src/cmd/src/frontend.rs
+++ src/cmd/src/frontend.rs
@@ -246,7 +246,9 @@ impl StartCommand {
             opts.mode = Mode::Distributed;
         }
 
-        opts.user_provider.clone_from(&self.user_provider);
+        if let Some(user_provider) = &self.user_provider {
+            opts.user_provider = Some(user_provider.clone());
+        }
 
         Ok(())
     }
