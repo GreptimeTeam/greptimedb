a67b2732db1646c3ec02fb75a0cb1a8438653a4a
J0hn50n133 <0xjohnsonlee@gmail.com>
2024-08-10 04:14:08 +0800

    fix: header and correct logic of update oid

--- src/catalog/src/system_schema/pg_catalog/pg_namespace/oid_map.rs
+++ src/catalog/src/system_schema/pg_catalog/pg_namespace/oid_map.rs
@@ -1,3 +1,17 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::hash::BuildHasher;
 use std::sync::Arc;
 
@@ -35,10 +49,52 @@ impl PGNamespaceOidMap {
         } else {
             let mut oid = self.hasher.hash_one(schema_name) as u32;
             while self.oid_is_used(oid) {
-                oid = self.hasher.hash_one(schema_name) as u32;
+                oid = self.hasher.hash_one(oid) as u32;
             }
             self.oid_map.insert(schema_name.to_string(), oid);
             oid
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn oid_is_stable() {
+        let oid_map_1 = PGNamespaceOidMap::new();
+        let oid_map_2 = PGNamespaceOidMap::new();
+
+        let schema = "schema";
+        let oid = oid_map_1.get_oid(schema);
+
+        // oid keep stable in the same instance
+        assert_eq!(oid, oid_map_1.get_oid(schema));
+
+        // oid keep stable between different instances
+        assert_eq!(oid, oid_map_2.get_oid(schema));
+    }
+
+    #[test]
+    fn oid_collision() {
+        let oid_map = PGNamespaceOidMap::new();
+
+        let key1 = "3178510";
+        let key2 = "4215648";
+
+        // have collision
+        assert_eq!(
+            oid_map.hasher.hash_one(key1) as u32,
+            oid_map.hasher.hash_one(key2) as u32
+        );
+
+        // insert them into oid_map
+        let oid1 = oid_map.get_oid(key1);
+        let oid2 = oid_map.get_oid(key2);
+
+        // they should have different id
+        assert_ne!(oid1, oid2);
     }
 }
