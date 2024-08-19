5524980267170b523df5ecde26fbd137ae3fc9b8
ozewr <l19ht@google.com>
2024-08-19 19:38:17 +0800

    remove todo #4065

--- src/mito2/src/wal/entry_reader.rs
+++ src/mito2/src/wal/entry_reader.rs
@@ -29,7 +29,6 @@ pub(crate) fn decode_raw_entry(raw_entry: Entry) -> Result<(EntryId, WalEntry)> 
     let entry_id = raw_entry.entry_id();
     let region_id = raw_entry.region_id();
     ensure!(raw_entry.is_complete(), CorruptedEntrySnafu { region_id });
-    // TODO(weny): implement the [Buf] for return value, avoid extra memory allocation.
     let buffer = raw_entry_buffer(raw_entry);
     let wal_entry = WalEntry::decode(buffer).context(DecodeWalSnafu { region_id })?;
     Ok((entry_id, wal_entry))
