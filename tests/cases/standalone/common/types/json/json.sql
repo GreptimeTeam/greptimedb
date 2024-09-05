315b1e613a021e4a08ed2adfbf7e6bb585ddf42e
CookiePieWw <profsyb@gmail.com>
2024-09-05 22:33:00 +0800

    test: add more sqlness test for json

--- tests/cases/standalone/common/types/json/json.sql
+++ tests/cases/standalone/common/types/json/json.sql
@@ -1,7 +1,78 @@
 CREATE TABLE jsons (j JSON, t timestamp time index);
 
 --Insert valid json strings--
-INSERT INTO jsons VALUES(to_json('{"a":1, "b":2, "c":3}'), 1), (to_json('{"a":1, "b":2, "c":3, "d":4}'), 2), (to_json('{"a":1, "b":2, "c":3, "d":4, "e":5}'), 3);
+INSERT INTO jsons VALUES('[null]', 0),
+('[true]', 1),
+('[false]', 2),
+('[0]', 3),
+('["foo"]', 4),
+('[]', 5),
+('{}', 6),
+('[0,1]', 7),
+('{"foo":"bar"}', 8),
+('{"a":null,"foo":"bar"}', 9),
+('[-1]', 10),
+('{"entities": {
+    "description": {
+        "urls": [
+            {
+                "url": "http://t.co/QMLJeFmfMT",
+                "expanded_url": "http://www.pixiv.net/member.php?id=4776",
+                "display_url": "pixiv.net/member.php?id=…",
+                "indices": [
+                    58,
+                    80
+                ]
+            },
+            {
+                "url": "http://t.co/LU8T7vmU3h",
+                "expanded_url": "http://ask.fm/KATANA77",
+                "display_url": "ask.fm/KATANA77",
+                "indices": [
+                    95,
+                    117
+                ]
+            }
+        ]
+    }
+}}', 11);
+
+INSERT INTO jsons VALUES(to_json('[null]'), 12),
+(to_json('[true]'), 13),
+(to_json('[false]'), 14),
+(to_json('[0]'), 15),
+(to_json('["foo"]'), 16),
+(to_json('[]'), 17),
+(to_json('{}'), 18),
+(to_json('[0,1]'), 19),
+(to_json('{"foo":"bar"}'), 20),
+(to_json('{"a":null,"foo":"bar"}'), 21),
+(to_json('[-1]'), 22),
+(to_json('[-2147483648]'), 23),
+(to_json('{"entities": {
+            "description": {
+                "urls": [
+                    {
+                        "url": "http://t.co/QMLJeFmfMT",
+                        "expanded_url": "http://www.pixiv.net/member.php?id=4776",
+                        "display_url": "pixiv.net/member.php?id=…",
+                        "indices": [
+                            58,
+                            80
+                        ]
+                    },
+                    {
+                        "url": "http://t.co/LU8T7vmU3h",
+                        "expanded_url": "http://ask.fm/KATANA77",
+                        "display_url": "ask.fm/KATANA77",
+                        "indices": [
+                            95,
+                            117
+                        ]
+                    }
+                ]
+            }
+        }}'), 24);
 
 SELECT json_to_string(j), t FROM jsons;
 
