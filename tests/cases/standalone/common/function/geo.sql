aaa9b329085de638e5122bf132c239359dba65d6
Ning Sun <sunng@protonmail.com>
2024-10-11 10:57:54 -0700

    feat: add more h3 functions (#4770)
    * feat: add more h3 grid functions
    
    * feat: add more traversal functions
    
    * refactor: update some function definitions
    
    * style: format
    
    * refactor: avoid creating slice in nested loop
    
    * feat: ensure column number and length
    
    * refactor: fix lint warnings
    
    * refactor: merge main
    
    * Apply suggestions from code review
    
    Co-authored-by: LFC <990479+MichaelScofield@users.noreply.github.com>
    
    * Update src/common/function/src/scalars/geo/h3.rs
    
    Co-authored-by: LFC <990479+MichaelScofield@users.noreply.github.com>
    
    * style: format
    
    ---------
    
    Co-authored-by: LFC <990479+MichaelScofield@users.noreply.github.com>

--- tests/cases/standalone/common/function/geo.sql
+++ tests/cases/standalone/common/function/geo.sql
@@ -26,18 +26,34 @@ SELECT h3_latlng_to_cell(37.76938, -122.3889, 8::UInt64), h3_latlng_to_cell_stri
 
 SELECT h3_cell_to_string(h3_latlng_to_cell(37.76938, -122.3889, 8::UInt64)) AS cell_str, h3_string_to_cell(h3_latlng_to_cell_string(37.76938, -122.3889, 8::UInt64)) AS cell_index;
 
-SELECT h3_cell_center_lat(h3_latlng_to_cell(37.76938, -122.3889, 8::UInt64)) AS cell_center_lat, h3_cell_center_lng(h3_latlng_to_cell(37.76938, -122.3889, 8::UInt64)) AS cell_center_lng;
+SELECT h3_cell_center_latlng(h3_latlng_to_cell(37.76938, -122.3889, 8::UInt64)) AS cell_center;
 
 SELECT
     h3_cell_resolution(cell) AS resolution,
     h3_cell_base(cell) AS base,
     h3_cell_is_pentagon(cell) AS pentagon,
     h3_cell_parent(cell, 6::UInt64) AS parent,
+    h3_cell_to_children(cell, 10::UInt64) AS children,
+    h3_cell_to_children_size(cell, 10) AS children_count,
+    h3_cell_to_child_pos(cell, 6) AS child_pos,
+    h3_child_pos_to_cell(25, cell, 11) AS child
 FROM (SELECT h3_latlng_to_cell(37.76938, -122.3889, 8::UInt64) AS cell);
 
-SELECT h3_is_neighbour(cell1, cell2)
-FROM (SELECT h3_latlng_to_cell(37.76938, -122.3889, 8::UInt64) AS cell1, h3_latlng_to_cell(36.76938, -122.3889, 8::UInt64) AS cell2);
+SELECT
+    h3_grid_disk(cell, 0) AS current_cell,
+    h3_grid_disk(cell, 3) AS grids,
+    h3_grid_disk_distances(cell, 3) AS all_grids,
+FROM (SELECT h3_latlng_to_cell(37.76938, -122.3889, 8::UInt64) AS cell);
 
+SELECT
+    h3_grid_distance(cell1, cell2) AS distance,
+    h3_grid_path_cells(cell1, cell2) AS path_cells,
+FROM
+    (
+      SELECT
+          h3_latlng_to_cell(37.76938, -122.3889, 8::UInt64) AS cell1,
+          h3_latlng_to_cell(39.634, -104.999, 8::UInt64) AS cell2
+    );
 
 SELECT geohash(37.76938, -122.3889, 9);
 
