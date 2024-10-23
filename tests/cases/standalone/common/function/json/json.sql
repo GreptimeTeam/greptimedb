0c3c671d1879d6fe025b1809d5637e4669e94283
kf zheng <100595273+kev1n8@users.noreply.github.com>
2024-10-23 17:52:35 +0800

    update test cases

--- tests/cases/standalone/common/function/json/json.sql
+++ tests/cases/standalone/common/function/json/json.sql
@@ -7,9 +7,9 @@ SELECT json_path_exists(parse_json('[1, 2]'), '[0]');
 
 SELECT json_path_exists(parse_json('[1, 2]'), '[2]');
 
-SELECT json_path_exists(parse_json('[1, 2]'), NULL);
+SELECT json_path_exists(parse_json('[1, 2]'), 'null');
 
--- SELECT json_path_exists(NULL, '$.a');
+SELECT json_path_exists(parse_json('null'), '$.a');
 
 --- json_path_match ---
 
@@ -19,6 +19,6 @@ SELECT json_path_match(parse_json('{"a":1,"b":[1,2,3]}'), '$.b[0] > 1');
 
 SELECT json_path_match(parse_json('{"a":1,"b":[1,2,3]}'), '$.b[1 to last] >= 2');
 
-SELECT json_path_match(parse_json('{"a":1,"b":[1,2,3]}'), NULL);
+SELECT json_path_match(parse_json('{"a":1,"b":[1,2,3]}'), 'null');
 
-SELECT json_path_match(NULL, '$.a == 1');
+SELECT json_path_match(parse_json('null'), '$.a == 1');
