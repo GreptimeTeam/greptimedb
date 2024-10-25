b9ee5a50dfaa1ac4d5454c5049cb456df7589578
CookiePieWw <profsyb@gmail.com>
2024-10-26 02:43:03 +0800

    test: update test

--- tests/cases/standalone/common/types/json/json.sql
+++ tests/cases/standalone/common/types/json/json.sql
@@ -37,19 +37,19 @@ INSERT INTO jsons VALUES('[null]', 0),
     }
 }}', 11);
 
-INSERT INTO jsons VALUES(parse_json('[null]'), 12),
-(parse_json('[true]'), 13),
-(parse_json('[false]'), 14),
-(parse_json('[0]'), 15),
-(parse_json('["foo"]'), 16),
-(parse_json('[]'), 17),
-(parse_json('{}'), 18),
-(parse_json('[0,1]'), 19),
-(parse_json('{"foo":"bar"}'), 20),
-(parse_json('{"a":null,"foo":"bar"}'), 21),
-(parse_json('[-1]'), 22),
-(parse_json('[-2147483648]'), 23),
-(parse_json('{"entities": {
+INSERT INTO jsons VALUES('[null]', 12),
+('[true]', 13),
+('[false]', 14),
+('[0]', 15),
+('["foo"]', 16),
+('[]', 17),
+('{}', 18),
+('[0,1]', 19),
+('{"foo":"bar"}', 20),
+('{"a":null,"foo":"bar"}', 21),
+('[-1]', 22),
+('[-2147483648]', 23),
+('{"entities": {
             "description": {
                 "urls": [
                     {
@@ -72,16 +72,16 @@ INSERT INTO jsons VALUES(parse_json('[null]'), 12),
                     }
                 ]
             }
-        }}'), 24);
+        }}', 24);
 
 SELECT json_to_string(j), t FROM jsons;
 
 --Insert invalid json strings--
 DELETE FROM jsons;
 
-INSERT INTO jsons VALUES(parse_json('{"a":1, "b":2, "c":3'), 4);
+INSERT INTO jsons VALUES('{"a":1, "b":2, "c":3', 4);
 
-INSERT INTO jsons VALUES(parse_json('Morning my friends, have a nice day :)'), 5);
+INSERT INTO jsons VALUES('Morning my friends, have a nice day :)', 5);
 
 SELECT json_to_string(j), t FROM jsons;
 
