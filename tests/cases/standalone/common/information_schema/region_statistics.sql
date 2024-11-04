9405d1c578909feca73cd552c2c37a38255e44f2
jeremyhi <jiachun_feng@proton.me>
2024-11-04 11:34:50 +0800

    feat: heartbeat_flush_threshold option (#4924)
    * feat: heartbeat_flush_threshold
    
    * chore: rename to flush_stats_factor
    
    * Update src/meta-srv/src/handler/collect_stats_handler.rs

--- tests/cases/standalone/common/information_schema/region_statistics.sql
+++ tests/cases/standalone/common/information_schema/region_statistics.sql
@@ -16,8 +16,7 @@ INSERT INTO test VALUES
        (11, 'b', 11),
        (21, 'c', 21);
 
--- SQLNESS SLEEP 11s
--- FIXME(dennis): we need to wait the datanode reporting stats info to metasrv.
+-- SQLNESS SLEEP 3s
 SELECT SUM(region_rows), SUM(disk_size), SUM(sst_size), SUM(index_size)
        FROM INFORMATION_SCHEMA.REGION_STATISTICS WHERE table_id
        IN (SELECT TABLE_ID FROM INFORMATION_SCHEMA.TABLES WHERE table_name = 'test' and table_schema = 'public');
