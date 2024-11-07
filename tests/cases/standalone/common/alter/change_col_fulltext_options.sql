7f064ca2b4d04c7390a3c12250bef3bacaeb4836
CookiePieWw <profsyb@gmail.com>
2024-11-07 17:21:08 +0800

    test: update sqlness test

--- tests/cases/standalone/common/alter/change_col_fulltext_options.sql
+++ tests/cases/standalone/common/alter/change_col_fulltext_options.sql
@@ -7,7 +7,17 @@ CREATE TABLE `test` (
 
 SHOW CREATE TABLE test;
 
+-- Write/read after altering column fulltext options
+INSERT INTO test VALUES ('hello', '2020-01-01 00:00:00'), 
+('world', '2020-01-01 00:00:01'), 
+('hello world', '2020-01-02 00:00:00'), 
+('world hello', '2020-01-02 00:00:01');
+
+SELECT * FROM test WHERE MATCHES(message, 'hello');
+
 ALTER TABLE test MODIFY COLUMN message SET FULLTEXT WITH(analyzer = 'Chinese', case_sensitive = 'true');
+
+SELECT * FROM test WHERE MATCHES(message, 'hello');
 
 -- SQLNESS ARG restart=true
 SHOW CREATE TABLE test;
