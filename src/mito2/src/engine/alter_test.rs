1d9bf9c0db1fc922462c530a0e126818b87dd5fa
CookiePieWw <profsyb@gmail.com>
2024-11-08 16:47:53 +0800

    test: add test for mito

--- src/mito2/src/engine/alter_test.rs
+++ src/mito2/src/engine/alter_test.rs
@@ -22,7 +22,7 @@ use common_error::ext::ErrorExt;
 use common_error::status_code::StatusCode;
 use common_recordbatch::RecordBatches;
 use datatypes::prelude::ConcreteDataType;
-use datatypes::schema::ColumnSchema;
+use datatypes::schema::{ColumnSchema, FulltextAnalyzer, FulltextOptions};
 use store_api::metadata::ColumnMetadata;
 use store_api::region_engine::{RegionEngine, RegionRole};
 use store_api::region_request::{
@@ -66,6 +66,36 @@ fn add_tag1() -> RegionAlterRequest {
             }],
         },
     }
+}
+
+fn alter_column_fulltext_options() -> RegionAlterRequest {
+    RegionAlterRequest {
+        schema_version: 0,
+        kind: AlterKind::ChangeColumnFulltext {
+            column_name: "tag_0".to_string(),
+            options: FulltextOptions {
+                enable: true,
+                analyzer: FulltextAnalyzer::English,
+                case_sensitive: false,
+            },
+        },
+    }
+}
+
+fn check_region_version(
+    engine: &MitoEngine,
+    region_id: RegionId,
+    last_entry_id: u64,
+    committed_sequence: u64,
+    flushed_entry_id: u64,
+    flushed_sequence: u64,
+) {
+    let region = engine.get_region(region_id).unwrap();
+    let version_data = region.version_control.current();
+    assert_eq!(last_entry_id, version_data.last_entry_id);
+    assert_eq!(committed_sequence, version_data.committed_sequence);
+    assert_eq!(flushed_entry_id, version_data.version.flushed_entry_id);
+    assert_eq!(flushed_sequence, version_data.version.flushed_sequence);
 }
 
 #[tokio::test]
@@ -116,15 +146,7 @@ async fn test_alter_region() {
 |       | 2     | 2.0     | 1970-01-01T00:00:02 |
 +-------+-------+---------+---------------------+";
     scan_check_after_alter(&engine, region_id, expected).await;
-    let check_region = |engine: &MitoEngine| {
-        let region = engine.get_region(region_id).unwrap();
-        let version_data = region.version_control.current();
-        assert_eq!(1, version_data.last_entry_id);
-        assert_eq!(3, version_data.committed_sequence);
-        assert_eq!(1, version_data.version.flushed_entry_id);
-        assert_eq!(3, version_data.version.flushed_sequence);
-    };
-    check_region(&engine);
+    check_region_version(&engine, region_id, 1, 3, 1, 3);
 
     // Reopen region.
     let engine = env.reopen_engine(engine, MitoConfig::default()).await;
@@ -141,7 +163,7 @@ async fn test_alter_region() {
         .await
         .unwrap();
     scan_check_after_alter(&engine, region_id, expected).await;
-    check_region(&engine);
+    check_region_version(&engine, region_id, 1, 3, 1, 3);
 }
 
 /// Build rows with schema (string, f64, ts_millis, string).
@@ -328,12 +350,7 @@ async fn test_alter_region_retry() {
 |       | a     | 1.0     | 1970-01-01T00:00:01 |
 +-------+-------+---------+---------------------+";
     scan_check_after_alter(&engine, region_id, expected).await;
-    let region = engine.get_region(region_id).unwrap();
-    let version_data = region.version_control.current();
-    assert_eq!(1, version_data.last_entry_id);
-    assert_eq!(2, version_data.committed_sequence);
-    assert_eq!(1, version_data.version.flushed_entry_id);
-    assert_eq!(2, version_data.version.flushed_sequence);
+    check_region_version(&engine, region_id, 1, 2, 1, 2);
 }
 
 #[tokio::test]
@@ -437,4 +454,83 @@ async fn test_alter_on_flushing() {
 |       | a     | 1.0     | 1970-01-01T00:00:01 |
 +-------+-------+---------+---------------------+";
     assert_eq!(expected, batches.pretty_print().unwrap());
+}
+
+#[tokio::test]
+async fn test_alter_column_fulltext_options() {
+    common_telemetry::init_default_ut_logging();
+
+    let mut env = TestEnv::new();
+    let listener = Arc::new(AlterFlushListener::default());
+    let engine = env
+        .create_engine_with(MitoConfig::default(), None, Some(listener.clone()))
+        .await;
+
+    let region_id = RegionId::new(1, 1);
+    let request = CreateRequestBuilder::new().build();
+
+    env.get_schema_metadata_manager()
+        .register_region_table_info(
+            region_id.table_id(),
+            "test_table",
+            "test_catalog",
+            "test_schema",
+            None,
+        )
+        .await;
+
+    let column_schemas = rows_schema(&request);
+    let region_dir = request.region_dir.clone();
+    engine
+        .handle_request(region_id, RegionRequest::Create(request))
+        .await
+        .unwrap();
+
+    let rows = Rows {
+        schema: column_schemas,
+        rows: build_rows(0, 3),
+    };
+    put_rows(&engine, region_id, rows).await;
+
+    let request = alter_column_fulltext_options();
+    engine
+        .handle_request(region_id, RegionRequest::Alter(request))
+        .await
+        .unwrap();
+
+    let expect_fulltext_options = FulltextOptions {
+        enable: true,
+        analyzer: FulltextAnalyzer::English,
+        case_sensitive: false,
+    };
+    let current_fulltext_options = engine
+        .get_region(region_id)
+        .unwrap()
+        .metadata()
+        .column_by_name("tag_0")
+        .unwrap()
+        .column_schema
+        .fulltext_options()
+        .unwrap()
+        .unwrap();
+
+    assert_eq!(expect_fulltext_options, current_fulltext_options);
+
+    check_region_version(&engine, region_id, 1, 3, 1, 3);
+
+    // Reopen region.
+    let engine = env.reopen_engine(engine, MitoConfig::default()).await;
+    engine
+        .handle_request(
+            region_id,
+            RegionRequest::Open(RegionOpenRequest {
+                engine: String::new(),
+                region_dir,
+                options: HashMap::default(),
+                skip_wal_replay: false,
+            }),
+        )
+        .await
+        .unwrap();
+    check_region_version(&engine, region_id, 1, 3, 1, 3);
 }
