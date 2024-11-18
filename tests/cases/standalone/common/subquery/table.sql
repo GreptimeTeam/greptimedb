24bcea4f4ce7fdf2eb6d8874c33f043275410df8
CookiePieWw <profsyb@gmail.com>
2024-11-18 16:17:42 +0800

    test: skip unsupported features and add sources

--- tests/cases/standalone/common/subquery/table.sql
+++ tests/cases/standalone/common/subquery/table.sql
@@ -1,15 +1,17 @@
--- aliasing
+-- aliasing, from:
+-- https://github.com/duckdb/duckdb/blob/9196dd9b0a163e6c8aada26218803d04be30c562/test/sql/subquery/table/test_aliasing.test
 CREATE TABLE a(ts TIMESTAMP TIME INDEX, i INTEGER);
 
 insert into a values (1, 42);
 
-select * from (select i as j from a group by j) where j = 42;
+SELECT * FROM (SELECT i AS j FROM a GROUP BY j) WHERE j = 42;
 
-select * from (select i as j from a group by i) where j = 42;
+SELECT * FROM (SELECT i AS j FROM a GROUP BY i) WHERE j = 42;
 
 DROP TABLE a;
 
--- nested table subquery
+-- nested table subquery, from:
+-- https://github.com/duckdb/duckdb/blob/2e4e2913266ddc46c7281d1b992228cb0095954b/test/sql/subquery/table/test_nested_table_subquery.test_slow
 CREATE TABLE test (ts TIMESTAMP TIME INDEX, i INTEGER, j INTEGER);
 
 INSERT INTO test VALUES (0, 3, 4), (1, 4, 5), (2, 5, 6);
@@ -20,10 +22,12 @@ SELECT i FROM (SELECT i + 1 AS i FROM (SELECT i + 1 AS i FROM (SELECT i + 1 AS i
 
 DROP TABLE test;
 
--- subquery union
+-- subquery union, from:
+-- https://github.com/duckdb/duckdb/blob/9196dd9b0a163e6c8aada26218803d04be30c562/test/sql/subquery/table/test_subquery_union.test
 SELECT * FROM (SELECT 42) UNION ALL SELECT * FROM (SELECT 43);
 
--- table subquery
+-- table subquery, from:
+-- https://github.com/duckdb/duckdb/blob/8704c7d0807d6ce1e2ebcdf6398e1b6cc050e507/test/sql/subquery/table/test_table_subquery.test
 CREATE TABLE test (ts TIMESTAMP TIME INDEX, i INTEGER, j INTEGER);
 
 INSERT INTO test VALUES (0, 3, 4), (1, 4, 5), (2, 5, 6);
@@ -36,15 +40,25 @@ SELECT a.i,a.j,b.r,b.j FROM (SELECT i, j FROM test) AS a INNER JOIN (SELECT i+1 
 
 SELECT * FROM (SELECT i, j FROM test) AS a, (SELECT i+1 AS r,j FROM test) AS b, test WHERE a.i=b.r AND test.j=a.i ORDER BY 1;
 
+SELECT sum(x) FROM (SELECT i AS x FROM test GROUP BY i) sq;
+
+SELECT sum(x) FROM (SELECT i+1 AS x FROM test GROUP BY x) sq;
+
 DROP TABLE test;
 
--- test unamed subquery
-SELECT * FROM (SELECT 42);
+-- test unamed subquery, from:
+-- https://github.com/duckdb/duckdb/blob/00a605270719941ca0412ad5d0a14b1bdfbf9eb5/test/sql/subquery/table/test_unnamed_subquery.test
+SELECT a FROM (SELECT 42 a);
 
-SELECT * FROM (SELECT 42), (SELECT 43);
+SELECT * FROM (SELECT 42 a), (SELECT 43 b);
 
 SELECT * FROM (VALUES (42, 43));
 
-SELECT * FROM (SELECT 42), (SELECT 43), (SELECT 44), (SELECT 45);
+SELECT * FROM (SELECT 42 a), (SELECT 43 b), (SELECT 44 c), (SELECT 45 d);
 
-SELECT * FROM (SELECT * FROM (SELECT 42), (SELECT 43)) JOIN (SELECT 44) ON (true) JOIN (SELECT 45) ON (true);
+SELECT * FROM (SELECT * FROM (SELECT 42 a), (SELECT 43 b)) JOIN (SELECT 44 c) ON (true) JOIN (SELECT 45 d) ON (true);
+
+-- skipped, unsupported feature: unnamed_subquery, see also:
+-- https://github.com/GreptimeTeam/greptimedb/issues/5012
+-- SELECT * FROM (SELECT unnamed_subquery.a FROM (SELECT 42 a)), (SELECT unnamed_subquery.b FROM (SELECT 43 b));
+-- SELECT unnamed_subquery.a, unnamed_subquery2.b FROM (SELECT 42 a), (SELECT 43 b);
