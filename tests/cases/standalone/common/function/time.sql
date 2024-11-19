0dd02e93cf005f1c7b424d0a5bcfaaa8e8428d23
dennis zhuang <killme2008@gmail.com>
2024-11-19 15:01:24 +0800

    feat: make greatest supports timestamp and datetime types (#5005)
    * feat: make greatest supports timestamp and datetime types
    
    * chore: style
    
    Co-authored-by: Lei, HUANG <6406592+v0y4g3r@users.noreply.github.com>
    
    * refactor: greatest with gt_time_types macro
    
    ---------
    
    Co-authored-by: Lei, HUANG <6406592+v0y4g3r@users.noreply.github.com>

--- tests/cases/standalone/common/function/time.sql
+++ tests/cases/standalone/common/function/time.sql
@@ -5,3 +5,5 @@ select current_time();
 select GREATEST('1999-01-30', '2023-03-01');
 
 select GREATEST('2000-02-11'::Date, '2020-12-30'::Date);
+
+select GREATEST('2021-07-01 00:00:00'::Timestamp, '2024-07-01 00:00:00'::Timestamp);
