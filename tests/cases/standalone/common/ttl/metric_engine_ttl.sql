1044cd1000918ecf2f65ea6fd407af8d83eea29e
Dennis Zhuang <killme2008@gmail.com>
2024-11-27 20:27:35 +0800

    test: adds sqlness test for TTL

---
+++ tests/cases/standalone/common/ttl/metric_engine_ttl.sql
@@ -0,0 +1,37 @@
+CREATE TABLE phy (ts timestamp time index, val double) engine=metric with ("physical_metric_table" = "", ttl = '1 second');
+
+CREATE TABLE test_ttl (ts timestamp time index, val double, host string primary key) engine = metric with ("on_physical_table" = "phy");
+
+INSERT INTO test_ttl(ts, val, host) VALUES
+       (now(), 1, 'host1'),
+       (now(), 2, 'host2'),
+       (now(), 3, 'host3');
+
+SELECT val, host FROM test_ttl;
+
+-- SQLNESS SLEEP 2s
+ADMIN flush_table('phy');
+
+ADMIN compact_table('phy');
+
+--- should be expired --
+SELECT val, host FROM test_ttl;
+
+ALTER TABLE phy SET ttl = '1 minute';
+
+INSERT INTO test_ttl(ts, val, host) VALUES
+       (now(), 1, 'host1'),
+       (now(), 2, 'host2'),
+       (now(), 3, 'host3');
+
+ADMIN flush_table('phy');
+
+ADMIN compact_table('phy');
+
+--- should not be expired --
+SELECT val, host FROM test_ttl;
+
+
+DROP TABLE test_ttl;
+
+DROP TABLE phy;
