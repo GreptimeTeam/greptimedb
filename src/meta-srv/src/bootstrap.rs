122e7daf89949c329f85abbe6615bb0193cf00d2
WenyXu <wenymedia@gmail.com>
2024-12-02 03:22:23 +0000

    feat: enable compression for metasrv client

--- src/meta-srv/src/bootstrap.rs
+++ src/meta-srv/src/bootstrap.rs
@@ -41,6 +41,7 @@ use tokio::net::TcpListener;
 use tokio::sync::mpsc::{self, Receiver, Sender};
 #[cfg(feature = "pg_kvbackend")]
 use tokio_postgres::NoTls;
+use tonic::codec::CompressionEncoding;
 use tonic::transport::server::{Router, TcpIncoming};
 
 use crate::election::etcd::EtcdElection;
@@ -181,10 +182,34 @@ pub async fn bootstrap_metasrv_with_router(
 pub fn router(metasrv: Arc<Metasrv>) -> Router {
     tonic::transport::Server::builder()
         .accept_http1(true) // for admin services
-        .add_service(HeartbeatServer::from_arc(metasrv.clone()))
-        .add_service(StoreServer::from_arc(metasrv.clone()))
-        .add_service(ClusterServer::from_arc(metasrv.clone()))
-        .add_service(ProcedureServiceServer::from_arc(metasrv.clone()))
+        .add_service(
+            HeartbeatServer::from_arc(metasrv.clone())
+                .accept_compressed(CompressionEncoding::Gzip)
+                .accept_compressed(CompressionEncoding::Zstd)
+                .send_compressed(CompressionEncoding::Gzip)
+                .send_compressed(CompressionEncoding::Zstd),
+        )
+        .add_service(
+            StoreServer::from_arc(metasrv.clone())
+                .accept_compressed(CompressionEncoding::Gzip)
+                .accept_compressed(CompressionEncoding::Zstd)
+                .send_compressed(CompressionEncoding::Gzip)
+                .send_compressed(CompressionEncoding::Zstd),
+        )
+        .add_service(
+            ClusterServer::from_arc(metasrv.clone())
+                .accept_compressed(CompressionEncoding::Gzip)
+                .accept_compressed(CompressionEncoding::Zstd)
+                .send_compressed(CompressionEncoding::Gzip)
+                .send_compressed(CompressionEncoding::Zstd),
+        )
+        .add_service(
+            ProcedureServiceServer::from_arc(metasrv.clone())
+                .accept_compressed(CompressionEncoding::Gzip)
+                .accept_compressed(CompressionEncoding::Zstd)
+                .send_compressed(CompressionEncoding::Gzip)
+                .send_compressed(CompressionEncoding::Zstd),
+        )
         .add_service(admin::make_admin_service(metasrv))
 }
 
