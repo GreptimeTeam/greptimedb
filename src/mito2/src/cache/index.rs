246f49ea867d3a3c7314d3397d685f437c14cc77
CookiePieWw <profsyb@gmail.com>
2024-12-05 16:44:24 +0800

    feat: cache inverted index by page instead of file

--- src/mito2/src/cache/index.rs
+++ src/mito2/src/cache/index.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::ops::Range;
 use std::sync::Arc;
 
 use api::v1::index::InvertedIndexMetas;
@@ -23,6 +24,7 @@ use index::inverted_index::FstMap;
 use prost::Message;
 use snafu::ResultExt;
 
+use super::PAGE_SIZE;
 use crate::metrics::{CACHE_BYTES, CACHE_HIT, CACHE_MISS};
 use crate::sst::file::FileId;
 
@@ -59,25 +61,33 @@ where
         offset: u64,
         size: u32,
     ) -> index::inverted_index::error::Result<Vec<u8>> {
-        let range = offset as usize..(offset + size as u64) as usize;
-        if let Some(cached) = self.cache.get_index(IndexKey {
-            file_id: self.file_id,
-        }) {
-            CACHE_HIT.with_label_values(&[INDEX_CONTENT_TYPE]).inc();
-            Ok(cached[range].to_vec())
-        } else {
-            let mut all_data = Vec::with_capacity(1024 * 1024);
-            self.inner.read_all(&mut all_data).await?;
-            let result = all_data[range].to_vec();
-            self.cache.put_index(
-                IndexKey {
-                    file_id: self.file_id,
-                },
-                Arc::new(all_data),
-            );
-            CACHE_MISS.with_label_values(&[INDEX_CONTENT_TYPE]).inc();
-            Ok(result)
+        let indexes = IndexKey::index(self.file_id, offset, size);
+        let mut data = Vec::with_capacity(size as usize);
+        let first_page_id = indexes[0].page_id;
+        let last_page_id = indexes.last().unwrap().page_id;
+        for index in indexes {
+            if let Some(cached) = self.cache.get_index(index.clone()) {
+                CACHE_HIT.with_label_values(&[INDEX_CONTENT_TYPE]).inc();
+                data.extend_from_slice(&cached);
+            } else {
+                CACHE_MISS.with_label_values(&[INDEX_CONTENT_TYPE]).inc();
+                let buf = self
+                    .inner
+                    .seek_read(index.page_id * (PAGE_SIZE as u64), PAGE_SIZE as u32)
+                    .await?;
+                let first = IndexKey::offset_to_first_range(offset, size);
+                let last = IndexKey::offset_to_last_range(offset, size);
+                if index.page_id == first_page_id {
+                    data.extend_from_slice(&buf[first.clone()]);
+                } else if index.page_id == last_page_id {
+                    data.extend_from_slice(&buf[last.clone()]);
+                } else {
+                    data.extend_from_slice(&buf);
+                }
+                self.cache.put_index(index, Arc::new(buf));
+            }
         }
+        Ok(data)
     }
 }
 
@@ -132,6 +142,42 @@ impl<R: InvertedIndexReader> InvertedIndexReader for CachedInvertedIndexBlobRead
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct IndexKey {
     file_id: FileId,
+    page_id: u64,
+}
+
+impl IndexKey {
+    fn offset_to_page_id(offset: u64) -> u64 {
+        (offset / (PAGE_SIZE as u64)) as u64
+    }
+
+    fn size_to_page_num(size: u32) -> u32 {
+        size / (PAGE_SIZE as u32)
+    }
+
+    /// Ranges of first page.
+    /// For example, if offset is 1000 and size is 2000, then the first page is 1000..4096.
+    fn offset_to_first_range(offset: u64, _size: u32) -> Range<usize> {
+        let first = (offset % (PAGE_SIZE as u64)) as usize..PAGE_SIZE;
+        first
+    }
+
+    /// Ranges of last page.
+    /// For example, if offset is 1000 and size is 2000, then the last page is 0..904.
+    fn offset_to_last_range(_offset: u64, size: u32) -> Range<usize> {
+        let last = 0..(size % (PAGE_SIZE as u32)) as usize;
+        last
+    }
+
+    pub fn index(file_id: FileId, offset: u64, size: u32) -> Vec<Self> {
+        let page_id = Self::offset_to_page_id(offset);
+        let page_num = Self::size_to_page_num(size);
+        (0..page_num)
+            .map(|i| Self {
+                file_id,
+                page_id: page_id + i as u64,
+            })
+            .collect()
+    }
 }
 
 pub type InvertedIndexCacheRef = Arc<InvertedIndexCache>;
@@ -176,11 +222,17 @@ impl InvertedIndexCache {
 
 impl InvertedIndexCache {
     pub fn get_index_metadata(&self, file_id: FileId) -> Option<Arc<InvertedIndexMetas>> {
-        self.index_metadata.get(&IndexKey { file_id })
+        self.index_metadata.get(&IndexKey {
+            file_id,
+            page_id: 0,
+        })
     }
 
     pub fn put_index_metadata(&self, file_id: FileId, metadata: Arc<InvertedIndexMetas>) {
-        let key = IndexKey { file_id };
+        let key = IndexKey {
+            file_id,
+            page_id: 0,
+        };
         CACHE_BYTES
             .with_label_values(&[INDEX_METADATA_TYPE])
             .add(index_metadata_weight(&key, &metadata).into());
