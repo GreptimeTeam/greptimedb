c5186dd7ea6cde2ee89deb52c93b154a53b9a1d5
discord9 <discord9@163.com>
2024-12-05 19:23:24 +0800

    tests: sqlness

--- tests/cases/standalone/common/ttl/ttl_instant.sql
+++ tests/cases/standalone/common/ttl/ttl_instant.sql
@@ -1,4 +1,8 @@
-CREATE TABLE test_ttl(ts TIMESTAMP TIME INDEX, val INT) WITH (ttl = 'instant');
+CREATE TABLE test_ttl(
+       ts TIMESTAMP TIME INDEX,
+       val INT,
+       PRIMARY KEY (`val`)
+) WITH (ttl = 'instant');
 
 SHOW CREATE TABLE test_ttl;
 
@@ -7,23 +11,126 @@ INSERT INTO
 VALUES
        (now(), 1),
        (now(), 2),
-       (now(), 3),
-       (now() + INTERVAL '1 SECOND', 4),
-       (now() - INTERVAL '1 SECOND', 5);
+       (now(), 3);
 
 SELECT
        val
 from
        test_ttl;
 
+-- SQLNESS SLEEP 2s
+ADMIN flush_table('test_ttl');
+
+ADMIN compact_table('test_ttl');
+
+SELECT
+       val
+from
+       test_ttl;
+
+ALTER TABLE test_ttl UNSET 'ttl';
+
 INSERT INTO
        test_ttl
 VALUES
        (now(), 1),
        (now(), 2),
-       (now(), 3),
-       (now() + INTERVAL '1 SECOND', 4),
-       (now() - INTERVAL '1 SECOND', 5);
+       (now(), 3);
+
+SELECT
+       val
+from
+       test_ttl;
+
+DROP TABLE test_ttl;
+
+CREATE TABLE test_ttl(
+       ts TIMESTAMP TIME INDEX,
+       val INT,
+       PRIMARY KEY (`val`)
+) WITH (ttl = '1s');
+
+SHOW CREATE TABLE test_ttl;
+
+INSERT INTO
+       test_ttl
+VALUES
+       (now(), 1),
+       (now(), 2),
+       (now(), 3);
+
+SELECT
+       val
+from
+       test_ttl;
+
+ADMIN flush_table('test_ttl');
+
+ADMIN compact_table('test_ttl');
+
+SELECT
+       val
+from
+       test_ttl;
+
+-- SQLNESS SLEEP 2s
+ADMIN flush_table('test_ttl');
+
+ADMIN compact_table('test_ttl');
+
+SELECT
+       val
+from
+       test_ttl;
+
+ALTER TABLE
+       test_ttl
+SET
+       ttl = '1d';
+
+INSERT INTO
+       test_ttl
+VALUES
+       (now(), 1),
+       (now(), 2),
+       (now(), 3);
+
+SELECT
+       val
+from
+       test_ttl;
+
+ALTER TABLE
+       test_ttl
+SET
+       ttl = 'instant';
+
+ADMIN flush_table('test_ttl');
+
+ADMIN compact_table('test_ttl');
+
+
+SELECT
+       val
+from
+       test_ttl;
+
+ALTER TABLE
+       test_ttl
+SET
+       ttl = '1s';
+
+INSERT INTO
+       test_ttl
+VALUES
+       (now(), 1),
+       (now(), 2),
+       (now(), 3);
+
+SELECT
+       val
+from
+       test_ttl;
 
 -- SQLNESS SLEEP 2s
 ADMIN flush_table('test_ttl');
