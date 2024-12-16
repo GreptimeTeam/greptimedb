81e7c3f9cea28d1bfb67b2939243292181c68d96
Ning Sun <sunning@greptime.com>
2024-12-15 23:49:18 -0800

    chore: add nix-shell development environment

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") {};
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    git
+    clang
+    gcc
+    mold
+    libgit2
+    protobuf
+    (fenix.fromToolchainFile {
+      dir = ./.;
+    })
+    cargo-nextest
+  ];
+
+}
