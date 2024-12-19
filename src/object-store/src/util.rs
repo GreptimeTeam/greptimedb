54186dc6e8e49f99ed5b4bf026e7f59ff4d41436
Ruihang Xia <waynestxia@gmail.com>
2024-12-19 11:42:05 +0800

    feat: bump opendal and switch prometheus layer to the upstream impl (#5179)
    * feat: bump opendal and switch prometheus layer to the upstream impl
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * remove unused files
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * fix tests
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * remove unused things
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * remove root dir on recovering cache
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * filter out non-files entry in test
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    ---------
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/object-store/src/util.rs
+++ src/object-store/src/util.rs
@@ -15,18 +15,11 @@
 use std::fmt::Display;
 
 use common_telemetry::{debug, error, trace};
-use futures::TryStreamExt;
 use opendal::layers::{LoggingInterceptor, LoggingLayer, TracingLayer};
 use opendal::raw::{AccessorInfo, Operation};
-use opendal::{Entry, ErrorKind, Lister};
+use opendal::ErrorKind;
 
-use crate::layers::PrometheusMetricsLayer;
 use crate::ObjectStore;
-
-/// Collect all entries from the [Lister].
-pub async fn collect(stream: Lister) -> Result<Vec<Entry>, opendal::Error> {
-    stream.try_collect::<Vec<_>>().await
-}
 
 /// Join two paths and normalize the output dir.
 ///
@@ -127,26 +120,12 @@ pub fn normalize_path(path: &str) -> String {
     p
 }
 
-// This logical tries to extract parent path from the object storage operation
-// the function also relies on assumption that the region path is built from
-// pattern `<data|index>/catalog/schema/table_id/....`
-//
-// this implementation tries to extract at most 3 levels of parent path
-pub(crate) fn extract_parent_path(path: &str) -> &str {
-    // split the path into `catalog`, `schema` and others
-    path.char_indices()
-        .filter(|&(_, c)| c == '/')
-        // we get the data/catalog/schema from path, split at the 3rd /
-        .nth(2)
-        .map_or(path, |(i, _)| &path[..i])
-}
-
 /// Attaches instrument layers to the object store.
 pub fn with_instrument_layers(object_store: ObjectStore, path_label: bool) -> ObjectStore {
     object_store
         .layer(LoggingLayer::new(DefaultLoggingInterceptor))
         .layer(TracingLayer)
-        .layer(PrometheusMetricsLayer::new(path_label))
+        .layer(crate::layers::build_prometheus_metrics_layer(path_label))
 }
 
 static LOGGING_TARGET: &str = "opendal::services";
@@ -262,29 +241,5 @@ mod tests {
         assert_eq!("abc/def", join_path(" abc", "/def "));
         assert_eq!("/abc", join_path("//", "/abc"));
         assert_eq!("abc/def", join_path("abc/", "//def"));
-    }
-
-    #[test]
-    fn test_path_extraction() {
-        assert_eq!(
-            "data/greptime/public",
-            extract_parent_path("data/greptime/public/1024/1024_0000000000/")
-        );
-
-        assert_eq!(
-            "data/greptime/public",
-            extract_parent_path("data/greptime/public/1/")
-        );
-
-        assert_eq!(
-            "data/greptime/public",
-            extract_parent_path("data/greptime/public")
-        );
-
-        assert_eq!("data/greptime/", extract_parent_path("data/greptime/"));
-
-        assert_eq!("data/", extract_parent_path("data/"));
-
-        assert_eq!("/", extract_parent_path("/"));
     }
 }
