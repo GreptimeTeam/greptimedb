1d28af7c6f6fe18bc3638e30a8a5d81a55b80c31
Ruihang Xia <waynestxia@gmail.com>
2024-12-26 12:07:55 +0800

    feat: add sqlness test for bloom filter index
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ tests/cases/standalone/common/select/skipping_index.sql
@@ -0,0 +1,34 @@
+create table
+    skipping_table (
+        ts timestamp time index,
+        id string skipping index,
+        `name` string skipping index
+        with
+            (granularity = 8192),
+    );
+
+INSERT INTO skipping_table VALUES (1664356800000, 'id1', 'name1');
+
+ADMIN FLUSH_TABLE('skipping_table');
+
+INSERT INTO skipping_table VALUES (1664356801000, 'id2', 'name2');
+
+ADMIN FLUSH_TABLE('skipping_table');
+
+INSERT INTO skipping_table VALUES (1664356802000, 'id3', 'name3');
+
+ADMIN FLUSH_TABLE('skipping_table');
+
+-- SQLNESS REPLACE (-+) -
+-- SQLNESS REPLACE (\s\s+) _
+-- SQLNESS REPLACE (peers.*) REDACTED
+-- SQLNESS REPLACE (metrics.*) REDACTED
+-- SQLNESS REPLACE region=\d+\(\d+,\s+\d+\) region=REDACTED
+EXPLAIN ANALYZE SELECT * FROM skipping_table WHERE id = 'id2' ORDER BY `name`;
+
+-- SQLNESS REPLACE (-+) -
+-- SQLNESS REPLACE (\s\s+) _
+-- SQLNESS REPLACE (peers.*) REDACTED
+-- SQLNESS REPLACE (metrics.*) REDACTED
+-- SQLNESS REPLACE region=\d+\(\d+,\s+\d+\) region=REDACTED
+EXPLAIN ANALYZE SELECT * FROM skipping_table WHERE id = 'id5' ORDER BY `name`;
