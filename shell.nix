63c3139c4b1543949566fda37e4a89daa12aa43a
Ning Sun <sunng@protonmail.com>
2025-01-03 11:13:04 +0800

    ci: update nix setup (#5272)

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
   fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") {};
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
@@ -17,10 +17,12 @@ pkgs.mkShell rec {
     })
     cargo-nextest
     taplo
+    curl
   ];
 
   buildInputs = with pkgs; [
     libgit2
+    libz
   ];
 
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
