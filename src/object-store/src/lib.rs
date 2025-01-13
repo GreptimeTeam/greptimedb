03a144fa56a19b695d237e2aefc60041f0232d9a
yihong <zouzou0208@gmail.com>
2025-01-13 12:08:36 +0800

    chore: drop useless import raw_normalize_path in object-store lib (#5349)
    chore: drop use less import raw_normalize_path in store lib
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- src/object-store/src/lib.rs
+++ src/object-store/src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub use opendal::raw::{normalize_path as raw_normalize_path, Access, HttpClient};
+pub use opendal::raw::{Access, HttpClient};
 pub use opendal::{
     services, Buffer, Builder as ObjectStoreBuilder, Entry, EntryMode, Error, ErrorKind,
     FuturesAsyncReader, FuturesAsyncWriter, Lister, Metakey, Operator as ObjectStore, Reader,
