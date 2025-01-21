e77880f10b1ca4c69fd5f58e2a5d98d8eb528ac1
evenyag <realevenyag@gmail.com>
2025-01-21 17:07:08 +0800

    chore: fix typo

--- src/flow/src/transform/plan.rs
+++ src/flow/src/transform/plan.rs
@@ -83,7 +83,7 @@ impl TypedPlan {
 
         // because this `input.schema` is incorrect for pre-expand substrait plan, so we have to use schema before expand multi-value
         // function to correctly transform it, and late rewrite it
-        // TODO(discord9): this logic is obselete since now expand happens in datafusion optimizer
+        // TODO(discord9): this logic is obsoleted since now expand happens in datafusion optimizer
         let schema_before_expand = {
             let input_schema = input.schema.clone();
             let auto_columns: HashSet<usize> =
