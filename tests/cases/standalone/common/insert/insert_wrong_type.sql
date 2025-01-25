adb5c3743c20a071fa28e19cae18e098c5b94ddb
yihong <zouzou0208@gmail.com>
2025-01-25 16:09:55 +0800

    fix: flush table panic when table has interval column close #3235 (#5422)
    * fix: flash table panic when table has interval column close #3235
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    * Revert "fix: flash table panic when table has interval column close #3235"
    
    This reverts commit ffc63efda39cd6ef525313b60ede061c5ec24b12.
    
    * fix: create table do not support interval type for now close #3235
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    * fix: sqlness
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    * fix: address comments
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    * fix: address comments fix conflict and more tests
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    * fix: address final comments drop useless sqlness tests
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    ---------
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- tests/cases/standalone/common/insert/insert_wrong_type.sql
+++
@@ -1,8 +0,0 @@
--- test for issue #3235
-CREATE TABLE b(i interval, ts timestamp time index);
--- should fail
-INSERT INTO b VALUES ('1 year', 1000);
--- success
-INSERT INTO b VALUES (interval '1 year', 1000);
-
-DROP TABLE b;
