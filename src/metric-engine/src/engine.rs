e7f6c0a453d05835284e4c52df48111f91259439
yihong0618 <zouzou0208@gmail.com>
2025-02-05 17:37:36 +0800

    fix: Delete statement not supported in metric engine close #4649
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- src/metric-engine/src/engine.rs
+++ src/metric-engine/src/engine.rs
@@ -164,7 +164,16 @@ impl RegionEngine for MetricEngine {
             }
             RegionRequest::Flush(req) => self.inner.flush_region(region_id, req).await,
             RegionRequest::Delete(_) | RegionRequest::Truncate(_) => {
-                UnsupportedRegionRequestSnafu { request }.fail()
+                if self.inner.is_physical_region(region_id) {
+                    self.inner
+                        .mito
+                        .handle_request(region_id, request)
+                        .await
+                        .context(error::MitoFlushOperationSnafu)
+                        .map(|response| response.affected_rows)
+                } else {
+                    UnsupportedRegionRequestSnafu { request }.fail()
+                }
             }
             RegionRequest::Catchup(req) => self.inner.catchup_region(region_id, req).await,
         };
