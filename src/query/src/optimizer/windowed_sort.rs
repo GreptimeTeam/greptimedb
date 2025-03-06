bfecdc61284b4f318d36380db35b6d26f3162cc5
discord9 <55937128+discord9@users.noreply.github.com>
2025-03-06 16:05:57 +0800

    fix: window sort not apply when other column alias to time index name (#5634)
    * fix: other col alias to time index column handle
    
    * test: update sqlness
    
    * chore: per review
    
    * test: more sqlness
    
    * test: mv some to optimizer folder
    
    * fix: resolve alias properly
    
    * fix: also retain old name
    
    * chore: remove wrong comment
    
    * chore: fix sqlness
    
    * test: standalone/dist more projection diff

--- src/query/src/optimizer/windowed_sort.rs
+++ src/query/src/optimizer/windowed_sort.rs
@@ -84,13 +84,16 @@ impl WindowedSortPhysicalRule {
                     let Some(scanner_info) = fetch_partition_range(sort_input.clone())? else {
                         return Ok(Transformed::no(plan));
                     };
+                    let input_schema = sort_input.schema();
 
                     if let Some(first_sort_expr) = sort_exec.expr().first()
                         && let Some(column_expr) = first_sort_expr
                             .expr
                             .as_any()
                             .downcast_ref::<PhysicalColumn>()
-                        && scanner_info.time_index.contains(column_expr.name())
+                        && scanner_info
+                            .time_index
+                            .contains(input_schema.field(column_expr.index()).name())
                     {
                     } else {
                         return Ok(Transformed::no(plan));
@@ -157,6 +160,7 @@ struct ScannerInfo {
 fn fetch_partition_range(input: Arc<dyn ExecutionPlan>) -> DataFusionResult<Option<ScannerInfo>> {
     let mut partition_ranges = None;
     let mut time_index = HashSet::new();
+    let mut alias_map = Vec::new();
     let mut tag_columns = None;
     let mut is_batch_coalesced = false;
 
@@ -174,15 +178,16 @@ fn fetch_partition_range(input: Arc<dyn ExecutionPlan>) -> DataFusionResult<Opti
             is_batch_coalesced = true;
         }
 
+        // TODO(discord9): do this in logical plan instead as it's lessy bugy there
         // Collects alias of the time index column.
         if let Some(projection) = plan.as_any().downcast_ref::<ProjectionExec>() {
             for (expr, output_name) in projection.expr() {
                 if let Some(column_expr) = expr.as_any().downcast_ref::<PhysicalColumn>() {
-                    if time_index.contains(column_expr.name()) {
-                        time_index.insert(output_name.clone());
-                    }
+                    alias_map.push((column_expr.name().to_string(), output_name.clone()));
                 }
             }
+            // resolve alias properly
+            time_index = resolve_alias(&alias_map, &time_index);
         }
 
         if let Some(region_scan_exec) = plan.as_any().downcast_ref::<RegionScanExec>() {
@@ -230,4 +235,82 @@ fn remove_repartition(
 
         Ok(Transformed::no(plan))
     })
+}
+
+/// Resolves alias of the time index column.
+///
+/// i.e if a is time index, alias= {a:b, b:c}, then result should be {a, b}(not {a, c}) because projection is not transitive
+/// if alias={b:a} and a is time index, then return empty
+fn resolve_alias(alias_map: &[(String, String)], time_index: &HashSet<String>) -> HashSet<String> {
+    // available old name for time index
+    let mut avail_old_name = time_index.clone();
+    let mut new_time_index = HashSet::new();
+    for (old, new) in alias_map {
+        if time_index.contains(old) {
+            new_time_index.insert(new.clone());
+        } else if time_index.contains(new) && old != new {
+            // other alias to time index, remove the old name
+            avail_old_name.remove(new);
+            continue;
+        }
+    }
+    // add the remaining time index that is not in alias map
+    new_time_index.extend(avail_old_name);
+    new_time_index
+}
+
+#[cfg(test)]
+mod test {
+    use itertools::Itertools;
+
+    use super::*;
+
+    #[test]
+    fn test_alias() {
+        let testcases = [
+            // notice the old name is still in the result
+            (
+                vec![("a", "b"), ("b", "c")],
+                HashSet::from(["a"]),
+                HashSet::from(["a", "b"]),
+            ),
+            // alias swap
+            (
+                vec![("b", "a"), ("a", "b")],
+                HashSet::from(["a"]),
+                HashSet::from(["b"]),
+            ),
+            (
+                vec![("b", "a"), ("b", "c")],
+                HashSet::from(["a"]),
+                HashSet::from([]),
+            ),
+            // not in alias map
+            (
+                vec![("c", "d"), ("d", "c")],
+                HashSet::from(["a"]),
+                HashSet::from(["a"]),
+            ),
+            // no alias
+            (vec![], HashSet::from(["a"]), HashSet::from(["a"])),
+            // empty time index
+            (vec![], HashSet::from([]), HashSet::from([])),
+        ];
+        for (alias_map, time_index, expected) in testcases {
+            let alias_map = alias_map
+                .into_iter()
+                .map(|(k, v)| (k.to_string(), v.to_string()))
+                .collect_vec();
+            let time_index = time_index.into_iter().map(|i| i.to_string()).collect();
+            let expected: HashSet<String> = expected.into_iter().map(|i| i.to_string()).collect();
+
+            assert_eq!(
+                expected,
+                resolve_alias(&alias_map, &time_index),
+                "alias_map={:?}, time_index={:?}",
+                alias_map,
+                time_index
+            );
+        }
+    }
 }
