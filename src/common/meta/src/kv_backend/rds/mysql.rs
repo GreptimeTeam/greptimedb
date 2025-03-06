f8c03d98121b431a01086d449750ebdac1f5722a
CookiePieWw <profsyb@gmail.com>
2025-03-06 18:08:48 +0800

    revert: mysql QAQ

--- src/common/meta/src/kv_backend/rds/mysql.rs
+++ src/common/meta/src/kv_backend/rds/mysql.rs
@@ -153,7 +153,8 @@ impl<'a> MySqlTemplateFactory<'a> {
         MySqlTemplateSet {
             table_name: table_name.to_string(),
             create_table_statement: format!(
-                "CREATE TABLE IF NOT EXISTS {table_name}(k VARBINARY(16384) PRIMARY KEY, v BLOB);",
+                // Cannot be more than 3072 bytes in PRIMARY KEY
+                "CREATE TABLE IF NOT EXISTS {table_name}(k VARBINARY(3072) PRIMARY KEY, v BLOB);",
             ),
             range_template: RangeTemplate {
                 point: format!("SELECT k, v FROM {table_name} WHERE k = ?;"),
