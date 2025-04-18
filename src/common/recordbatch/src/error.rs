a5c443f734af33c9466de73810db02748c4f68d4
Yingwen <realevenyag@gmail.com>
2025-04-18 17:36:28 +0800

    perf: keep compiled regex in SimpleFilterEvaluator to avoid re-compiling (#5919)
    * feat: cache regex in evaluator
    
    * chore: fix warnings
    
    * chore: add reference
    
    * refactor: address CR comments
    
    * Add negative to state
    * Don't create the evaluator if the regex is invalid
    
    * test: add test for maybe_build_regex

--- src/common/recordbatch/src/error.rs
+++ src/common/recordbatch/src/error.rs
@@ -23,7 +23,7 @@ use datatypes::prelude::ConcreteDataType;
 use datatypes::schema::SchemaRef;
 use snafu::{Location, Snafu};
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[derive(Snafu)]
 #[snafu(visibility(pub))]
