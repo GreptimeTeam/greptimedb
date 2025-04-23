a16643065041b760de0e1642023f7adcac12fc8e
evenyag <realevenyag@gmail.com>
2025-04-24 06:08:42 +0800

    chore: fix typos

--- src/mito2/src/read/series_scan.rs
+++ src/mito2/src/read/series_scan.rs
@@ -293,7 +293,7 @@ struct SeriesDistributor {
     /// Senders of all partitions.
     senders: SenderList,
     /// Metrics set to report.
-    /// The distributor report the metrics as an addtional partition.
+    /// The distributor report the metrics as an additional partition.
     /// This may double the scan cost of the [SeriesScan] metrics. We can
     /// get per-partition metrics in verbose mode to see the metrics of the
     /// distributor.
