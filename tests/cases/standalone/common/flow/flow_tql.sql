75512a996ff47408665376c38a0e2e0440557e1f
discord9 <discord9@163.com>
2025-05-08 17:55:04 +0800

    tests: sqlness

---
+++ tests/cases/standalone/common/flow/flow_tql.sql
@@ -0,0 +1,37 @@
+CREATE TABLE http_requests (
+  ts timestamp(3) time index,
+  host STRING,
+  idc STRING,
+  val BIGINT,
+  PRIMARY KEY(host, idc),
+);
+
+CREATE FLOW calc_reqs SINK TO cnt_reqs AS
+TQL EVAL (now() - '1m'::interval, now(), '5s') count_values("status_code", http_requests);
+
+INSERT INTO TABLE http_requests VALUES
+    (now() - '15s'::interval, 'host1', 'idc1', 200),
+    (now() - '15s'::interval, 'host2', 'idc1', 200),
+    (now() - '15s'::interval, 'host3', 'idc2', 200),
+    (now() - '15s'::interval, 'host4', 'idc2', 401),
+    (now() - '10s'::interval, 'host1', 'idc1', 404),
+    (now() - '10s'::interval, 'host2', 'idc1', 401),
+    (now() - '10s'::interval, 'host3', 'idc2', 404),
+    (now() - '10s'::interval, 'host4', 'idc2', 500),
+    (now() - '5s'::interval, 'host1', 'idc1', 200),
+    (now() - '5s'::interval, 'host2', 'idc1', 200),
+    (now() - '5s'::interval, 'host3', 'idc2', 201),
+    (now() - '5s'::interval, 'host4', 'idc2', 201),
+    (now(), 'host1', 'idc1', 500),
+    (now(), 'host2', 'idc1', 500),
+    (now(), 'host3', 'idc2', 500),
+    (now(), 'host4', 'idc2', 500);
+
+-- SQLNESS REPLACE (ADMIN\sFLUSH_FLOW\('\w+'\)\s+\|\n\+-+\+\n\|\s+)[0-9]+\s+\| $1 FLOW_FLUSHED  |
+ADMIN FLUSH_FLOW('calc_reqs');
+
+SELECT "count(http_requests.val)" as cnt, status_code FROM cnt_reqs ORDER BY status_code, cnt DESC;
+
+DROP FLOW calc_reqs;
+DROP TABLE http_requests;
+DROP TABLE cnt_reqs;
