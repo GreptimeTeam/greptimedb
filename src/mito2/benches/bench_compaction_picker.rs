4051cc09371f6674f1ea322bb27b003921c86cbe
Lei, HUANG <lhuang@greptime.com>
2025-05-19 02:41:59 +0000

    fix license header

--- src/mito2/benches/bench_compaction_picker.rs
+++ src/mito2/benches/bench_compaction_picker.rs
@@ -1,3 +1,17 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use mito2::compaction::run::{
     find_overlapping_items, find_sorted_runs, merge_seq_files, reduce_runs, Item, Ranged,
