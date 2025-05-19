b4a6eeea5601b582ef81e7f569c91a25ed40c8e0
shuiyisong <xixing.sys@gmail.com>
2025-05-19 17:32:11 +0800

    chore: update nix sha

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         lib = nixpkgs.lib;
         rustToolchain = fenix.packages.${system}.fromToolchainName {
           name = (lib.importTOML ./rust-toolchain.toml).toolchain.channel;
-          sha256 = "sha256-arzEYlWLGGYeOhECHpBxQd2joZ4rPKV3qLNnZ+eql6A=";
+          sha256 = "sha256-tJJr8oqX3YD+ohhPK7jlt/7kvKBnBqJVjYtoFr520d4=";
         };
       in
       {
