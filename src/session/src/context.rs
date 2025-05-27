f1eb74fd2ce8b4d8ec26fae0ddd2d43996fe7683
shuiyisong <xixing.sys@gmail.com>
2025-05-27 19:07:24 +0800

    chore: remove unnecessory clone fn

--- src/session/src/context.rs
+++ src/session/src/context.rs
@@ -306,10 +306,6 @@ impl QueryContext {
         self.extensions.clone()
     }
 
-    pub fn clone_inner(&self) -> QueryContext {
-        self.clone()
-    }
-
     /// Default to double quote and fallback to back quote
     pub fn quote_style(&self) -> char {
         if self.sql_dialect().is_delimited_identifier_start('"') {
