4dd1f237b08a374ca1a6c733b2b7921d9e76de2a
evenyag <realevenyag@gmail.com>
2025-07-01 23:02:06 +0800

    chore: log entries in test
    Signed-off-by: evenyag <realevenyag@gmail.com>

--- src/mito2/src/engine/drop_test.rs
+++ src/mito2/src/engine/drop_test.rs
@@ -186,6 +186,25 @@ async fn test_engine_drop_region_for_custom_store() {
         .access_layer
         .build_region_dir(custom_region_id);
 
+    common_telemetry::info!(
+        "global_region_dir: {global_region_dir}, custom_region_dir: {custom_region_dir}"
+    );
+
+    let entries = object_store_manager
+        .find("Gcs")
+        .unwrap()
+        .list(&custom_region_dir)
+        .await
+        .unwrap();
+    common_telemetry::info!("Before drop, Gcs entries: {:?}", entries);
+    let entries = object_store_manager
+        .find("default")
+        .unwrap()
+        .list(&global_region_dir)
+        .await
+        .unwrap();
+    common_telemetry::info!("Before drop,default entries: {:?}", entries);
+
     // Both these regions should exist before dropping the custom region.
     assert!(object_store_manager
         .find("Gcs")
@@ -212,6 +231,21 @@ async fn test_engine_drop_region_for_custom_store() {
 
     // Wait for drop task.
     listener.wait().await;
+
+    let entries = object_store_manager
+        .find("Gcs")
+        .unwrap()
+        .list(&custom_region_dir)
+        .await
+        .unwrap();
+    common_telemetry::info!("After drop, Gcs entries: {:?}", entries);
+    let entries = object_store_manager
+        .find("default")
+        .unwrap()
+        .list(&global_region_dir)
+        .await
+        .unwrap();
+    common_telemetry::info!("After drop,default entries: {:?}", entries);
 
     assert!(!object_store_manager
         .find("Gcs")
