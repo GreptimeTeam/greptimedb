cb46301e80482699a52f5ca21f0ab8028d0ec33f
discord9 <discord9@163.com>
2025-07-07 16:54:17 +0800

    fix: test sum refactor&fix wrong state types
    Signed-off-by: discord9 <discord9@163.com>

--- src/common/function/src/aggrs/aggr_wrapper.rs
+++ src/common/function/src/aggrs/aggr_wrapper.rs
@@ -191,6 +191,11 @@ impl AggregateUDFImpl for StateWrapper {
     fn signature(&self) -> &Signature {
         self.inner.signature()
     }
+
+    /// Coerce types also do nothing, as optimzer should be able to already make struct types
+    fn coerce_types(&self, arg_types: &[DataType]) -> datafusion_common::Result<Vec<DataType>> {
+        self.inner.coerce_types(arg_types)
+    }
 }
 
 /// The wrapper's input is the same as the original aggregate function's input,
@@ -263,11 +268,28 @@ impl StateTypeLookup {
         &self,
         state_types: &[DataType],
     ) -> datafusion_common::Result<Option<Vec<DataType>>> {
+        let state_types = if state_types.len() == 1 && matches!(state_types[0], DataType::Struct(_))
+        {
+            // unpack single struct type
+            let struct_type = state_types[0].clone();
+            if let DataType::Struct(struct_ty) = struct_type {
+                let state_types = struct_ty
+                    .iter()
+                    .map(|f| f.data_type().clone())
+                    .collect::<Vec<_>>();
+                state_types
+            } else {
+                state_types.to_vec()
+            }
+        } else {
+            state_types.to_vec()
+        };
+
         let is_decimal =
             |ty: &DataType| matches!(ty, DataType::Decimal128(_, _) | DataType::Decimal256(_, _));
         let need_change_ty = state_types.iter().any(is_decimal);
         if !need_change_ty {
-            return Ok(self.mapping.get(state_types).cloned());
+            return Ok(self.mapping.get(&state_types).cloned());
         }
 
         for (k, v) in self.mapping.iter() {
@@ -536,6 +558,20 @@ impl MergeWrapper {
     pub fn inner(&self) -> &AggregateUDF {
         &self.inner
     }
+
+    fn get_old_input_type(
+        &self,
+        state_types: &[DataType],
+    ) -> datafusion_common::Result<Vec<DataType>> {
+        self.state_to_input_types
+            .get_input_types(state_types)?
+            .ok_or_else(|| {
+                datafusion_common::DataFusionError::Internal(format!(
+                    "No input types found for state fields: {:?}",
+                    state_types
+                ))
+            })
+    }
 }
 
 impl AggregateUDFImpl for MergeWrapper {
@@ -548,15 +584,7 @@ impl AggregateUDFImpl for MergeWrapper {
             .iter()
             .map(|e| e.data_type(acc_args.schema))
             .collect::<datafusion_common::Result<Vec<_>>>()?;
-        let input_types = self
-            .state_to_input_types
-            .get_input_types(&state_types)?
-            .ok_or_else(|| {
-                datafusion_common::DataFusionError::Internal(format!(
-                    "No input types found for state fields: {:?}",
-                    state_types
-                ))
-            })?;
+        let input_types = self.get_old_input_type(&state_types)?;
         let input_schema = arrow_schema::Schema::new(
             input_types
                 .iter()
@@ -617,6 +645,13 @@ impl AggregateUDFImpl for MergeWrapper {
 
     /// Coerce types also do nothing, as optimzer should be able to already make struct types
     fn coerce_types(&self, arg_types: &[DataType]) -> datafusion_common::Result<Vec<DataType>> {
+        // just check if the arg_types are only one and is struct array
+        if arg_types.len() != 1 || !matches!(arg_types.first(), Some(DataType::Struct(_))) {
+            return Err(datafusion_common::DataFusionError::Internal(format!(
+                "Expected one struct type as input, got: {:?}",
+                arg_types
+            )));
+        }
         Ok(arg_types.to_vec())
     }
 
@@ -625,7 +660,10 @@ impl AggregateUDFImpl for MergeWrapper {
         &self,
         args: datafusion_expr::function::StateFieldsArgs,
     ) -> datafusion_common::Result<Vec<Field>> {
-        let old_return_type = self.inner.return_type(args.input_types)?;
+        // current input types is actually the state fields of the original aggregate function,
+        let input_types = self.get_old_input_type(args.input_types)?;
+        let old_return_type = self.inner.return_type(&input_types)?;
+        // reconstruct `StateFieldsArgs` with the original aggregate function's name and return type.
         let state_fields_args = StateFieldsArgs {
             name: self.inner().name(),
             input_types: args.input_types,
@@ -690,18 +728,250 @@ impl Accumulator for MergeAccum {
 
 #[cfg(test)]
 mod tests {
+    use std::any::Any;
+    use std::pin::Pin;
     use std::sync::Arc;
+    use std::task::{Context, Poll};
 
     use arrow::array::{Float64Array, Int64Array, UInt64Array};
+    use arrow::record_batch::RecordBatch;
+    use arrow_schema::SchemaRef;
+    use datafusion::catalog::{Session, TableProvider};
+    use datafusion::datasource::DefaultTableSource;
+    use datafusion::execution::{RecordBatchStream, SendableRecordBatchStream, TaskContext};
+    use datafusion::physical_plan::aggregates::AggregateExec;
+    use datafusion::physical_plan::execution_plan::{Boundedness, EmissionType};
+    use datafusion::physical_plan::{DisplayAs, DisplayFormatType, ExecutionPlan, PlanProperties};
+    use datafusion::physical_planner::{DefaultPhysicalPlanner, PhysicalPlanner};
+    use datafusion::prelude::SessionContext;
+    use datafusion_common::{Column, DFSchema, TableReference};
+    use datafusion_expr::expr::AggregateFunction;
     use datafusion_expr::function::AccumulatorArgs;
-    use datafusion_expr::Signature;
-    use datafusion_physical_expr::LexOrdering;
+    use datafusion_expr::{Aggregate, Expr, LogicalPlan, Signature, TableScan};
+    use datafusion_physical_expr::{EquivalenceProperties, LexOrdering, Partitioning};
+    use futures::Stream;
     use pretty_assertions::assert_eq;
 
     use super::*;
 
-    #[test]
-    fn test_sum_udaf() {
+    #[derive(Debug)]
+    pub struct MockInputExec {
+        input: Vec<RecordBatch>,
+        schema: SchemaRef,
+        properties: PlanProperties,
+    }
+
+    impl MockInputExec {
+        pub fn new(input: Vec<RecordBatch>, schema: SchemaRef) -> Self {
+            Self {
+                properties: PlanProperties::new(
+                    EquivalenceProperties::new(schema.clone()),
+                    Partitioning::UnknownPartitioning(1),
+                    EmissionType::Incremental,
+                    Boundedness::Bounded,
+                ),
+                input,
+                schema,
+            }
+        }
+    }
+
+    impl DisplayAs for MockInputExec {
+        fn fmt_as(&self, _t: DisplayFormatType, _f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            unimplemented!()
+        }
+    }
+
+    impl ExecutionPlan for MockInputExec {
+        fn name(&self) -> &str {
+            "MockInputExec"
+        }
+
+        fn as_any(&self) -> &dyn Any {
+            self
+        }
+
+        fn properties(&self) -> &PlanProperties {
+            &self.properties
+        }
+
+        fn children(&self) -> Vec<&Arc<dyn ExecutionPlan>> {
+            vec![]
+        }
+
+        fn with_new_children(
+            self: Arc<Self>,
+            _children: Vec<Arc<dyn ExecutionPlan>>,
+        ) -> datafusion_common::Result<Arc<dyn ExecutionPlan>> {
+            Ok(self)
+        }
+
+        fn execute(
+            &self,
+            _partition: usize,
+            _context: Arc<TaskContext>,
+        ) -> datafusion_common::Result<SendableRecordBatchStream> {
+            let stream = MockStream {
+                stream: self.input.clone(),
+                schema: self.schema.clone(),
+                idx: 0,
+            };
+            Ok(Box::pin(stream))
+        }
+    }
+
+    struct MockStream {
+        stream: Vec<RecordBatch>,
+        schema: SchemaRef,
+        idx: usize,
+    }
+
+    impl Stream for MockStream {
+        type Item = datafusion_common::Result<RecordBatch>;
+        fn poll_next(
+            mut self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+        ) -> Poll<Option<datafusion_common::Result<RecordBatch>>> {
+            if self.idx < self.stream.len() {
+                let ret = self.stream[self.idx].clone();
+                self.idx += 1;
+                Poll::Ready(Some(Ok(ret)))
+            } else {
+                Poll::Ready(None)
+            }
+        }
+    }
+
+    impl RecordBatchStream for MockStream {
+        fn schema(&self) -> SchemaRef {
+            self.schema.clone()
+        }
+    }
+
+    #[derive(Debug)]
+    struct DummyTableProvider {
+        schema: Arc<arrow_schema::Schema>,
+    }
+
+    impl DummyTableProvider {
+        pub fn new(schema: Arc<arrow_schema::Schema>) -> Self {
+            Self { schema }
+        }
+    }
+
+    impl Default for DummyTableProvider {
+        fn default() -> Self {
+            Self {
+                schema: Arc::new(arrow_schema::Schema::new(vec![Field::new(
+                    "number",
+                    DataType::Int64,
+                    true,
+                )])),
+            }
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl TableProvider for DummyTableProvider {
+        fn as_any(&self) -> &dyn std::any::Any {
+            self
+        }
+
+        fn schema(&self) -> Arc<arrow_schema::Schema> {
+            self.schema.clone()
+        }
+
+        fn table_type(&self) -> datafusion_expr::TableType {
+            datafusion_expr::TableType::Base
+        }
+
+        async fn scan(
+            &self,
+            _state: &dyn Session,
+            _projection: Option<&Vec<usize>>,
+            _filters: &[Expr],
+            _limit: Option<usize>,
+        ) -> datafusion::error::Result<Arc<dyn ExecutionPlan>> {
+            Ok(Arc::new(MockInputExec::new(vec![], self.schema.clone())))
+        }
+    }
+
+    fn dummy_table_scan() -> LogicalPlan {
+        let table_provider = Arc::new(DummyTableProvider::default());
+        let table_source = DefaultTableSource::new(table_provider);
+        LogicalPlan::TableScan(
+            TableScan::try_new(
+                TableReference::bare("Number"),
+                Arc::new(table_source),
+                None,
+                vec![],
+                None,
+            )
+            .unwrap(),
+        )
+    }
+
+    fn dummy_table_scan_with_schema(
+        schema: Arc<DFSchema>,
+        table_name: impl Into<TableReference>,
+    ) -> LogicalPlan {
+        let table_provider = Arc::new(DummyTableProvider::new(schema.inner().clone()));
+        let table_source = DefaultTableSource::new(table_provider);
+        LogicalPlan::TableScan(
+            TableScan::try_new(table_name, Arc::new(table_source), None, vec![], None).unwrap(),
+        )
+    }
+
+    fn dummy_aggr_state_plan(state_udf: Arc<AggregateUDF>) -> LogicalPlan {
+        let table_scan = dummy_table_scan();
+        let aggr_func = AggregateFunction::new_udf(
+            state_udf,
+            vec![Expr::Column(Column::new_unqualified("number"))],
+            false,
+            None,
+            None,
+            None,
+        );
+        LogicalPlan::Aggregate(
+            Aggregate::try_new(
+                Arc::new(table_scan),
+                vec![],
+                vec![Expr::AggregateFunction(aggr_func)],
+            )
+            .unwrap(),
+        )
+    }
+
+    fn dummy_aggr_merge_plan(input_schema: Arc<DFSchema>, func: Arc<AggregateUDF>) -> LogicalPlan {
+        let table = DummyTableProvider::new(input_schema.inner().clone());
+        let table_source = DefaultTableSource::new(Arc::new(table));
+        let table_scan = LogicalPlan::TableScan(
+            TableScan::try_new(
+                TableReference::bare("MergeState"),
+                Arc::new(table_source),
+                None,
+                vec![],
+                None,
+            )
+            .unwrap(),
+        );
+        let col_name = input_schema.field(0).name().clone();
+        let column = Expr::Column(Column::new_unqualified(&col_name));
+        let aggr_func = AggregateFunction::new_udf(func, vec![column], false, None, None, None);
+        LogicalPlan::Aggregate(
+            Aggregate::try_new(
+                Arc::new(table_scan),
+                vec![],
+                vec![Expr::AggregateFunction(aggr_func)],
+            )
+            .unwrap(),
+        )
+    }
+
+    #[tokio::test]
+    async fn test_sum_udaf() {
+        let ctx = SessionContext::new();
+
         let sum = datafusion::functions_aggregate::sum::sum_udaf();
         let sum = (*sum).clone();
 
@@ -741,24 +1011,76 @@ mod tests {
         };
         assert_eq!(wrapper, expected);
 
+        // create the child logical plan for the state function, which usually should be running on datanode
+        let aggr_state_plan =
+            dummy_aggr_state_plan(Arc::new(wrapper.state_function().clone().into()));
+        let expected_plan = LogicalPlan::Aggregate(
+            Aggregate::try_new(
+                Arc::new(dummy_table_scan()),
+                vec![],
+                vec![Expr::AggregateFunction(AggregateFunction::new_udf(
+                    Arc::new(wrapper.state_function().clone().into()),
+                    vec![Expr::Column(Column::new_unqualified("number"))],
+                    false,
+                    None,
+                    None,
+                    None,
+                ))],
+            )
+            .unwrap(),
+        );
+        assert_eq!(aggr_state_plan, expected_plan);
+        assert_eq!(
+            aggr_state_plan.schema().as_arrow(),
+            &arrow_schema::Schema::new(vec![Field::new(
+                "__sum_state(number)",
+                DataType::Struct(vec![Field::new("sum[sum]", DataType::Int64, true)].into()),
+                true,
+            )])
+        );
+
+        // the state schema should be the input schema of the merge function
+        let state_schema = aggr_state_plan.schema().clone();
+
+        let expected_merge_plan = LogicalPlan::Aggregate(
+            Aggregate::try_new(
+                Arc::new(dummy_table_scan_with_schema(
+                    state_schema.clone(),
+                    TableReference::bare("MergeState"),
+                )),
+                vec![],
+                vec![Expr::AggregateFunction(AggregateFunction::new_udf(
+                    Arc::new(wrapper.merge_function().clone().into()),
+                    vec![Expr::Column(Column::new_unqualified("__sum_state(number)"))],
+                    false,
+                    None,
+                    None,
+                    None,
+                ))],
+            )
+            .unwrap(),
+        );
+
+        let aggr_merge_plan = dummy_aggr_merge_plan(
+            state_schema,
+            Arc::new(wrapper.merge_function().clone().into()),
+        );
+        assert_eq!(aggr_merge_plan, expected_merge_plan);
+
         // evaluate the state function
         let input = Int64Array::from(vec![Some(1), Some(2), None, Some(3)]);
         let values = vec![Arc::new(input) as arrow::array::ArrayRef];
 
-        let state_func = wrapper.state_function();
-        let accum_args = AccumulatorArgs {
-            return_type: &sum.return_type(&[DataType::Int64]).unwrap(),
-            schema: &arrow_schema::Schema::new(vec![Field::new("col", DataType::Int64, true)]),
-            ignore_nulls: false,
-            ordering_req: LexOrdering::empty(),
-            is_reversed: false,
-            name: state_func.name(),
-            is_distinct: false,
-            exprs: &[Arc::new(
-                datafusion::physical_expr::expressions::Column::new("col", 0),
-            )],
-        };
-        let mut state_accum = state_func.accumulator(accum_args).unwrap();
+        let phy_aggr_state_plan = DefaultPhysicalPlanner::default()
+            .create_physical_plan(&aggr_state_plan, &ctx.state())
+            .await
+            .unwrap();
+        let aggr_exec = phy_aggr_state_plan
+            .as_any()
+            .downcast_ref::<AggregateExec>()
+            .unwrap();
+        let aggr_func_expr = &aggr_exec.aggr_expr()[0];
+        let mut state_accum = aggr_func_expr.create_accumulator().unwrap();
 
         state_accum.update_batch(&values).unwrap();
         let state = state_accum.state().unwrap();
@@ -788,24 +1110,17 @@ mod tests {
         )
         .unwrap();
 
-        let merge_func = wrapper.merge_function();
-        let merge_accum_args = AccumulatorArgs {
-            return_type: &sum.return_type(&[DataType::Int64]).unwrap(),
-            schema: &arrow_schema::Schema::new(vec![Field::new(
-                "original_input[0]",
-                DataType::Int64,
-                true,
-            )]),
-            ignore_nulls: false,
-            ordering_req: LexOrdering::empty(),
-            is_reversed: false,
-            name: state_func.name(),
-            is_distinct: false,
-            exprs: &[Arc::new(
-                datafusion::physical_expr::expressions::Column::new("original_input[0]", 0),
-            )],
-        };
-        let mut merge_accum = merge_func.accumulator(merge_accum_args).unwrap();
+        let phy_aggr_merge_plan = DefaultPhysicalPlanner::default()
+            .create_physical_plan(&aggr_merge_plan, &ctx.state())
+            .await
+            .unwrap();
+        let aggr_exec = phy_aggr_merge_plan
+            .as_any()
+            .downcast_ref::<AggregateExec>()
+            .unwrap();
+        let aggr_func_expr = &aggr_exec.aggr_expr()[0];
+        let mut merge_accum = aggr_func_expr.create_accumulator().unwrap();
+
         merge_accum
             .update_batch(&[Arc::new(merge_input_struct_arr)])
             .unwrap();
