939260e9026abf1f5850ce4b0b871700b968410f
Dennis Zhuang <killme2008@gmail.com>
2025-07-15 22:58:00 +0800

    fix: license header and assertion
    Signed-off-by: Dennis Zhuang <killme2008@gmail.com>

--- src/servers/src/http/result/null_result.rs
+++ src/servers/src/http/result/null_result.rs
@@ -1,3 +1,17 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::fmt::Write;
 
 use axum::http::{header, HeaderValue};
@@ -144,6 +158,6 @@ mod tests {
         let body_bytes = to_bytes(response.into_body(), 1024).await.unwrap();
         let body = String::from_utf8(body_bytes.to_vec()).unwrap();
         assert!(body.contains("42 rows in set."));
-        assert!(body.contains("1234 ms."));
+        assert!(body.contains("Elapsed: 1234 ms."));
     }
 }
