1ab42d607ffd061e4363d7ba5fafe552cdf983bf
discord9 <discord9@163.com>
2025-07-16 21:20:00 +0800

    test: bugged case
    Signed-off-by: discord9 <discord9@163.com>

---
+++ tests/cases/distributed/explain/step_aggr_advance.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS aggr_optimize_not (
+  a STRING NULL,
+  b STRING NULL,
+  c STRING NULL,
+  d STRING NULL,
+  greptime_timestamp TIMESTAMP(3) NOT NULL,
+  greptime_value DOUBLE NULL,
+  TIME INDEX (greptime_timestamp),
+  PRIMARY KEY (a, b, c, d)
+)
+PARTITION ON COLUMNS (a, b, c) (
+      a < 'b',
+      a >= 'b',
+);
+
+-- This query shouldn't push down aggregation even if group by columns are partitioned.
+-- because sort is already pushed down.
+-- If it does, it will cause a wrong result.
+tql eval (1752591864, 1752592164, '30s') max by (a, b, c) (max_over_time(aggr_optimize_not[2m]));
+
+-- explain at 0s, 5s and 10s. No point at 0s.
+-- SQLNESS REPLACE (RoundRobinBatch.*) REDACTED
+-- SQLNESS REPLACE (peers.*) REDACTED
+tql explain (1752591864, 1752592164, '30s') max by (a, b, c) (max_over_time(aggr_optimize_not[2m]));
+
+-- SQLNESS REPLACE (metrics.*) REDACTED
+-- SQLNESS REPLACE (RoundRobinBatch.*) REDACTED
+-- SQLNESS REPLACE (Hash.*) REDACTED
+-- SQLNESS REPLACE (-+) -
+-- SQLNESS REPLACE (\s\s+) _
+-- SQLNESS REPLACE (peers.*) REDACTED
+-- SQLNESS REPLACE region=\d+\(\d+,\s+\d+\) region=REDACTED
+tql analyze (1752591864, 1752592164, '30s') max by (a, b, c) (max_over_time(aggr_optimize_not[2m]));
+
+-- TODO(discord9): more cases for aggr push down interacting with partitioning&tql
