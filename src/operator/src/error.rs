f10a61b30756cec0644658fac79ad6ec9c2788ba
Zhenchi <zhongzc_arch@outlook.com>
2025-07-23 07:57:33 +0000

    fix snafu
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- src/operator/src/error.rs
+++ src/operator/src/error.rs
@@ -419,13 +419,6 @@ pub enum Error {
         source: datatypes::error::Error,
     },
 
-    #[snafu(display("Failed to deserialize partition in meta to partition def"))]
-    DeserializePartition {
-        #[snafu(implicit)]
-        location: Location,
-        source: partition::error::Error,
-    },
-
     #[snafu(display("Failed to describe schema for given statement"))]
     DescribeStatement {
         #[snafu(implicit)]
@@ -955,8 +948,7 @@ impl ErrorExt for Error {
             | Error::DescribeStatement { source, .. } => source.status_code(),
             Error::AlterExprToRequest { source, .. } => source.status_code(),
             Error::External { source, .. } => source.status_code(),
-            Error::DeserializePartition { source, .. }
-            | Error::FindTablePartitionRule { source, .. }
+            Error::FindTablePartitionRule { source, .. }
             | Error::SplitInsert { source, .. }
             | Error::SplitDelete { source, .. }
             | Error::FindRegionLeader { source, .. } => source.status_code(),
