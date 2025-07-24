b01be5efc5760e093753893fda5a90b6298c9b03
Lanqing Yang <lanqingy93@gmail.com>
2025-07-24 02:11:04 -0700

    feat: move metasrv admin to http server while keep tonic for backward compatibility (#6466)
    * feat: move metasrv admin to http server while keep tonic for backward compatibility
    
    Signed-off-by: lyang24 <lanqingy93@gmail.com>
    
    * refactor with nest method
    
    Signed-off-by: lyang24 <lanqingy93@gmail.com>
    
    ---------
    
    Signed-off-by: lyang24 <lanqingy93@gmail.com>
    Co-authored-by: lyang24 <lanqingy@usc.edu>

--- src/meta-srv/src/bootstrap.rs
+++ src/meta-srv/src/bootstrap.rs
@@ -72,6 +72,7 @@ use crate::selector::round_robin::RoundRobinSelector;
 use crate::selector::weight_compute::RegionNumsBasedWeightCompute;
 use crate::selector::SelectorType;
 use crate::service::admin;
+use crate::service::admin::admin_axum_router;
 use crate::{error, Result};
 
 pub struct MetasrvInstance {
@@ -98,12 +99,16 @@ impl MetasrvInstance {
     pub async fn new(metasrv: Metasrv) -> Result<MetasrvInstance> {
         let opts = metasrv.options().clone();
         let plugins = metasrv.plugins().clone();
+        let metasrv = Arc::new(metasrv);
 
-        let builder = HttpServerBuilder::new(opts.http.clone())
+        // Wire up the admin_axum_router as an extra router
+        let extra_routers = admin_axum_router(metasrv.clone());
+
+        let mut builder = HttpServerBuilder::new(opts.http.clone())
             .with_metrics_handler(MetricsHandler)
             .with_greptime_config_options(opts.to_toml().context(error::TomlFormatSnafu)?);
+        builder = builder.with_extra_router(extra_routers);
 
-        let metasrv = Arc::new(metasrv);
         // put metasrv into plugins for later use
         plugins.insert::<Arc<Metasrv>>(metasrv.clone());
         let export_metrics_task = ExportMetricsTask::try_new(&opts.export_metrics, Some(&plugins))
@@ -150,6 +155,7 @@ impl MetasrvInstance {
 
         self.signal_sender = Some(tx);
 
+        // Start gRPC server with admin services for backward compatibility
         let mut router = router(self.metasrv.clone());
         if let Some(configurator) = self.metasrv.plugins().get::<ConfiguratorRef>() {
             router = configurator.config_grpc(router);
