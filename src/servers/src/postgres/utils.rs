864625e9fea68d632c74f89ca6434f252b563b9d
yihong0618 <zouzou0208@gmail.com>
2025-07-25 17:39:53 +0800

    fix: headers ?
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- src/servers/src/postgres/utils.rs
+++ src/servers/src/postgres/utils.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use common_error::ext::ErrorExt;
 use common_telemetry::{debug, error};
 use pgwire::error::PgWireError;
