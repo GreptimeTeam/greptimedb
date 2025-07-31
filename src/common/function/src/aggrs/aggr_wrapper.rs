9e8844070a9dd7aade69cf41d2716e95d454857b
discord9 <discord9@163.com>
2025-07-31 19:10:26 +0800

    chore: per review
    Signed-off-by: discord9 <discord9@163.com>

--- src/common/function/src/aggrs/aggr_wrapper.rs
+++ src/common/function/src/aggrs/aggr_wrapper.rs
@@ -105,12 +105,9 @@ impl StateMergeHelper {
 
         let state_func = supported.into_iter().filter_map(|f| {
             StateWrapper::new((*f).clone())
-                .inspect_err(|e| {
-                    common_telemetry::error!(
-                        "Failed to register state function for {:?} with error={e:?}",
-                        f
-                    )
-                })
+                .inspect_err(
+                    |e| common_telemetry::error!(e;"Failed to register state function for {:?}", f),
+                )
                 .ok()
                 .map(AggregateUDF::new_from_impl)
         });
