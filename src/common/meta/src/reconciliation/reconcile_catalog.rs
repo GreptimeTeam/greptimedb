452c165ba0e0a2faee2b2635c1c92d3118578b9e
Weny Xu <wenymedia@gmail.com>
2025-08-01 19:03:00 +0800

    feat: introduce reconcile catalog procedure (#6613)
    Signed-off-by: WenyXu <wenymedia@gmail.com>

--- src/common/meta/src/reconciliation/reconcile_catalog.rs
+++ src/common/meta/src/reconciliation/reconcile_catalog.rs
@@ -12,18 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub(crate) mod end;
-pub(crate) mod reconcile_logical_tables;
-pub(crate) mod reconcile_tables;
-pub(crate) mod start;
-pub(crate) mod utils;
-
 use std::any::Any;
-use std::collections::HashMap;
 use std::fmt::Debug;
 
-use async_trait::async_trait;
-use common_procedure::error::{FromJsonSnafu, ToJsonSnafu};
+use common_procedure::error::FromJsonSnafu;
 use common_procedure::{
     Context as ProcedureContext, Error as ProcedureError, LockKey, Procedure, ProcedureId,
     Result as ProcedureResult, Status,
@@ -31,23 +23,22 @@ use common_procedure::{
 use futures::stream::BoxStream;
 use serde::{Deserialize, Serialize};
 use snafu::ResultExt;
-use store_api::storage::TableId;
-use table::table_name::TableName;
 
 use crate::cache_invalidator::CacheInvalidatorRef;
 use crate::error::Result;
-use crate::key::table_name::TableNameValue;
 use crate::key::TableMetadataManagerRef;
-use crate::lock_key::{CatalogLock, SchemaLock};
+use crate::lock_key::CatalogLock;
 use crate::node_manager::NodeManagerRef;
-use crate::reconciliation::reconcile_database::start::ReconcileDatabaseStart;
+use crate::reconciliation::reconcile_catalog::start::ReconcileCatalogStart;
 use crate::reconciliation::reconcile_database::utils::wait_for_inflight_subprocedures;
 use crate::reconciliation::reconcile_table::resolve_column_metadata::ResolveStrategy;
 use crate::reconciliation::utils::Context;
 
-pub(crate) const DEFAULT_PARALLELISM: usize = 64;
+pub(crate) mod end;
+pub(crate) mod reconcile_databases;
+pub(crate) mod start;
 
-pub(crate) struct ReconcileDatabaseContext {
+pub(crate) struct ReconcileCatalogContext {
     pub node_manager: NodeManagerRef,
     pub table_metadata_manager: TableMetadataManagerRef,
     pub cache_invalidator: CacheInvalidatorRef,
@@ -55,7 +46,7 @@ pub(crate) struct ReconcileDatabaseContext {
     volatile_ctx: VolatileContext,
 }
 
-impl ReconcileDatabaseContext {
+impl ReconcileCatalogContext {
     pub fn new(ctx: Context, persistent_ctx: PersistentContext) -> Self {
         Self {
             node_manager: ctx.node_manager,
@@ -66,20 +57,18 @@ impl ReconcileDatabaseContext {
         }
     }
 
-    pub(crate) async fn wait_for_inflight_subprocedures(
+    pub(crate) async fn wait_for_inflight_subprocedure(
         &mut self,
         procedure_ctx: &ProcedureContext,
     ) -> Result<()> {
-        if !self.volatile_ctx.inflight_subprocedures.is_empty() {
+        if let Some(procedure_id) = self.volatile_ctx.inflight_subprocedure {
             wait_for_inflight_subprocedures(
                 procedure_ctx,
-                &self.volatile_ctx.inflight_subprocedures,
-                self.persistent_ctx.fail_fast,
+                &[procedure_id],
+                self.persistent_ctx.fast_fail,
             )
             .await?;
-            self.volatile_ctx.inflight_subprocedures.clear();
         }
-
         Ok(())
     }
 }
@@ -87,75 +76,45 @@ impl ReconcileDatabaseContext {
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct PersistentContext {
     catalog: String,
-    schema: String,
-    fail_fast: bool,
-    parallelism: usize,
+    fast_fail: bool,
     resolve_strategy: ResolveStrategy,
-    is_subprocedure: bool,
 }
 
 impl PersistentContext {
-    pub fn new(
-        catalog: String,
-        schema: String,
-        fail_fast: bool,
-        parallelism: usize,
-        resolve_strategy: ResolveStrategy,
-        is_subprocedure: bool,
-    ) -> Self {
+    pub fn new(catalog: String, fast_fail: bool, resolve_strategy: ResolveStrategy) -> Self {
         Self {
             catalog,
-            schema,
-            fail_fast,
-            parallelism,
+            fast_fail,
             resolve_strategy,
-            is_subprocedure,
         }
     }
 }
 
 #[derive(Default)]
 pub(crate) struct VolatileContext {
-    /// Stores pending physical tables.
-    pending_tables: Vec<(TableId, TableName)>,
-    /// Stores pending logical tables associated with each physical table.
-    ///
-    /// - Key: Table ID of the physical table.
-    /// - Value: Vector of (TableId, TableName) tuples representing logical tables belonging to the physical table.
-    pending_logical_tables: HashMap<TableId, Vec<(TableId, TableName)>>,
-    /// Stores inflight subprocedures.
-    inflight_subprocedures: Vec<ProcedureId>,
-    /// Stores the stream of tables.
-    tables: Option<BoxStream<'static, Result<(String, TableNameValue)>>>,
+    /// Stores the stream of catalogs.
+    schemas: Option<BoxStream<'static, Result<String>>>,
+    /// Stores the inflight subprocedure.
+    inflight_subprocedure: Option<ProcedureId>,
 }
 
-pub struct ReconcileDatabaseProcedure {
-    pub context: ReconcileDatabaseContext,
+pub struct ReconcileCatalogProcedure {
+    pub context: ReconcileCatalogContext,
     state: Box<dyn State>,
 }
 
-impl ReconcileDatabaseProcedure {
-    pub const TYPE_NAME: &'static str = "metasrv-procedure::ReconcileDatabase";
+impl ReconcileCatalogProcedure {
+    pub const TYPE_NAME: &'static str = "metasrv-procedure::ReconcileCatalog";
 
     pub fn new(
         ctx: Context,
         catalog: String,
-        schema: String,
-        fail_fast: bool,
-        parallelism: usize,
+        fast_fail: bool,
         resolve_strategy: ResolveStrategy,
-        is_subprocedure: bool,
     ) -> Self {
-        let persistent_ctx = PersistentContext::new(
-            catalog,
-            schema,
-            fail_fast,
-            parallelism,
-            resolve_strategy,
-            is_subprocedure,
-        );
-        let context = ReconcileDatabaseContext::new(ctx, persistent_ctx);
-        let state = Box::new(ReconcileDatabaseStart);
+        let persistent_ctx = PersistentContext::new(catalog, fast_fail, resolve_strategy);
+        let context = ReconcileCatalogContext::new(ctx, persistent_ctx);
+        let state = Box::new(ReconcileCatalogStart);
         Self { context, state }
     }
 
@@ -164,7 +123,7 @@ impl ReconcileDatabaseProcedure {
             state,
             persistent_ctx,
         } = serde_json::from_str(json).context(FromJsonSnafu)?;
-        let context = ReconcileDatabaseContext::new(ctx, persistent_ctx);
+        let context = ReconcileCatalogContext::new(ctx, persistent_ctx);
         Ok(Self { context, state })
     }
 }
@@ -181,8 +140,8 @@ struct ProcedureDataOwned {
     persistent_ctx: PersistentContext,
 }
 
-#[async_trait]
-impl Procedure for ReconcileDatabaseProcedure {
+#[async_trait::async_trait]
+impl Procedure for ReconcileCatalogProcedure {
     fn type_name(&self) -> &str {
         Self::TYPE_NAME
     }
@@ -210,26 +169,18 @@ impl Procedure for ReconcileDatabaseProcedure {
             state: self.state.as_ref(),
             persistent_ctx: &self.context.persistent_ctx,
         };
-        serde_json::to_string(&data).context(ToJsonSnafu)
+        serde_json::to_string(&data).context(FromJsonSnafu)
     }
 
     fn lock_key(&self) -> LockKey {
         let catalog = &self.context.persistent_ctx.catalog;
-        let schema = &self.context.persistent_ctx.schema;
-        // If the procedure is a subprocedure, only lock the schema.
-        if self.context.persistent_ctx.is_subprocedure {
-            return LockKey::new(vec![SchemaLock::write(catalog, schema).into()]);
-        }
 
-        LockKey::new(vec![
-            CatalogLock::Read(catalog).into(),
-            SchemaLock::write(catalog, schema).into(),
-        ])
+        LockKey::new(vec![CatalogLock::Write(catalog).into()])
     }
 }
 
 #[async_trait::async_trait]
-#[typetag::serde(tag = "reconcile_database_state")]
+#[typetag::serde(tag = "reconcile_catalog_state")]
 pub(crate) trait State: Sync + Send + Debug {
     fn name(&self) -> &'static str {
         let type_name = std::any::type_name::<Self>();
@@ -239,7 +190,7 @@ pub(crate) trait State: Sync + Send + Debug {
 
     async fn next(
         &mut self,
-        ctx: &mut ReconcileDatabaseContext,
+        ctx: &mut ReconcileCatalogContext,
         procedure_ctx: &ProcedureContext,
     ) -> Result<(Box<dyn State>, Status)>;
 
