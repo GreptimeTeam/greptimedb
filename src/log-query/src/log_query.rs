4aa715877ff10aefe33e69d2d3bb1463009f6b32
Ruihang Xia <waynestxia@gmail.com>
2025-08-05 07:04:15 +0800

    feat: use column expr with filters in LogQuery
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/log-query/src/log_query.rs
+++ src/log-query/src/log_query.rs
@@ -42,7 +42,7 @@ pub struct LogQuery {
     // Filters
     /// Conjunction of filters to apply for the raw logs.
     ///
-    /// Filters here can only refer to the columns from the original log.
+    /// Filters here can apply to any LogExpr.
     pub filters: Vec<ColumnFilters>,
     /// Adjacent lines to return. Applies to all filters above.
     ///
@@ -90,8 +90,7 @@ pub enum LogExpr {
         alias: String,
     },
     Filter {
-        expr: Box<LogExpr>,
-        filter: ContentFilter,
+        filter: ColumnFilters,
     },
 }
 
@@ -282,12 +281,12 @@ impl TimeFilter {
     }
 }
 
-/// Represents a column with filters to query.
-#[derive(Debug, Serialize, Deserialize)]
+/// Represents an expression with filters to query.
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ColumnFilters {
-    /// Case-sensitive column name to query.
-    pub column_name: String,
-    /// Filters to apply to the column. Can be empty.
+    /// Expression to apply filters to. Can be a column reference or any other LogExpr.
+    pub expr: Box<LogExpr>,
+    /// Filters to apply to the expression result. Can be empty.
     pub filters: Vec<ContentFilter>,
 }
 
