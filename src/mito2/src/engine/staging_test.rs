25f926ea7d51490e5a66132d3db66391ebb444aa
Ruihang Xia <waynestxia@gmail.com>
2025-08-11 20:17:47 -0700

    feat: mito region staging state (#6664)
    * fix: not mark all deleted when partial trunc (#6654)
    
    * fix: not mark all deleted when partial trunc&not update manifest when partial file range is empty
    
    Signed-off-by: discord9 <discord9@163.com>
    
    * docs: note
    
    Signed-off-by: discord9 <discord9@163.com>
    
    ---------
    
    Signed-off-by: discord9 <discord9@163.com>
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * some tests and DdlRequest
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * stage transit
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * address CR comments
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    * correct error type
    
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    
    ---------
    
    Signed-off-by: discord9 <discord9@163.com>
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>
    Co-authored-by: discord9 <55937128+discord9@users.noreply.github.com>

---
+++ src/mito2/src/engine/staging_test.rs
@@ -0,0 +1,180 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Integration tests for staging state functionality.
+
+use store_api::region_engine::RegionEngine;
+use store_api::region_request::{RegionAlterRequest, RegionRequest, RegionTruncateRequest};
+use store_api::storage::RegionId;
+
+use crate::config::MitoConfig;
+use crate::request::WorkerRequest;
+use crate::test_util::{CreateRequestBuilder, TestEnv};
+
+#[tokio::test]
+async fn test_staging_state_integration() {
+    let mut env = TestEnv::new().await;
+    let engine = env.create_engine(MitoConfig::default()).await;
+
+    let region_id = RegionId::new(1, 1);
+    let request = CreateRequestBuilder::new().build();
+
+    // Create region
+    engine
+        .handle_request(region_id, RegionRequest::Create(request))
+        .await
+        .unwrap();
+
+    // Test external API patterns work correctly
+    use store_api::region_engine::SettableRegionRoleState;
+
+    let (role_req, _receiver) = WorkerRequest::new_set_readonly_gracefully(
+        region_id,
+        SettableRegionRoleState::StagingLeader,
+    );
+    match role_req {
+        WorkerRequest::SetRegionRoleStateGracefully {
+            region_id: req_region_id,
+            region_role_state,
+            ..
+        } => {
+            assert_eq!(req_region_id, region_id);
+            assert_eq!(region_role_state, SettableRegionRoleState::StagingLeader);
+        }
+        _ => panic!("Expected SetRegionRoleStateGracefully request"),
+    }
+
+    let (role_req, _receiver) =
+        WorkerRequest::new_set_readonly_gracefully(region_id, SettableRegionRoleState::Leader);
+    match role_req {
+        WorkerRequest::SetRegionRoleStateGracefully {
+            region_id: req_region_id,
+            region_role_state,
+            ..
+        } => {
+            assert_eq!(req_region_id, region_id);
+            assert_eq!(region_role_state, SettableRegionRoleState::Leader);
+        }
+        _ => panic!("Expected SetRegionRoleStateGracefully request"),
+    }
+}
+
+#[tokio::test]
+async fn test_staging_blocks_alter_operations() {
+    let mut env = TestEnv::new().await;
+    let engine = env.create_engine(MitoConfig::default()).await;
+
+    let region_id = RegionId::new(1, 1);
+    let request = CreateRequestBuilder::new().build();
+
+    // Create region
+    engine
+        .handle_request(region_id, RegionRequest::Create(request))
+        .await
+        .unwrap();
+
+    // Note: In the current implementation, we can't directly test staging mode
+    // through the engine interface since staging transitions are handled
+    // through worker requests. This test demonstrates the pattern that would
+    // be used once external control interfaces are implemented.
+
+    // Test that ALTER operations would be blocked in staging mode
+    let alter_request = RegionAlterRequest {
+        kind: store_api::region_request::AlterKind::AddColumns { columns: vec![] },
+    };
+
+    // This currently succeeds since we're not in staging mode
+    let result = engine
+        .handle_request(region_id, RegionRequest::Alter(alter_request))
+        .await;
+    assert!(result.is_ok(), "ALTER should succeed in normal mode");
+}
+
+#[tokio::test]
+async fn test_staging_blocks_truncate_operations() {
+    let mut env = TestEnv::new().await;
+    let engine = env.create_engine(MitoConfig::default()).await;
+
+    let region_id = RegionId::new(1, 1);
+    let request = CreateRequestBuilder::new().build();
+
+    // Create region
+    engine
+        .handle_request(region_id, RegionRequest::Create(request))
+        .await
+        .unwrap();
+
+    // Test that TRUNCATE operations would be blocked in staging mode
+    let truncate_request = RegionTruncateRequest::All;
+
+    // This currently succeeds since we're not in staging mode
+    let result = engine
+        .handle_request(region_id, RegionRequest::Truncate(truncate_request))
+        .await;
+    assert!(result.is_ok(), "TRUNCATE should succeed in normal mode");
+}
+
+#[tokio::test]
+async fn test_staging_state_validation_patterns() {
+    use crate::region::{RegionLeaderState, RegionRoleState};
+
+    // Test the state validation patterns used throughout the codebase
+    let staging_state = RegionRoleState::Leader(RegionLeaderState::Staging);
+    let writable_state = RegionRoleState::Leader(RegionLeaderState::Writable);
+
+    // Test staging detection
+    let is_staging = staging_state == RegionRoleState::Leader(RegionLeaderState::Staging);
+    assert!(is_staging, "Should correctly identify staging state");
+
+    let is_not_staging = writable_state == RegionRoleState::Leader(RegionLeaderState::Staging);
+    assert!(
+        !is_not_staging,
+        "Should correctly identify non-staging state"
+    );
+
+    // Test writable state check
+    let staging_is_writable = matches!(
+        staging_state,
+        RegionRoleState::Leader(RegionLeaderState::Writable)
+            | RegionRoleState::Leader(RegionLeaderState::Staging)
+    );
+    assert!(staging_is_writable, "Staging regions should be writable");
+
+    let writable_is_writable = matches!(
+        writable_state,
+        RegionRoleState::Leader(RegionLeaderState::Writable)
+            | RegionRoleState::Leader(RegionLeaderState::Staging)
+    );
+    assert!(writable_is_writable, "Writable regions should be writable");
+
+    // Test flushable state check
+    let staging_is_flushable = matches!(
+        staging_state,
+        RegionRoleState::Leader(RegionLeaderState::Writable)
+            | RegionRoleState::Leader(RegionLeaderState::Staging)
+            | RegionRoleState::Leader(RegionLeaderState::Downgrading)
+    );
+    assert!(staging_is_flushable, "Staging regions should be flushable");
+
+    let writable_is_flushable = matches!(
+        writable_state,
+        RegionRoleState::Leader(RegionLeaderState::Writable)
+            | RegionRoleState::Leader(RegionLeaderState::Staging)
+            | RegionRoleState::Leader(RegionLeaderState::Downgrading)
+    );
+    assert!(
+        writable_is_flushable,
+        "Writable regions should be flushable"
+    );
+}
