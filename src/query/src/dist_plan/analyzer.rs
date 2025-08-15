d28a7e6e82f7bb89c547a415e4c168894d05d812
luofucong <luofc@foxmail.com>
2025-08-15 16:48:39 +0800

    try following PR comments
    Signed-off-by: luofucong <luofc@foxmail.com>

--- src/query/src/dist_plan/analyzer.rs
+++ src/query/src/dist_plan/analyzer.rs
@@ -155,7 +155,7 @@ impl DistPlannerAnalyzer {
             .collect::<DfResult<Vec<_>>>()?;
 
         // Some plans that are special treated (should not call `with_new_exprs` on them)
-        if !matches!(plan, LogicalPlan::Unnest(_) | LogicalPlan::Join(_)) {
+        if !matches!(plan, LogicalPlan::Unnest(_)) {
             let inputs = plan.inputs().into_iter().cloned().collect::<Vec<_>>();
             Ok(Transformed::yes(plan.with_new_exprs(exprs, inputs)?))
         } else {
