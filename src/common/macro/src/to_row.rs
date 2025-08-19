c2d06450ccfc32d265b38443c58542e1230a023c
WenyXu <wenymedia@gmail.com>
2025-08-19 09:00:35 +0000

    feat: introduce `ToRow` derive marco
    Signed-off-by: WenyXu <wenymedia@gmail.com>

---
+++ src/common/macro/src/to_row.rs
@@ -0,0 +1,494 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use greptime_proto::v1::ColumnDataType;
+use proc_macro2::{Ident, TokenStream as TokenStream2};
+use quote::{format_ident, quote};
+use syn::meta::ParseNestedMeta;
+use syn::spanned::Spanned;
+use syn::{
+    AngleBracketedGenericArguments, Attribute, Data, DataStruct, DeriveInput, Fields, FieldsNamed,
+    GenericArgument, LitStr, Meta, Path, PathArguments, PathSegment, Result, Type, TypePath,
+    TypeReference,
+};
+
+const META_KEY_COL: &str = "col";
+const META_KEY_NAME: &str = "name";
+const META_KEY_DATATYPE: &str = "datatype";
+const META_KEY_SEMANTIC: &str = "semantic";
+
+pub(crate) fn derive_to_row_impl(input: DeriveInput) -> Result<TokenStream2> {
+    let Data::Struct(DataStruct {
+        fields: Fields::Named(FieldsNamed { named, .. }),
+        ..
+    }) = &input.data
+    else {
+        return Err(syn::Error::new(
+            input.span(),
+            "ToRow can only be derived for structs",
+        ));
+    };
+
+    let ident = input.ident;
+    let generics = input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let fields = named.iter().map(|field| {
+        (
+            field.ident.as_ref().expect("field must have an ident"),
+            &field.ty,
+        )
+    });
+
+    // Ident of fields.
+    let idents = fields.clone().map(|(ident, _)| ident).collect::<Vec<_>>();
+    // Infer column data types for each field.
+    let infer_column_data_types = fields
+        .clone()
+        .map(|(_, ty)| field_type(ty))
+        .flat_map(|ty| {
+            ty.extract_ident()
+                .map(convert_primitive_type_to_column_data_type)
+        })
+        .collect::<Vec<_>>();
+
+    // Find attributes for each field.
+    let column_attributes = named
+        .iter()
+        .map(|field| {
+            let attrs = &field.attrs;
+            let attr = find_column_attribute(attrs);
+            match attr {
+                Some(attr) => parse_attribute(attr),
+                None => Ok(ColumnAttribute::default()),
+            }
+        })
+        .collect::<Result<Vec<ColumnAttribute>>>()?;
+
+    // Implement `to_value` methods for each field.
+    let impl_to_value_methods = fields
+        .clone()
+        .zip(infer_column_data_types.iter())
+        .zip(column_attributes.iter())
+        .map(|(((ident, ty), column_data_type), column_attribute)| {
+            let field_type = field_type(ty);
+            impl_to_value_method(
+                ident,
+                field_type.is_optional(),
+                column_data_type,
+                column_attribute,
+            )
+        })
+        .collect::<Result<Vec<TokenStream2>>>()?;
+
+    // Implement `to_row` method.
+    let impl_to_row_method = impl_to_row_method(&idents);
+
+    // Implement `schema` method.
+    let impl_schema_method =
+        impl_schema_method(&idents, &infer_column_data_types, &column_attributes)?;
+
+    Ok(quote! {
+        impl #impl_generics #ident #ty_generics #where_clause {
+            #( #impl_to_value_methods )*
+
+            #impl_to_row_method
+
+            #impl_schema_method
+        }
+    })
+}
+
+fn impl_schema_method(
+    idents: &[&Ident],
+    infer_column_data_types: &[Option<ColumnDataType>],
+    column_attributes: &[ColumnAttribute],
+) -> Result<TokenStream2> {
+    let schemas: Vec<TokenStream2> = idents
+        .iter()
+        .zip(infer_column_data_types.iter())
+        .zip(column_attributes.iter())
+        .map(|((ident, column_data_type), column_attribute)| {
+            let Some(column_data_type) = get_column_data_type(column_data_type, column_attribute)
+            else {
+                return Err(syn::Error::new(
+                    ident.span(),
+                    format!(
+                        "expected to set data type explicitly via [({META_KEY_COL}({META_KEY_DATATYPE} = \"...\"))]"
+                    ),
+                ));
+            };
+
+            // Uses user explicit name or field name as column name.
+            let name = column_attribute
+                .name
+                .clone()
+                .unwrap_or_else(|| ident.to_string());
+            let name = syn::LitStr::new(&name, ident.span());
+            let column_data_type =
+                syn::LitInt::new(&(column_data_type as i32).to_string(), ident.span());
+            let semantic_type_val = match column_attribute.semantic_type {
+                SemanticType::Field => greptime_proto::v1::SemanticType::Field,
+                SemanticType::Tag => greptime_proto::v1::SemanticType::Tag,
+                SemanticType::Timestamp => greptime_proto::v1::SemanticType::Timestamp,
+            } as i32;
+            let semantic_type = syn::LitInt::new(&semantic_type_val.to_string(), ident.span());
+
+            Ok(quote! {
+                greptime_proto::v1::ColumnSchema {
+                    column_name: #name.to_string(),
+                    datatype: #column_data_type,
+                    datatype_extension: None,
+                    options: None,
+                    semantic_type: #semantic_type,
+                }
+            })
+        })
+        .collect::<Result<_>>()?;
+
+    Ok(quote! {
+        pub fn schema(&self) -> Vec<greptime_proto::v1::ColumnSchema> {
+            vec![ #(#schemas),* ]
+        }
+    })
+}
+
+fn impl_to_value_method(
+    ident: &Ident,
+    is_optional: bool,
+    infer_column_data_type: &Option<ColumnDataType>,
+    column_attribute: &ColumnAttribute,
+) -> Result<TokenStream2> {
+    let Some(column_data_type) = get_column_data_type(infer_column_data_type, column_attribute)
+    else {
+        return Err(syn::Error::new(
+            ident.span(),
+            format!(
+                "expected to set data type explicitly via [({META_KEY_COL}({META_KEY_DATATYPE} = \"...\"))]"
+            ),
+        ));
+    };
+
+    let method_name = format_ident!("{}_to_value", ident);
+    let value_data = convert_column_data_type_to_value_data_ident(&column_data_type);
+    let body = if is_optional {
+        quote! {
+            match &self.#ident {
+                Some(v) => greptime_proto::v1::Value {
+                    value_data: Some(greptime_proto::v1::value::ValueData::#value_data(v.clone().into())),
+                },
+                None => greptime_proto::v1::Value { value_data: None },
+            }
+        }
+    } else {
+        quote! {
+            greptime_proto::v1::Value {
+                value_data: Some(greptime_proto::v1::value::ValueData::#value_data(self.#ident.clone().into())),
+            }
+        }
+    };
+    Ok(quote! {
+        fn #method_name(&self) -> greptime_proto::v1::Value {
+            #body
+        }
+    })
+}
+
+fn impl_to_row_method(idents: &[&Ident]) -> TokenStream2 {
+    let methods: Vec<syn::Ident> = idents
+        .iter()
+        .map(|ident| format_ident!("{}_to_value", ident))
+        .collect();
+
+    quote! {
+        pub fn to_row(&self) -> greptime_proto::v1::Row {
+            greptime_proto::v1::Row {
+                values: vec![
+                    #( self.#methods() ),*
+                ]
+            }
+        }
+    }
+}
+
+fn get_column_data_type(
+    infer_column_data_type: &Option<ColumnDataType>,
+    attribute: &ColumnAttribute,
+) -> Option<ColumnDataType> {
+    if let Some(column_data_type) = attribute.column_data_type {
+        return Some(column_data_type);
+    }
+    *infer_column_data_type
+}
+
+fn convert_column_data_type_to_value_data_ident(column_data_type: &ColumnDataType) -> Ident {
+    let suffix = "Value";
+    match column_data_type {
+        ColumnDataType::Boolean => format_ident!("{}{}", "Bool", suffix),
+        ColumnDataType::Int8 => format_ident!("{}{}", "I8", suffix),
+        ColumnDataType::Int16 => format_ident!("{}{}", "I16", suffix),
+        ColumnDataType::Int32 => format_ident!("{}{}", "I32", suffix),
+        ColumnDataType::Int64 => format_ident!("{}{}", "I64", suffix),
+        ColumnDataType::Uint8 => format_ident!("{}{}", "U8", suffix),
+        ColumnDataType::Uint16 => format_ident!("{}{}", "U16", suffix),
+        ColumnDataType::Uint32 => format_ident!("{}{}", "U32", suffix),
+        ColumnDataType::Uint64 => format_ident!("{}{}", "U64", suffix),
+        ColumnDataType::Float32 => format_ident!("{}{}", "F32", suffix),
+        ColumnDataType::Float64 => format_ident!("{}{}", "F64", suffix),
+        ColumnDataType::Binary => format_ident!("{}{}", "Binary", suffix),
+        ColumnDataType::String => format_ident!("{}{}", "String", suffix),
+        ColumnDataType::Date => format_ident!("{}{}", "Date", suffix),
+        ColumnDataType::Datetime => format_ident!("{}{}", "Datetime", suffix),
+        ColumnDataType::TimestampSecond => format_ident!("{}{}", "TimestampSecond", suffix),
+        ColumnDataType::TimestampMillisecond => {
+            format_ident!("{}{}", "TimestampMillisecond", suffix)
+        }
+        ColumnDataType::TimestampMicrosecond => {
+            format_ident!("{}{}", "TimestampMicrosecond", suffix)
+        }
+        ColumnDataType::TimestampNanosecond => format_ident!("{}{}", "TimestampNanosecond", suffix),
+        ColumnDataType::TimeSecond => format_ident!("{}{}", "TimeSecond", suffix),
+        ColumnDataType::TimeMillisecond => format_ident!("{}{}", "TimeMillisecond", suffix),
+        ColumnDataType::TimeMicrosecond => format_ident!("{}{}", "TimeMicrosecond", suffix),
+        ColumnDataType::TimeNanosecond => format_ident!("{}{}", "TimeNanosecond", suffix),
+        ColumnDataType::IntervalYearMonth => format_ident!("{}{}", "IntervalYearMonth", suffix),
+        ColumnDataType::IntervalDayTime => format_ident!("{}{}", "IntervalDayTime", suffix),
+        ColumnDataType::IntervalMonthDayNano => {
+            format_ident!("{}{}", "IntervalMonthDayNano", suffix)
+        }
+        ColumnDataType::Decimal128 => format_ident!("{}{}", "Decimal128", suffix),
+        ColumnDataType::Json => format_ident!("{}{}", "Json", suffix),
+        ColumnDataType::Vector => format_ident!("{}{}", "Vector", suffix),
+    }
+}
+
+fn field_type(ty: &Type) -> FieldType<'_> {
+    if let Type::Reference(TypeReference { elem, .. }) = ty {
+        return field_type(elem);
+    }
+
+    if let Type::Path(TypePath {
+        qself: _,
+        path: Path {
+            leading_colon,
+            segments,
+        },
+    }) = ty
+    {
+        if leading_colon.is_none() && segments.len() == 1 {
+            if let Some(PathSegment {
+                ident,
+                arguments:
+                    PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }),
+            }) = segments.first()
+            {
+                if let (1, Some(GenericArgument::Type(t))) = (args.len(), args.first()) {
+                    if ident == "Option" {
+                        return FieldType::Optional(t);
+                    }
+                }
+            }
+        }
+    }
+
+    FieldType::Required(ty)
+}
+
+fn extract_ident_from_type(ty: &Type) -> Option<&Ident> {
+    match ty {
+        Type::Path(TypePath { qself: None, path }) => path.get_ident(),
+        Type::Reference(type_ref) => extract_ident_from_type(&type_ref.elem),
+        Type::Group(type_group) => extract_ident_from_type(&type_group.elem),
+        _ => None,
+    }
+}
+
+fn convert_primitive_type_to_column_data_type(ident: &Ident) -> Option<ColumnDataType> {
+    match ident.to_string().as_str() {
+        "i8" => Some(ColumnDataType::Int8),
+        "i16" => Some(ColumnDataType::Int16),
+        "i32" => Some(ColumnDataType::Int32),
+        "i64" => Some(ColumnDataType::Int64),
+        "u8" => Some(ColumnDataType::Uint8),
+        "u16" => Some(ColumnDataType::Uint16),
+        "u32" => Some(ColumnDataType::Uint32),
+        "u64" => Some(ColumnDataType::Uint64),
+        "f32" => Some(ColumnDataType::Float32),
+        "f64" => Some(ColumnDataType::Float64),
+        "bool" => Some(ColumnDataType::Boolean),
+        _ => None,
+    }
+}
+
+fn convert_timestamp_type_to_column_data_type(ident: &str) -> Option<ColumnDataType> {
+    match ident {
+        "timestampsecond" => Some(ColumnDataType::TimestampSecond),
+        "timestampmillisecond" => Some(ColumnDataType::TimestampMillisecond),
+        "timestamptimemicrosecond" => Some(ColumnDataType::TimestampMicrosecond),
+        "timestamptimenanosecond" => Some(ColumnDataType::TimestampNanosecond),
+        _ => None,
+    }
+}
+
+fn convert_date_type_to_column_data_type(ident: &str) -> Option<ColumnDataType> {
+    match ident {
+        "date" => Some(ColumnDataType::Date),
+        "datetime" => Some(ColumnDataType::Datetime),
+        _ => None,
+    }
+}
+
+fn convert_time_type_to_column_data_type(ident: &str) -> Option<ColumnDataType> {
+    match ident {
+        "timemillisecond" => Some(ColumnDataType::TimeMillisecond),
+        "timemicrosecond" => Some(ColumnDataType::TimeMicrosecond),
+        "timenanosecond" => Some(ColumnDataType::TimeNanosecond),
+        _ => None,
+    }
+}
+
+fn convert_complex_type_to_column_data_type(ident: &str) -> Option<ColumnDataType> {
+    match ident {
+        "string" => Some(ColumnDataType::String),
+        "json" => Some(ColumnDataType::Json),
+        "decimal128" => Some(ColumnDataType::Decimal128),
+        "vector" => Some(ColumnDataType::Vector),
+        _ => None,
+    }
+}
+
+fn semantic_type_from_ident(ident: &str) -> Option<SemanticType> {
+    // Ignores the case of the identifier.
+    let lowercase = ident.to_lowercase();
+    match lowercase.as_str() {
+        "field" => Some(SemanticType::Field),
+        "tag" => Some(SemanticType::Tag),
+        "timestamp" => Some(SemanticType::Timestamp),
+        _ => None,
+    }
+}
+
+fn convert_field_type_to_column_data_type(ident: &str) -> Option<ColumnDataType> {
+    // Ignores the case of the identifier.
+    let lowercase = ident.to_lowercase();
+    if let Some(value) = convert_timestamp_type_to_column_data_type(&lowercase) {
+        return Some(value);
+    }
+    if let Some(value) = convert_date_type_to_column_data_type(&lowercase) {
+        return Some(value);
+    }
+    if let Some(value) = convert_time_type_to_column_data_type(&lowercase) {
+        return Some(value);
+    }
+    if let Some(value) = convert_complex_type_to_column_data_type(&lowercase) {
+        return Some(value);
+    }
+    None
+}
+
+#[derive(Default)]
+enum SemanticType {
+    #[default]
+    Field,
+    Tag,
+    Timestamp,
+}
+
+enum FieldType<'a> {
+    Required(&'a Type),
+    Optional(&'a Type),
+}
+
+impl FieldType<'_> {
+    fn is_optional(&self) -> bool {
+        matches!(self, FieldType::Optional(_))
+    }
+
+    fn extract_ident(&self) -> Option<&Ident> {
+        match self {
+            FieldType::Required(ty) => extract_ident_from_type(ty),
+            FieldType::Optional(ty) => extract_ident_from_type(ty),
+        }
+    }
+}
+
+#[derive(Default)]
+struct ColumnAttribute {
+    name: Option<String>,
+    column_data_type: Option<ColumnDataType>,
+    semantic_type: SemanticType,
+}
+
+fn find_column_attribute(attrs: &[Attribute]) -> Option<&Attribute> {
+    attrs
+        .iter()
+        .find(|attr| matches!(&attr.meta, Meta::List(list) if list.path.is_ident(META_KEY_COL)))
+}
+
+fn parse_attribute(attr: &Attribute) -> Result<ColumnAttribute> {
+    match &attr.meta {
+        Meta::List(list) if list.path.is_ident(META_KEY_COL) => {
+            let mut attribute = ColumnAttribute::default();
+            list.parse_nested_meta(|meta| {
+                parse_column_attribute(&meta, &mut attribute)
+            })?;
+            Ok(attribute)
+        }
+        _ => Err(syn::Error::new(
+            attr.span(),
+            format!(
+                "expected `{META_KEY_COL}({META_KEY_NAME} = \"...\", {META_KEY_DATATYPE} = \"...\", {META_KEY_SEMANTIC} = \"...\")`"
+            ),
+        )),
+    }
+}
+
+fn parse_column_attribute(
+    meta: &ParseNestedMeta<'_>,
+    attribute: &mut ColumnAttribute,
+) -> Result<()> {
+    let Some(ident) = meta.path.get_ident() else {
+        return Err(meta.error(format!("expected `{META_KEY_COL}({META_KEY_NAME} = \"...\", {META_KEY_DATATYPE} = \"...\", {META_KEY_SEMANTIC} = \"...\")`")));
+    };
+
+    match ident.to_string().as_str() {
+        META_KEY_NAME => {
+            let value = meta.value()?;
+            let s: LitStr = value.parse()?;
+            attribute.name = Some(s.value());
+        }
+        META_KEY_DATATYPE => {
+            let value = meta.value()?;
+            let s: LitStr = value.parse()?;
+            let ident = s.value();
+            let Some(value) = convert_field_type_to_column_data_type(&ident) else {
+                return Err(meta.error(format!("unexpected {META_KEY_DATATYPE}: {ident}")));
+            };
+            attribute.column_data_type = Some(value);
+        }
+        META_KEY_SEMANTIC => {
+            let value = meta.value()?;
+            let s: LitStr = value.parse()?;
+
+            let ident = s.value();
+            let Some(value) = semantic_type_from_ident(&ident) else {
+                return Err(meta.error(format!("unexpected {META_KEY_SEMANTIC}: {ident}")));
+            };
+            attribute.semantic_type = value;
+        }
+        attr => return Err(meta.error(format!("unexpected attribute: {attr}"))),
+    }
+
+    Ok(())
+}
--- src/common/macro/tests/test_derive.rs
+++ src/common/macro/tests/test_derive.rs
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use common_macro::{as_aggr_func_creator, AggrFuncTypeStore};
+use common_macro::{as_aggr_func_creator, AggrFuncTypeStore, ToRow};
+use greptime_proto::v1::{ColumnDataType, ColumnSchema, Row, SemanticType};
 use static_assertions::{assert_fields, assert_impl_all};
 
 #[as_aggr_func_creator]
@@ -25,4 +26,150 @@ fn test_derive() {
     let _ = Foo::default();
     assert_fields!(Foo: input_types);
     assert_impl_all!(Foo: std::fmt::Debug, Default, common_query::logical_plan::accumulator::AggrFuncTypeStore);
+}
+
+#[derive(ToRow)]
+struct ToRowOwned {
+    my_value: i32,
+    #[col(name = "string_value", datatype = "string", semantic = "tag")]
+    my_string: String,
+    my_bool: bool,
+    my_float: f32,
+    #[col(
+        name = "timestamp_value",
+        semantic = "Timestamp",
+        datatype = "TimestampMillisecond"
+    )]
+    my_timestamp: i64,
+}
+
+#[test]
+fn test_to_row() {
+    let test = ToRowOwned {
+        my_value: 1,
+        my_string: "test".to_string(),
+        my_bool: true,
+        my_float: 1.0,
+        my_timestamp: 1718563200000,
+    };
+    let row = test.to_row();
+    assert_row(&row);
+    let schema = test.schema();
+    assert_schema(&schema);
+}
+
+#[derive(ToRow)]
+struct ToRowRef<'a> {
+    my_value: &'a i32,
+    #[col(name = "string_value", datatype = "string", semantic = "tag")]
+    my_string: &'a String,
+    my_bool: &'a bool,
+    my_float: &'a f32,
+    #[col(
+        name = "timestamp_value",
+        semantic = "Timestamp",
+        datatype = "TimestampMillisecond"
+    )]
+    my_timestamp: &'a i64,
+}
+
+#[test]
+fn test_to_row_ref() {
+    let string = "test".to_string();
+    let test = ToRowRef {
+        my_value: &1,
+        my_string: &string,
+        my_bool: &true,
+        my_float: &1.0,
+        my_timestamp: &1718563200000,
+    };
+    let row = test.to_row();
+    assert_row(&row);
+    let schema = test.schema();
+    assert_schema(&schema);
+}
+
+#[derive(ToRow)]
+struct ToRowOptional {
+    my_value: Option<i32>,
+    #[col(name = "string_value", datatype = "string", semantic = "tag")]
+    my_string: Option<String>,
+    my_bool: Option<bool>,
+    my_float: Option<f32>,
+    #[col(
+        name = "timestamp_value",
+        semantic = "Timestamp",
+        datatype = "TimestampMillisecond"
+    )]
+    my_timestamp: i64,
+}
+
+#[test]
+fn test_to_row_optional() {
+    let test = ToRowOptional {
+        my_value: None,
+        my_string: None,
+        my_bool: None,
+        my_float: None,
+        my_timestamp: 1718563200000,
+    };
+
+    let row = test.to_row();
+    assert_eq!(row.values.len(), 5);
+    assert_eq!(row.values[0].value_data, None);
+    assert_eq!(row.values[1].value_data, None);
+    assert_eq!(row.values[2].value_data, None);
+    assert_eq!(row.values[3].value_data, None);
+    assert_eq!(
+        row.values[4].value_data,
+        Some(greptime_proto::v1::value::ValueData::TimestampMillisecondValue(1718563200000))
+    );
+}
+
+fn assert_row(row: &Row) {
+    assert_eq!(row.values.len(), 5);
+    assert_eq!(
+        row.values[0].value_data,
+        Some(greptime_proto::v1::value::ValueData::I32Value(1))
+    );
+    assert_eq!(
+        row.values[1].value_data,
+        Some(greptime_proto::v1::value::ValueData::StringValue(
+            "test".to_string()
+        ))
+    );
+    assert_eq!(
+        row.values[2].value_data,
+        Some(greptime_proto::v1::value::ValueData::BoolValue(true))
+    );
+    assert_eq!(
+        row.values[3].value_data,
+        Some(greptime_proto::v1::value::ValueData::F32Value(1.0))
+    );
+    assert_eq!(
+        row.values[4].value_data,
+        Some(greptime_proto::v1::value::ValueData::TimestampMillisecondValue(1718563200000))
+    );
+}
+
+fn assert_schema(schema: &[ColumnSchema]) {
+    assert_eq!(schema.len(), 5);
+    assert_eq!(schema[0].column_name, "my_value");
+    assert_eq!(schema[0].datatype, ColumnDataType::Int32 as i32);
+    assert_eq!(schema[0].semantic_type, SemanticType::Field as i32);
+    assert_eq!(schema[1].column_name, "string_value");
+    assert_eq!(schema[1].datatype, ColumnDataType::String as i32);
+    assert_eq!(schema[1].semantic_type, SemanticType::Tag as i32);
+    assert_eq!(schema[2].column_name, "my_bool");
+    assert_eq!(schema[2].datatype, ColumnDataType::Boolean as i32);
+    assert_eq!(schema[2].semantic_type, SemanticType::Field as i32);
+    assert_eq!(schema[3].column_name, "my_float");
+    assert_eq!(schema[3].datatype, ColumnDataType::Float32 as i32);
+    assert_eq!(schema[3].semantic_type, SemanticType::Field as i32);
+    assert_eq!(schema[4].column_name, "timestamp_value");
+    assert_eq!(
+        schema[4].datatype,
+        ColumnDataType::TimestampMillisecond as i32
+    );
+    assert_eq!(schema[4].semantic_type, SemanticType::Timestamp as i32);
 }
