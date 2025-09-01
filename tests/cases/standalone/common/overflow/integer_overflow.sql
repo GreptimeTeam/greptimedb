b8cdec96f8a9a215801c2fe8122e77e9db994e3e
Dennis Zhuang <killme2008@gmail.com>
2025-09-01 20:03:55 +0800

    fix: integer flow test
    Signed-off-by: Dennis Zhuang <killme2008@gmail.com>

--- tests/cases/standalone/common/overflow/integer_overflow.sql
+++ tests/cases/standalone/common/overflow/integer_overflow.sql
@@ -4,27 +4,33 @@
 -- TINYINT addition tests
 SELECT 100::TINYINT + 1::TINYINT;
 
-SELECT 100::TINYINT + 50::TINYINT;  -- overflow: wraps to -106
+-- overflow: wraps to -106
+SELECT 100::TINYINT + 50::TINYINT;
 
 SELECT 0::TINYINT + (-127)::TINYINT;
 
-SELECT (-2)::TINYINT + (-127)::TINYINT;  -- underflow: wraps to 127
+  -- underflow: wraps to 127
+SELECT (-2)::TINYINT + (-127)::TINYINT;
 
 -- SMALLINT addition tests
 SELECT 30000::SMALLINT + 1::SMALLINT;
 
-SELECT 30000::SMALLINT + 5000::SMALLINT;  -- overflow: wraps to -30536
+ -- overflow: wraps to -30536
+SELECT 30000::SMALLINT + 5000::SMALLINT;
 
 SELECT 0::SMALLINT + (-32767)::SMALLINT;
 
-SELECT (-2)::SMALLINT + (-32767)::SMALLINT;  -- underflow: wraps to 32767
+-- underflow: wraps to 32767
+SELECT (-2)::SMALLINT + (-32767)::SMALLINT;
 
 -- INTEGER addition tests
 SELECT 2147483640::INTEGER + 1::INTEGER;
 
-SELECT 2147483640::INTEGER + 5000::INTEGER;  -- overflow: wraps
+-- overflow: wraps
+SELECT 2147483640::INTEGER + 5000::INTEGER;
 
 -- BIGINT addition tests
 SELECT 9223372036854775800::BIGINT + 1::BIGINT;
 
-SELECT 9223372036854775800::BIGINT + 1000::BIGINT;  -- overflow: wraps
+-- overflow: wraps
+SELECT 9223372036854775800::BIGINT + 1000::BIGINT;
