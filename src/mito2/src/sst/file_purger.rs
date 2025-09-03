240061771df67b1e6ad3afee88152fb95f1b488b
Zhenchi <zhongzc_arch@outlook.com>
2025-09-03 15:11:05 +0800

    fix: move prune_region_dir to region drop (#6891)
    * fix: move prune_region_dir to region drop
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * address comments
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    ---------
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    Signed-off-by: evenyag <realevenyag@gmail.com>

--- src/mito2/src/sst/file_purger.rs
+++ src/mito2/src/sst/file_purger.rs
@@ -131,12 +131,13 @@ impl FilePurger for LocalFilePurger {
                 error!(e; "Failed to purge stager with index file, file_id: {}, region: {}",
                     file_meta.file_id, file_meta.region_id);
             }
+            let file_id = file_meta.file_id;
             if let Err(e) = sst_layer
                 .intermediate_manager()
-                .prune_region_dir(&file_meta.region_id)
+                .prune_sst_dir(&file_meta.region_id, &file_id)
                 .await
             {
-                error!(e; "Failed to prune intermediate region directory, region_id: {}", file_meta.region_id);
+                error!(e; "Failed to prune intermediate sst directory, region_id: {}, file_id: {}", file_meta.region_id, file_id);
             }
         })) {
             error!(e; "Failed to schedule the file purge request");
