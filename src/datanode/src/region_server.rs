264d05d20e1f8eb434dbfaa16e7ad3ea8b6970fa
Zhenchi <zhongzc_arch@outlook.com>
2025-09-09 11:29:04 +0800

    feat: add InformationExtension.inspect_datanode for datanode inspection (#6921)
    * feat: add InformationExtension.inspect_datanode for datanode inspection
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * aggregate results from all datanodes
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * fix fmt
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * fix unreleased mito engine
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    ---------
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- src/datanode/src/region_server.rs
+++ src/datanode/src/region_server.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod catalog;
+
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::ops::Deref;
@@ -39,13 +41,11 @@ use common_telemetry::tracing::{self, info_span};
 use common_telemetry::tracing_context::{FutureExt, TracingContext};
 use common_telemetry::{debug, error, info, warn};
 use dashmap::DashMap;
-use datafusion::datasource::{TableProvider, provider_as_source};
-use datafusion::error::Result as DfResult;
-use datafusion_common::tree_node::{Transformed, TreeNode, TreeNodeRewriter};
-use datafusion_expr::{LogicalPlan, TableSource};
+use datafusion::datasource::TableProvider;
+use datafusion_common::tree_node::TreeNode;
 use futures_util::future::try_join_all;
 use metric_engine::engine::MetricEngine;
-use mito2::engine::MITO_ENGINE_NAME;
+use mito2::engine::{MITO_ENGINE_NAME, MitoEngine};
 use prost::Message;
 use query::QueryEngineRef;
 pub use query::dummy_catalog::{
@@ -82,6 +82,7 @@ use crate::error::{
     Result, SerializeJsonSnafu, StopRegionEngineSnafu, UnexpectedSnafu, UnsupportedOutputSnafu,
 };
 use crate::event_listener::RegionServerEventListenerRef;
+use crate::region_server::catalog::{NameAwareCatalogList, NameAwareDataSourceInjectorBuilder};
 
 #[derive(Clone)]
 pub struct RegionServer {
@@ -215,10 +216,11 @@ impl RegionServer {
         };
 
         let region_id = RegionId::from_u64(request.region_id);
-        let provider = self
-            .table_provider(region_id, Some(query_ctx.clone()))
-            .await?;
-        let catalog_list = Arc::new(DummyCatalogList::with_table_provider(provider));
+        let catalog_list = Arc::new(NameAwareCatalogList::new(
+            self.clone(),
+            region_id,
+            query_ctx.clone(),
+        ));
 
         if query_ctx.explain_verbose() {
             common_telemetry::info!("Handle remote read for region: {}", region_id);
@@ -259,33 +261,15 @@ impl RegionServer {
         let ctx = request.header.as_ref().map(|h| h.into());
         let query_ctx = Arc::new(ctx.unwrap_or_else(|| QueryContextBuilder::default().build()));
 
-        let provider = self
-            .table_provider(request.region_id, Some(query_ctx.clone()))
+        let injector_builder = NameAwareDataSourceInjectorBuilder::from_plan(&request.plan)
+            .context(DataFusionSnafu)?;
+        let mut injector = injector_builder
+            .build(self, request.region_id, query_ctx.clone())
             .await?;
-
-        struct RegionDataSourceInjector {
-            source: Arc<dyn TableSource>,
-        }
-
-        impl TreeNodeRewriter for RegionDataSourceInjector {
-            type Node = LogicalPlan;
-
-            fn f_up(&mut self, node: Self::Node) -> DfResult<Transformed<Self::Node>> {
-                Ok(match node {
-                    LogicalPlan::TableScan(mut scan) => {
-                        scan.source = self.source.clone();
-                        Transformed::yes(LogicalPlan::TableScan(scan))
-                    }
-                    _ => Transformed::no(node),
-                })
-            }
-        }
 
         let plan = request
             .plan
-            .rewrite(&mut RegionDataSourceInjector {
-                source: provider_as_source(provider),
-            })
+            .rewrite(&mut injector)
             .context(DataFusionSnafu)?
             .data;
 
@@ -697,6 +681,10 @@ struct RegionServerInner {
     parallelism: Option<RegionServerParallelism>,
     // The topic stats reporter.
     topic_stats_reporter: RwLock<Option<Box<dyn TopicStatsReporter>>>,
+    // HACK(zhongzc): Direct MitoEngine handle for diagnostics. This couples the
+    // server with a concrete engine; acceptable for now to fetch Mito-specific
+    // info (e.g., list SSTs). Consider a diagnostics trait later.
+    mito_engine: RwLock<Option<MitoEngine>>,
 }
 
 struct RegionServerParallelism {
@@ -763,11 +751,18 @@ impl RegionServerInner {
             table_provider_factory,
             parallelism,
             topic_stats_reporter: RwLock::new(None),
+            mito_engine: RwLock::new(None),
         }
     }
 
     pub fn register_engine(&self, engine: RegionEngineRef) {
         let engine_name = engine.name();
+        if engine_name == MITO_ENGINE_NAME
+            && let Some(mito_engine) = engine.as_any().downcast_ref::<MitoEngine>()
+        {
+            *self.mito_engine.write().unwrap() = Some(mito_engine.clone());
+        }
+
         info!("Region Engine {engine_name} is registered");
         self.engines
             .write()
@@ -1286,6 +1281,7 @@ impl RegionServerInner {
         self.region_map.clear();
         info!("closed {num_regions} regions");
 
+        drop(self.mito_engine.write().unwrap().take());
         let engines = self.engines.write().unwrap().drain().collect::<Vec<_>>();
         for (engine_name, engine) in engines {
             engine
