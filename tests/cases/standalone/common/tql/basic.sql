17d16da4835fb6a21bc81a087b0ee770249517b0
dennis zhuang <killme2008@gmail.com>
2025-09-26 11:43:50 +0800

    feat: supports expression in TQL params (#7014)
    * feat: supports expression in TQL params
    
    Signed-off-by: Dennis Zhuang <killme2008@gmail.com>
    
    * chore: by cr comments
    
    Signed-off-by: Dennis Zhuang <killme2008@gmail.com>
    
    * chore: comment
    
    Signed-off-by: Dennis Zhuang <killme2008@gmail.com>
    
    * chore: by cr comments
    
    Signed-off-by: Dennis Zhuang <killme2008@gmail.com>
    
    ---------
    
    Signed-off-by: Dennis Zhuang <killme2008@gmail.com>

--- tests/cases/standalone/common/tql/basic.sql
+++ tests/cases/standalone/common/tql/basic.sql
@@ -58,3 +58,46 @@ TQL EVAL (0, 10, '5s') test{__field__="Field_I"};
 TQL EVAL (0, 10, '5s') test{__field__="field_i"};
 
 drop table test;
+
+CREATE TABLE metrics(val DOUBLE, ts TIMESTAMP TIME INDEX, host STRING PRIMARY KEY);
+
+-- Insert sample data with fixed timestamps for predictable testing
+INSERT INTO metrics VALUES 
+    (10.0, '2024-01-01 00:00:00', 'host1'),
+    (15.0, '2024-01-01 06:00:00', 'host1'),
+    (20.0, '2024-01-01 12:00:00', 'host1'),
+    (25.0, '2024-01-01 18:00:00', 'host1'),
+    (30.0, '2024-01-02 00:00:00', 'host1'),
+    (35.0, '2024-01-02 06:00:00', 'host1'),
+    (40.0, '2024-01-02 12:00:00', 'host1'),
+    (12.0, '2024-01-01 02:00:00', 'host2'),
+    (18.0, '2024-01-01 08:00:00', 'host2'),
+    (22.0, '2024-01-01 14:00:00', 'host2');
+
+-- Test TQL with date_trunc function
+-- SQLNESS SORT_RESULT 2 1
+TQL EVAL (date_trunc('day', '2024-01-01 10:00:00'::timestamp), 
+          date_trunc('day', '2024-01-02 10:00:00'::timestamp), 
+          '6h') last_over_time(metrics[12h]);
+
+-- Test TQL with date_trunc and interval arithmetic (now() - interval '1' day pattern)
+-- SQLNESS SORT_RESULT 2 1
+TQL EVAL (date_trunc('day', '2024-01-02 00:00:00'::timestamp) - interval '1' day,
+          date_trunc('day', '2024-01-02 00:00:00'::timestamp),
+          '4h') metrics{host="host1"};
+
+-- Test TQL with hour-based queries
+-- SQLNESS SORT_RESULT 2 1
+TQL EVAL ('2024-01-01 06:00:00'::timestamp,
+          '2024-01-01 18:00:00'::timestamp,
+          '3h', 
+          '6h') metrics;
+
+-- Test TQL with complex interval arithmetic
+-- SQLNESS SORT_RESULT 2 1
+TQL EVAL ('2024-01-01 00:00:00'::timestamp + interval '12' hour,
+          '2024-01-02 00:00:00'::timestamp - interval '6' hour,
+          '6h', 
+          '6h') metrics{host="host2"};
+
+DROP TABLE metrics;
