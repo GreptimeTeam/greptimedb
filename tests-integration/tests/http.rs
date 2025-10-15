5555d3988cdbac603bc56a0b09a5bc7fdb509c75
jeremyhi <fengjiachun@gmail.com>
2025-10-15 15:44:02 +0800

    feat: add metrics for query memory tracker
    Signed-off-by: jeremyhi <fengjiachun@gmail.com>

--- tests-integration/tests/http.rs
+++ tests-integration/tests/http.rs
@@ -1577,6 +1577,8 @@ fn drop_lines_with_inconsistent_results(input: String) -> String {
         "enable_virtual_host_style =",
         "cache_path =",
         "cache_capacity =",
+        "memory_pool_size =",
+        "scan_memory_limit =",
         "sas_token =",
         "scope =",
         "num_workers =",
