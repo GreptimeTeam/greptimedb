ac65ede033408c6c281b53031e9aa01da2ad35eb
discord9 <55937128+discord9@users.noreply.github.com>
2025-10-16 13:30:56 +0800

    feat: memtable seq range read (#6950)
    * feat: seq range memtable read
    
    Signed-off-by: discord9 <discord9@163.com>
    
    * test: from&range
    
    Signed-off-by: discord9 <discord9@163.com>
    
    * wt
    
    Signed-off-by: discord9 <discord9@163.com>
    
    * after rebase fix
    
    Signed-off-by: discord9 <discord9@163.com>
    
    * refactor: per review
    
    Signed-off-by: discord9 <discord9@163.com>
    
    * docs: better naming&emphaise
    
    Signed-off-by: discord9 <discord9@163.com>
    
    * refactor: use filter method
    
    Signed-off-by: discord9 <discord9@163.com>
    
    * tests: unwrap
    
    Signed-off-by: discord9 <discord9@163.com>
    
    ---------
    
    Signed-off-by: discord9 <discord9@163.com>

--- src/store-api/src/storage/types.rs
+++ src/store-api/src/storage/types.rs
@@ -14,6 +14,63 @@
 
 //! Common types.
 
+use datatypes::arrow::array::{BooleanArray, Datum, UInt64Array};
+
 /// Represents a sequence number of data in storage. The offset of logstore can be used
 /// as a sequence number.
 pub type SequenceNumber = u64;
+
+/// A range of sequence numbers.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum SequenceRange {
+    Gt {
+        /// Exclusive lower bound
+        min: SequenceNumber,
+    },
+    LtEq {
+        /// Inclusive upper bound
+        max: SequenceNumber,
+    },
+    GtLtEq {
+        /// Exclusive lower bound
+        min: SequenceNumber,
+        /// Inclusive upper bound
+        max: SequenceNumber,
+    },
+}
+
+impl SequenceRange {
+    pub fn new(min: Option<SequenceNumber>, max: Option<SequenceNumber>) -> Option<Self> {
+        match (min, max) {
+            (Some(min), Some(max)) => Some(SequenceRange::GtLtEq { min, max }),
+            (Some(min), None) => Some(SequenceRange::Gt { min }),
+            (None, Some(max)) => Some(SequenceRange::LtEq { max }),
+            (None, None) => None,
+        }
+    }
+
+    pub fn filter(
+        &self,
+        seqs: &dyn Datum,
+    ) -> Result<BooleanArray, datatypes::arrow::error::ArrowError> {
+        match self {
+            SequenceRange::Gt { min } => {
+                let min = UInt64Array::new_scalar(*min);
+                let pred = datafusion_common::arrow::compute::kernels::cmp::gt(seqs, &min)?;
+                Ok(pred)
+            }
+            SequenceRange::LtEq { max } => {
+                let max = UInt64Array::new_scalar(*max);
+                let pred = datafusion_common::arrow::compute::kernels::cmp::lt_eq(seqs, &max)?;
+                Ok(pred)
+            }
+            SequenceRange::GtLtEq { min, max } => {
+                let min = UInt64Array::new_scalar(*min);
+                let max = UInt64Array::new_scalar(*max);
+                let pred_min = datafusion_common::arrow::compute::kernels::cmp::gt(seqs, &min)?;
+                let pred_max = datafusion_common::arrow::compute::kernels::cmp::lt_eq(seqs, &max)?;
+                datafusion_common::arrow::compute::kernels::boolean::and(&pred_min, &pred_max)
+            }
+        }
+    }
+}
