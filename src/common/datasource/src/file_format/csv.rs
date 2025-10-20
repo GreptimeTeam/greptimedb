405f5038bc1fc5a9ff9af207061d7c5a0529690a
Alan Tang <jmtangcs@gmail.com>
2025-10-20 10:52:43 +0800

    feat: add optional parameters for copy table from command
    Signed-off-by: Alan Tang <jmtangcs@gmail.com>

--- src/common/datasource/src/file_format/csv.rs
+++ src/common/datasource/src/file_format/csv.rs
@@ -37,6 +37,8 @@ use crate::util::normalize_infer_schema;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CsvFormat {
     pub has_header: bool,
+    pub header: bool,
+    pub skip_bad_records: bool,
     pub delimiter: u8,
     pub schema_infer_max_record: Option<usize>,
     pub compression_type: CompressionType,
@@ -84,6 +86,24 @@ impl TryFrom<&HashMap<String, String>> for CsvFormat {
                 .build()
             })?;
         };
+        if let Some(header) = value.get(file_format::FORMAT_HEADER) {
+            format.header = header.parse().map_err(|_| {
+                error::ParseFormatSnafu {
+                    key: file_format::FORMAT_HEADER,
+                    value: header,
+                }
+                .build()
+            })?;
+        }
+        if let Some(skip_bad_records) = value.get(file_format::FORMAT_SKIP_BAD_RECORDS) {
+            format.header = skip_bad_records.parse().map_err(|_| {
+                error::ParseFormatSnafu {
+                    key: file_format::FORMAT_SKIP_BAD_RECORDS,
+                    value: skip_bad_records,
+                }
+                .build()
+            })?;
+        }
         if let Some(timestamp_format) = value.get(file_format::TIMESTAMP_FORMAT) {
             format.timestamp_format = Some(timestamp_format.clone());
         }
@@ -101,6 +121,8 @@ impl Default for CsvFormat {
     fn default() -> Self {
         Self {
             has_header: true,
+            header: true,
+            skip_bad_records: false,
             delimiter: b',',
             schema_infer_max_record: Some(file_format::DEFAULT_SCHEMA_INFER_MAX_RECORD),
             compression_type: CompressionType::Uncompressed,
@@ -331,6 +353,8 @@ mod tests {
                 schema_infer_max_record: Some(2000),
                 delimiter: b'\t',
                 has_header: false,
+                header: true,
+                skip_bad_records: false,
                 timestamp_format: None,
                 time_format: None,
                 date_format: None
