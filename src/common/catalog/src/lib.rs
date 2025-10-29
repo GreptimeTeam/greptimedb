9471f95cd2a75507b5cac744de853d671e4f9c64
shuiyisong <xixing.sys@gmail.com>
2025-10-29 17:36:36 +0800

    chore: remove unused feature gate
    Signed-off-by: shuiyisong <xixing.sys@gmail.com>

--- src/common/catalog/src/lib.rs
+++ src/common/catalog/src/lib.rs
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(const_option_ops)]
-#![feature(const_trait_impl)]
-
 use consts::DEFAULT_CATALOG_NAME;
 
 pub mod consts;
