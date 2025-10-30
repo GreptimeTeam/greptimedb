59f7bdda1b090e1315957f1b6653be1b967a1da4
discord9 <discord9@163.com>
2025-10-30 20:22:53 +0800

    chore: rename
    Signed-off-by: discord9 <discord9@163.com>

--- src/mito2/src/gc.rs
+++ src/mito2/src/gc.rs
@@ -44,7 +44,7 @@ use crate::error::{
 };
 use crate::manifest::manager::{RegionManifestManager, RegionManifestOptions, RemoveFileOptions};
 use crate::manifest::storage::manifest_compress_type;
-use crate::metrics::GC_DEL_FILE_CNT;
+use crate::metrics::GC_DELETE_FILE_CNT;
 use crate::region::opener::new_manifest_dir;
 use crate::sst::file::delete_files;
 use crate::sst::location::{self, region_dir_from_table_dir};
@@ -386,7 +386,7 @@ impl LocalGcWorker {
         .await?;
 
         // FIXME(discord9): if files are already deleted before calling delete_files, the metric will be inaccurate, no clean way to fix it now
-        GC_DEL_FILE_CNT.add(file_ids.len() as i64);
+        GC_DELETE_FILE_CNT.add(file_ids.len() as i64);
 
         Ok(())
     }
