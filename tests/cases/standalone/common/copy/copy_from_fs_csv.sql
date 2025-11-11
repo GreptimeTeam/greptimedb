df33ca18771fe2cadd4a201e6bbf1a70588bd986
Alan Tang <jmtangcs@gmail.com>
2025-11-11 17:24:57 +0800

    feat(csv): support has_header and skip_bad_record options, and add related test cases
    Signed-off-by: Alan Tang <jmtangcs@gmail.com>

--- tests/cases/standalone/common/copy/copy_from_fs_csv.sql
+++ tests/cases/standalone/common/copy/copy_from_fs_csv.sql
@@ -11,19 +11,31 @@ insert into
 values
     ('host3', 99.9, 444.4, 1722077263000);
 
+
+CREATE TABLE demo_2(host string, ts TIMESTAMP time index);
+
+insert into
+    demo_2(host, ts)
+values
+    ('host4', 1755276557000),
+    ('host5', 1755276558000),
+    ('host6', 1822077263000);
+
+Copy demo_2 TO '${SQLNESS_HOME}/demo/export/csv/demo_2.csv' with (format='csv');
+
 Copy demo TO '${SQLNESS_HOME}/demo/export/csv/demo.csv' with (format='csv');
+
+CREATE TABLE has_header(host string, ts timestamp time index);
+
+Copy has_header FROM '${SQLNESS_HOME}/demo/export/csv/' with (pattern = 'demo*', format='csv', has_header='true');
+
+select * from has_header order by ts;
 
 CREATE TABLE with_filename(host string, cpu double, memory double, ts timestamp time index);
 
 Copy with_filename FROM '${SQLNESS_HOME}/demo/export/csv/demo.csv' with (format='csv', start_time='2022-06-15 07:02:37', end_time='2022-06-15 07:02:39');
 
 select * from with_filename order by ts;
-
-CREATE TABLE with_json(host string, cpu double, memory double, jsons JSON, ts timestamp time index);
-
-Copy with_json FROM '${SQLNESS_HOME}/demo/export/json/demo.json' with (format='json');
-
-select host, cpu, memory, json_to_string(jsons), ts from with_json order by ts;
 
 -- SQLNESS PROTOCOL MYSQL
 select host, cpu, memory, jsons, ts from demo where host != 'host3';
@@ -57,9 +69,11 @@ select * from demo_with_less_columns order by ts;
 
 drop table demo;
 
-drop table with_filename;
+drop table demo_2;
 
-drop table with_json;
+drop table has_header;
+
+drop table with_filename;
 
 drop table with_path;
 
