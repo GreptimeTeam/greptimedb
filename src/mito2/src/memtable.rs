f150a6c705ded26c3d0235f47a489533c621df53
evenyag <realevenyag@gmail.com>
2025-11-11 16:13:06 +0800

    fix: create correct memtable when setting sst_format explicitly
    Signed-off-by: evenyag <realevenyag@gmail.com>

--- src/mito2/src/memtable.rs
+++ src/mito2/src/memtable.rs
@@ -399,6 +399,8 @@ impl MemtableBuilderProvider {
     }
 
     pub(crate) fn builder_for_options(&self, options: &RegionOptions) -> MemtableBuilderRef {
+        common_telemetry::info!("Builder for options: {:?}", options);
+
         let dedup = options.need_dedup();
         let merge_mode = options.merge_mode();
         let flat_format = options
@@ -422,6 +424,7 @@ impl MemtableBuilderProvider {
             );
         }
 
+        // The format is not flat.
         match &options.memtable {
             Some(MemtableOptions::TimeSeries) => Arc::new(TimeSeriesMemtableBuilder::new(
                 self.write_buffer_manager.clone(),
@@ -440,22 +443,15 @@ impl MemtableBuilderProvider {
                     self.write_buffer_manager.clone(),
                 ))
             }
-            None => self.default_memtable_builder(dedup, merge_mode),
+            None => self.default_primary_key_memtable_builder(dedup, merge_mode),
         }
     }
 
-    fn default_memtable_builder(&self, dedup: bool, merge_mode: MergeMode) -> MemtableBuilderRef {
-        if self.config.default_experimental_flat_format {
-            return Arc::new(
-                BulkMemtableBuilder::new(
-                    self.write_buffer_manager.clone(),
-                    !dedup, // append_mode: true if not dedup, false if dedup
-                    merge_mode,
-                )
-                .with_compact_dispatcher(self.compact_dispatcher.clone()),
-            );
-        }
-
+    fn default_primary_key_memtable_builder(
+        &self,
+        dedup: bool,
+        merge_mode: MergeMode,
+    ) -> MemtableBuilderRef {
         match &self.config.memtable {
             MemtableConfig::PartitionTree(config) => {
                 let mut config = config.clone();
