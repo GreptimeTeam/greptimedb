d60e6789a0b3afeaf409766de190035ac88d7f3f
Alan Tang <jmtangcs@gmail.com>
2025-11-13 16:32:12 +0800

    fix: fix the integration test error
    Signed-off-by: Alan Tang <jmtangcs@gmail.com>

--- tests-integration/src/tests/instance_test.rs
+++ tests-integration/src/tests/instance_test.rs
@@ -2326,7 +2326,10 @@ async fn test_cast_type_issue_1594(instance: Arc<dyn MockInstance>) {
 
     let output = execute_sql(
         &instance,
-        &format!("copy tsbs_cpu from '{}' WITH(FORMAT='csv');", &filepath),
+        &format!(
+            "copy tsbs_cpu from '{}' WITH(FORMAT='csv', header='false');",
+            &filepath
+        ),
     )
     .await;
 
