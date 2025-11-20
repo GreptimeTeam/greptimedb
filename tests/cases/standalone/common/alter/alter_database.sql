7f1da171500cc3726cd24735e0461681ffa40dc8
WaterWhisperer <waterwhisperer24@qq.com>
2025-11-20 09:39:35 +0800

    feat: support alter database compaction options (#7251)
    Signed-off-by: WaterWhisperer <waterwhisperer24@qq.com>

--- tests/cases/standalone/common/alter/alter_database.sql
+++ tests/cases/standalone/common/alter/alter_database.sql
@@ -32,5 +32,63 @@ SHOW CREATE DATABASE alter_database;
 -- SQLNESS ARG restart=true
 SHOW CREATE DATABASE alter_database;
 
+ALTER DATABASE alter_database SET 'compaction.type'='twcs';
+
+SHOW CREATE DATABASE alter_database;
+
+ALTER DATABASE alter_database SET 'compaction.twcs.time_window'='2h';
+
+SHOW CREATE DATABASE alter_database;
+
+ALTER DATABASE alter_database SET 'compaction.twcs.trigger_file_num'='8';
+
+ALTER DATABASE alter_database SET 'compaction.twcs.max_output_file_size'='512MB';
+
+SHOW CREATE DATABASE alter_database;
+
+ALTER DATABASE alter_database SET 'compaction.twcs.time_window'='1d';
+
+SHOW CREATE DATABASE alter_database;
+
+-- SQLNESS ARG restart=true
+SHOW CREATE DATABASE alter_database;
+
+ALTER DATABASE alter_database UNSET 'compaction.twcs.trigger_file_num';
+
+SHOW CREATE DATABASE alter_database;
+
+ALTER DATABASE alter_database UNSET 'compaction.twcs.time_window';
+
+SHOW CREATE DATABASE alter_database;
+
+ALTER DATABASE alter_database SET 'compaction.twcs.fallback_to_local'='true';
+
+SHOW CREATE DATABASE alter_database;
+
+ALTER DATABASE alter_database UNSET 'compaction.type';
+
+ALTER DATABASE alter_database UNSET 'compaction.twcs.max_output_file_size';
+
+ALTER DATABASE alter_database UNSET 'compaction.twcs.fallback_to_local';
+
+SHOW CREATE DATABASE alter_database;
+
+-- SQLNESS ARG restart=true
+SHOW CREATE DATABASE alter_database;
+
+ALTER DATABASE alter_database SET 'invalid.compaction.option'='value';
+
+ALTER DATABASE alter_database SET 'ttl'='1h';
+
+ALTER DATABASE alter_database SET 'compaction.type'='twcs';
+
+ALTER DATABASE alter_database SET 'compaction.twcs.time_window'='30m';
+
+SHOW CREATE DATABASE alter_database;
+
+ALTER DATABASE alter_database UNSET 'ttl';
+
+SHOW CREATE DATABASE alter_database;
+
 DROP DATABASE alter_database;
 
