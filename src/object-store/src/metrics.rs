5cc1b5f55db0aea4b899a9dbba7b5460cb57846f
evenyag <realevenyag@gmail.com>
2025-12-05 20:58:46 +0800

    refactor: remove read cache
    Signed-off-by: evenyag <realevenyag@gmail.com>

--- src/object-store/src/metrics.rs
+++ src/object-store/src/metrics.rs
@@ -13,38 +13,3 @@
 // limitations under the License.
 
 //! object-store metrics
-
-/// Cache hit counter, no matter what the cache result is.
-use lazy_static::lazy_static;
-use prometheus::*;
-
-lazy_static! {
-    /// Cache hit counter, no matter what the cache result is.
-    pub static ref OBJECT_STORE_LRU_CACHE_HIT: IntCounterVec = register_int_counter_vec!(
-        "greptime_object_store_lru_cache_hit",
-        "object store lru cache hit",
-        &["result"]
-    )
-    .unwrap();
-    /// Cache miss counter
-    pub static ref OBJECT_STORE_LRU_CACHE_MISS: IntCounter =
-        register_int_counter!("greptime_object_store_lru_cache_miss", "object store lru cache miss")
-            .unwrap();
-    /// Object store read error counter
-    pub static ref OBJECT_STORE_READ_ERROR: IntCounterVec = register_int_counter_vec!(
-        "greptime_object_store_read_errors",
-        "object store read errors",
-        &["kind"]
-    )
-    .unwrap();
-
-    /// Cache entry number
-    pub static ref OBJECT_STORE_LRU_CACHE_ENTRIES: IntGauge =
-        register_int_gauge!("greptime_object_store_lru_cache_entries", "object store lru cache entries")
-            .unwrap();
-
-    /// Cache size in bytes
-    pub static ref OBJECT_STORE_LRU_CACHE_BYTES: IntGauge =
-        register_int_gauge!("greptime_object_store_lru_cache_bytes",  "object store lru cache bytes")
-            .unwrap();
-}
