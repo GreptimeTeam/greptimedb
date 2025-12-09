04fb035560c17c1bd2289f59d4966ab500a59bd3
discord9 <discord9@163.com>
2025-12-09 15:49:06 +0800

    chore: unused
    Signed-off-by: discord9 <discord9@163.com>

--- tests-integration/src/tests/gc.rs
+++ tests-integration/src/tests/gc.rs
@@ -12,18 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::{HashMap, HashSet};
-use std::time::Duration;
+use std::collections::HashSet;
 
 use common_meta::key::TableMetadataManagerRef;
-use common_procedure::ProcedureWithId;
-use common_telemetry::info;
-use common_test_util::recordbatch::check_output_stream;
 use futures::TryStreamExt as _;
 use itertools::Itertools;
-use meta_srv::gc::{BatchGcProcedure, GcSchedulerOptions, Region2Peers};
-use mito2::gc::GcConfig;
-use rand::rand_core::le;
+use meta_srv::gc::Region2Peers;
 use store_api::storage::RegionId;
 use table::metadata::TableId;
 
