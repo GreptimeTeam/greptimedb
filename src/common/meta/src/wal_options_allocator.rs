353853feb184b452c28fd9753bc358473ae8b5f9
OmCheeLin <19563671928@163.com>
2026-01-09 14:24:49 +0800

    chore: fix
    Signed-off-by: OmCheeLin <19563671928@163.com>

--- src/common/meta/src/wal_options_allocator.rs
+++ src/common/meta/src/wal_options_allocator.rs
@@ -30,10 +30,10 @@ use crate::error::{EncodeWalOptionsSnafu, InvalidTopicNamePrefixSnafu, Result};
 use crate::key::TOPIC_NAME_PATTERN_REGEX;
 use crate::kv_backend::KvBackendRef;
 use crate::leadership_notifier::LeadershipChangeListener;
-pub use crate::wal_options_allocator::topic_creator::{
+pub use topic_creator::{
     build_kafka_client, build_kafka_topic_creator,
 };
-use crate::wal_options_allocator::topic_pool::KafkaTopicPool;
+use topic_pool::KafkaTopicPool;
 
 /// Allocates wal options in region granularity.
 #[derive(Default, Debug)]
