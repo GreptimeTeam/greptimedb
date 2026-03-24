be27fa7add803a1463219eded5f1d7e8fbb6e7ac
Ning Sun <sunning@greptime.com>
2026-03-24 14:59:47 +0800

    chore: remove as_millis_f64

--- src/common/meta/src/lib.rs
+++ src/common/meta/src/lib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(duration_millis_float)]
-
 pub mod cache;
 pub mod cache_invalidator;
 pub mod cluster;
