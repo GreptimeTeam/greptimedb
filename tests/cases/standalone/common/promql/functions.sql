d667728fdeb5a55e63665f070453b4649801af60
shuiyisong <113876041+shuiyisong@users.noreply.github.com>
2026-07-13 11:27:06 +0800

    chore!: update promql-parser to v0.10.0, remove `holt_winters` (#8457)
    * chore: update promql parser and fix compile
    
    Signed-off-by: shuiyisong <xixing.sys@gmail.com>
    
    * fix: sqlness
    
    Signed-off-by: shuiyisong <xixing.sys@gmail.com>
    
    * fix: cr issue
    
    Signed-off-by: shuiyisong <xixing.sys@gmail.com>
    
    * fix: cr issue
    
    Signed-off-by: shuiyisong <xixing.sys@gmail.com>
    
    ---------
    
    Signed-off-by: shuiyisong <xixing.sys@gmail.com>

--- tests/cases/standalone/common/promql/functions.sql
+++ tests/cases/standalone/common/promql/functions.sql
@@ -34,10 +34,6 @@ tql eval (3, 3, '1s') predict_linear(prom_series[3s], 40 + 2);
 -- SQLNESS SORT_RESULT 3 1
 tql eval (10, 10, '1s') double_exponential_smoothing(prom_series[10s], 0.4 + 0.1, 0.1);
 
--- holt_winters (backward compatibility)
--- SQLNESS SORT_RESULT 3 1
-tql eval (10, 10, '1s') holt_winters(prom_series[10s], 0.4 + 0.1, 0.1);
-
 DROP TABLE prom_series;
 
 CREATE TABLE
