837b701e687228c19c4ba5371fabca2810192afc
fys <40801205+fengys1996@users.noreply.github.com>
2026-07-23 10:29:31 +0000

    fix(json2): encode deeply nested values as jsonb (#8612)
    * fix(json2): encode deeply nested values as jsonb
    
    Signed-off-by: fys <fengys1996@gmail.com>
    
    * add more sqlness
    
    Signed-off-by: fys <fengys1996@gmail.com>
    
    * some changes
    
    Signed-off-by: fys <fengys1996@gmail.com>
    
    * fix(json2): limit type hint path depth
    
    Signed-off-by: fys <fengys1996@gmail.com>
    
    ---------
    
    Signed-off-by: fys <fengys1996@gmail.com>

--- tests/cases/standalone/common/types/json/json2_type_hints.sql
+++ tests/cases/standalone/common/types/json/json2_type_hints.sql
@@ -48,6 +48,28 @@ CREATE TABLE json2_type_hints_timestamp (
     )
 );
 
+-- A type hint at the maximum supported depth is accepted.
+CREATE TABLE json2_type_hint_depth_50 (
+    ts TIMESTAMP TIME INDEX,
+    j JSON2 (
+        l01.l02.l03.l04.l05.l06.l07.l08.l09.l10.l11.l12.l13.l14.l15.l16.l17.l18.l19.l20.l21.l22.l23.l24.l25.l26.l27.l28.l29.l30.l31.l32.l33.l34.l35.l36.l37.l38.l39.l40.l41.l42.l43.l44.l45.l46.l47.l48.l49.l50 STRING
+    )
+) WITH (
+    'append_mode' = 'true'
+);
+
+-- A type hint beyond the structured depth limit is rejected at CREATE TABLE.
+CREATE TABLE json2_type_hint_depth_51 (
+    ts TIMESTAMP TIME INDEX,
+    j JSON2 (
+        l01.l02.l03.l04.l05.l06.l07.l08.l09.l10.l11.l12.l13.l14.l15.l16.l17.l18.l19.l20.l21.l22.l23.l24.l25.l26.l27.l28.l29.l30.l31.l32.l33.l34.l35.l36.l37.l38.l39.l40.l41.l42.l43.l44.l45.l46.l47.l48.l49.l50.l51 STRING
+    )
+) WITH (
+    'append_mode' = 'true'
+);
+
 DROP TABLE json2_type_hints;
 
 DROP TABLE json2_type_hints_required;
+
+DROP TABLE json2_type_hint_depth_50;
