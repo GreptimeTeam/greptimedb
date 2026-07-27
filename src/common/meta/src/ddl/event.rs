4864fb71b2b8c4e4e2364fa3db399f1441f6e2a0
WenyXu <wenymedia@gmail.com>
2026-07-27 06:35:53 +0000

    feat(meta): add database DDL procedure events
    Signed-off-by: WenyXu <wenymedia@gmail.com>

--- src/common/meta/src/ddl/event.rs
+++ src/common/meta/src/ddl/event.rs
@@ -12,14 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod alter_logical_tables;
-mod alter_table;
-mod create_flow;
-mod create_logical_tables;
-mod create_table;
-pub(crate) mod create_view;
-mod drop_database;
-mod drop_flow;
-mod drop_table;
-mod drop_view;
-mod event;
+pub(crate) mod database;
