50dc35521faed6767ff5a24d8be5a1aceaba96f2
WenyXu <wenymedia@gmail.com>
2026-07-28 06:38:55 +0000

    test(integration): use singular view event module
    Signed-off-by: WenyXu <wenymedia@gmail.com>

--- tests-integration/tests/main.rs
+++ tests-integration/tests/main.rs
@@ -32,7 +32,7 @@ mod repartition;
 #[macro_use]
 mod repartition_expr_version;
 mod mysql;
-mod view_procedure_events;
+mod view_procedure_event;
 
 grpc_tests!(File, S3, S3WithCache, Oss, Azblob, Gcs);
 
