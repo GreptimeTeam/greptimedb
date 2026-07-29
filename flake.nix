80980095322a9c8737ed74eda02094d5e772846b
Ning Sun <sunng@protonmail.com>
2026-07-29 21:44:58 +0800

    chore: update nix flake to 26.05 (#8681)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development environment flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
