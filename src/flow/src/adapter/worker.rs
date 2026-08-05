d2c2a47b493b02e9cac88c9f3b5db8203117577c
onepizzateam <palakjha916@gmail.com>
2026-08-05 18:14:09 +0530

    fix(flow): apply rustfmt to get_full_flow_stat return type
    Signed-off-by: onepizzateam <palakjha916@gmail.com>

--- src/flow/src/adapter/worker.rs
+++ src/flow/src/adapter/worker.rs
@@ -204,7 +204,14 @@ impl WorkerHandle {
 
     pub async fn get_full_flow_stat(
         &self,
-    ) -> Result<(BTreeMap<FlowId, usize>, BTreeMap<FlowId, i64>, BTreeMap<FlowId, i64>), Error> {
+    ) -> Result<
+        (
+            BTreeMap<FlowId, usize>,
+            BTreeMap<FlowId, i64>,
+            BTreeMap<FlowId, i64>,
+        ),
+        Error,
+    > {
         let ret = self
             .itc_client
             .call_with_resp(Request::QueryFullFlowStat)
