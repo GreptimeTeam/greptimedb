cf7a913fef55c1c10cf5cb19740d935444766887
Lei, HUANG <ratuthomm@gmail.com>
2026-08-13 19:35:29 +0800

    fix: remove unused trace test import
    Signed-off-by: Lei, HUANG <ratuthomm@gmail.com>

--- src/frontend/src/instance/otlp/trace_ingest/tests.rs
+++ src/frontend/src/instance/otlp/trace_ingest/tests.rs
@@ -32,7 +32,6 @@ use servers::otlp::trace::SERVICE_NAME_COLUMN;
 use servers::otlp::trace::attributes::Attributes;
 use servers::otlp::trace::span::{SpanEvents, SpanLinks, TraceSpan};
 use servers::otlp::trace::v1::{TraceBinaryType, TraceRetryColumn};
-use servers::query_handler::TraceIngestOutcome;
 
 use super::{
     ChunkFailureReaction, Instance, TraceChunkRetry, TraceChunkSchemaState, TraceRequestSchema,
