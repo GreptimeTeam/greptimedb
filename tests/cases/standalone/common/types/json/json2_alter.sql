6337fe6c251511552f90e93aaab1e68e948b2fe4
fys <fengys1996@gmail.com>
2026-09-04 15:52:40 +0800

    test(sql): expand JSON2 alter syntax coverage
    Signed-off-by: fys <fengys1996@gmail.com>

--- tests/cases/standalone/common/types/json/json2_alter.sql
+++ tests/cases/standalone/common/types/json/json2_alter.sql
@@ -7,6 +7,19 @@ CREATE TABLE application_logs (
 
 ALTER TABLE application_logs
     MODIFY COLUMN attrs JSON2 (
+        max_auto_expanded_paths = 2000
+    );
+
+ALTER TABLE application_logs
+    MODIFY COLUMN attrs JSON2 (
+        trace_id STRING,
+        user.id STRING NOT NULL,
+        user.name STRING DEFAULT 'anonymous',
+        request_id STRING INVERTED INDEX
+    );
+
+ALTER TABLE application_logs
+    MODIFY COLUMN attrs JSON2 (
         max_auto_expanded_paths = 2000,
         trace_id STRING,
         user.id STRING NOT NULL,
