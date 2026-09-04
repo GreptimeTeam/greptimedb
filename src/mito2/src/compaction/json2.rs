d67d3501a9b55da2cb20be302920bb332bafe514
LFC <990479+MichaelScofield@users.noreply.github.com>
2026-09-04 09:16:28 +0000

    fix(json2): keep empty structs in remainder (#9027)
    Signed-off-by: luofucong <luofc@foxmail.com>

--- src/mito2/src/compaction/json2.rs
+++ src/mito2/src/compaction/json2.rs
@@ -247,6 +247,9 @@ fn select_dynamic_hints(
         .iter()
         .filter(|(path, stat)| {
             !stat.is_type_conflicted
+                // TODO(LFC): Instead of "primitive only", consider retaining stable compound types
+                // that are safe to write to Parquet, as flush does. Or better, unite the two 
+                // selection process.
                 && stat.data_type.is_primitive()
                 && !has_ancestor_path(path)
                 && !has_descendant_path(path)
