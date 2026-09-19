565cba48a9bb67df20fa80bd9c22f07b41c290d1
dhruvxvaishnav <dhruvvaishnav687@gmail.com>
2026-09-19 12:42:36 +0530

    fix: rename failed-insert metric label to code
    Signed-off-by: dhruvxvaishnav <dhruvvaishnav687@gmail.com>

--- src/datanode/src/metrics.rs
+++ src/datanode/src/metrics.rs
@@ -17,6 +17,7 @@ use prometheus::*;
 
 /// Region request type label.
 pub const REGION_REQUEST_TYPE: &str = "datanode_region_request_type";
+const REGION_REQUEST_CODE: &str = "code";
 
 pub const REGION_ROLE: &str = "region_role";
 pub const REGION_ID: &str = "region_id";
@@ -89,7 +90,7 @@ lazy_static! {
     pub static ref REGION_SERVER_INSERT_FAIL_COUNT: IntCounterVec = register_int_counter_vec!(
         "greptime_datanode_region_failed_insert_count",
         "failed region server insert requests count",
-        &[REGION_REQUEST_TYPE, "status"]
+        &[REGION_REQUEST_TYPE, REGION_REQUEST_CODE]
     )
     .unwrap();
 
