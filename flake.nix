9ad5f851b10a5ac285bc0ee1fc887c70117e005e
Ning Sun <sunning@greptime.com>
2026-09-23 16:00:37 +0800

    chore: update flake checksum

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         lib = nixpkgs.lib;
         rustToolchain = fenix.packages.${system}.fromToolchainName {
           name = (lib.importTOML ./rust-toolchain.toml).toolchain.channel;
-          sha256 = "sha256-rboGKQLH4eDuiY01SINOqmXUFUNr9F4awoFZGzib17o=";
+          sha256 = "sha256-h+t2xTBz5yt2YIO+1VMIIGlCU7gyp2LYOFvaV1nwOXU=";
         };
       in
       {
